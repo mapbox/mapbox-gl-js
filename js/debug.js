712e83e4d696726f63924ece9a8e019bac8b90cb
Konstantin Käfer <github@kkaefer.com>
2013-09-06 12:34:46 +0200

    add names

--- js/debug.js
+++ js/debug.js
@@ -2,10 +2,10 @@ function Debug(map) {
     var gui = new dat.GUI({ autoPlace: false });
     document.getElementById('map').appendChild(gui.domElement);
 
-    gui.add(map, 'debug');
-    gui.add(map, 'repaint');
-    gui.add(map, 'resetNorth');
-    gui.add(map.transform, 'lat').min(-85).max(85).step(0.01).listen();
-    gui.add(map.transform, 'lon').min(-180).max(180).step(0.01).listen();
+    gui.add(map, 'debug').name('Tile Borders');
+    gui.add(map, 'repaint').name('Repaint');
+    gui.add(map, 'resetNorth').name('Reset North');
+    gui.add(map.transform, 'lat').name('Latitude').min(-85).max(85).step(0.01).listen();
+    gui.add(map.transform, 'lon').name('Longitude').min(-180).max(180).step(0.01).listen();
     // gui.add(map.transform, 'zoom').min(0).max(24).listen();
 }
