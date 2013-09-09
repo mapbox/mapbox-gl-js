321a32bd7f68c517b0e7d0fa2fa4ad94cd58ea83
Dmitri Gaskin <dmitrig01@gmail.com>
2013-09-09 17:41:52 -0400

    Calculate free space using Guillotine-BAF-SAS

--- js/labeltexture.js
+++ js/labeltexture.js
@@ -1,14 +1,3 @@
-function measureLineHeight(font) {
-    var p = document.createElement('p'),
-        height;
-    p.style.font = font;
-    p.innerText = 'Ag';
-    document.body.appendChild(p);
-    height = p.offsetHeight;
-    document.body.removeChild(p);
-    return height;
-}
-
 function LabelTextureManager(map) {
     this.canvases = [];
     this.contexts = [];
@@ -23,10 +12,11 @@ function LabelTextureManager(map) {
 
 LabelTextureManager.prototype.newCanvas = function() {
     this.cursor = { x: 0, y: 0, ny: 0 };
+    this.free = [{ x: 0, y: 0, w: 1024, h: 128 }];
 
     var canvas = document.createElement('canvas');
     canvas.width = 1024 * this.map.pixelRatio;
-    canvas.height = 32;
+    canvas.height = 128;
     this.canvases.push(canvas);
     // document.body.appendChild(canvas);
 
@@ -57,10 +47,91 @@ LabelTextureManager.prototype.addGlyph = function(font, rotation, glyph) {
     // TODO: do a better job calculating where we should place each glyph on the canvas.
     // They could be quite a bit more tightly packed than they are now.
     this.contexts[0].font = font;
+    var metrics = this.measure(font, rotation, glyph);
+
+    // Decide on a best fit.
+    // BAF algorithm.
+    var smallest = Infinity, smallestI = -1;
+    for (var i = 0; i < this.free.length; i++) {
+        if (metrics.boxWidth < this.free[i].w // it fits width
+            && metrics.boxHeight < this.free[i].h // it fits height
+            && (this.free[i].w*this.free[i].h) < smallest) { // The area is smaller than the smallest
+            smallest = this.free[i].w*this.free[i].h;
+            smallestI = i;
+        }
+    }
+    var rect = this.free[smallestI];
+    // Pack into top left
+    var coords = {
+        x: (rect.x) / this.pixelRatio, // upper-left
+        y: (rect.y) / this.pixelRatio, // upper-left
+        w: (metrics.boxWidth) / this.pixelRatio,
+        h: metrics.boxHeight / this.pixelRatio,
+        trueW: metrics.width / this.pixelRatio,
+        trueH: metrics.height / this.pixelRatio
+    };
+    
+    var toTopLeftOfBox = [ rect.x, rect.y ];
+    var r = vectorAdd(toTopLeftOfBox, metrics.r);
+    r = rotate(-this.rotation, r);
+    this.contexts[0].fillText(glyph, r[0], r[1]);
+    // SAS
+    var b1, b2;
+    if (rect.w < rect.h) {
+        // split horizontally
+        // +--+---+
+        // |__|___|  <-- b1
+        // +------+  <-- b2
+        b1 = { x: rect.x + metrics.boxWidth, y: rect.y, w: rect.w - metrics.boxWidth, h: metrics.boxHeight };
+        b2 = { x: rect.x, y: rect.y + metrics.boxHeight, w: rect.w, h: rect.h - metrics.boxHeight };
+    }
+    else {
+        // split vertically
+        // +--+---+
+        // |__|   | <-- b1
+        // +--|---+ <-- b2
+        b1 = { x: rect.x + metrics.boxWidth, y: rect.y, w: rect.w - metrics.boxWidth, h: rect.h };
+        b2 = { x: rect.x, y: rect.y + metrics.boxHeight, w: metrics.boxWidth, h: rect.h - metrics.boxHeight };
+    }
+    this.free.splice(smallestI, 1);
+    // Only save the free spaces if they're big enough that something might actually fit in them.
+    if ((b1.w * b1.h) > 90) {
+        this.free.push(b1);
+    }
+    if ((b2.w * b2.h) > 90) {
+        this.free.push(b2);
+    }
+
+    if (!this.glyphs[font]) this.glyphs[font] = {};
+    if (!this.glyphs[font][rotation]) this.glyphs[font][rotation] = {};
+
+    this.glyphs[font][rotation][glyph] = coords;
+    this.updated = true;
+};
+
+/*
+LabelTextureManager.prototype.drawFree = function() {
+    for (var i = 0; i < this.free.length; i++) {
+        var free = this.free[i];
+        this.contexts[0].beginPath();
+        this.contexts[0].lineWidth = 2;
+        this.contexts[0].strokeStyle = 'rgba(0, 0, 200, 0.3)';
+        this.contexts[0].rect(free.x, free.y, free.w, free.h);
+        this.contexts[0].stroke();
+    }
+};
+*/
+
+LabelTextureManager.prototype.measure = function(font, rotation, glyph) {
     var metrics = this.contexts[0].measureText(glyph);
 
     if (!(font in this.lineHeights)) {
-        this.lineHeights[font] = measureLineHeight(font);
+        var p = document.createElement('p');
+        p.style.font = font;
+        p.innerText = 'Ag';
+        document.body.appendChild(p);
+        this.lineHeights[font] = p.offsetHeight;
+        document.body.removeChild(p);
     }
     metrics.height = this.lineHeights[font];
 
@@ -80,51 +151,12 @@ LabelTextureManager.prototype.addGlyph = function(font, rotation, glyph) {
     metrics.boxHeight = extent.top - extent.bottom;
     metrics.boxWidth = extent.right - extent.left;
 
-    // Advance cursor.
-    var cursor = this.cursor;
-    if (cursor.x + metrics.width + 20 > this.canvases[0].width) {
-        cursor.x = 0;
-        cursor.y = cursor.ny;
-
-        if (cursor.y + metrics.boxHeight > this.canvases[0].height) {
-            this.canvases[0].height = this.canvases[0].height * 2;
-            this.contexts[0].textBaseline = 'top';
-            this.contexts[0].font = font;
-
-            // Todo: do this for all fonts/glyphs/rotations:
-            for (var _glyph in this.glyphs[font][rotation]) {
-                this.contexts[0].fillText(_glyph, this.glyphs[font][rotation][_glyph].r[0], this.glyphs[font][rotation][_glyph].r[1]);
-            }
-        }
-    }
-
-    var toTopLeftOfBox = [ cursor.x, cursor.y ],
-        fromTopLeftToMiddle = [ metrics.boxWidth / 2, metrics.boxHeight / 2 ],
+    var fromTopLeftToMiddle = [ metrics.boxWidth / 2, metrics.boxHeight / 2 ],
         fromMiddleToTopLeft = rotate(this.rotation, [ -metrics.width / 2, -metrics.height / 2 ]);
-    var r = vectorAdd(toTopLeftOfBox, vectorAdd(fromTopLeftToMiddle, fromMiddleToTopLeft));
 
-    var coords = {
-        x: (cursor.x) / this.pixelRatio, // upper-left
-        y: (cursor.y) / this.pixelRatio, // upper-left
-        w: (metrics.boxWidth) / this.pixelRatio,
-        h: metrics.boxHeight / this.pixelRatio,
-        trueW: metrics.width / this.pixelRatio,
-        trueH: metrics.height / this.pixelRatio,
-        r: r
-    };
+    metrics.r = vectorAdd(fromTopLeftToMiddle, fromMiddleToTopLeft);
 
-    r = rotate(-this.rotation, r);
-    //console.log(glyph);
-    this.contexts[0].fillText(glyph, r[0], r[1]);
-
-    cursor.ny = Math.max(cursor.ny, cursor.y + metrics.boxHeight);
-    cursor.x += metrics.boxWidth;
-
-    if (!this.glyphs[font]) this.glyphs[font] = {};
-    if (!this.glyphs[font][rotation]) this.glyphs[font][rotation] = {};
-
-    this.glyphs[font][rotation][glyph] = coords;
-    this.updated = true;
+    return metrics;
 };
 
 LabelTextureManager.prototype.getGlyph = function(font, rotation, glyph) {
