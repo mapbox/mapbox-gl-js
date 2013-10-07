1bb0d6960ed7ca88e1911fc3250adbad9df04c7a
Tom MacWright <tom@macwright.org>
2013-10-07 15:26:00 -0400

    Use mipmap. Fixes #112

--- js/rastertile.js
+++ js/rastertile.js
@@ -29,12 +29,12 @@ RasterTile.prototype.bind = function(gl) {
     if (!this.texture) {
         this.texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.img);
-
+        gl.generateMipmap(gl.TEXTURE_2D);
     } else {
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
     }
