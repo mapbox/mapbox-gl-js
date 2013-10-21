fb8e4e681f97852a14afbacf8d0aa93f179a6dbc
Konstantin Käfer <github@kkaefer.com>
2013-10-21 16:27:15 +0200

    dramatically speed up glyph loading

--- js/glyphatlas.js
+++ js/glyphatlas.js
@@ -13,6 +13,8 @@ function GlyphAtlas(width, height) {
     document.body.appendChild(this.canvas);
     this.ctx = this.canvas.getContext('2d');
     // END DEBUG
+
+    this.data = new Uint8Array(width * height);
 }
 
 GlyphAtlas.prototype.addGlyph = function(name, glyph, buffer) {
@@ -47,30 +49,21 @@ GlyphAtlas.prototype.addGlyph = function(name, glyph, buffer) {
 
     var rect = this.bin.allocate(pack_width, pack_height);
     if (rect.x < 0) {
-        console.warn('glyph bitmap overflow');
+        // console.warn('glyph bitmap overflow');
         return { glyph: glyph, rect: null };
     }
 
     this.index[key] = rect;
 
-    // DEBUG
-    var data = this.ctx.getImageData(rect.x, rect.y, buffered_width, buffered_height);
+    var target = this.data;
     var source = glyph.bitmap;
-    var target = data.data;
-    var length = source.length;
-    for (var i = 0, j = 0; i < length; i++, j += 4) {
-        target[j + 0] = source[i];
-        target[j + 1] = source[i];
-        target[j + 2] = source[i];
-        target[j + 3] = 255;
+    for (var y = 0; y < buffered_height; y++) {
+        var y1 = this.width * (rect.y + y) + rect.x;
+        var y2 = buffered_width * y;
+        for (var x = 0; x < buffered_width; x++) {
+            target[y1 + x] = source[y2 + x];
+        }
     }
-    this.ctx.putImageData(data, rect.x, rect.y);
-    // END DEBUG
-
-    var gl = this.gl;
-    this.bind(gl);
-    gl.texSubImage2D(gl.TEXTURE_2D, 0, rect.x, rect.y, gl.LUMINANCE, gl.UNSIGNED_BYTE,
-        this.ctx.getImageData(rect.x, rect.y, pack_width, pack_height));
 
     this.dirty = true;
 
@@ -82,7 +75,6 @@ GlyphAtlas.prototype.bind = function(gl) {
     if (!this.texture) {
         this.texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, gl.LUMINANCE, gl.UNSIGNED_BYTE, this.canvas);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -96,6 +88,20 @@ GlyphAtlas.prototype.bind = function(gl) {
 GlyphAtlas.prototype.updateTexture = function(gl) {
     this.bind(gl);
     if (this.dirty) {
+
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, this.width, this.height, 0, gl.ALPHA, gl.UNSIGNED_BYTE, this.data);
+
+        // DEBUG
+        var data = this.ctx.getImageData(0, 0, this.width, this.height);
+        for (var i = 0, j = 0; i < this.data.length; i++, j += 4) {
+            data.data[j] = this.data[i];
+            data.data[j+1] = this.data[i];
+            data.data[j+2] = this.data[i];
+            data.data[j+3] = 255;
+        }
+        this.ctx.putImageData(data, 0, 0);
+        // END DEBUG
+
         this.dirty = false;
     }
 };
