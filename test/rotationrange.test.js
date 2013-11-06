9b054c5d4401fe8586e1780c1228b3eecf08474d
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-11-06 12:04:05 -0500

    add rotation collision test

--- test/rotationrange.test.js
+++ test/rotationrange.test.js
@@ -5,70 +5,6 @@ var PI = Math.PI;
 
 function deg(x) { return x/PI * 180; }
 
-describe('#rotationRange', function() {
-    it('', function() {
-        var c = rc.rotationRange({
-            anchor: {
-                x: 2736,
-                y: 3433
-            },
-            box: {
-                x1: 2313,
-                x2: 2433,
-                y1: 3361,
-                y2: 3529
-            },
-            rotate: true,
-        },{
-            anchor: {
-                x: 2735,
-                y: 2825
-            },
-            box: {
-                x1: 2762,
-                x2: 2930,
-                y1: 2807,
-                y2: 2903
-            },
-            rotate: true,
-            range: [Math.PI * 2, 0]
-        });
-        console.log(c);
-    });
-    /*
-    it('', function() {
-        var c = rc.rotationRange({
-            anchor: {
-                x: 2359,
-                y: 3471
-            },
-            box: {
-                x1: 2545,
-                x2: 2665,
-                y1: 3393,
-                y2: 3561
-            },
-            rotate: true,
-        }, {
-            anchor: {
-                x: 1247,
-                y: 2443
-            },
-            box: {
-                x1: 1934,
-                x2: 2054,
-                y1: 2395,
-                y2: 2539
-            },
-            rotate: true,
-            range: [Math.PI * 2, 0]
-        });
-        console.log(c, c.map(deg));
-    });
-        */
-});
-
-/*
 describe('#mergeCollisions', function() {
     it('merges overlapping ranges', function() {
         expect(rc.mergeCollisions([[3/8*PI, 5/8*PI], [4/8*PI, 6/8*PI], [1/8*PI, 2/8*PI]], [2*PI, 0]))
@@ -158,7 +94,6 @@ describe('#circleEdgeCollisions', function() {
 
 describe('#rotatingRotatingCollisions', function() {
     it('basically works', function() {
-        var root2 = Math.sqrt(2);
         var c = rc.rotatingRotatingCollisions({
             anchor: { x: 0, y: 0 },
             box: { x1: -1, x2: 1, y1: 0, y2: 0 }
@@ -172,5 +107,16 @@ describe('#rotatingRotatingCollisions', function() {
         expect(Math.round(deg(c[1][0]))).to.eql(315);
         expect(Math.round(deg(c[1][1]))).to.eql(315);
     });
+
+    it('checks if the two boxes are close enough to collide at that angle', function() {
+        var c = rc.rotatingRotatingCollisions({
+            anchor: { x: 0, y: 0 },
+            box: { x1: -1, x2: 1, y1: 0, y2: 0 }
+        }, {
+            anchor: { x: 2, y: 2 },
+            box: { x1: 1, x2: 3, y1: 2, y2: 2 }
+        });
+
+        expect(c).to.eql([]);
+    });
 });
-*/
