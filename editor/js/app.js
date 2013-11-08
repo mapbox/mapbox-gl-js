8af0cd2719a93717190db2052d4cfe5697e8218e
Konstantin Käfer <github@kkaefer.com>
2013-11-08 12:07:03 +0100

    use a Layer class

--- editor/js/app.js
+++ editor/js/app.js
@@ -7,6 +7,100 @@ $(function() {
 
 var util = llmr.util;
 
+
+function Layer(layer, bucket, map) {
+    this.layer = layer;
+    this.bucket = bucket;
+    this.map = map;
+
+    this.root = $('<li class="layer">').data('layer', this);
+    var header = $('<div class="header">').appendTo(this.root);
+    var handle = $('<div class="icon handle-icon">');
+    var type = $('<div>').addClass('icon').addClass(bucket.type + '-icon').attr('title', titlecase(bucket.type));
+    var color = $('<div class="color">').css("background", layer.color);
+    var name = $('<div class="name">');
+    var remove = $('<div class="icon remove-icon">');
+
+    if (bucket.type == 'background') {
+        this.root.addClass('background');
+        name.text('Background');
+        header.append(type, color, name);
+    } else {
+        name.text(layer.bucket);
+        header.append(handle, type, color, name, remove);
+    }
+
+    header.click(this.activate.bind(this));
+    remove.click(this.remove.bind(this));
+}
+
+Layer.prototype.deactivate = function() {
+    this.root.removeClass('active');
+    this.root.find('.colorpicker').remove();
+    this.root.find('.linewidth').remove();
+};
+
+Layer.prototype.activate = function() {
+    var self = this;
+
+    if (this.root.is('.active')) {
+        this.deactivate();
+        return;
+    }
+
+    // remove all other "new" layers
+    this.root.siblings('.layer.new').remove();
+    this.root.siblings('.layer.active').each(function(i, item) {
+        $(item).data('layer').deactivate();
+    });
+
+    this.root.addClass('active');
+
+    var picker = $("<div class='colorpicker'></div>");
+    var layer = this.layer;
+
+
+    var hsv = Color.RGB_HSV(css2rgb(layer.color));
+    new Color.Picker({
+        hue: hsv.H,
+        sat: hsv.S,
+        val: hsv.V,
+        element: picker[0],
+        callback: function(hex) {
+            layer.color = '#' + hex;
+            self.root.find('.color').css('background', layer.color);
+            $(self).trigger('update');
+        }
+    });
+
+    this.root.append(picker);
+
+    var bucket = this.bucket;
+
+    if (bucket && bucket.type === 'line') {
+        var stops = layer.width.slice(1);
+        var widget = new LineWidthWidget(stops);
+        widget.on('stops', function(stops) {
+            layer.width = ['stops'].concat(stops);
+            $(self).trigger('update');
+        });
+
+        this.map.on('zoom', function(e) {
+            widget.setPivot(self.map.transform.z + 1);
+        });
+
+        widget.setPivot(self.map.transform.z + 1);
+
+        widget.canvas.appendTo(this.root[0]);
+    }
+
+};
+
+Layer.prototype.remove = function() {
+    this.root.remove();
+    $(this).trigger('remove');
+};
+
 function App() {
     var app = this;
 
@@ -32,50 +126,22 @@ function App() {
 
     $('#layers').sortable({
         axis: "y",
-        handle: ".handle",
+        items: ".layer:not(.background)",
+        handle: ".handle-icon",
         cursor: "-webkit-grabbing",
         change: function(e, ui) {
-            app.updateStyles(app.getLayerStyles(ui.placeholder[0], ui.item[0]));
+            app.updateStyles(app.getStyles(ui.placeholder[0], ui.item[0]));
         }
     });
 
-    // Background
-    var background = $('.layer.background')
-    $('.header', background).click(function() {
-        // Activate this layer.
-        app.switchLayer($());
-        background.addClass('active');
+    // Background layer
+    this.createLayer({ color: to_css_color(app.map.style.background) }, { type: 'background' });
 
-        var picker = $("<div class='colorpicker'></div>");
-
-        var color = app.map.style.background;
-        var rgb = { R: color[0] * 255, G: color[1] * 255, B: color[2] * 255 };
-        var hsv = Color.RGB_HSV(rgb);
-        new Color.Picker({
-            hue: hsv.H,
-            sat: hsv.S,
-            val: hsv.V,
-            element: picker[0],
-            callback: function(hex) {
-                background.find('.color').css('background', '#' + hex);
-                app.map.changeBackgroundStyle('#' + hex);            
-            }
-        });
-
-        background.append(picker);
-    });
-
-
-    var color = app.map.style.background;
-    background.find('.color').css('background', 'rgb(' + (color[0] * 255) + ',' + (color[1] * 255) + ',' + (color[2] * 255) + ')');
-
-
+    // Actual layers
     for (var i = 0; i < this.map.style.layers.length; i++) {
         var layer = this.map.style.layers[i];
         var bucket = this.map.style.buckets[layer.bucket];
-
-        var item = this.createLayer(layer, bucket);
-        $('#layers').append(item);
+        this.createLayer(layer, bucket);
     }
 
     $("#add-layer").click(function() {
@@ -91,10 +157,13 @@ function App() {
             .append(remove = $('<div class="icon remove-icon">'))
         );
 
+        // Deactivate all other layers.
+        $('#layers > .layer').each(function(i, layer) {
+            $(layer).data('layer').deactivate();
+        });
+
         $('#layers').append(item);
 
-        // Activate this layer.
-        app.switchLayer($());
         item.addClass('active');
 
         remove.click(function() {
@@ -122,7 +191,7 @@ function App() {
 
             var newItem = app.createLayer(layer, bucket);
             item.after(newItem);
-            app.switchLayer(newItem);
+            newItem.activate();
             app.updateStyles();
         });
     });
@@ -132,17 +201,36 @@ App.prototype.updateZoomLevel = function() {
     $('#zoomlevel').text("z" + util.formatNumber(this.map.transform.z + 1, 2));
 };
 
-App.prototype.getLayerStyles = function(placeholder, item) {
+App.prototype.getStyles = function(placeholder, item) {
+    var background;
     var layers = $('#layers > li.layer').map(function(i, layer) {
-        if (layer == placeholder) return $(item).data('layer');
-        else if (layer == item) return false;
-        else return $(layer).data('layer');
+        if (layer == item) return;
+        var data = $(layer == placeholder ? item : layer).data('layer');
+
+        if (data.bucket.type == 'background') {
+            background = data.layer;
+        } else {
+            return data.layer;
+        }
     });
-    return Array.prototype.slice.call(layers).filter(function(e) { return e; });
+    return {
+        background: background.color,
+        layers: Array.prototype.slice.call(layers).filter(function(e) { return e; })
+    };
 };
 
-App.prototype.updateStyles = function(layers) {
-    this.map.style.layers = layers || this.getLayerStyles();
+App.prototype.createLayer = function(layer, bucket) {
+    var app = this;
+    var item = new Layer(layer, bucket, this.map);
+    $('#layers').append(item.root);
+    $(item).bind('update remove', function() { app.updateStyles(); });
+    return item;
+};
+
+App.prototype.updateStyles = function(style) {
+    if (!style) style = this.getStyles();
+    this.map.style.layers = style.layers;
+    this.map.changeBackgroundStyle(style.background);
     this.map.changeLayerStyles();
 };
 
@@ -151,99 +239,14 @@ function titlecase(str) {
     return str.replace(/\w\S*/g, function(txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
 }
 
-App.prototype.createLayer = function(layer, bucket) {
-    var app = this;
-    var handle, type, color, name, remove;
-    var header;
-
-    var item = $('<li class="layer">')
-        .append(header = $('<div class="header">')
-            .append(handle = $('<div class="icon handle-icon">'))
-            .append(type = $('<div>').addClass('icon').addClass(bucket.type + '-icon').attr('title', titlecase(bucket.type)))
-            .append(color = $('<div class="color">').css("background", layer.color))
-            .append(name = $('<div class="name">').text(layer.bucket))
-            .append(remove = $('<div class="icon remove-icon">'))
-        );
-
-    item.data('layer', layer);
-
-    header.click(function() {
-        app.switchLayer(item);
-    });
-
-    remove.click(function() {
-        item.remove();
-        app.updateStyles();
-    });
-
-    return item;
-};
-
-App.prototype.switchLayer = function(item) {
-    var app = this;
-
-    var disable = item.is('.active');
-
-    $('.layer.active').each(function(i, item) {
-        app.setInactiveLayer($(item));
-    });
-
-    if (!disable && item.length) {
-        app.setActiveLayer(item);
-    }
-};
-
-App.prototype.setInactiveLayer = function(item) {
-    item.removeClass('active');
-    item.children('.colorpicker, .linewidth').remove();
-};
-
-App.prototype.setActiveLayer = function(item) {
-    // remove all other "new" layers
-    $('#layers > .layer.new').remove();
-
-    var app = this;
-    item.addClass('active');
-    
-    var picker = $("<div class='colorpicker'></div>");
-    var layer = item.data('layer');
-
-
-    var hsv = Color.RGB_HSV(css2rgb(layer.color));
-    new Color.Picker({
-        hue: hsv.H,
-        sat: hsv.S,
-        val: hsv.V,
-        element: picker[0],
-        callback: function(hex) {
-            layer.color = '#' + hex;
-            item.find('.color').css('background', layer.color);
-            app.updateStyles();
-        }
-    });
-
-    item.append(picker);
-
-    var bucket = this.map.style.buckets[layer.bucket];
-
-    if (bucket && bucket.type === 'line') {
-        var stops = layer.width.slice(1);
-        var widget = new LineWidthWidget(stops);
-        widget.on('stops', function(stops) {
-            layer.width = ['stops'].concat(stops);
-            app.updateStyles();
-        });
-
-        this.map.on('zoom', function(e) {
-            widget.setPivot(app.map.transform.z + 1);
-        });
-
-        widget.setPivot(app.map.transform.z + 1);
-
-        widget.canvas.appendTo(item[0]);
-    }
-
-};
+function to_css_color(color) {
+    var r = Math.round(color[0] * 255),
+        g = Math.round(color[1] * 255),
+        b = Math.round(color[2] * 255);
+    return '#' + (r < 16 ? '0' : '') + r.toString(16) +
+                 (g < 16 ? '0' : '') + g.toString(16) +
+                 (b < 16 ? '0' : '') + b.toString(16);
+}
 
 function css2rgb(c) {
     var x = function(i, size) {
