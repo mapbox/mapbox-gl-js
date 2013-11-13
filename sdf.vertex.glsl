d13e9f97570d38b45b6fa5d9727a7b8bc59aaff7
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-11-13 17:04:49 -0500

    curved labels. messy, slow, but working.

--- sdf.vertex.glsl
+++ sdf.vertex.glsl
@@ -24,18 +24,20 @@ varying vec2 v_tex;
 
 void main() {
 
-    float rev = 1.0;
+    float rev = 0.0;
     // We're using an int16 range for the angles.
-    if (abs(a_angle + u_angle) > 32767.0 && u_flip > 0.0) rev = -1.0;
+    //if (abs(a_angle + u_angle) > 32767.0 && u_flip > 0.0) rev = -1.0;
+    if (abs(mod(a_angle + u_angle/2.0, 65536.0) - 32767.0) < 16384.0 && u_flip > 0.0) rev = -1.0;
+    //rev = 1.0;
 
     // If the label should be invisible, we move the vertex outside
     // of the view plane so that the triangle gets clipped. This makes it easier
     // for us to create degenerate triangle strips.
-    float z = 2.0 - step(a_minzoom, u_zoom) - (1.0 - step(a_maxzoom, u_zoom));
+    float z = 2.0 - step(a_minzoom, u_zoom) - (1.0 - step(a_maxzoom, u_zoom)) - rev;
 
     float angle = mod(u_angle/2.0 + 65536.0, 65536.0);
     z += step(a_rangeend, angle) * step(angle, a_rangestart);
 
-    gl_Position = u_posmatrix * vec4(a_pos, 0, 1) + rev * u_exmatrix * vec4(a_offset / 64.0, z, 0);
+    gl_Position = u_posmatrix * vec4(a_pos, 0, 1) + u_exmatrix * vec4(a_offset / 64.0, z, 0);
     v_tex = a_tex * 4.0 / u_texsize;
 }
