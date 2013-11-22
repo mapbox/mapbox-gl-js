45bcdb36110b0f0998a190525ee7fb0bc35e1ca9
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-11-22 11:56:25 -0500

    fade labels based on zoom level

--- sdf.fragment.glsl
+++ sdf.fragment.glsl
@@ -6,9 +6,10 @@ uniform float u_buffer;
 uniform float u_gamma;
 
 varying vec2 v_tex;
+varying float v_alpha;
 
 void main() {
     float dist = texture2D(u_texture, v_tex).a;
-    float alpha = smoothstep(u_buffer - u_gamma, u_buffer + u_gamma, dist);
+    float alpha = smoothstep(u_buffer - u_gamma, u_buffer + u_gamma, dist) * v_alpha;
     gl_FragColor = u_color * alpha;
 }
