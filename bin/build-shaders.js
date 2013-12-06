6ee76d2da59972337c2a2c578d0187a88ede1bbf
Konstantin Käfer <github@kkaefer.com>
2013-12-06 17:19:07 +0100

    add shader minifier

--- bin/build-shaders.js
+++ bin/build-shaders.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
+var glslunit = require('./glsl-compiler-min.js');
 
 module.exports = function() {
+    var name;
     var shaders = {};
 
     var shaderFiles = fs.readdirSync('shaders');
@@ -9,7 +14,8 @@ module.exports = function() {
     for (var i = 0; i < shaderFiles.length; i++) {
         var parts = shaderFiles[i].match(/^(.+)\.(vertex|fragment)\.glsl$/);
         if (parts) {
-            var name = parts[1], type = parts[2];
+            name = parts[1];
+            var type = parts[2];
             if (!(name in shaders)) {
                 shaders[name] = {};
             }
@@ -17,8 +23,15 @@ module.exports = function() {
         }
     }
 
-    var code = "// NOTE: DO NOT CHANGE THIS FILE. IT IS AUTOMATICALLY GENERATED.\n\n";
-    code += "module.exports = " + JSON.stringify(shaders, null, 4) + ";\n";
+    for (name in shaders) {
+      var compiler = new glslunit.compiler.DemoCompiler(shaders[name].vertex, shaders[name].fragment);
+      var result = compiler.compileProgram();
+      shaders[name].vertex = glslunit.Generator.getSourceCode(result.vertexAst);
+      shaders[name].fragment = glslunit.Generator.getSourceCode(result.fragmentAst);
+    }
+
+    var code = '// NOTE: DO NOT CHANGE THIS FILE. IT IS AUTOMATICALLY GENERATED.\n\n';
+    code += 'module.exports = ' + JSON.stringify(shaders, null, 4) + ';\n';
 
     fs.writeFileSync('js/render/shaders.js', code);
 };
