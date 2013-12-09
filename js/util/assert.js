998d76654375d1eea642d3c0273cbd93a9d35c74
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-12-09 12:36:47 -0500

    fix disabling assert

--- js/util/assert.js
+++ js/util/assert.js
@@ -63,4 +63,6 @@ assert.equal = function equal(actual, expected, message) {
     if (actual != expected) fail(actual, expected, message, '==', assert.equal);
 };
 
+} else {
+    module.exports = undefined;
 }
