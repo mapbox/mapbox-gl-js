a10c33a101beb29cd50de50d80805c97d8db8d6c
Konstantin Käfer <github@kkaefer.com>
2014-01-03 17:04:47 +0100

    fix debug/tile boundaries

--- js/render/painter.js
+++ js/render/painter.js
@@ -822,13 +822,15 @@ function drawText(gl, painter, layer, layerStyle, tile, stats, params, bucket_in
 }
 
 function drawDebug(gl, painter, tile, stats, params) {
-    gl.disable(gl.STENCIL_TEST);
+    // Blend to the front, not the back.
+    gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+
     gl.switchShader(painter.debugShader, painter.posMatrix, painter.exMatrix);
 
     // draw bounding rectangle
     gl.bindBuffer(gl.ARRAY_BUFFER, painter.debugBuffer);
     gl.vertexAttribPointer(painter.debugShader.a_pos, painter.bufferProperties.debugItemSize, gl.SHORT, false, 0, 0);
-    gl.uniform4f(painter.debugShader.u_color, 1, 1, 1, 1);
+    gl.uniform4f(painter.debugShader.u_color, 1, 0, 0, 1);
     gl.lineWidth(4);
     gl.drawArrays(gl.LINE_STRIP, 0, painter.bufferProperties.debugNumItems);
 
@@ -852,6 +854,9 @@ function drawDebug(gl, painter, tile, stats, params) {
     gl.lineWidth(1 * window.devicePixelRatio);
     gl.uniform4f(painter.debugShader.u_color, 0, 0, 0, 1);
     gl.drawArrays(gl.LINES, 0, vertices.length / painter.bufferProperties.textItemSize);
+
+    // Revert blending mode to blend to the back.
+    gl.blendFunc(gl.ONE_MINUS_DST_ALPHA, gl.ONE);
 }
 
 function drawVertices(gl, painter, layer, layerStyle, tile, stats, params) {
