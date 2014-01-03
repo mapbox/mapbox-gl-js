b0bcc4d630dcc7f2b6a4125dc68bfcd1ad78a198
Konstantin Käfer <github@kkaefer.com>
2014-01-03 16:47:06 +0100

    fix debug vertex display

---
+++ debug_point.fragment.glsl
@@ -0,0 +1,7 @@
+precision mediump float;
+
+uniform vec4 u_color;
+
+void main() {
+    gl_FragColor = u_color;
+}
--- fill.vertex.glsl
+++ fill.vertex.glsl
@@ -5,4 +5,5 @@ uniform mat4 u_posmatrix;
 
 void main() {
     gl_Position = u_posmatrix * vec4(a_pos, 0, 1);
+    gl_PointSize = 2.0;
 }
