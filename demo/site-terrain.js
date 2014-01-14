9f3702acbe58684ac6b307b2e4725106ad47a947
Konstantin Käfer <mail@kkaefer.com>
2014-01-14 14:30:24 +0100

    add back terrain test

--- demo/site-terrain.js
+++ demo/site-terrain.js
@@ -8,6 +8,12 @@ domready(function() {
                 type: 'vector',
                 urls: ['/gl/tiles/terrain/{z}-{x}-{y}.vector.pbf'],
                 zooms: [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+            },
+            'satellite': {
+                type: 'raster',
+                urls: ['//api.tiles.mapbox.com/v3/aibram.map-vlob92uz/{z}/{x}/{y}.png'],
+                zooms: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+                enabled: false
             }
         },
         maxZoom: 20,
