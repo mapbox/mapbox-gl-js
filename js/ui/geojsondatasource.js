1702fa5d746ed941af8ab8859302a47cac9b06a9
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-01-22 13:29:46 -0500

    rejoin polygon segments after tiling

--- js/ui/geojsondatasource.js
+++ js/ui/geojsondatasource.js
@@ -55,7 +55,7 @@ GeoJSONDatasource.prototype._tileGeoJSON = function(geojson) {
 
 
 GeoJSONDatasource.prototype._tileFeature = function(feature) {
-    var coords = feature.geometry.coordinates;
+    var coords = feature.geometry.coordinates[0];
 
     var tilesize = 512;
     var tileExtent = 4096;
@@ -122,19 +122,70 @@ GeoJSONDatasource.prototype._tileFeature = function(feature) {
                         y: ((prevCoord.row + exit * dy) - y) * tileExtent
                     };
                     tile[tile.length - 1].push(point);
-                    // TODO add extra points for polygons
 
                 // add the point itself
                 } else {
                     point = {
                         x: (coord.column - x) * tileExtent,
-                        y: (coord.row - y) * tileExtent
+                        y: (coord.row - y) * tileExtent,
+                        inside: true
                     };
                     if (!tile) tiles[tileID] = tile = [[point]];
                     else tile[tile.length - 1].push(point);
                 }
             }
         }
+    }
+
+    for (var id in tiles) {
+
+        var fillable = true; // todo unhardcode
+
+        var segments = tiles[id];
+        var ring = [];
+
+        if (fillable) {
+            // join all segments so we can draw the feature as an area
+
+            if (segments[0][0].inside && segments.length > 1) {
+                // if there is more than one segment, combine the last
+                // and first so that all start and end on tile edges
+                var last = segments.pop();
+                Array.prototype.unshift.apply(segments[0], last.slice(0, last.length - 1));
+            }
+
+            var start = edgeDist(segments[0][0], tileExtent, padding);
+
+            for (var k = 0; k < segments.length; k++) {
+                var thisExit = edgeDist(segments[k][segments[k].length - 1], tileExtent, padding);
+                var nextEntry = edgeDist(segments[(k + 1) % segments.length][0], tileExtent, padding);
+
+                var startToExit = (thisExit - start + 4) % 4;
+                var startToNextEntry = (nextEntry - start + 4) % 4;
+                var direction = startToExit < startToNextEntry ? 1 : -1;
+                if (startToNextEntry === 0) direction = 1;
+
+                var round = direction > 0 ? Math.ceil : Math.floor;
+                for (var c = round(thisExit) % 4; c != round(nextEntry) % 4; c = (c + direction + 4) % 4) {
+                    var corner = corners[c];
+                    segments[k].push({
+                        x: (corner.x + (corner.x - 0.5 > 0 ? 1 : -1) * padding) * tileExtent,
+                        y: (corner.y + (corner.y - 0.5 > 0 ? 1 : -1) * padding) * tileExtent
+                    });
+                }
+            }
+
+            for (var m = 0; m < segments.length; m++) {
+                ring = ring.concat(segments[m]);
+            }
+        }
+
+        this.alltiles[id] = this.alltiles[id] || [];
+        this.alltiles[id].push({
+            properties: feature.properties,
+            coords: [ring],
+            type: typeMapping[feature.geometry.type]
+        });
     }
 
 };
@@ -144,3 +195,25 @@ var typeMapping = {
     'LineString': 'line',
     'Polygon': 'fill'
 };
+
+var corners = [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 0, y: 1 }];
+
+
+// convert a point in the tile to distance along the edge from the top left
+function edgeDist(point, tileExtent, padding) {
+    var extent = tileExtent + padding * 2;
+    var x = point.x / extent;
+    var y = point.y / extent;
+    var d;
+    if (Math.abs(y - 0.5) >= Math.abs(x - 0.5)) {
+        d = Math.round(y) * 2 + (y < 0.5 ? x : 1 - x);
+    } else {
+        d = Math.round(1 - x) * 2 + (x > 0.5 ? y : 1 - y) + 1;
+    }
+
+    return d % 4;
+}
