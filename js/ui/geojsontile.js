a2707e9801fbd3cefe2207f473465615a0a18ef3
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-01-29 11:02:59 -0500

    add spatial querying for geojson datasources

--- js/ui/geojsontile.js
+++ js/ui/geojsontile.js
@@ -3,6 +3,7 @@
 var Tile = require('./tile.js');
 var Transform = require('./transform.js');
 var Geometry = require('../geometry/geometry.js');
+var FeatureTree = require('../geometry/featuretree.js');
 var util = require('../util/util.js');
 var Bucket = require('../geometry/bucket.js');
 
@@ -13,7 +14,16 @@ function GeoJSONTile(map, features, zoom) {
     this.features = features;
 
     this.geometry = new Geometry();
+    this.featureTree = new FeatureTree(getGeometry, getType);
 
+}
+
+function getGeometry(feature) {
+    return feature.coords;
+}
+
+function getType(feature) {
+    return feature.type;
 }
 
 GeoJSONTile.prototype = Object.create(Tile.prototype);
@@ -60,7 +70,13 @@ GeoJSONTile.prototype._parse = function(features) {
         bucket.start();
 
         for (var i = 0; i < bucket.features.length; i++) {
-            bucket.addFeature(bucket.features[i].coords);
+            var feature = bucket.features[i];
+
+            bucket.addFeature(feature.coords);
+
+            var bbox = getbbox(feature.coords);
+            console.log(bbox);
+            this.featureTree.insert(bbox, name, feature);
         }
 
         bucket.end();
@@ -80,3 +96,35 @@ GeoJSONTile.prototype._load = function() {
 // noops
 GeoJSONTile.prototype.abort = function() { };
 GeoJSONTile.prototype.remove = function() { };
+
+GeoJSONTile.prototype.featuresAt = function(pos, params, callback) {
+    this.featureTree.query({
+        id: this.id,
+        x: pos.x,
+        y: pos.y,
+        scale: pos.scale,
+        params: params
+    }, callback);
+};
+
+function getbbox(rings) {
+    var x1 = Infinity,
+        x2 = -Infinity,
+        y1 = Infinity,
+        y2 = -Infinity;
+
+    for (var i = 0; i < rings.length; i++) {
+        var ring = rings[i];
+
+        for (var j = 0; j < ring.length; j++) {
+            var coord = ring[j];
+
+            x1 = Math.min(x1, coord.x);
+            x2 = Math.max(x2, coord.x);
+            y1 = Math.min(y1, coord.y);
+            y2 = Math.max(y2, coord.y);
+        }
+    }
+
+    return { x1: x1, y1: y1, x2: x2, y2: y2 };
+}
