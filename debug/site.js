81d541bdb5a7e20a8af1da2b9f4b562443a44e88
Vladimir Agafonkin <agafonkin@gmail.com>
2014-02-04 13:39:40 +0200

    remove unnecessary domready lib, minor debug cleanup

--- debug/site.js
+++ debug/site.js
@@ -1,38 +1,34 @@
-var DEBUG = true;
-var ds;
 
-domready(function() {
-    globalMap = new llmr.Map({
-        container: document.getElementById('map'),
-        datasources: {
-            "mapbox streets": {
-                type: 'vector',
-                id: 'streets',
-                urls: ['/gl/tiles/plain/{z}-{x}-{y}.vector.pbf'],
-                // urls: ['http://api.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4/{z}/{x}/{y}.vector.pbf'],
-                zooms: [0, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14],
-            },
-            "satellite": {
-                type: 'raster',
-                id: 'satellite',
-                urls: ['/gl/raster/{z}-{x}-{y}.png'],
-                urls: ['http://api.tiles.mapbox.com/v3/aibram.map-vlob92uz/{z}/{x}/{y}.png'],
-                zooms: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
-                enabled: false
-            }
+var map = new llmr.Map({
+    container: document.getElementById('map'),
+    datasources: {
+        "mapbox streets": {
+            type: 'vector',
+            id: 'streets',
+            urls: ['/gl/tiles/plain/{z}-{x}-{y}.vector.pbf'],
+            // urls: ['http://api.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4/{z}/{x}/{y}.vector.pbf'],
+            zooms: [0, 2, 3, 4, 5, 6, 7, 8, 10, 12, 13, 14],
         },
-        maxZoom: 20,
-        zoom: 15,
-        lat: 38.912753,
-        lon: -77.032194,
-        rotation: 0,
-        style: style_json,
-        hash: true
-    });
-
-    // add geojson overlay
-    ds = new llmr.GeoJSONDatasource({ type: 'Feature', properties: {}, geometry: route.routes[0].geometry}, globalMap);
-    globalMap.addDatasource('geojson', ds);
-
-    new Debug(globalMap);
+        "satellite": {
+            type: 'raster',
+            id: 'satellite',
+            // urls: ['/gl/raster/{z}-{x}-{y}.png'],
+            urls: ['http://api.tiles.mapbox.com/v3/aibram.map-vlob92uz/{z}/{x}/{y}.png'],
+            zooms: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17],
+            enabled: false
+        }
+    },
+    maxZoom: 20,
+    zoom: 15,
+    lat: 38.912753,
+    lon: -77.032194,
+    rotation: 0,
+    style: style_json,
+    hash: true
 });
+
+// add geojson overlay
+var geojson = new llmr.GeoJSONDatasource({ type: 'Feature', properties: {}, geometry: route.routes[0].geometry}, map);
+map.addDatasource('geojson', geojson);
+
+new Debug(map);
