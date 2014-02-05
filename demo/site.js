d60a77387f8675790ef4f1a75ebbd628b86d7409
Vladimir Agafonkin <agafonkin@gmail.com>
2014-02-05 19:15:28 +0200

    update vector tile links to point to staging server

--- demo/site.js
+++ demo/site.js
@@ -4,19 +4,18 @@ var map = new llmr.Map({
     datasources: {
         'streets': {
             type: 'vector',
-            urls: ['/gl/tiles/plain/{z}-{x}-{y}.vector.pbf'],
+            urls: ['http://a.gl-api-us-east-1.tilestream.net/v3/mapbox.mapbox-streets-v4/{z}/{x}/{y}.gl.pbf'],
             zooms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         },
         'terrain': {
             type: 'vector',
-            urls: ['/gl/tiles/terrain/{z}-{x}-{y}.vector.pbf'],
+            urls: ['http://a.gl-api-us-east-1.tilestream.net/v3/aj.mapbox-streets-outdoors-sf/{z}/{x}/{y}.vector.pbf'],
             zooms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         },
         'satellite': {
             type: 'raster',
-            urls: ['//api.tiles.mapbox.com/v3/aibram.map-vlob92uz/{z}/{x}/{y}.png'],
-            zooms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-            enabled: false
+            urls: ['http://api.tiles.mapbox.com/v3/aibram.map-vlob92uz/{z}/{x}/{y}.png'],
+            zooms: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
         }
     },
     maxZoom: 20,
