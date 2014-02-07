3b89c491a764af0147c4029fb44bbb9e58eb0790
Eden Halperin <eden@mapbox.com>
2014-02-07 15:38:54 -0800

    add dot shader if no image specified in point bucket, closes #249

---
+++ dot.fragment.glsl
@@ -0,0 +1,11 @@
+precision mediump float;
+
+uniform vec4 u_color;
+uniform float u_fade;
+
+void main() {
+	float dist = length(gl_PointCoord - vec2(0.5, 0.5));
+	float t = smoothstep(0.5-(u_fade/2.0), 0.5, dist);
+
+    gl_FragColor = mix(u_color, vec4(0.0, 0.0, 0.0, 0.0), t);
+}
