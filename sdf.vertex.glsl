156446b8da9c49364f4065594cf4b8fefa8e5c9f
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-02-19 09:14:54 -0500

    convert -pi..pi angle to 0..2pi in js not shader

--- sdf.vertex.glsl
+++ sdf.vertex.glsl
@@ -37,7 +37,7 @@ void main() {
     // a_angle is angle of the label, 0..256 representing 0..2PI, where 0 is horizontal text
     float rotated = mod(a_angle + u_angle, 256.0);
     // if the label rotates with the map, and if the rotated label is upside down, hide it
-    if (u_flip > 0.0 && rotated > 64.0 && rotated <= 192.0) rev = 1.0;
+    if (u_flip > 0.0 && rotated >= 64.0 && rotated < 192.0) rev = 1.0;
 
     // If the label should be invisible, we move the vertex outside
     // of the view plane so that the triangle gets clipped. This makes it easier
@@ -63,11 +63,9 @@ void main() {
     // if label has been faded out, clip it
     z += step(v_alpha, 0.0);
 
-    float angle = mod(u_angle, 256.0);
-
     // all the angles are 0..256 representing 0..2PI
     // hide if (angle >= a_rangeend && angle < rangestart)
-    z += step(a_rangeend, angle) * (1.0 - step(a_rangestart, angle));
+    z += step(a_rangeend, u_angle) * (1.0 - step(a_rangestart, u_angle));
 
     gl_Position = u_posmatrix * vec4(a_pos, 0, 1) + u_exmatrix * vec4(a_offset / 64.0, z, 0);
     v_tex = a_tex * 4.0 / u_texsize;
