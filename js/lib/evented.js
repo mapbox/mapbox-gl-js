4dde580ef5c239fc87fba06755e7198d0cf85a48
Vladimir Agafonkin <agafonkin@gmail.com>
2014-03-06 17:21:18 +0200

    cleanup events code

--- js/lib/evented.js
+++ js/lib/evented.js
@@ -2,26 +2,32 @@
 
 module.exports = {
     on: function(evt, fn) {
-        if (typeof evt !== 'string' || /\s/.test(evt)) throw new Error('can only bind single string events, got ' + JSON.stringify(evt));
-        if (!this._events) Object.defineProperty(this, "_events", { value: {} });
-        if (!this._events[evt]) this._events[evt] = [];
+        if (typeof evt !== 'string' || /\s/.test(evt)) {
+            throw new Error('can only bind single string events, got ' + JSON.stringify(evt));
+        }
+
+        this._events = this._events || {};
+        this._events[evt] = this._events[evt] || [];
         this._events[evt].push(fn);
+
         return this;
     },
 
     off: function(evt, fn) {
-        if (this._events && this._events[evt]) {
+        if (!this._events) return this;
+
+        if (this._events[evt]) {
             if (fn) {
                 var idx = this._events[evt].indexOf(fn);
                 if (idx >= 0) { this._events[evt].splice(idx, 1); }
             } else {
                 delete this._events[evt];
             }
-        } else if (this._events && !evt) {
-            for (evt in this._events) {
-                delete this._events[evt];
-            }
+
+        } else if (!evt) {
+            this._events = {};
         }
+
         return this;
     },
 
