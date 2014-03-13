1f562b4f6c4bb42208a918655ef66227a8f4b7db
Vladimir Agafonkin <agafonkin@gmail.com>
2014-03-13 10:18:24 -0700

    added a LatLng class

--- js/llmr.js
+++ js/llmr.js
@@ -7,11 +7,17 @@ if (typeof window === 'undefined') {
     var llmr = module.exports = window.llmr = {};
 
     llmr.Map = require('./ui/map.js');
+
     llmr.Source = require('./ui/source.js');
+    llmr.GeoJSONSource = require('./ui/geojsonsource');
+
     llmr.Style = require('./style/style.js');
     llmr.StyleDeclaration = require('./style/styledeclaration.js');
+
+    llmr.LatLng = require('./geometry/latlng.js');
+
     llmr.Tile = require('./ui/tile.js');
+
     llmr.Evented = require('./lib/evented.js');
     llmr.util = require('./util/util.js');
-    llmr.GeoJSONSource = require('./ui/geojsonsource');
 }
