a888306e6f4481bff9d21a440ab3a415b2430b48
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-05-09 14:28:04 -0400

    move calculation from frag to vertex shader

--- composite.fragment.glsl
+++ composite.fragment.glsl
@@ -6,5 +6,5 @@ uniform float u_opacity;
 varying vec2 v_pos;
 
 void main() {
-    gl_FragColor = texture2D(u_image, v_pos / 2.0 + 0.5) * u_opacity;
+    gl_FragColor = texture2D(u_image, v_pos) * u_opacity;
 }
