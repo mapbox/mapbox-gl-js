5e3f677a5ff3cd3b91b4b6287999dfd389064e8e
Young Hahn <young@mapbox.com>
2014-05-30 19:56:21 -0400

    Refactor shaping tests to use fixtures and add the simplest independent placement test.

---
+++ test/text-placement.js
@@ -0,0 +1,48 @@
+'use strict';
+var test = require('tape').test;
+var Geometry = require('../js/geometry/geometry.js');
+var Placement = require('../js/text/placement.js');
+var Shaping = require('../js/text/shaping.js');
+var GlyphAtlas = require('../js/text/glyphatlas');
+var glyphs = require('./fixtures/fontstack-glyphs.json');
+
+test('Placement', function(t) {
+    var geometry = new Geometry();
+    var placement = new Placement(geometry, 12, 512);
+    t.ok(placement);
+
+    var atlas = new GlyphAtlas(1024,1024);
+    var rects = {};
+    for (var id in glyphs) {
+        glyphs[id].bitmap = true;
+        rects[id] = atlas.addGlyph(id, 'Test', glyphs[id], 3);
+    }
+
+    var stacks = { 'Test': {
+        glyphs: glyphs,
+        rects: rects
+    }};
+    var shaping = (function() {
+        var oneEm = 24;
+        var name = 'Test';
+        return Shaping.shape('abcde', name, stacks, 15 * oneEm, oneEm, 0.5, 0 * oneEm);
+    })();
+
+    var a, b;
+
+    // text-max-size is required here and is not defaulted.
+    // Without it collision detection fails.
+    a = JSON.stringify(placement.collision);
+    placement.addFeature([{x:2048,y:2048}], { 'text-max-size':12 }, stacks, shaping);
+    b = JSON.stringify(placement.collision);
+
+    t.notEqual(a, b, 'places feature');
+
+    a = JSON.stringify(placement.collision);
+    placement.addFeature([{x:2048,y:2048}], { 'text-max-size':12 }, stacks, shaping);
+    b = JSON.stringify(placement.collision);
+
+    t.equal(a, b, 'detects collision and does not place feature');
+
+    t.end();
+});
--- test/text-shaping.js
+++ test/text-shaping.js
@@ -1,51 +1,34 @@
 'use strict';
 var test = require('tape').test;
 var shaping = require('../js/text/shaping.js');
+var fs = require('fs');
+var UPDATE = process && process.env.UPDATE;
 
 test('shaping', function(t) {
     var oneEm = 24;
-    var name = 'Arial';
-    var stacks = { 'Arial': { glyphs: {
-        32: { advance:20 },
-        97: { advance:20 },
-        98: { advance:20 },
-        99: { advance:20 },
-        100: { advance:20 },
-        101: { advance:20 }
-    }}};
+    var name = 'Test';
+    var stacks = {
+        'Test': {
+            glyphs: require('./fixtures/fontstack-glyphs.json')
+        }
+    };
+
+    var shaped;
 
     // Default shaping.
-    t.deepEqual([
-        { fontstack: 'Arial', glyph: 97, x: -50, y: 0 },
-        { fontstack: 'Arial', glyph: 98, x: -30, y: 0 },
-        { fontstack: 'Arial', glyph: 99, x: -10, y: 0 },
-        { fontstack: 'Arial', glyph: 100, x: 10, y: 0 },
-        { fontstack: 'Arial', glyph: 101, x: 30, y: 0 }
-    ], shaping.shape('abcde', name, stacks, 15 * oneEm, oneEm, 0.5, 0 * oneEm)); 
+    shaped = shaping.shape('abcde', name, stacks, 15 * oneEm, oneEm, 0.5, 0 * oneEm);
+    if (UPDATE) fs.writeFileSync(__dirname + '/expected/text-shaping-default.json', JSON.stringify(shaped, null, 2));
+    t.deepEqual(require('./expected/text-shaping-default.json'), shaped);
 
     // Letter spacing.
-    t.deepEqual([
-        { fontstack: 'Arial', glyph: 97, x: -56, y: 0 },
-        { fontstack: 'Arial', glyph: 98, x: -33, y: 0 },
-        { fontstack: 'Arial', glyph: 99, x: -10, y: 0 },
-        { fontstack: 'Arial', glyph: 100, x: 13, y: 0 },
-        { fontstack: 'Arial', glyph: 101, x: 36, y: 0 }
-    ], shaping.shape('abcde', name, stacks, 15 * oneEm, oneEm, 0.5, 0.125 * oneEm)); 
+    shaped = shaping.shape('abcde', name, stacks, 15 * oneEm, oneEm, 0.5, 0.125 * oneEm);
+    if (UPDATE) fs.writeFileSync(__dirname + '/expected/text-shaping-spacing.json', JSON.stringify(shaped, null, 2));
+    t.deepEqual(require('./expected/text-shaping-spacing.json'), shaped);
 
     // Line break.
-    t.deepEqual([
-        { fontstack: 'Arial', glyph: 97, x: -50, y: 0 },
-        { fontstack: 'Arial', glyph: 98, x: -30, y: 0 },
-        { fontstack: 'Arial', glyph: 99, x: -10, y: 0 },
-        { fontstack: 'Arial', glyph: 100, x: 10, y: 0 },
-        { fontstack: 'Arial', glyph: 101, x: 30, y: 0 },
-        { fontstack: 'Arial', glyph: 32, x: 100, y: 0 },
-        { fontstack: 'Arial', glyph: 97, x: -50, y: 24 },
-        { fontstack: 'Arial', glyph: 98, x: -30, y: 24 },
-        { fontstack: 'Arial', glyph: 99, x: -10, y: 24 },
-        { fontstack: 'Arial', glyph: 100, x: 10, y: 24 },
-        { fontstack: 'Arial', glyph: 101, x: 30, y: 24 }
-    ], shaping.shape('abcde abcde', name, stacks, 4 * oneEm, oneEm, 0.5, 0 * oneEm)); 
+    shaped = shaping.shape('abcde abcde', name, stacks, 4 * oneEm, oneEm, 0.5, 0 * oneEm);
+    if (UPDATE) fs.writeFileSync(__dirname + '/expected/text-shaping-linebreak.json', JSON.stringify(shaped, null, 2));
+    t.deepEqual(require('./expected/text-shaping-linebreak.json'), shaped);
 
     t.end();
 });
