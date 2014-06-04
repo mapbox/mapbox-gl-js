3390d0af5f2def48f31b132e570deb3984e21a0e
Young Hahn <young@mapbox.com>
2014-06-04 11:19:16 -0400

    Revert size from varying to uniform and use bucket info for point-image and point-size.

--- point.vertex.glsl
+++ point.vertex.glsl
@@ -5,12 +5,12 @@ precision mediump float;
 #define PI 3.1415926535897932384626
 
 uniform mat4 u_posmatrix;
+uniform vec2 u_size;
 uniform mat2 u_rotationmatrix;
 uniform float u_zoom;
 uniform vec2 u_texsize;
 
 attribute vec2 a_pos;
-attribute vec2 a_size;
 attribute vec2 a_tl;
 attribute vec2 a_br;
 attribute float a_angle;
@@ -23,7 +23,7 @@ varying vec2 v_br;
 void main(void) {
 
     gl_Position = u_posmatrix * vec4(a_pos, 0, 1);
-    gl_PointSize = a_size.x * root2;
+    gl_PointSize = u_size.x * root2;
 
     // if u_zoom < a_minzoom hide this point
     gl_Position.z += 1.0 - step(a_minzoom, u_zoom);
