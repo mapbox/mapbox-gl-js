725e8b9a3f73c0e4c2bd86734e993e026fa5dd1d
Vladimir Agafonkin <agafonkin@gmail.com>
2014-06-05 00:06:29 +0300

    simplify point shader

--- shaders/point.fragment.glsl
+++ shaders/point.fragment.glsl
@@ -20,9 +20,5 @@ void main(void) {
         color.rgb = 1.0 - color.rgb;
     }
 
-    if (u_color.a > 0.0) {
-        gl_FragColor = u_color * color;
-    } else {
-        gl_FragColor = vec4(color.rgb * color.a, color.a);
-    }
+    gl_FragColor = u_color * color;
 }
