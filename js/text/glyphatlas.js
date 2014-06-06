f07f2a4527ce8bbe2150bd81f0a32258bb4a777f
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-06-06 00:22:44 -0400

    don't reallocate texture memory for glyphAtlas...
    each time it gets update. Overwrite the previous instead.

--- js/text/glyphatlas.js
+++ js/text/glyphatlas.js
@@ -171,6 +171,7 @@ GlyphAtlas.prototype.bind = function(gl) {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, this.width, this.height, 0, gl.ALPHA, gl.UNSIGNED_BYTE, null);
 
     } else {
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
@@ -181,7 +182,7 @@ GlyphAtlas.prototype.updateTexture = function(gl) {
     this.bind(gl);
     if (this.dirty) {
 
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.ALPHA, this.width, this.height, 0, gl.ALPHA, gl.UNSIGNED_BYTE, this.data);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.width, this.height, gl.ALPHA, gl.UNSIGNED_BYTE, this.data);
 
         // DEBUG
         if (this.ctx) {
