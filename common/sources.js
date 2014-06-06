f80af490103b38d4c87154d0b4ac5bc4da9cff01
Young Hahn <young@mapbox.com>
2014-06-06 12:29:21 -0400

    terrain => outdoors.

---
+++ common/sources.js
@@ -0,0 +1,9 @@
+window.sources = {
+    "mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5": {
+        type: 'vector',
+        url: 'http://a.tiles.mapbox.com/v3/mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5/{z}/{x}/{y}.vector.pbf',
+        glyphs: 'http://mapbox.s3.amazonaws.com/gl-glyphs-256/{fontstack}/{range}.pbf',
+        tileSize: 512,
+        maxZoom: 14
+    }
+};
