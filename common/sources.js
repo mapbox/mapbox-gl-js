c4f2c19b853217c7ffcd2a88b04d9d86ce62186f
Young Hahn <young@mapbox.com>
2014-06-06 12:31:47 -0400

    httpsify

--- common/sources.js
+++ common/sources.js
@@ -1,8 +1,8 @@
 window.sources = {
     "mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5": {
         type: 'vector',
-        url: 'http://a.tiles.mapbox.com/v3/mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5/{z}/{x}/{y}.vector.pbf',
-        glyphs: 'http://mapbox.s3.amazonaws.com/gl-glyphs-256/{fontstack}/{range}.pbf',
+        url: 'https://a.tiles.mapbox.com/v3/mapbox.mapbox-terrain-v1,mapbox.mapbox-streets-v5/{z}/{x}/{y}.vector.pbf',
+        glyphs: 'https://mapbox.s3.amazonaws.com/gl-glyphs-256/{fontstack}/{range}.pbf',
         tileSize: 512,
         maxZoom: 14
     }
