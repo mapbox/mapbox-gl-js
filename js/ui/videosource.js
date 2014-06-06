7083b28ad9b186a716ac8b8664d650db462c1f22
Ansis Brammanis <ansis.brammanis@gmail.com>
2014-06-05 23:59:41 -0400

    upload video frames with texSubImage2D instead
    of texImage2D which allocates memory. Much faster.

--- js/ui/videosource.js
+++ js/ui/videosource.js
@@ -130,12 +130,13 @@ VideoSource.prototype.bind = function(gl) {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.video);
 
     } else {
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.video);
     }
 
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.video);
 };
 
 
