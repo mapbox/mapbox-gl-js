f8dc07d72d18c20aceb964c65053050c4f765529
Young Hahn <young@mapbox.com>
2014-06-15 12:51:50 -0400

    Updates for spec v2, handle enum and default-value=>default changes.

--- js/style/calculatedstyle.js
+++ js/style/calculatedstyle.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var reference = require('mapbox-gl-style-spec').latest;
+var reference = require('mapbox-gl-style-spec').v2;
 
 module.exports = CalculatedStyle;
 
@@ -11,7 +11,7 @@ function CalculatedStyle() {}
 function addDefaultValues() {
     var style = reference.style;
     for (var prop in style) {
-        var value = style[prop]['default-value'];
+        var value = style[prop]['default'];
         if (value !== undefined) {
             CalculatedStyle.prototype[prop] = value;
         }
