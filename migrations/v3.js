53d9d554230ee3a5e5f3cc604a75c793eb5a6eaf
Young Hahn <young@mapbox.com>
2014-06-18 17:07:09 -0400

    Remove commented out code.

--- migrations/v3.js
+++ migrations/v3.js
@@ -35,48 +35,6 @@ function converter(v2) {
     return v3;
 }
 
-/*
-Converter.prototype.convertStyles = function(styles) {
-    this.types = {};
-
-    for (var style_name in styles) {
-        var style = styles[style_name];
-        for (var name in style) {
-
-            var old_rules = style[name];
-            var rules = {};
-            for (var old_rule_name in old_rules) {
-                var new_rule_name = old_rule_name.replace(/(^|-)point-/, '$1icon-');
-
-                if (new_rule_name == 'icon-size') {
-                    rules[new_rule_name] = old_rules[old_rule_name];
-                    rules[new_rule_name] = Math.max(rules[new_rule_name][0], rules[new_rule_name][1]);
-                } else if (new_rule_name != 'icon-antialias') {
-                    rules[new_rule_name] = old_rules[old_rule_name];
-                }
-            }
-
-            style[name] = rules;
-
-            for (var rule_name in rules) {
-                var match = rule_name.match(/(?:^|-)(icon|line|fill|text)-/);
-                if (match) {
-                    this.types[name] = match[1];
-                    break;
-                }
-            }
-
-            if (rules['fill-color'] && rules['line-color']) {
-                rules['fill-stroke-color'] = rules['line-color'];
-                delete rules['line-color'];
-            }
-        }
-    }
-
-    return styles;
-};
-*/
-
 function convertLayer(memo, v2, buckets, styles, constants) {
     var j, k, val, known;
     var v3 = {};
@@ -187,31 +145,6 @@ function convertLayer(memo, v2, buckets, styles, constants) {
             v3[styleName][k] = val;
         }
     }
-    /*
-    if (layer.filter) {
-        layer.source = layer.filter.source;
-        layer.layer = layer.filter.layer;
-        delete layer.filter.source;
-        delete layer.filter.layer;
-
-        if (layer.filter.feature_type) {
-            layer.geometry = layer.filter.feature_type;
-            delete layer.filter.feature_type;
-        }
-
-        if (Object.keys(layer.filter).length === 0) {
-            delete layer.filter;
-        }
-    }
-    */
-
-    /*
-    if (layer.layers) {
-        layer.layers = this.convertLayers(layer.layers);
-    } else {
-        layer.type = this.types[layer.id];
-    }
-    */
 
     return v3;
 }
