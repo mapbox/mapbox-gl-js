a3e86bec743666a2a7490ace48a3209952dce11a
John Firebaugh <john.firebaugh@gmail.com>
2014-06-30 14:08:39 -0700

    Fix build

--- test/worker.test.js
+++ test/worker.test.js
@@ -4,3 +4,8 @@ var test = require('tape').test,
     WorkerTile = require('../js/worker/workertile.js');
 
 // Stub for code coverage
+
+test('basic', function(t) {
+    new WorkerTile(null, {});
+    t.end();
+});
