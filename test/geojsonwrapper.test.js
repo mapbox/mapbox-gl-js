12403dc46f7c3dd5724c81712debdae9116a59f6
Tom MacWright <tom@macwright.org>
2014-07-02 12:38:03 -0400

    Expand tests for geojsonwrapper

--- test/geojsonwrapper.test.js
+++ test/geojsonwrapper.test.js
@@ -1,20 +1,24 @@
 'use strict';
 
 var test = require('tape').test,
-    WorkerTile = require('../js/worker/workertile'),
     Wrapper = require('../js/worker/geojsonwrapper');
 
-// Stub for code coverage
-
 test('geojsonwrapper', function(t) {
+
     var features = [{
         type: 'LineString',
         coords: [[{ x: 0, y: 0 }, {x:10, y:10}]],
-        properties: {}
+        properties: { hello: 'world' }
     }];
+
     var wrap = new Wrapper(features);
     var feature = wrap.feature(0);
+
     t.ok(feature, 'gets a feature');
     t.deepEqual(feature.bbox(), [0, 0, 10, 10], 'bbox');
+    t.equal(feature._type, 2, '_type');
+    t.deepEqual(feature.properties, {hello:'world'}, 'properties');
+
     t.end();
+
 });
