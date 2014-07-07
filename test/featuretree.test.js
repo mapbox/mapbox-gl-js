93c6185572981667881e7023c109aed5db2b3c2b
Tom MacWright <tom@macwright.org>
2014-07-07 17:42:06 -0400

    Add more featuretree. Refs #511

--- test/featuretree.test.js
+++ test/featuretree.test.js
@@ -28,3 +28,29 @@ test('featuretree', function(t) {
         t.end();
     });
 });
+
+test('featuretree with args', function(t) {
+    var tile = new vt.VectorTile(new Protobuf(new Uint8Array(fs.readFileSync(__dirname + '/fixtures/mbsv5-6-18-23.vector.pbf'))));
+    function getType(feature) {
+        return vt.VectorTileFeature.types[feature.type];
+    }
+    function getGeometry(feature) {
+        return feature.loadGeometry();
+    }
+    var ft = new FeatureTree(getGeometry, getType);
+    var feature = tile.layers.road.feature(0);
+    t.ok(feature);
+    t.ok(ft, 'can be created');
+    ft.insert(feature.bbox(), 'road', feature);
+    ft.query({
+        params: {
+            radius: 5
+        },
+        x: 0,
+        y: 0,
+    }, function(err, features) {
+        t.deepEqual(features, []);
+        t.equal(err, null);
+        t.end();
+    });
+});
