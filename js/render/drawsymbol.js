ca0e4e2bbefe7bb29992d2498a1b15b4a9371666
Vladimir Agafonkin <agafonkin@gmail.com>
2014-07-07 15:02:27 -0700

    fix type for symbol-rotation-alignment

--- js/render/drawsymbol.js
+++ js/render/drawsymbol.js
@@ -86,7 +86,7 @@ function drawSymbol(gl, painter, bucket, layerStyle, posMatrix, params, imageSpr
     // adjust min/max zooms for variable font sies
     var zoomAdjust = Math.log(fontSize / info[prefix + '-max-size']) / Math.LN2 || 0;
 
-    var flip = info['symbol-rotation-aligment'] !== 'viewport' && info[prefix + '-keep-upright'];
+    var flip = info['symbol-rotation-alignment'] !== 'viewport' && info[prefix + '-keep-upright'];
     gl.uniform1f(shader.u_flip, flip ? 1 : 0);
     gl.uniform1f(shader.u_angle, (angle + 256) % 256);
     gl.uniform1f(shader.u_zoom, (painter.transform.zoom - zoomAdjust) * 10); // current zoom level
