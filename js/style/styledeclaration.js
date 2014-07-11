7b8707b8165dc9a58a17b43548b9255be6fe536b
Ansis Brammanis <brammanis@gmail.com>
2014-07-11 16:03:53 -0700

    consolidate functions
    exponential, linear, and min function types are removed
    stops function has a configurable exponential base

--- js/style/styledeclaration.js
+++ js/style/styledeclaration.js
@@ -88,97 +88,52 @@ function parseColor(value) {
     return color;
 }
 
-
-var functionParsers = StyleDeclaration.functionParsers = {
-    linear: linear,
-    exponential: exponential,
-    min: min,
-    stops: stopsFn
-};
-
 function parseFunction(fn, color) {
     if (fn.fn) {
-        if (!functionParsers[fn.fn]) {
+        if (fn.fn !== 'stops') {
             throw new Error('The function "' + fn.fn + '" does not exist');
         }
-        return functionParsers[fn.fn](fn, color);
+        return stopsFn(fn, color);
     } else {
         return fn;
     }
 }
 
-/*
- * Function parsers
- */
-
-function linear(params) {
-    var z_base = +params.z || 0,
-        val = +params.val || 0,
-        slope = +params.slope || 0,
-        min = +params.min || 0,
-        max = +params.max || Infinity;
-    return function(z) {
-        return Math.min(Math.max(min, val + (z - z_base) * slope), max);
-    };
-}
-
-function exponential(params) {
-    var z_base = +params.z || 0,
-        val = +params.val || 0,
-        slope = +params.slope || 0,
-        min = +params.min || 0,
-        max = +params.max || Infinity,
-        base = +params.base || 1.75;
-    return function(z) {
-        return Math.min(Math.max(min, val + Math.pow(base, (z - z_base)) * slope), max);
-    };
-}
-
-function min(params) {
-    var min_z = +params.min || 0;
-    return function(z) {
-        return z >= min_z;
-    };
-}
-
 function stopsFn(params, color) {
     var stops = params.stops;
-    return function(z) {
-        z += 1;
+    var base = params.base || (color ? 1.05 : 1.75);
 
+    return function(z) {
+
+        // find the two stops which the current z is between
         var low = null;
         var high = null;
-
         for (var i = 0; i < stops.length; i++) {
             var stop = stops[i];
-            if (stop[0] <= z && (!low || low[0] < stop[0])) low = stop;
-            if (stop[0] >= z && (!high || high[0] > stop[0])) high = stop;
+            if (stop[0] <= z) low = stop;
+            if (stop[0] > z) {
+                high = stop;
+                break;
+            }
         }
 
         if (low && high) {
-            if (high[0] == low[0] || high[1] == low[1]) {
-                if (color) return prepareColor(low[2]);
-                return low[1];
-            }
-            var factor = (z - low[0]) / (high[0] - low[0]);
+            var zoomDiff = high[0] - low[0];
+            var t = (Math.pow(base, z - low[0]) - 1) / (Math.pow(base, zoomDiff) - 1);
+            if (color) return prepareColor(interpColor(low[2], high[2], t));
+            else return util.interp(low[1], high[1], t);
 
-            // If color, interpolate between values
-            if (color) return prepareColor(interpColor(low[2], high[2], factor));
-            // Linear interpolation if base is 0
-            if (low[1] === 0) return factor * high[1];
-            // Exponential interpolation between the values
-            return low[1] * Math.pow(high[1] / low[1], factor);
-        } else if (high || low) {
-            // use the closest stop for z beyond the stops range
-            if (color) return low ? prepareColor(low[2]) : prepareColor(high[2]);
-            return low ? low[1] : high[1];
+        } else if (low) {
+            if (color) return prepareColor(low[2]);
+            else return low[1];
 
-            // Exponential extrapolation of the low or high value
-            //var edge = high || low;
-            //return Math.pow(2, z) * (edge.val / Math.pow(2, edge.z));
+        } else if (high) {
+            if (color) return prepareColor(high[2]);
+            else return high[1];
+
         } else {
-            // No stop defined.
-            return 1;
+            if (color) return [0, 0, 0, 1];
+            else return 1;
         }
     };
 }
