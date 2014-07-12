3cefb7f62f24c7a529d94b6c9fbe55ad95163cba
John Firebaugh <john.firebaugh@gmail.com>
2014-07-11 18:14:45 -0700

    Organize test files

---
+++ test/js/geometry/linebucket.test.js
@@ -0,0 +1,57 @@
+'use strict';
+var test = require('tape').test;
+
+var fs = require('fs');
+var Protobuf = require('pbf');
+var VectorTile = require('vector-tile').VectorTile;
+var LineBucket = require('../../../js/geometry/linebucket.js');
+var LineVertexBuffer = require('../../../js/geometry/linevertexbuffer.js');
+var LineElementBuffer = require('../../../js/geometry/lineelementbuffer.js');
+var Point = require('point-geometry');
+
+// Load a line feature from fixture tile.
+var vt = new VectorTile(new Protobuf(new Uint8Array(fs.readFileSync(__dirname + '/../../fixtures/mbsv5-6-18-23.vector.pbf'))));
+var feature = vt.layers.road.feature(0);
+
+test('LineBucket', function(t) {
+    var info = {};
+    var buffers = {
+        lineVertex: new LineVertexBuffer(),
+        lineElement: new LineElementBuffer()
+    };
+    var bucket = new LineBucket(info, buffers);
+    t.ok(bucket);
+
+    var pointWithScale = new Point(0, 0);
+    pointWithScale.scale = 10;
+
+    // should throw in the future?
+    t.equal(bucket.addLine([
+        new Point(0, 0)
+    ]), undefined);
+
+    // should also throw in the future?
+    // this is a closed single-segment line
+    t.equal(bucket.addLine([
+        new Point(0, 0),
+        new Point(0, 0)
+    ]), undefined);
+
+    t.equal(bucket.addLine([
+        new Point(0, 0),
+        new Point(10, 10),
+        new Point(10, 20)
+    ]), undefined);
+
+    t.equal(bucket.addLine([
+        new Point(0, 0),
+        new Point(10, 10),
+        new Point(10, 20),
+        new Point(0, 0)
+    ]), undefined);
+
+    t.equal(bucket.addFeature(feature.loadGeometry()), undefined);
+
+    t.end();
+});
+
---
+++ test/js/geometry/symbolbucket.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var test = require('tape').test;
+
+var fs = require('fs');
+var Protobuf = require('pbf');
+var VectorTile = require('vector-tile').VectorTile;
+var SymbolBucket = require('../../../js/geometry/symbolbucket.js');
+var GlyphVertexBuffer = require('../../../js/geometry/glyphvertexbuffer.js');
+var IconVertexBuffer = require('../../../js/geometry/iconvertexbuffer.js');
+var Collision = require('../../../js/text/collision.js');
+var GlyphAtlas = require('../../../js/text/glyphatlas');
+var RenderProperties = require('../../../js/style/renderproperties.js');
+
+// Load a point feature from fixture tile.
+var vt = new VectorTile(new Protobuf(new Uint8Array(fs.readFileSync(__dirname + '/../../fixtures/mbsv5-6-18-23.vector.pbf'))));
+var feature = vt.layers.place_label.feature(10);
+var glyphs = JSON.parse(fs.readFileSync(__dirname + '/../../fixtures/fontstack-glyphs.json'));
+
+test('SymbolBucket', function(t) {
+    var info = new RenderProperties.symbol({ type: 'symbol', 'text-font': 'Test' });
+    var buffers = {
+        glyphVertex: new GlyphVertexBuffer(),
+        iconVertex: new IconVertexBuffer()
+    };
+    var collision = new Collision(6, 4096, 512);
+    var bucket = new SymbolBucket(info, buffers, collision);
+    t.ok(bucket);
+    bucket.data = { text_features: ['abcde'] };
+
+    var atlas = new GlyphAtlas(1024,1024);
+    var rects = {};
+    for (var id in glyphs) {
+        glyphs[id].bitmap = true;
+        rects[id] = atlas.addGlyph(id, 'Test', glyphs[id], 3);
+    }
+    bucket.stacks = { 'Test': {
+        glyphs: glyphs,
+        rects: rects
+    }};
+
+    var a = JSON.stringify(collision);
+    bucket.features = [feature];
+    t.equal(bucket.addFeatures(), undefined);
+    var b = JSON.stringify(collision);
+
+    t.notEqual(a, b, 'places feature');
+
+    a = JSON.stringify(collision);
+    // try adding again
+    t.equal(bucket.addFeatures(), undefined);
+    b = JSON.stringify(collision);
+
+    t.equal(a, b, 'detects collision and does not place feature');
+
+    t.end();
+});
+
