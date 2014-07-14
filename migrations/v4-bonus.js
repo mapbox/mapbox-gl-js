77bbb0b7d7a84f7a4959f79bf2376008cf50cd16
Ansis Brammanis <brammanis@gmail.com>
2014-07-14 12:21:36 -0700

    add function migration

--- migrations/v4-bonus.js
+++ migrations/v4-bonus.js
@@ -24,11 +24,8 @@ function convertLayer(layer) {
             var newfn;
             if (oldfn.fn === 'stops') {
                 newfn = { stops: oldfn.stops };
-            // @TODO convert linear/exponential functions into stops functions.
-            } else if (oldfn.fn === 'linear') {
-                newfn = { stops: [] };
-            } else if (oldfn.fn === 'exponential') {
-                newfn = { stops: [] };
+            } else if (oldfn.fn === 'linear' || oldfn.fn === 'exponential') {
+                newfn = migrateFn(oldfn);
             }
             // Decrement zoom levels by 1.
             newfn.stops = newfn.stops.map(deczoom);
@@ -36,7 +33,75 @@ function convertLayer(layer) {
         }
     }
 
+    if (layer.layers) layer.layers.forEach(convertLayer);
+
     function deczoom(pair) {
         return [Math.max(0, pair[0] - 1), pair[1]];
     }
+}
+
+function migrateFn(params) {
+    var newparams = {};
+    var minZoom = 0;
+    var maxZoom = 20;
+    var fn;
+    if (params.fn === 'exponential') {
+        fn = exponential(params);
+        if (params.base) newparams.base = params.base;
+        if (params.min !== undefined) minZoom = reverseExponential(params, Math.max(params.min, params.val));
+        if (params.max !== undefined) maxZoom = reverseExponential(params, params.max);
+        newparams.stops = [
+            [minZoom, fn(minZoom)],
+            [maxZoom, fn(maxZoom)]
+        ];
+    } else if (params.fn === 'linear') {
+        fn = linear(params);
+        newparams.base = 1.01;
+        if (params.min !== undefined) minZoom = reverseLinear(params, params.min);
+        if (params.max !== undefined) maxZoom = reverseLinear(params, params.max);
+        newparams.stops = [
+            [minZoom, fn(minZoom)],
+            [maxZoom, fn(maxZoom)]
+        ];
+    }
+
+    return newparams;
+}
+
+function reverseExponential(params, value) {
+    var z_base = +params.z || 0,
+        val = +params.val || 0,
+        slope = +params.slope || 0,
+        base = +params.base || 1.75;
+    return Math.log(((value - val) || 0.01) / slope) / Math.log(base) + z_base;
+}
+
+function reverseLinear(params, value) {
+    var z_base = +params.z || 0,
+        val = +params.val || 0,
+        slope = +params.slope || 0;
+    return (value - val) / slope + z_base;
+}
+
+function linear(params) {
+    var z_base = +params.z || 0,
+        val = +params.val || 0,
+        slope = +params.slope || 0,
+        min = +params.min || 0,
+        max = +params.max || Infinity;
+    return function(z) {
+        return Math.min(Math.max(min, val + (z - z_base) * slope), max);
+    };
+}
+
+function exponential(params) {
+    var z_base = +params.z || 0,
+        val = +params.val || 0,
+        slope = +params.slope || 0,
+        min = +params.min || 0,
+        max = +params.max || Infinity,
+        base = +params.base || 1.75;
+    return function(z) {
+        return Math.min(Math.max(min, val + Math.pow(base, (z - z_base)) * slope), max);
+    };
 }
