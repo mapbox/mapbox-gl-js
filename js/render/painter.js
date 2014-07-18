cf0a1589abe60773d814d8f94da24a8bef0f7c6e
Ansis Brammanis <brammanis@gmail.com>
2014-07-17 21:03:59 -0700

    fix matrix translation

--- js/render/painter.js
+++ js/render/painter.js
@@ -362,7 +362,7 @@ GLPainter.prototype.drawStencilBuffer = function() {
 
 GLPainter.prototype.translateMatrix = function(matrix, translate, z) {
 
-    if (!translate) return matrix;
+    if (!translate[0] && !translate[1]) return matrix;
 
     var translatedMatrix;
     var tilePixelRatio = this.transform.scale / (1 << z) / 8;
@@ -372,7 +372,7 @@ GLPainter.prototype.translateMatrix = function(matrix, translate, z) {
         0
     ];
     translatedMatrix = new Float32Array(16);
-    mat4.translate(translatedMatrix, this.tile.posMatrix, translation);
+    mat4.translate(translatedMatrix, matrix, translation);
 
     return translatedMatrix;
 };
