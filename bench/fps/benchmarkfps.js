87f9e1f4a8e0056afe6ec150c76ec5b62efd8217
Ansis Brammanis <brammanis@gmail.com>
2014-07-24 15:55:41 -0700

    display fps benchmark results

--- bench/fps/benchmarkfps.js
+++ bench/fps/benchmarkfps.js
@@ -1,4 +1,44 @@
-function FrameBench(urls, duration, setup, teardown, done) {
+function Benchmark(urls, duration, setup, teardown) {
+    RunBenchmark(urls, duration, setup, teardown, done);
+
+    function done(frames) {
+        document.body.innerHTML = '';
+        var settings = document.createElement('div');
+        settings.innerHTML = 'duration: ' + duration;
+        document.body.appendChild(settings);
+
+        for (var i = 0; i < frames.length; i++) {
+            var fps = Benchmark.util.mean(frames[i]);
+            fps = Math.round(fps * 10) / 10;
+            var div = document.createElement('div');
+            div.innerHTML = fps + ' fps ' + urls[i];
+            document.body.appendChild(div);
+        }
+    }
+}
+
+Benchmark.util = {};
+
+Benchmark.util.onMapLoaded = function(map, callback) {
+  map.on('change:style', function() {
+      var check = window.setInterval(function() {
+          for (var s in map.sources) {
+              var source = map.sources[s];
+              for (var t in source.tiles) {
+                  if (!source.tiles[t].loaded) return;
+              }
+          }
+          window.clearInterval(check);
+          callback();
+      }, 100);
+  });
+};
+
+Benchmark.util.mean = function(frames) {
+    return (frames.length - 1) * 1000 / (frames[frames.length - 1] - frames[0]);
+};
+
+function RunBenchmark(urls, duration, setup, teardown, done) {
 
     var queryString = window.location.href.split('?')[1];
     var versionNum = parseInt(queryString && queryString.split('/')[0], 10);
---
+++ bench/fps/site.js
@@ -0,0 +1,33 @@
+var urls = [
+    'http://localhost:8001/dist/mapbox-gl-dev.js',
+    'http://localhost:8001/dist/mapbox-gl-dev.js',
+];
+
+var duration = 3000;
+
+Benchmark(urls, duration, setup, teardown);
+
+function setup(state, callback) {
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWlicmFtIiwiYSI6IkZfak1UWW8ifQ.czocTs_bwAYlC_JxXijA2A';
+    document.getElementById('map').innerHTML = '';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        zoom: 15,
+        center: [38.912753, -77.032194],
+        style: 'http://localhost:8001/debug/style.json',
+        hash: true
+    });
+
+    state.map = map;
+
+    Benchmark.util.onMapLoaded(map, function() {
+        map.repaint = true;
+        callback();
+    });
+}
+
+function teardown(state, callback) {
+    state.map.repaint = false;
+    callback();
+}
