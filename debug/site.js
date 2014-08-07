d895fbbdb692c3a07842645216f52f65ec01205a
Vladimir Agafonkin <agafonkin@gmail.com>
2014-08-07 11:32:23 +0300

    cleanup debug page code

--- debug/site.js
+++ debug/site.js
@@ -1,14 +1,5 @@
 
-var match = location.search.match(/access_token=([^&\/]*)/);
-var accessToken = match && match[1];
-
-if (accessToken) {
-    localStorage.accessToken = accessToken;
-} else {
-    accessToken = localStorage.accessToken;
-}
-
-mapboxgl.accessToken = accessToken;
+mapboxgl.accessToken = getAccessToken();
 
 var map = new mapboxgl.Map({
     container: 'map',
@@ -20,7 +11,6 @@ var map = new mapboxgl.Map({
 
 new mapboxgl.Navigation(map);
 
-// add geojson overlay
 var geojson = new mapboxgl.GeoJSONSource({
     data: {
         type: 'Feature',
@@ -31,6 +21,7 @@ var geojson = new mapboxgl.GeoJSONSource({
 
 map.addSource('geojson', geojson);
 
+// keyboard shortcut for comparing rendering with Mapbox GL native
 document.onkeypress = function(e) {
     if (e.charCode === 111 && !e.shiftKey && !e.metaKey && !e.altKey) {
         var center = map.getCenter();
@@ -38,3 +29,16 @@ document.onkeypress = function(e) {
         return false;
     }
 };
+
+function getAccessToken() {
+    var match = location.search.match(/access_token=([^&\/]*)/);
+    var accessToken = match && match[1];
+
+    if (accessToken) {
+        localStorage.accessToken = accessToken;
+    } else {
+        accessToken = localStorage.accessToken;
+    }
+
+    return accessToken;
+}
