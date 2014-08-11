fec2b4937494b473f3c6841afc532c17444a45b8
Vladimir Agafonkin <agafonkin@gmail.com>
2014-08-11 17:54:56 +0300

    fix merging adjacent same-name roads for all cases

--- js/data/symbolbucket.js
+++ js/data/symbolbucket.js
@@ -92,21 +92,38 @@ SymbolBucket.prototype.addFeatures = function() {
             var last = geom[0].length - 1,
                 startKey = text + ':' + geom[0][0].x + ':' + geom[0][0].y,
                 endKey = text + ':' + geom[0][last].x + ':' + geom[0][last].y,
-                i;
+                i, j;
 
-            if (startKey in endIndex) {
-                // found a same-text line that ends where the current starts;
+            if ((startKey in endIndex) && (endKey in startIndex) && (endIndex[startKey] !== startIndex[endKey])) {
+                // found lines with the same text adjacent to both ends of the current line
                 i = endIndex[startKey];
-                geom[0].shift();
+                j = startIndex[endKey];
+
+                mergedGeometries[i][0].pop();
+                mergedGeometries[j][0].shift();
+                mergedGeometries[i][0] = mergedGeometries[i][0].concat(geom[0]).concat(mergedGeometries[j][0]);
+
+                delete endIndex[startKey];
+                delete startIndex[endKey];
+
+                var last2 = mergedGeometries[j][0][mergedGeometries[j][0].length - 1];
+                endIndex[text + ':' + last2.x + ':' + last2.y] = i;
+
+                mergedGeometries[j] = null;
+
+            } else if (startKey in endIndex) {
+                // found line with the same text adjacent to the start of the current line
+                i = endIndex[startKey];
+                mergedGeometries[i][0].pop();
                 mergedGeometries[i][0] = mergedGeometries[i][0].concat(geom[0]);
 
                 delete endIndex[startKey];
                 endIndex[endKey] = i;
 
             } else if (endKey in startIndex) {
-                // found a same-text line that starts where the current ends
+                // found line with the same text adjacent to the end of the current line
                 i = startIndex[endKey];
-                geom[0].pop();
+                mergedGeometries[i][0].shift();
                 mergedGeometries[i][0] = geom[0].concat(mergedGeometries[i][0]);
 
                 delete startIndex[endKey];
@@ -129,6 +146,7 @@ SymbolBucket.prototype.addFeatures = function() {
     }
 
     for (k = 0; k < features.length; k++) {
+        if (!geometries[k]) continue;
 
         var shaping = false;
         if (textFeatures[k]) {
