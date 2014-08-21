fa1fcfbeb1e5b6d427f9e989213c3296ebe9d3af
John Firebaugh <john.firebaugh@gmail.com>
2014-08-21 16:15:43 -0700

    Start v5

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports = {};
 module.exports.v2 = require('./reference/v2.json');
 module.exports.v3 = require('./reference/v3.json');
 module.exports.v4 = require('./reference/v4.json');
+module.exports.v5 = require('./reference/v5.json');
