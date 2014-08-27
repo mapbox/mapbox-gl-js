443cdbcca24c931c3990eda51545a6555b3470ee
Vladimir Agafonkin <agafonkin@gmail.com>
2014-08-27 13:08:47 +0300

    do not render tiles if no style loaded, close #729

--- js/source/source.js
+++ js/source/source.js
@@ -242,7 +242,8 @@ Source.prototype = util.inherit(Evented, {
 
     // Removes tiles that are outside the viewport and adds new tiles that are inside the viewport.
     _updateTiles: function() {
-        if (!this.map || !this.map.loadNewTiles || !this.loadNewTiles || !this.map.style.sources[this.id]) return;
+        if (!this.map || !this.map.loadNewTiles || !this.loadNewTiles ||
+            !this.map.style || !this.map.style.sources || !this.map.style.sources[this.id]) return;
 
         var zoom = Math.floor(this._getZoom());
         var required = this._getCoveringTiles().sort(this._centerOut.bind(this));
