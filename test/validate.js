1004c9561a92b1f3edc0c18b84b684b1ad7b0c69
John Firebaugh <john.firebaugh@gmail.com>
2014-10-25 23:51:44 -0700

    Test that all mapbox-gl-styles validate

--- test/validate.js
+++ test/validate.js
@@ -4,25 +4,27 @@
 var t = require('tape'),
     glob = require('glob'),
     fs = require('fs'),
+    path = require('path'),
     validate = require('../');
 
 var UPDATE = !!process.env.UPDATE;
 
-t('style validity', function(t) {
-    glob.sync(__dirname + '/fixture/*.input.json').forEach(function(file) {
-        t.test(file, function(t) {
-            var outputfile = file.replace('.input', '.output');
-            var style = fs.readFileSync(file);
-            var result = validate(style);
-
-            // Put it through the wringer to strip keys for undefined line errors.
-            result = JSON.parse(JSON.stringify(result));
-
-            if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
-            var expect = JSON.parse(fs.readFileSync(outputfile));
-            t.deepEqual(result, expect);
-            t.end();
-        });
+glob.sync(__dirname + '/fixture/*.input.json').forEach(function(file) {
+    t(path.basename(file), function(t) {
+        var outputfile = file.replace('.input', '.output');
+        var style = fs.readFileSync(file);
+        var result = validate(style);
+        if (UPDATE) fs.writeFileSync(outputfile, JSON.stringify(result, null, 2));
+        var expect = JSON.parse(fs.readFileSync(outputfile));
+        t.deepEqual(result, expect);
+        t.end();
     });
-    t.end();
 });
+
+var styles = require('mapbox-gl-styles');
+for (var k in styles) {
+    t(k, function(t) {
+        t.deepEqual(validate(JSON.stringify(styles[k])), []);
+        t.end();
+    });
+}
