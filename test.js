6e3ff81e41fd9a90424e295ae5394d663588401b
John Firebaugh <john.firebaugh@gmail.com>
2014-11-01 18:36:33 -0400

    More tests

--- test.js
+++ test.js
@@ -10,211 +10,267 @@ test('degenerate', function(t) {
     t.end();
 });
 
-test('==', function(t) {
-    var f1 = filter(['==', 'foo', 'bar']);
-    t.equal(f1({properties: {foo: 'bar'}}), true);
-    t.equal(f1({properties: {foo: 'baz'}}), false);
-
-    var f2 = filter(['==', 'foo', 0]);
-    t.equal(f2({properties: {foo: 0}}), true);
-    t.equal(f2({properties: {foo: 1}}), false);
-    t.equal(f2({properties: {foo: '0'}}), false);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['==', '$type', 'LineString']);
-    t.equal(f3({type: 1}), false);
-    t.equal(f3({type: 2}), true);
-
-    var f4 = filter(['==', 'foo', 0]);
-    t.equal(f4({properties: {}}), false);
-
+test('==, string', function(t) {
+    var f = filter(['==', 'foo', 'bar']);
+    t.equal(f({properties: {foo: 'bar'}}), true);
+    t.equal(f({properties: {foo: 'baz'}}), false);
     t.end();
 });
 
-test('!=', function(t) {
-    var f1 = filter(['!=', 'foo', 'bar']);
-    t.equal(f1({properties: {foo: 'bar'}}), false);
-    t.equal(f1({properties: {foo: 'baz'}}), true);
-
-    var f2 = filter(['!=', 'foo', 0]);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: 1}}), true);
-    t.equal(f2({properties: {foo: '0'}}), true);
-    t.equal(f2({properties: {foo: null}}), true);
-    t.equal(f2({properties: {foo: undefined}}), true);
-
-    var f3 = filter(['!=', '$type', 'LineString']);
-    t.equal(f3({type: 1}), true);
-    t.equal(f3({type: 2}), false);
-
-    var f4 = filter(['!=', 'foo', 0]);
-    t.equal(f4({properties: {}}), true);
-
+test('==, number', function(t) {
+    var f = filter(['==', 'foo', 0]);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
     t.end();
 });
 
-test('<', function(t) {
-    var f1 = filter(['<', 'foo', 0]);
-    t.equal(f1({properties: {foo: 1}}), false);
-    t.equal(f1({properties: {foo: 0}}), false);
-    t.equal(f1({properties: {foo: -1}}), true);
-    t.equal(f1({properties: {foo: '1'}}), false);
-    t.equal(f1({properties: {foo: '0'}}), false);
-    t.equal(f1({properties: {foo: '-1'}}), false);
-
-    var f2 = filter(['<', 'foo', '0']);
-    t.equal(f2({properties: {foo: -1}}), false);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: 1}}), false);
-    t.equal(f2({properties: {foo: '1'}}), false);
-    t.equal(f2({properties: {foo: '0'}}), false);
-    t.equal(f2({properties: {foo: '-1'}}), true);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['<', 'foo', 0]);
-    t.equal(f3({properties: {}}), false);
-
+test('==, $type', function(t) {
+    var f = filter(['==', '$type', 'LineString']);
+    t.equal(f({type: 1}), false);
+    t.equal(f({type: 2}), true);
     t.end();
 });
 
-test('<=', function(t) {
-    var f1 = filter(['<=', 'foo', 0]);
-    t.equal(f1({properties: {foo: 1}}), false);
-    t.equal(f1({properties: {foo: 0}}), true);
-    t.equal(f1({properties: {foo: -1}}), true);
-    t.equal(f1({properties: {foo: '1'}}), false);
-    t.equal(f1({properties: {foo: '0'}}), false);
-    t.equal(f1({properties: {foo: '-1'}}), false);
-
-    var f2 = filter(['<=', 'foo', '0']);
-    t.equal(f2({properties: {foo: -1}}), false);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: 1}}), false);
-    t.equal(f2({properties: {foo: '1'}}), false);
-    t.equal(f2({properties: {foo: '0'}}), true);
-    t.equal(f2({properties: {foo: '-1'}}), true);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['<=', 'foo', 0]);
-    t.equal(f3({properties: {}}), false);
-
+test('!=, string', function(t) {
+    var f = filter(['!=', 'foo', 'bar']);
+    t.equal(f({properties: {foo: 'bar'}}), false);
+    t.equal(f({properties: {foo: 'baz'}}), true);
     t.end();
 });
 
-test('>', function(t) {
-    var f1 = filter(['>', 'foo', 0]);
-    t.equal(f1({properties: {foo: 1}}), true);
-    t.equal(f1({properties: {foo: 0}}), false);
-    t.equal(f1({properties: {foo: -1}}), false);
-    t.equal(f1({properties: {foo: '1'}}), false);
-    t.equal(f1({properties: {foo: '0'}}), false);
-    t.equal(f1({properties: {foo: '-1'}}), false);
-
-    var f2 = filter(['>', 'foo', '0']);
-    t.equal(f2({properties: {foo: -1}}), false);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: 1}}), false);
-    t.equal(f2({properties: {foo: '1'}}), true);
-    t.equal(f2({properties: {foo: '0'}}), false);
-    t.equal(f2({properties: {foo: '-1'}}), false);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['>', 'foo', 0]);
-    t.equal(f3({properties: {}}), false);
-
+test('!=, number', function(t) {
+    var f = filter(['!=', 'foo', 0]);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), true);
+    t.equal(f({properties: {foo: '0'}}), true);
+    t.equal(f({properties: {foo: true}}), true);
+    t.equal(f({properties: {foo: false}}), true);
+    t.equal(f({properties: {foo: null}}), true);
+    t.equal(f({properties: {foo: undefined}}), true);
+    t.equal(f({properties: {}}), true);
     t.end();
 });
 
-test('>=', function(t) {
-    var f1 = filter(['>=', 'foo', 0]);
-    t.equal(f1({properties: {foo: 1}}), true);
-    t.equal(f1({properties: {foo: 0}}), true);
-    t.equal(f1({properties: {foo: -1}}), false);
-    t.equal(f1({properties: {foo: '1'}}), false);
-    t.equal(f1({properties: {foo: '0'}}), false);
-    t.equal(f1({properties: {foo: '-1'}}), false);
-
-    var f2 = filter(['>=', 'foo', '0']);
-    t.equal(f2({properties: {foo: -1}}), false);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: 1}}), false);
-    t.equal(f2({properties: {foo: '1'}}), true);
-    t.equal(f2({properties: {foo: '0'}}), true);
-    t.equal(f2({properties: {foo: '-1'}}), false);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['>=', 'foo', 0]);
-    t.equal(f3({properties: {}}), false);
-
+test('!=, $type', function(t) {
+    var f = filter(['!=', '$type', 'LineString']);
+    t.equal(f({type: 1}), true);
+    t.equal(f({type: 2}), false);
     t.end();
 });
 
-test('in', function(t) {
-    var f1 = filter(['in', 'foo']);
-    t.equal(f1({properties: {foo: 1}}), false);
-
-    var f2 = filter(['in', 'foo', '0']);
-    t.equal(f2({properties: {foo: 0}}), false);
-    t.equal(f2({properties: {foo: '0'}}), true);
-    t.equal(f2({properties: {foo: null}}), false);
-    t.equal(f2({properties: {foo: undefined}}), false);
-
-    var f3 = filter(['in', 'foo', 0]);
-    t.equal(f3({properties: {foo: 0}}), true);
-    t.equal(f3({properties: {foo: '0'}}), false);
-    t.equal(f3({properties: {foo: null}}), false);
-    t.equal(f3({properties: {foo: undefined}}), false);
-
-    var f4 = filter(['in', 'foo', 0, 1]);
-    t.equal(f4({properties: {foo: 0}}), true);
-    t.equal(f4({properties: {foo: 1}}), true);
-    t.equal(f4({properties: {foo: 3}}), false);
-
-    var f5 = filter(['in', '$type', 'LineString', 'Polygon']);
-    t.equal(f5({type: 1}), false);
-    t.equal(f5({type: 2}), true);
-    t.equal(f5({type: 3}), true);
-
-    var f6 = filter(['in', 'foo', 0]);
-    t.equal(f6({properties: {}}), false);
-
+test('<, number', function(t) {
+    var f = filter(['<', 'foo', 0]);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: -1}}), true);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
     t.end();
 });
 
-test('!in', function(t) {
-    var f1 = filter(['!in', 'foo']);
-    t.equal(f1({properties: {foo: 1}}), true);
+test('<, string', function(t) {
+    var f = filter(['<', 'foo', '0']);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), true);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.end();
+});
 
-    var f2 = filter(['!in', 'foo', '0']);
-    t.equal(f2({properties: {foo: 0}}), true);
-    t.equal(f2({properties: {foo: '0'}}), false);
-    t.equal(f2({properties: {foo: null}}), true);
-    t.equal(f2({properties: {foo: undefined}}), true);
+test('<=, number', function(t) {
+    var f = filter(['<=', 'foo', 0]);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: -1}}), true);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
+    t.end();
+});
 
-    var f3 = filter(['!in', 'foo', 0]);
-    t.equal(f3({properties: {foo: 0}}), false);
-    t.equal(f3({properties: {foo: '0'}}), true);
-    t.equal(f3({properties: {foo: null}}), true);
-    t.equal(f3({properties: {foo: undefined}}), true);
+test('<=, string', function(t) {
+    var f = filter(['<=', 'foo', '0']);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), true);
+    t.equal(f({properties: {foo: '-1'}}), true);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.end();
+});
 
-    var f4 = filter(['!in', 'foo', 0, 1]);
-    t.equal(f4({properties: {foo: 0}}), false);
-    t.equal(f4({properties: {foo: 1}}), false);
-    t.equal(f4({properties: {foo: 3}}), true);
+test('>, number', function(t) {
+    var f = filter(['>', 'foo', 0]);
+    t.equal(f({properties: {foo: 1}}), true);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
+    t.end();
+});
 
-    var f5 = filter(['!in', '$type', 'LineString', 'Polygon']);
-    t.equal(f5({type: 1}), true);
-    t.equal(f5({type: 2}), false);
-    t.equal(f5({type: 3}), false);
+test('>, string', function(t) {
+    var f = filter(['>', 'foo', '0']);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: '1'}}), true);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.end();
+});
 
-    var f6 = filter(['!in', 'foo', 0]);
-    t.equal(f6({properties: {}}), true);
+test('>=, number', function(t) {
+    var f = filter(['>=', 'foo', 0]);
+    t.equal(f({properties: {foo: 1}}), true);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: '1'}}), false);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
+    t.end();
+});
 
+test('>=, string', function(t) {
+    var f = filter(['>=', 'foo', '0']);
+    t.equal(f({properties: {foo: -1}}), false);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: '1'}}), true);
+    t.equal(f({properties: {foo: '0'}}), true);
+    t.equal(f({properties: {foo: '-1'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.end();
+});
+
+test('in, degenerate', function(t) {
+    var f = filter(['in', 'foo']);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.end();
+});
+
+test('in, string', function(t) {
+    var f = filter(['in', 'foo', '0']);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: '0'}}), true);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.equal(f({properties: {}}), false);
+    t.end();
+});
+
+test('in, number', function(t) {
+    var f = filter(['in', 'foo', 0]);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: true}}), false);
+    t.equal(f({properties: {foo: false}}), false);
+    t.equal(f({properties: {foo: null}}), false);
+    t.equal(f({properties: {foo: undefined}}), false);
+    t.end();
+});
+
+test('in, multiple', function(t) {
+    var f = filter(['in', 'foo', 0, 1]);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: 1}}), true);
+    t.equal(f({properties: {foo: 3}}), false);
+    t.end();
+});
+
+test('in, $type', function(t) {
+    var f = filter(['in', '$type', 'LineString', 'Polygon']);
+    t.equal(f({type: 1}), false);
+    t.equal(f({type: 2}), true);
+    t.equal(f({type: 3}), true);
+    t.end();
+});
+
+test('!in, degenerate', function(t) {
+    var f = filter(['!in', 'foo']);
+    t.equal(f({properties: {foo: 1}}), true);
+    t.end();
+});
+
+test('!in, string', function(t) {
+    var f = filter(['!in', 'foo', '0']);
+    t.equal(f({properties: {foo: 0}}), true);
+    t.equal(f({properties: {foo: '0'}}), false);
+    t.equal(f({properties: {foo: null}}), true);
+    t.equal(f({properties: {foo: undefined}}), true);
+    t.equal(f({properties: {}}), true);
+    t.end();
+});
+
+test('!in, number', function(t) {
+    var f = filter(['!in', 'foo', 0]);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: '0'}}), true);
+    t.equal(f({properties: {foo: null}}), true);
+    t.equal(f({properties: {foo: undefined}}), true);
+    t.end();
+});
+
+test('!in, multiple', function(t) {
+    var f = filter(['!in', 'foo', 0, 1]);
+    t.equal(f({properties: {foo: 0}}), false);
+    t.equal(f({properties: {foo: 1}}), false);
+    t.equal(f({properties: {foo: 3}}), true);
+    t.end();
+});
+
+test('!in, $type', function(t) {
+    var f = filter(['!in', '$type', 'LineString', 'Polygon']);
+    t.equal(f({type: 1}), true);
+    t.equal(f({type: 2}), false);
+    t.equal(f({type: 3}), false);
     t.end();
 });
 
