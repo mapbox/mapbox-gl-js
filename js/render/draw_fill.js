fa14b42b460830357473361abbd39c293905d980
Vladimir Agafonkin <agafonkin@gmail.com>
2015-01-22 13:25:08 +0200

    cleanup after rebase

--- js/render/draw_fill.js
+++ js/render/draw_fill.js
@@ -48,7 +48,7 @@ function drawFill(painter, layer, posMatrix, tile) {
     } else {
         // Draw filling rectangle.
         shader = painter.fillShader;
-        gl.switchShader(shader, params.padded || translatedPosMatrix);
+        gl.switchShader(shader, translatedPosMatrix);
         gl.uniform4fv(shader.u_color, color);
     }
 
