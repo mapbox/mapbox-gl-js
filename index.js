a278dfffe768f491a87186165875fddaad111243
John Firebaugh <john.firebaugh@gmail.com>
2015-02-09 16:03:32 -0800

    Add `latest` alias

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-module.exports = {};
-module.exports.v2 = require('./reference/v2.json');
-module.exports.v3 = require('./reference/v3.json');
-module.exports.v4 = require('./reference/v4.json');
-module.exports.v5 = require('./reference/v5.json');
-module.exports.v6 = require('./reference/v6.json');
-module.exports.v7 = require('./reference/v7.json');
+exports.v2 = require('./reference/v2.json');
+exports.v3 = require('./reference/v3.json');
+exports.v4 = require('./reference/v4.json');
+exports.v5 = require('./reference/v5.json');
+exports.v6 = require('./reference/v6.json');
+exports.v7 = require('./reference/v7.json');
+exports.latest = exports.v7;
