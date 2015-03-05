f69f11cb3d6d9e61c6e09646dcf5d6f3f0b4c312
John Firebaugh <john.firebaugh@gmail.com>
2015-03-05 09:42:04 -0800

    Move validate/parsed.js into lib

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.format = require('./lib/format');
 exports.migrate = require('./lib/migrate');
 exports.validate = require('./lib/validate');
-exports.validate.parsed = require('./validate/parsed');
+exports.validate.parsed = require('./lib/validate/parsed');
--- test/validate.js
+++ test/validate.js
@@ -28,3 +28,17 @@ glob.sync(styles).forEach(function(k) {
         t.end();
     });
 });
+
+var fixtures = glob.sync(__dirname + '/fixture/*.input.json');
+var style = JSON.parse(fs.readFileSync(fixtures[0]));
+
+t('validate.parsed exists', function(t) {
+    t.equal(typeof validate.parsed, 'function');
+    t.end();
+});
+
+t('errors from validate.parsed do not contain line numbers', function(t) {
+    var result = validate.parsed(style);
+    t.equal(result[0].line, undefined);
+    t.end();
+});
