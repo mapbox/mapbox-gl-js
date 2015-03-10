c304d095e7275b54b9dd5283773f68a354078e0c
Ansis Brammanis <brammanis@gmail.com>
2015-03-10 11:12:12 -0700

    if no line label anchors fit, try one in the middle
    See the comment in the commit for more details.

--- js/symbol/get_anchors.js
+++ js/symbol/get_anchors.js
@@ -12,13 +12,21 @@ function getAnchors(line, spacing, maxAngle, shapedText, glyphSize, boxScale, ov
     // potential label passes text-max-angle check and has enough froom to fit
     // on the line.
 
-    var angleWindowSize = 3 / 5 * glyphSize * boxScale;
+    var angleWindowSize = shapedText ?
+        3 / 5 * glyphSize * boxScale :
+        0;
 
     // Offset the first anchor by half the label length (or half the spacing distance for icons).
     // Add a bit of extra offset to avoid collisions at T intersections.
     var labelLength = shapedText ? shapedText.right - shapedText.left : spacing;
     var extraOffset = glyphSize * 2;
     var offset = ((labelLength / 2 + extraOffset) * boxScale * overscaling) % spacing;
+
+    return resample(line, offset, spacing, angleWindowSize, maxAngle, labelLength * boxScale, false);
+}
+
+
+function resample(line, offset, spacing, angleWindowSize, maxAngle, labelLength, placeAtMiddle) {
 
     var distance = 0,
         markedDistance = offset ? offset - spacing : 0,
@@ -44,7 +52,7 @@ function getAnchors(line, spacing, maxAngle, shapedText, glyphSize, boxScale, ov
             if (x >= 0 && x < 4096 && y >= 0 && y < 4096) {
                 var anchor = new Anchor(x, y, angle, undefined, i);
 
-                if (!shapedText || checkMaxAngle(line, anchor, labelLength * boxScale, angleWindowSize, maxAngle)) {
+                if (!angleWindowSize || checkMaxAngle(line, anchor, labelLength, angleWindowSize, maxAngle)) {
                     anchors.push(anchor);
                 }
             }
@@ -53,6 +61,15 @@ function getAnchors(line, spacing, maxAngle, shapedText, glyphSize, boxScale, ov
         }
 
         distance += segmentDist;
+    }
+
+    if (!placeAtMiddle && !anchors.length) {
+        // The first attempt at finding anchors at which labels can be placed failed.
+        // Try again, but this time just try placing one anchor at the middle of the line.
+        // This has the most effect for short lines in overscaled tiles, since the
+        // initial offset used in overscaled tiles is calculated to align labels with positions in
+        // parent tiles instead of placing the label as close to the beginning as possible.
+        anchors = resample(line, distance / 2, spacing, angleWindowSize, maxAngle, labelLength, true);
     }
 
     return anchors;
