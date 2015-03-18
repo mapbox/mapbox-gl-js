7740a35312eccbfbc93ebd02e4c276b811fb1a62
John Firebaugh <john.firebaugh@gmail.com>
2015-03-17 21:16:49 -0700

    Improve LatLng docs

--- js/geo/lat_lng.js
+++ js/geo/lat_lng.js
@@ -7,11 +7,10 @@ var wrap = require('../util/util').wrap;
 /**
  * Create a latitude, longitude object from a given latitude and longitude pair in degrees.
  *
- * @class mapboxgl.LatLng
+ * @class LatLng
  * @classdesc A representation of a latitude and longitude point, in degrees.
- * @param {Number} latitude
- * @param {Number} longitude
- * @returns {mapboxgl.LatLng} `this`
+ * @param {number} latitude
+ * @param {number} longitude
  * @example
  * var latlng = new mapboxgl.LatLng(37.76, -122.44);
  *
@@ -24,18 +23,28 @@ function LatLng(lat, lng) {
     this.lng = +lng;
 }
 
+/**
+ * Return a new `LatLng` object whose longitude is wrapped to the range (-180, 180).
+ *
+ * @returns {LatLng}
+ */
 LatLng.prototype.wrap = function () {
     return new LatLng(this.lat, wrap(this.lng, -180, 180));
 };
 
-// constructs LatLng from an array if necessary
-
-LatLng.convert = function (a) {
-    if (a instanceof LatLng) {
-        return a;
+/**
+ * Convert an array to a `LatLng` object, or return an existing `LatLng` object
+ * unchanged.
+ *
+ * @param {Array<number>|LatLng} input `input` to convert
+ * @returns {LatLng}
+ */
+LatLng.convert = function (input) {
+    if (input instanceof LatLng) {
+        return input;
     }
-    if (Array.isArray(a)) {
-        return new LatLng(a[0], a[1]);
+    if (Array.isArray(input)) {
+        return new LatLng(input[0], input[1]);
     }
-    return a;
+    return input;
 };
