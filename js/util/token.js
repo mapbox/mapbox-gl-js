1fb7d707aa3ff3c6cf90797ec1bc9854bf8cd635
John Firebaugh <john.firebaugh@gmail.com>
2015-03-23 17:36:33 -0700

    Simplify resolveTokens

--- js/util/token.js
+++ js/util/token.js
@@ -2,15 +2,8 @@
 
 module.exports = resolveTokens;
 
-var tokenPattern = /{([^{}()\[\]<>$=:;.,^]+)}/;
-
-function resolveTokens(properties, expression) {
-    var match;
-    var value;
-    var text = expression;
-    while ((match = text.match(tokenPattern))) {
-        value = typeof properties[match[1]] === 'undefined' ? '' : properties[match[1]];
-        text = text.replace(match[0], value);
-    }
-    return text;
+function resolveTokens(properties, text) {
+    return text.replace(/{([^{}()\[\]<>$=:;.,^]+)}/g, function(match, key) {
+        return key in properties ? properties[key] : '';
+    });
 }
