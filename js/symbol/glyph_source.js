a2703793bd2bf438215e20ed8141083ac032eb38
Tom MacWright <tom@macwright.org>
2015-04-15 14:43:58 -0700

    Document glyph source

--- js/symbol/glyph_source.js
+++ js/symbol/glyph_source.js
@@ -7,6 +7,14 @@ var Protobuf = require('pbf');
 
 module.exports = GlyphSource;
 
+/**
+ * A glyph source has a URL from which to load new glyphs and owns a GlyphAtlas
+ * that stores currently-loaded glyphs.
+ *
+ * @param {string} url glyph template url
+ * @param {Object} glyphAtlas glyph atlas object
+ * @private
+ */
 function GlyphSource(url, glyphAtlas) {
     this.url = url && normalizeURL(url);
     this.glyphAtlas = glyphAtlas;
@@ -102,6 +110,16 @@ GlyphSource.prototype.loadRange = function(fontstack, range, callback) {
     }
 };
 
+/**
+ * Use CNAME sharding to load a specific glyph range over a randomized
+ * but consistent subdomain.
+ * @param {string} fontstack comma-joined fonts
+ * @param {string} range comma-joined range
+ * @param {url} url templated url
+ * @param {string} [subdomains=abc] subdomains as a string where each letter is one.
+ * @returns {string} a url to load that section of glyphs
+ * @private
+ */
 function glyphUrl(fontstack, range, url, subdomains) {
     subdomains = subdomains || 'abc';
 
