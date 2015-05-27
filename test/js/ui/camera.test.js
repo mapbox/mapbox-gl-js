ea68bdeb67e9e67c8827d2af68eba3217e59a0e5
John Firebaugh <john.firebaugh@gmail.com>
2015-05-26 22:17:56 -0700

    Consolidate camera methods

--- test/js/ui/camera.test.js
+++ test/js/ui/camera.test.js
@@ -16,6 +16,176 @@ test('Map', function(t) {
         }, options));
     }
 
+    t.test('#jumpTo', function(t) {
+        var map = createMap();
+
+        t.test('sets center', function(t) {
+            map.jumpTo({center: [1, 2]});
+            t.deepEqual(map.getCenter(), { lat: 1, lng: 2 });
+            t.end();
+        });
+
+        t.test('keeps current center if not specified', function(t) {
+            map.jumpTo({});
+            t.deepEqual(map.getCenter(), { lat: 1, lng: 2 });
+            t.end();
+        });
+
+        t.test('sets zoom', function(t) {
+            map.jumpTo({zoom: 3});
+            t.deepEqual(map.getZoom(), 3);
+            t.end();
+        });
+
+        t.test('keeps current zoom if not specified', function(t) {
+            map.jumpTo({});
+            t.deepEqual(map.getZoom(), 3);
+            t.end();
+        });
+
+        t.test('sets bearing', function(t) {
+            map.jumpTo({bearing: 4});
+            t.deepEqual(map.getBearing(), 4);
+            t.end();
+        });
+
+        t.test('keeps current bearing if not specified', function(t) {
+            map.jumpTo({});
+            t.deepEqual(map.getBearing(), 4);
+            t.end();
+        });
+
+        t.test('sets pitch', function(t) {
+            map.jumpTo({pitch: 45});
+            t.deepEqual(map.getPitch(), 45);
+            t.end();
+        });
+
+        t.test('keeps current pitch if not specified', function(t) {
+            map.jumpTo({});
+            t.deepEqual(map.getPitch(), 45);
+            t.end();
+        });
+
+        t.test('sets multiple properties', function(t) {
+            map.jumpTo({
+                center: [1, 2],
+                zoom: 3,
+                bearing: 180,
+                pitch: 45
+            });
+            t.deepEqual(map.getCenter(), { lat: 1, lng: 2 });
+            t.deepEqual(map.getZoom(), 3);
+            t.deepEqual(map.getBearing(), 180);
+            t.deepEqual(map.getPitch(), 45);
+            t.end();
+        });
+
+        t.test('emits move events', function(t) {
+            var started, ended;
+            map.on('movestart', function() { started = true; })
+                .on('moveend', function() { ended = true; });
+            map.jumpTo({center: [1, 2]});
+            t.ok(started);
+            t.ok(ended);
+            t.end();
+        });
+
+        t.test('cancels in-progress easing', function(t) {
+            map.panTo([3, 4]);
+            t.ok(map.isEasing());
+            map.jumpTo({center: [1, 2]});
+            t.ok(!map.isEasing());
+            t.end();
+        });
+
+        t.end();
+    });
+
+    t.test('#setCenter', function(t) {
+        var map = createMap();
+
+        t.test('sets center', function(t) {
+            map.setCenter([1, 2]);
+            t.deepEqual(map.getCenter(), { lat: 1, lng: 2 });
+            t.end();
+        });
+
+        t.test('emits move events', function(t) {
+            var started, ended;
+            map.on('movestart', function() { started = true; })
+                .on('moveend', function() { ended = true; });
+            map.setCenter([1, 2]);
+            t.ok(started);
+            t.ok(ended);
+            t.end();
+        });
+
+        t.test('cancels in-progress easing', function(t) {
+            map.panTo([3, 4]);
+            t.ok(map.isEasing());
+            map.setCenter([1, 2]);
+            t.ok(!map.isEasing());
+            t.end();
+        });
+    });
+
+    t.test('#setZoom', function(t) {
+        var map = createMap();
+
+        t.test('sets zoom', function(t) {
+            map.setZoom(3);
+            t.deepEqual(map.getZoom(), 3);
+            t.end();
+        });
+
+        t.test('emits move events', function(t) {
+            var started, ended;
+            map.on('movestart', function() { started = true; })
+                .on('moveend', function() { ended = true; });
+            map.setZoom(3);
+            t.ok(started);
+            t.ok(ended);
+            t.end();
+        });
+
+        t.test('cancels in-progress easing', function(t) {
+            map.panTo([3, 4]);
+            t.ok(map.isEasing());
+            map.setZoom(3);
+            t.ok(!map.isEasing());
+            t.end();
+        });
+    });
+
+    t.test('#setBearing', function(t) {
+        var map = createMap();
+
+        t.test('sets bearing', function(t) {
+            map.setBearing(4);
+            t.deepEqual(map.getBearing(), 4);
+            t.end();
+        });
+
+        t.test('emits move events', function(t) {
+            var started, ended;
+            map.on('movestart', function() { started = true; })
+                .on('moveend', function() { ended = true; });
+            map.setBearing(4);
+            t.ok(started);
+            t.ok(ended);
+            t.end();
+        });
+
+        t.test('cancels in-progress easing', function(t) {
+            map.panTo([3, 4]);
+            t.ok(map.isEasing());
+            map.setBearing(4);
+            t.ok(!map.isEasing());
+            t.end();
+        });
+    });
+
     t.test('#panBy', function(t) {
         t.test('pans by specified amount', function(t) {
             var map = createMap();
