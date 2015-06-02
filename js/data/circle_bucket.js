263d57c89dae0bf1eaeb559381890d87cab26e51
Scott Andrews <scott@mapbox.com>
2015-06-02 10:25:43 -0700

    Stub circles support
    Basic support to allow circle in v8 styles. Both the circle_bucket and
    draw_circle need to be implemented.
    
    This stub will not draw circles, but is sufficient to keep from failing
    when it encounters a circle in a stylesheet.

---
+++ js/data/circle_bucket.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = CircleBucket;
+
+function CircleBucket(buffers, layoutProperties, collision, overscaling, collisionDebug) {
+    console.warn('new CircleBucket is not implemented');
+}
+
+CircleBucket.prototype.addFeatures = function() {
+    console.warn('CircleBucket.prototype.addFeatures is not implemented');
+};
---
+++ js/render/draw_circle.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = drawCircles;
+
+function drawCircles(painter, layer, posMatrix, tile) {
+    console.warn('drawCircles is not implemented', painter, layer, posMatrix, tile);
+}
