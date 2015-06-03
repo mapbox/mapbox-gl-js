1a8e6c1c851b02ad32913e9fc00a5c9b08d7cba5
Tom MacWright <tom@macwright.org>
2015-06-03 16:45:24 -0400

    Add two triangles in the vertex buffer for each circle

--- js/data/circle_bucket.js
+++ js/data/circle_bucket.js
@@ -1,20 +1,75 @@
 'use strict';
 
+var ElementGroups = require('./element_groups');
+
 module.exports = CircleBucket;
 
 /**
  * A container for all circle data
+ *
+ * Circles are represented by two triangles.
+ *
+ * Each corner has a pos that is the center of the circle and an extrusion
+ * vector that is where it points.
  */
 function CircleBucket(buffers, layoutProperties, collision, overscaling, collisionDebug) {
     this.buffers = buffers;
-    this.layoutProperties = layoutProperties;
-    this.collision = collision;
-    this.overscaling = overscaling;
-    this.collisionDebug = collisionDebug;
-
-    this.symbolInstances = [];
+    this.elementGroups = new ElementGroups(
+        buffers.circleVertex,
+        buffers.circleElement);
 }
 
 CircleBucket.prototype.addFeatures = function() {
-    console.warn('CircleBucket.prototype.addFeatures is not implemented');
+    for (var i = 0; i < this.features.length; i++) {
+
+        this.elementGroups.makeRoomFor(6);
+
+        var geometry = this.features[i].loadGeometry();
+
+        // this geometry will be of the Point type, and we'll derive
+        // two triangles from it.
+        //
+        //     2
+        //    /|
+        // 1 / |
+        //   \ |
+        //    \|
+        //     3
+        //
+        // 1
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            -1, 0);
+        // 2
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            0, -1);
+        // 3
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            0, 1);
+
+
+        // 2
+        // |\
+        // | \1
+        // | /
+        // |/
+        // 3
+        //
+        // 1
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            1, 0);
+        // 2
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            0, -1);
+        // 3
+        this.buffers.circleVertex.add(
+            geometry.x, geometry.y,
+            0, 1);
+
+        this.elementGroups.current.vertexLength += 6;
+    }
 };
--- js/render/draw_circle.js
+++ js/render/draw_circle.js
@@ -3,5 +3,5 @@
 module.exports = drawCircles;
 
 function drawCircles(painter, layer, posMatrix, tile) {
-    console.warn('drawCircles is not implemented', painter, layer, posMatrix, tile);
+    // console.warn('drawCircles is not implemented', painter, layer, posMatrix, tile);
 }
