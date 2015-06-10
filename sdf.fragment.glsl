b549be41a65d456bea8a48ef4588f0f33a20d700
Lucas Wojciechowski <lucas@mapbox.com>
2015-06-10 15:36:00 -0400

    Made sf buffer an attribute / varying pair, always enable vertex attribs after disabling

--- sdf.fragment.glsl
+++ sdf.fragment.glsl
@@ -1,15 +1,15 @@
 uniform sampler2D u_texture;
-uniform float u_buffer;
 uniform float u_gamma;
 
 varying vec2 v_tex;
 varying float v_alpha;
 varying float v_gamma_scale;
 varying vec4 v_color;
+varying float v_buffer;
 
 void main() {
     float gamma = u_gamma * v_gamma_scale;
     float dist = texture2D(u_texture, v_tex).a;
-    float alpha = smoothstep(u_buffer - gamma, u_buffer + gamma, dist) * v_alpha;
+    float alpha = smoothstep(v_buffer - gamma, v_buffer + gamma, dist) * v_alpha;
     gl_FragColor = v_color * alpha;
 }
