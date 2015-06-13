d333d4c3ac05cd343a60e26ace7e4266af022b61
Tom MacWright <tom@macwright.org>
2015-06-12 21:17:18 -0500

    Simplify derivation of v_extrude

--- shaders/circle.vertex.glsl
+++ shaders/circle.vertex.glsl
@@ -10,9 +10,8 @@ varying vec2 v_extrude;
 
 void main(void) {
     // unencode the extrusion vector that we snuck into the a_pos vector
-    v_extrude = vec2(
-        mod(a_pos.x, 2.0) * 2.0 - 1.0,
-        mod(a_pos.y, 2.0) * 2.0 - 1.0);
+    v_extrude = vec2(mod(a_pos, 2.0) * 2.0 - 1.0);
+
     vec4 extrude = u_exmatrix * vec4(v_extrude * u_size, 0, 0);
     // multiply a_pos by 0.5, since we had it * 2 in order to sneak
     // in extrusion data
