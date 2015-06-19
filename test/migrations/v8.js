6dea4586ee4cd13649f39da16b7a07f1b8b581bb
John Firebaugh <john.firebaugh@gmail.com>
2015-06-19 15:39:08 -0700

    Fix indentation

--- test/migrations/v8.js
+++ test/migrations/v8.js
@@ -3,111 +3,117 @@
 var t = require('tape'),
     migrate = require('../../migrations/v8');
 
-t('split text-font', function(t) {
+t('split text-font', function (t) {
     var input = {
-      "version": 7,
-      "sources": {
-        "vector": {
-          "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "text-font": "Helvetica, Arial",
-            "text-field": "{foo}"
-          }
-        }
-      ]
+        "version": 7,
+        "sources": {
+            "vector": {
+                "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "text-font": "Helvetica, Arial",
+                    "text-field": "{foo}"
+                }
+            }
+        ]
     };
 
     var output = {
-      "version": 8,
-      "sources": {
-        "vector": {
-          "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "text-font": ["Helvetica", "Arial"],
-            "text-field": "{foo}"
-          }
-        }
-      ]
+        "version": 8,
+        "sources": {
+            "vector": {
+                "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "text-font": ["Helvetica", "Arial"],
+                    "text-field": "{foo}"
+                }
+            }
+        ]
     };
 
     t.deepEqual(migrate(input), output, 'splits text-font');
     t.end();
 });
 
-t('rename symbol-min-distance', function(t) {
+t('rename symbol-min-distance', function (t) {
     var input = {
-      "version": 7,
-      "sources": {
-        "vector": {
-          "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "symbol-min-distance": 2
-          }
-        }
-      ]
+        "version": 7,
+        "sources": {
+            "vector": {
+                "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "symbol-min-distance": 2
+                }
+            }
+        ]
     };
 
     var output = {
-      "version": 8,
-      "sources": {
-        "vector": {
-          "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "symbol-spacing": 2
-          }
-        }
-      ]
+        "version": 8,
+        "sources": {
+            "vector": {
+                "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "symbol-spacing": 2
+                }
+            }
+        ]
     };
 
     t.deepEqual(migrate(input), output, 'renames symbol-min-distance');
     t.end();
 });
 
-t('renames urls', function(t) {
+t('renames urls', function (t) {
     var input = {
-      "version": 7,
-      "sources": { "vector": { "type": "video", "url": ["foo"],
-          coordinates: [[1, 0], [1, 0], [1, 0], [1, 0]]
-      }}
+        "version": 7,
+        "sources": {
+            "vector": {
+                "type": "video", "url": ["foo"],
+                coordinates: [[1, 0], [1, 0], [1, 0], [1, 0]]
+            }
+        }
     };
 
     var output = {
-      "version": 8,
-      "sources": { "vector": { "type": "video", "urls": ["foo"],
-          coordinates: [[0, 1], [0, 1], [0, 1], [0, 1]]
-      }}
+        "version": 8,
+        "sources": {
+            "vector": {
+                "type": "video", "urls": ["foo"],
+                coordinates: [[0, 1], [0, 1], [0, 1], [0, 1]]
+            }
+        }
     };
 
     t.deepEqual(migrate(input), output, 'renames url and flips coordinates of of video');
@@ -115,194 +121,194 @@ t('renames urls', function(t) {
 });
 
 
-t('migrate interpolated scales', function(t) {
+t('migrate interpolated scales', function (t) {
     var input = {
-      "version": 7,
-      "sources": {
-        "vector": {
-          "type": "vector",
-          "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [{
-        "id": "scales",
-        "type": "symbol",
-        "source": "vector",
-        "source-layer": "layer",
-        "layout": {
-          "line-width": {
-            base: 2,
-            stops: [[1,2], [3,6]]
-          }
-        }
-      }]
+        "version": 7,
+        "sources": {
+            "vector": {
+                "type": "vector",
+                "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [{
+            "id": "scales",
+            "type": "symbol",
+            "source": "vector",
+            "source-layer": "layer",
+            "layout": {
+                "line-width": {
+                    base: 2,
+                    stops: [[1, 2], [3, 6]]
+                }
+            }
+        }]
     };
 
     var output = {
-      "version": 8,
-      "sources": {
-        "vector": {
-          "type": "vector",
-          "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [{
-        "id": "scales",
-        "type": "symbol",
-        "source": "vector",
-        "source-layer": "layer",
-        "layout": {
-          "line-width": {
-            type: "exponential",
-            base: 2,
-            domain: [1, 3],
-            range: [2, 6]
-          }
-        }
-      }]
+        "version": 8,
+        "sources": {
+            "vector": {
+                "type": "vector",
+                "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [{
+            "id": "scales",
+            "type": "symbol",
+            "source": "vector",
+            "source-layer": "layer",
+            "layout": {
+                "line-width": {
+                    type: "exponential",
+                    base: 2,
+                    domain: [1, 3],
+                    range: [2, 6]
+                }
+            }
+        }]
     };
 
     t.deepEqual(migrate(input), output);
     t.end();
 });
 
-t('migrate piecewise-constant scales', function(t) {
+t('migrate piecewise-constant scales', function (t) {
     var input = {
-      "version": 7,
-      "sources": {
-        "vector": {
-          "type": "vector",
-          "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [{
-        "id": "scales",
-        "type": "symbol",
-        "source": "vector",
-        "source-layer": "layer",
-        "layout": {
-          "text-transform": {
-            stops: [[1, "uppercase"], [3, "lowercase"]],
-          }
-        }
-      }]
+        "version": 7,
+        "sources": {
+            "vector": {
+                "type": "vector",
+                "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [{
+            "id": "scales",
+            "type": "symbol",
+            "source": "vector",
+            "source-layer": "layer",
+            "layout": {
+                "text-transform": {
+                    stops: [[1, "uppercase"], [3, "lowercase"]],
+                }
+            }
+        }]
     };
 
     var output = {
-      "version": 8,
-      "sources": {
-        "vector": {
-          "type": "vector",
-          "url": "mapbox://mapbox.mapbox-streets-v5"
-        }
-      },
-      "layers": [{
-        "id": "scales",
-        "type": "symbol",
-        "source": "vector",
-        "source-layer": "layer",
-        "layout": {
-          "text-transform": {
-            type: "interval",
-            domain: [1, 3],
-            range: ["uppercase", "uppercase", "lowercase"],
-          }
-        }
-      }]
+        "version": 8,
+        "sources": {
+            "vector": {
+                "type": "vector",
+                "url": "mapbox://mapbox.mapbox-streets-v5"
+            }
+        },
+        "layers": [{
+            "id": "scales",
+            "type": "symbol",
+            "source": "vector",
+            "source-layer": "layer",
+            "layout": {
+                "text-transform": {
+                    type: "interval",
+                    domain: [1, 3],
+                    range: ["uppercase", "uppercase", "lowercase"],
+                }
+            }
+        }]
     };
 
     t.deepEqual(migrate(input), output);
     t.end();
 });
 
-t('infer and update opacity constant', function(t) {
+t('infer and update opacity constant', function (t) {
     var input = {
-      "version": 7,
-      "constants": {
-        "@foo": 0.5
-      },
-      "sources": {
-        "vector": { "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5" }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "fill",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "fill-opacity": "@foo"
-          }
-        }
-      ]
+        "version": 7,
+        "constants": {
+            "@foo": 0.5
+        },
+        "sources": {
+            "vector": {"type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"}
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "fill",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "fill-opacity": "@foo"
+                }
+            }
+        ]
     };
 
     var output = {
-      "version": 8,
-      "constants": {
-        "@foo": { "type": "opacity", "value": 0.5 }
-      },
-      "sources": {
-        "vector": { "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5" }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "fill",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "fill-opacity": "@foo"
-          }
-        }
-      ]
+        "version": 8,
+        "constants": {
+            "@foo": {"type": "opacity", "value": 0.5}
+        },
+        "sources": {
+            "vector": {"type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"}
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "fill",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "fill-opacity": "@foo"
+                }
+            }
+        ]
     };
 
     t.deepEqual(migrate(input), output, 'infers opacity type');
     t.end();
 });
 
-t('infer and update fontstack constant', function(t) {
+t('infer and update fontstack constant', function (t) {
     var input = {
-      "version": 7,
-      "constants": {
-        "@foo": "Arial Unicode,Foo Bar"
-      },
-      "sources": {
-        "vector": { "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5" }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "text-font": "@foo"
-          }
-        }
-      ]
+        "version": 7,
+        "constants": {
+            "@foo": "Arial Unicode,Foo Bar"
+        },
+        "sources": {
+            "vector": {"type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"}
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "text-font": "@foo"
+                }
+            }
+        ]
     };
 
     var output = {
-      "version": 8,
-      "constants": {
-        "@foo": { "type": "font-array", "value": ["Arial Unicode", "Foo Bar"] }
-      },
-      "sources": {
-        "vector": { "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5" }
-      },
-      "layers": [
-        {
-          "id": "minimum",
-          "type": "symbol",
-          "source": "vector",
-          "source-layer": "layer",
-          "layout": {
-            "text-font": "@foo"
-          }
-        }
-      ]
+        "version": 8,
+        "constants": {
+            "@foo": {"type": "font-array", "value": ["Arial Unicode", "Foo Bar"]}
+        },
+        "sources": {
+            "vector": {"type": "vector", "url": "mapbox://mapbox.mapbox-streets-v5"}
+        },
+        "layers": [
+            {
+                "id": "minimum",
+                "type": "symbol",
+                "source": "vector",
+                "source-layer": "layer",
+                "layout": {
+                    "text-font": "@foo"
+                }
+            }
+        ]
     };
 
     t.deepEqual(migrate(input), output, 'infers opacity type');
