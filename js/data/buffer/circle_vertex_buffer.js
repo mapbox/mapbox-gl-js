3ec7f46305e64bc1695135daefe48f2e99eaf815
Lucas Wojciechowski <lucas@mapbox.com>
2015-06-23 14:01:43 -0700

    Allow arbitrary width vertex buffers for circles

--- js/data/buffer/circle_vertex_buffer.js
+++ js/data/buffer/circle_vertex_buffer.js
@@ -16,26 +16,14 @@ function CircleVertexBuffer(buffer) {
 CircleVertexBuffer.prototype = util.inherit(Buffer, {
     defaultLength: 2048 * 16,
 
-    itemSize: 4, // 2 bytes per short * 4 of them
-
     add: function(x, y, extrudeX, extrudeY) {
-        var pos = this.pos,
-            pos2 = pos / 2;
-
         this.resize();
 
         // pack the extrusion of -1 or 1 into one short
-        this.shorts[pos2 + 0] = (x * 2) + ((extrudeX + 1) / 2);
-        this.shorts[pos2 + 1] = (y * 2) + ((extrudeY + 1) / 2);
+        this.shorts[this.pos / 2 + 0] = (x * 2) + ((extrudeX + 1) / 2);
+        this.shorts[this.pos / 2 + 1] = (y * 2) + ((extrudeY + 1) / 2);
 
         this.pos += this.itemSize;
-    },
-
-    bind: function(gl, shader, offset) {
-        Buffer.prototype.bind.call(this, gl);
-
-        gl.vertexAttribPointer(shader.a_pos, 2,
-            gl.SHORT, false,
-            this.itemSize, offset + 0);
     }
+
 });
