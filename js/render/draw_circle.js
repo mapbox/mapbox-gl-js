9d43bf1b6d816669aaf51da6a5c8f972440cee10
Lucas Wojciechowski <lucas@mapbox.com>
2015-08-06 15:42:09 -0700

    Added circle-translate

--- js/render/draw_circle.js
+++ js/render/draw_circle.js
@@ -8,6 +8,8 @@ function drawCircles(painter, layer, posMatrix, tile) {
     // short-circuit if tile is empty
     if (!tile.buffers) return;
 
+    posMatrix = painter.translateMatrix(posMatrix, tile, layer.paint['circle-translate'], layer.paint['circle-translate-anchor']);
+
     var elementGroups = tile.elementGroups[layer.ref || layer.id];
     if (!elementGroups) return;
 
@@ -17,7 +19,7 @@ function drawCircles(painter, layer, posMatrix, tile) {
     // large circles are not clipped to tiles
     gl.disable(gl.STENCIL_TEST);
 
-    gl.switchShader(painter.circleShader, tile.posMatrix, tile.exMatrix);
+    gl.switchShader(painter.circleShader, posMatrix, tile.exMatrix);
 
     var vertex = tile.buffers.circleVertex;
     var shader = painter.circleShader;
