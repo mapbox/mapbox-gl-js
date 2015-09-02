4e8eb9ac7fc280181ef9000900d7a3f41b3734f6
John Firebaugh <john.firebaugh@gmail.com>
2015-09-02 15:19:18 -0700

    Update docs for v8

--- docs/_generate/generate.js
+++ docs/_generate/generate.js
@@ -4,7 +4,7 @@
 
 var fs = require('fs'),
     path = require('path'),
-    ref = require('../../reference/v7.json'),
+    ref = require('../..').latest,
     _ = require('underscore');
 
 function tmpl(x) {
