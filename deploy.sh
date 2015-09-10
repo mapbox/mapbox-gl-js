cf074ec171940ef8df78557a5391929233dafc54
John Firebaugh <john.firebaugh@gmail.com>
2015-09-10 14:07:06 -0700

    Apply cache-control max-age to assets (fixes #1424)

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [ -z $TAG ]; then
 fi
 
 function gzipped_cp {
-    gzip --stdout dist/$1 | aws s3 cp --acl public-read --content-encoding gzip --content-type $2 - s3://mapbox/mapbox-gl-js/$TAG/$1
+    gzip --stdout dist/$1 | aws s3 cp --acl public-read --cache-control max-age=2592000 --content-encoding gzip --content-type $2 - s3://mapbox/mapbox-gl-js/$TAG/$1
     echo "upload: dist/$1 to s3://mapbox/mapbox-gl-js/$TAG/$1 (gzipped)"
 }
 
@@ -19,4 +19,4 @@ gzipped_cp mapbox-gl.js.map application/octet-stream
 gzipped_cp mapbox-gl-dev.js application/javascript
 gzipped_cp mapbox-gl.css    text/css
 
-aws s3 cp --recursive --acl public-read dist/font/ s3://mapbox/mapbox-gl-js/$1/font/
+aws s3 cp --recursive --acl public-read --cache-control max-age=2592000 dist/font/ s3://mapbox/mapbox-gl-js/$1/font/
