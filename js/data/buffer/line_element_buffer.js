8405964fe91946d0eeba6860b5b509585ef3dd99
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-09-28 11:34:51 -0400

    convert line element buffer to buffer2

--- js/data/buffer/line_element_buffer.js
+++ js/data/buffer/line_element_buffer.js
@@ -1,27 +1,24 @@
 'use strict';
 
 var util = require('../../util/util');
-var Buffer = require('./buffer');
+var Buffer2 = require('../buffer2');
 
-module.exports = LineElementBuffer;
-
-function LineElementBuffer(buffer) {
-    Buffer.call(this, buffer);
+function LineElementBuffer(options) {
+    Buffer2.call(this, options || {
+        type: Buffer2.BufferType.ELEMENT,
+        attributes: {
+            verticies: {
+                components: 3,
+                type: Buffer2.AttributeType.UNSIGNED_SHORT
+            }
+        }
+    });
 }
 
-LineElementBuffer.prototype = util.inherit(Buffer, {
-    itemSize: 6, // bytes per triangle (3 * unsigned short == 6 bytes)
-    arrayType: 'ELEMENT_ARRAY_BUFFER',
-
-    add: function(a, b, c) {
-        var pos2 = this.pos / 2;
-
-        this.resize();
-
-        this.ushorts[pos2 + 0] = a;
-        this.ushorts[pos2 + 1] = b;
-        this.ushorts[pos2 + 2] = c;
-
-        this.pos += this.itemSize;
+LineElementBuffer.prototype = util.inherit(Buffer2, {
+    add: function(a, b, c){
+        this.push([a, b, c]);
     }
 });
+
+module.exports = LineElementBuffer;
