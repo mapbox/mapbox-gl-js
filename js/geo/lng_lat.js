ae86f25d73a61ab5f6b3ee9f3d00930125cf69f1
Bryan Housel <bryan@mapbox.com>
2015-10-05 17:00:00 -0400

    Fix examples that still used Lat,Lng

--- js/geo/lng_lat.js
+++ js/geo/lng_lat.js
@@ -11,8 +11,8 @@ var wrap = require('../util/util').wrap;
  * Note that any Mapbox GL method that accepts a `LngLat` object can also accept an
  * `Array` and will perform an implicit conversion.  The following lines are equivalent:
  ```
- map.setCenter([-79.469, 39.261]);
- map.setCenter( new mapboxgl.LngLat(-79.469, 39.261) );
+ map.setCenter([-73.9749, 40.7736]);
+ map.setCenter( new mapboxgl.LngLat(-73.9749, 40.7736) );
  ```
  *
  * @class LngLat
@@ -20,7 +20,7 @@ var wrap = require('../util/util').wrap;
  * @param {number} lng longitude
  * @param {number} lat latitude
  * @example
- * var lnglat = new mapboxgl.LngLat(37.76, -122.44);
+ * var ll = new mapboxgl.LngLat(-73.9749, 40.7736);
  */
 function LngLat(lng, lat) {
     if (isNaN(lng) || isNaN(lat)) {
@@ -38,9 +38,9 @@ function LngLat(lng, lat) {
  *
  * @returns {LngLat} wrapped LngLat object
  * @example
- * var point = mapboxgl.LngLat(0, 200);
- * var wrapped = point.wrap();
- * wrapped.lng; // = -160
+ * var ll = new mapboxgl.LngLat(286.0251, 40.7736);
+ * var wrapped = ll.wrap();
+ * wrapped.lng; // = -73.9749
  */
 LngLat.prototype.wrap = function () {
     return new LngLat(wrap(this.lng, -180, 180), this.lat);
@@ -77,9 +77,9 @@ LngLat.prototype.toString = function () {
  * @param {Array<number>|LngLat} input `input` to convert
  * @returns {LngLat} LngLat object or original input
  * @example
- * var ll = mapboxgl.LngLat.convert([10, 10]);
- * var ll2 = new mapboxgl.LngLat(10, 10);
- * ll // = ll2
+ * var arr = [-73.9749, 40.7736];
+ * var ll = mapboxgl.LngLat.convert(arr);
+ * ll;   // = LngLat {lng: -73.9749, lat: 40.7736}
  */
 LngLat.convert = function (input) {
     if (input instanceof LngLat) {
