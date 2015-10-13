42874471c5e881027dbdbb11621dd8b3382cbe6d
Lucas Wojciechowski <lucas@mapbox.com>
2015-10-13 15:56:06 -0700

    Renamed LayerType files

--- js/layer_type/index.js
+++ js/layer_type/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    circle: require('./circle'),
-    fill: require('./fill'),
-    line: require('./line'),
-    symbol: require('./symbol')
+    circle: require('./circle_layer_type'),
+    fill: require('./fill_layer_type'),
+    line: require('./line_layer_type'),
+    symbol: require('./symbol_layer_type')
 };
