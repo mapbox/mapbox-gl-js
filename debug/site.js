48d9aa85f4b6fb89bdb282aa448913f88a592d51
Lucas Wojciechowski <lucas@mapbox.com>
2015-10-16 16:05:38 -0700

    Improved buffer stats on debug page

--- debug/site.js
+++ debug/site.js
@@ -54,36 +54,24 @@ map.on('style.load', function() {
         }
     });
 
-    var statsAverages = {};
-    var statsSamples = 0;
-    var statsSlowest = null;
-    map.on('tile.stats', function(stats) {
-        for (var k in stats) {
-            statsSamples++;
-            statsAverages[k] = ((statsAverages[k] || 0) + stats[k]) / statsSamples;
-            if (!statsSlowest || statsAverages[k] > statsAverages[statsSlowest]) statsSlowest = k;
-        }
-        document.getElementById('bucket-stats').innerHTML = renderTileStats();
-    });
-
-    function renderTileStats() {
+    var bufferTimes = {};
+    map.on('tile.stats', function(bufferTimes) {
         var _stats = [];
-        for (var name in statsAverages) {
-            var value = Math.round(statsAverages[name] * 1000);
+        for (var name in bufferTimes) {
+            var value = Math.round(bufferTimes[name]);
             if (isNaN(value)) continue;
             var width = value;
             _stats.push({name: name, value: value, width: width});
         }
         _stats = _stats.sort(function(a, b) { return b.value - a.value }).slice(0, 10);
 
-        var output = '';
+        var html = '';
         for (var i in _stats) {
-            output += '<div style="width:' + _stats[i].width + 'px">' + _stats[i].value + 'ms - ' + _stats[i].name + '</div>';
+            html += '<div style="width:' + _stats[i].width + 'px">' + _stats[i].value + 'ms - ' + _stats[i].name + '</div>';
         }
 
-        return output;
-
-    }
+        document.getElementById('buffer').innerHTML = html;
+    });
 });
 
 map.on('click', function(e) {
@@ -101,8 +89,8 @@ document.getElementById('collision-debug-checkbox').onclick = function() {
     map.collisionDebug = !!this.checked;
 };
 
-document.getElementById('bucket-stats-checkbox').onclick = function() {
-    document.getElementById('bucket-stats').style.display = this.checked ? 'block' : 'none';
+document.getElementById('buffer-checkbox').onclick = function() {
+    document.getElementById('buffer').style.display = this.checked ? 'block' : 'none';
 };
 
 // keyboard shortcut for comparing rendering with Mapbox GL native
