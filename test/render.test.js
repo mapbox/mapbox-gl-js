6f08362ba1aa92c6a6e3f932a434e96e7c976dbc
John Firebaugh <john.firebaugh@gmail.com>
2015-12-08 16:54:17 -0800

    Update test-suite

--- test/render.test.js
+++ test/render.test.js
@@ -22,10 +22,6 @@ suite.run('js', {tests: tests}, function(style, options, callback) {
                 remove: function() {}
             }
         },
-        center: options.center,
-        zoom: options.zoom,
-        pitch: options.pitch,
-        bearing: options.bearing,
         style: style,
         classes: options.classes,
         interactive: false,
