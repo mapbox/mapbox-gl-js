fd2f262d8c848443c000844a34a9216c040199c4
John Firebaugh <john.firebaugh@gmail.com>
2015-12-22 11:52:11 -0800

    Document Popup anchor option

--- js/ui/popup.js
+++ js/ui/popup.js
@@ -13,6 +13,9 @@ var LngLat = require('../geo/lng_lat');
  * @param {Object} options
  * @param {boolean} options.closeButton
  * @param {boolean} options.closeOnClick
+ * @param {string} options.anchor - One of "top", "bottom", "left", "right", "top-left",
+ * "top-right", "bottom-left", or "bottom-right", describing where the popup's anchor
+ * relative to the coordinate set via `setLngLat`.
  * @example
  * var tooltip = new mapboxgl.Popup()
  *   .setLngLat(e.lngLat)
