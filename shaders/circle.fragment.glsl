c991e8f306215801164a966b91ba5386898a2d7b
Lucas Wojciechowski <lucas@mapbox.com>
2016-01-06 10:24:30 -0800

    Attribute-ify circle shaders

--- shaders/circle.fragment.glsl
+++ shaders/circle.fragment.glsl
@@ -1,11 +1,11 @@
 precision mediump float;
 
-uniform lowp vec4 u_color;
 uniform lowp float u_blur;
 
+varying lowp vec4 v_color;
 varying vec2 v_extrude;
 
 void main() {
     float t = smoothstep(1.0 - u_blur, 1.0, length(v_extrude));
-    gl_FragColor = u_color * (1.0 - t);
+    gl_FragColor = v_color * (1.0 - t);
 }
