cf92516e63ecb88fc13c9e27a3883cb3d95043f1
Ansis Brammanis <brammanis@gmail.com>
2016-01-28 21:41:20 -0800

    [core] support tiles with non-4096 extents
    Convert all geometries to the maximum extent supported by our buffers
    and then use that constant extent everywhere else.

--- raster.vertex.glsl
+++ raster.vertex.glsl
@@ -8,6 +8,6 @@ varying vec2 v_pos;
 
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
-    float dimension = (4096.0 + 2.0 * u_buffer);
+    float dimension = (8192.0 + 2.0 * u_buffer);
     v_pos = (a_pos / dimension) + (u_buffer / dimension);
 }
