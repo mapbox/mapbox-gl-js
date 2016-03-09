57a9d150e507818d72b1045696a564d40278ebb4
Lucas Wojciechowski <lucas@mapbox.com>
2016-03-09 11:42:32 -0800

    Use a layers object instead of an array in WorkerTile

--- test/js/source/worker_tile.test.js
+++ test/js/source/worker_tile.test.js
@@ -6,25 +6,34 @@ var Wrapper = require('../../../js/source/geojson_wrapper');
 var TileCoord = require('../../../js/source/tile_coord');
 
 test('basic', function(t) {
-    var buckets = [{
-        id: 'test',
-        source: 'source',
-        type: 'circle',
-        layout: {},
-        compare: function () { return true; }
-    }];
-
     var features = [{
         type: 1,
         geometry: [0, 0],
         tags: {}
     }];
 
-    var tile = new WorkerTile({uid: '', zoom: 0, maxZoom: 20, tileSize: 512, source: 'source',
-        coord: new TileCoord(1, 1, 1), overscaling: 1 });
+    var tile = new WorkerTile({
+        uid: '',
+        zoom: 0,
+        maxZoom: 20,
+        tileSize: 512,
+        source: 'source',
+        coord: new TileCoord(1, 1, 1),
+        overscaling: 1
+    });
 
     t.test('basic worker tile', function(t) {
-        tile.parse(new Wrapper(features), buckets, {}, null, function(err, result) {
+        var layers = {
+            test: {
+                id: 'test',
+                source: 'source',
+                type: 'circle',
+                layout: {},
+                compare: function () { return true; }
+            }
+        };
+
+        tile.parse(new Wrapper(features), layers, {}, null, function(err, result) {
             t.equal(err, null);
             t.ok(result.buckets[0]);
             t.end();
@@ -32,14 +41,24 @@ test('basic', function(t) {
     });
 
     t.test('hidden layers', function(t) {
-        buckets.push({
-            id: 'test-hidden',
-            source: 'source',
-            type: 'fill',
-            layout: { visibility: 'none' },
-            compare: function () { return true; }
-        });
-        tile.parse(new Wrapper(features), buckets, {}, null, function(err, result) {
+        var layers = {
+            'test': {
+                id: 'test',
+                source: 'source',
+                type: 'circle',
+                layout: {},
+                compare: function () { return true; }
+            },
+            'test-hidden': {
+                id: 'test-hidden',
+                source: 'source',
+                type: 'fill',
+                layout: { visibility: 'none' },
+                compare: function () { return true; }
+            }
+        };
+
+        tile.parse(new Wrapper(features), layers, {}, null, function(err, result) {
             t.equal(err, null);
             t.equal(Object.keys(result.buckets[0].elementGroups).length, 1, 'element groups exclude hidden layer');
             t.end();
