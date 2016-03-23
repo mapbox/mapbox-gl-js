405847845d25e8548c086b49a34284d0b3ef5cd2
Lucas Wojciechowski <lucas@mapbox.com>
2016-03-23 11:33:38 -0700

    Fix test coverage

--- ci.sh
+++ ci.sh
@@ -3,35 +3,40 @@
 set -e
 set -o pipefail
 
+# add npm packages to $PATH
+export PATH=$(pwd)/node_modules/.bin:$PATH
+
 # set up code coverage instrumentation
-rm -rf coverage
-export npm_config_coverage=true
+rm -rf coverage .nyc_output
 
-npm test
+# run unit tests
+tap --reporter dot --coverage --no-coverage-report test/js/*/*.js
 
-# allow writing core files
+# allow writing core files for render tests
 ulimit -c unlimited -S
 echo 'ulimit -c: '`ulimit -c`
 echo '/proc/sys/kernel/core_pattern: '`cat /proc/sys/kernel/core_pattern`
 sysctl kernel.core_pattern
 
-npm run test-suite
-RESULT=$?
+# run render tests
+istanbul cover --dir .nyc_output --include-pid --report none --print none test/render.test.js &&
+istanbul cover --dir .nyc_output --include-pid --report none --print none test/query.test.js
+EXIT_CODE=$?
 
-if [[ ${RESULT} != 0 ]]; then
-    echo "The program crashed with exit code ${RESULT}. We're now trying to output the core dump."
+# collect core dumps from render tests
+if [[ ${EXIT_CODE} != 0 ]]; then
+    echo "The program crashed with exit code ${EXIT_CODE}. We're now trying to output the core dump."
 fi
-
-# output core dump if we got one
 for DUMP in $(find ./ -maxdepth 1 -name 'core*' -print); do
     gdb `which node` ${DUMP} -ex "thread apply all bt" -ex "set pagination 0" -batch
     rm -rf ${DUMP}
 done
 
-npm run coverage
+# send coverage report to coveralls
+nyc report --reporter=lcov
 node ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
 
-# now we should present the original error code so the run cleanly stops
-if [[ ${RESULT} != 0 ]]; then
-    exit $RESULT
+# return original error code
+if [[ ${EXIT_CODE} != 0 ]]; then
+    exit $EXIT_CODE
 fi
