be4700279de8472f643667534708e4309c4a8604
Ansis Brammanis <brammanis@gmail.com>
2016-03-31 14:33:37 -0700

    better symbol fading with texture lookups
    - this is simpler than prediction opacity based on current zooming speed
    - this is smoother: Symbols don't flicker when changing zoom speed or zooming in
      and out rapidly.
    
    fix #590

--- sdf.fragment.glsl
+++ sdf.fragment.glsl
@@ -1,17 +1,19 @@
 precision mediump float;
 
 uniform sampler2D u_texture;
+uniform sampler2D u_fadetexture;
 uniform lowp vec4 u_color;
 uniform lowp float u_buffer;
 uniform lowp float u_gamma;
 
 varying vec2 v_tex;
-varying float v_alpha;
+varying vec2 v_fade_tex;
 varying float v_gamma_scale;
 
 void main() {
-    lowp float gamma = u_gamma * v_gamma_scale;
     lowp float dist = texture2D(u_texture, v_tex).a;
-    lowp float alpha = smoothstep(u_buffer - gamma, u_buffer + gamma, dist) * v_alpha;
+    lowp float fade_alpha = texture2D(u_fadetexture, v_fade_tex).a;
+    lowp float gamma = u_gamma * v_gamma_scale;
+    lowp float alpha = smoothstep(u_buffer - gamma, u_buffer + gamma, dist) * fade_alpha;
     gl_FragColor = u_color * alpha;
 }
