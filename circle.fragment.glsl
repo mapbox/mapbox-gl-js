a09dee9e1c95545468f3fc92b1fd5ec6c1fafeaa
Ansis Brammanis <brammanis@gmail.com>
2016-04-08 20:51:04 -0700

    disable attributes based on their paint property
    This replaces a per-attribute `isDisabled` function with paint property
    key. All data-driven attributes must now depend on only a single paint
    property.
    
    The `a_color` attribute used to depend on both the `circle-color` and
    `circle-opacity` paint properties. This splits them up into two
    attributes/uniforms, each of which only depends on one paint property.
    
    I think it will be simpler to have all attributes be based on a single
    style property. We will need to split up:
    - a_color, which depends on -color and -opacity
    - u_linewidth, which depends on line-width and line-width-gap

--- circle.fragment.glsl
+++ circle.fragment.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
 
 uniform lowp float u_blur;
+uniform lowp float u_opacity;
 
 varying lowp vec4 v_color;
 varying vec2 v_extrude;
@@ -8,5 +9,5 @@ varying lowp float v_antialiasblur;
 
 void main() {
     float t = smoothstep(1.0 - max(u_blur, v_antialiasblur), 1.0, length(v_extrude));
-    gl_FragColor = v_color * (1.0 - t);
+    gl_FragColor = v_color * (1.0 - t) * u_opacity;
 }
