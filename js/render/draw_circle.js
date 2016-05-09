b0afb6d27e059549bbbfcac3bde20532c1ae491b
Ansis Brammanis <ansis.brammanis@gmail.com>
2016-05-09 13:54:02 -0400

    fix #2541, scale circles in pitch views (#2544)

--- js/render/draw_circle.js
+++ js/render/draw_circle.js
@@ -27,7 +27,9 @@ function drawCircles(painter, source, layer, coords) {
 
         var program = painter.useProgram('circle', bucket.getProgramMacros('circle', layer));
 
-        gl.uniform2fv(program.u_extrude_scale, painter.transform.pixelsToGLUnits);
+        gl.uniform2f(program.u_extrude_scale,
+                painter.transform.pixelsToGLUnits[0] * painter.transform.altitude,
+                painter.transform.pixelsToGLUnits[1] * painter.transform.altitude);
         gl.uniform1f(program.u_blur, layer.paint['circle-blur']);
         gl.uniform1f(program.u_devicepixelratio, browser.devicePixelRatio);
         gl.uniform1f(program.u_opacity, layer.paint['circle-opacity']);
