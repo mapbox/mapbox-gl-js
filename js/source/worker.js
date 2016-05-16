97bc31b1f79f8403f6f3f8b932bc53e2d50e5ec0
John Firebaugh <john.firebaugh@gmail.com>
2016-05-16 09:09:56 -0700

    Fix GeoJSON sources in query tests
    When writing, pbf returns a Uint8Array in the browser, but a Buffer in node. Using .buffer on a Buffer without checking byteOffset or byteLength is not safe due to implementation details of Buffer.

--- js/source/worker.js
+++ js/source/worker.js
@@ -207,8 +207,12 @@ util.extend(Worker.prototype, {
         if (geoJSONTile) {
             var geojsonWrapper = new GeoJSONWrapper(geoJSONTile.features);
             geojsonWrapper.name = '_geojsonTileLayer';
-            var rawTileData = vtpbf({ layers: { '_geojsonTileLayer': geojsonWrapper }}).buffer;
-            tile.parse(geojsonWrapper, this.layerFamilies, this.actor, rawTileData, callback);
+            var pbf = vtpbf({ layers: { '_geojsonTileLayer': geojsonWrapper }});
+            if (pbf.byteOffset !== 0 || pbf.byteLength !== pbf.buffer.byteLength) {
+                // Compatibility with node Buffer (https://github.com/mapbox/pbf/issues/35)
+                pbf = new Uint8Array(pbf);
+            }
+            tile.parse(geojsonWrapper, this.layerFamilies, this.actor, pbf.buffer, callback);
         } else {
             return callback(null, null); // nothing in the given tile
         }
