mapbox/mapbox-gl-js

commit 4dd6786282e7d3e9e7b2a7fd866c3637d9e88737

Refactor "Interaction" class, fix #2575 (#2583)
* Refactor Interaction, add tests, fix #2575

* Restore Interaction docs

js/ui/bind_handlers.js
@@ -1,0 +1,142 @@
+'use strict';
+
+var DOM = require('../util/dom');
+var Point = require('point-geometry');
+
+var handlers = {
+    scrollZoom: require('./handler/scroll_zoom'),
+    boxZoom: require('./handler/box_zoom'),
+    dragRotate: require('./handler/drag_rotate'),
+    dragPan: require('./handler/drag_pan'),
+    keyboard: require('./handler/keyboard'),
+    doubleClickZoom: require('./handler/dblclick_zoom'),
+    touchZoomRotate: require('./handler/touch_zoom_rotate')
+};
+
+module.exports = function bindHandlers(map, options) {
+    var el = map.getCanvasContainer();
+    var contextMenuEvent = null;
+    var startPos = null;
+    var tapped = null;
+
+    for (var name in handlers) {
+        map[name] = new handlers[name](map);
+        if (options.name) {
+            map[name].enable();
+        }
+    }
+
+    el.addEventListener('mousedown', onMouseDown, false);
+    el.addEventListener('mouseup', onMouseUp, false);
+    el.addEventListener('mousemove', onMouseMove, false);
+    el.addEventListener('touchstart', onTouchStart, false);
+    el.addEventListener('touchend', onTouchEnd, false);
+    el.addEventListener('touchmove', onTouchMove, false);
+    el.addEventListener('touchcancel', onTouchCancel, false);
+    el.addEventListener('click', onClick, false);
+    el.addEventListener('dblclick', onDblClick, false);
+    el.addEventListener('contextmenu', onContextMenu, false);
+
+    function onMouseDown(e) {
+        map.stop();
+        startPos = DOM.mousePos(el, e);
+        fireMouseEvent('mousedown', e);
+    }
+
+    function onMouseUp(e) {
+        var rotating = map.dragRotate && map.dragRotate.isActive();
+
+        if (contextMenuEvent && !rotating) {
+            fireMouseEvent('contextmenu', contextMenuEvent);
+        }
+
+        contextMenuEvent = null;
+        fireMouseEvent('mouseup', e);
+    }
+
+    function onMouseMove(e) {
+        if (map.dragPan && map.dragPan.isActive()) return;
+        if (map.dragRotate && map.dragRotate.isActive()) return;
+
+        var target = e.toElement || e.target;
+        while (target && target !== el) target = target.parentNode;
+        if (target !== el) return;
+
+        fireMouseEvent('mousemove', e);
+    }
+
+    function onTouchStart(e) {
+        map.stop();
+        fireTouchEvent('touchstart', e);
+
+        if (!e.touches || e.touches.length > 1) return;
+
+        if (!tapped) {
+            tapped = setTimeout(onTouchTimeout, 300);
+
+        } else {
+            clearTimeout(tapped);
+            tapped = null;
+            fireMouseEvent('dblclick', e);
+        }
+    }
+
+    function onTouchMove(e) {
+        fireTouchEvent('touchmove', e);
+    }
+
+    function onTouchEnd(e) {
+        fireTouchEvent('touchend', e);
+    }
+
+    function onTouchCancel(e) {
+        fireTouchEvent('touchcancel', e);
+    }
+
+    function onTouchTimeout() {
+        tapped = null;
+    }
+
+    function onClick(e) {
+        var pos = DOM.mousePos(el, e);
+
+        if (pos.equals(startPos)) {
+            fireMouseEvent('click', e);
+        }
+    }
+
+    function onDblClick(e) {
+        fireMouseEvent('dblclick', e);
+        e.preventDefault();
+    }
+
+    function onContextMenu(e) {
+        contextMenuEvent = e;
+        e.preventDefault();
+    }
+
+    function fireMouseEvent(type, e) {
+        var pos = DOM.mousePos(el, e);
+
+        return map.fire(type, {
+            lngLat: map.unproject(pos),
+            point: pos,
+            originalEvent: e
+        });
+    }
+
+    function fireTouchEvent(type, e) {
+        var touches = DOM.touchPos(el, e);
+        var singular = touches.reduce(function(prev, curr, i, arr) {
+            return prev.add(curr.div(arr.length));
+        }, new Point(0, 0));
+
+        return map.fire(type, {
+            lngLat: map.unproject(singular),
+            point: singular,
+            lngLats: touches.map(function(t) { return map.unproject(t); }, this),
+            points: touches,
+            originalEvent: e
+        });
+    }
+};

js/util/dom.js
@@ -4,6 +4,7 @@ exports.create = function (tagName, className, container) {
     return {
         offsetWidth: container.offsetWidth,
         offsetHeight: container.offsetHeight,
-        remove: function () {}
+        remove: function () {},
+        addEventListener: function() {}
     };
 };

test/js/ui/map.test.js
@@ -32,6 +32,45 @@ test('Map', function(t) {
         t.end();
     });
 
+    t.test('disables handlers', function(t) {
+        t.test('disables all handlers', function(t) {
+            var map = createMap({interactive: false});
+
+            t.notOk(map.boxZoom.isEnabled());
+            t.notOk(map.doubleClickZoom.isEnabled());
+            t.notOk(map.dragPan.isEnabled());
+            t.notOk(map.dragRotate.isEnabled());
+            t.notOk(map.keyboard.isEnabled());
+            t.notOk(map.scrollZoom.isEnabled());
+            t.notOk(map.touchZoomRotate.isEnabled());
+
+            t.end();
+        });
+
+        var handlerNames = [
+            'scrollZoom',
+            'boxZoom',
+            'dragRotate',
+            'dragPan',
+            'keyboard',
+            'doubleClickZoom',
+            'touchZoomRotate'
+        ];
+        handlerNames.forEach(function(handlerName) {
+            t.test('disables "' + handlerName + '" handler', function(t) {
+                var options = {};
+                options[handlerName] = false;
+                var map = createMap(options);
+
+                t.notOk(map[handlerName].isEnabled());
+
+                t.end();
+            });
+        });
+
+        t.end();
+    });
+
     t.test('emits load event after a style is set', function(t) {
         var map = createMap();
 

js/ui/interaction.js
@@ -1,325 +1,0 @@
-'use strict';
-
-var handlers = {
-    scrollZoom: require('./handler/scroll_zoom'),
-    boxZoom: require('./handler/box_zoom'),
-    dragRotate: require('./handler/drag_rotate'),
-    dragPan: require('./handler/drag_pan'),
-    keyboard: require('./handler/keyboard'),
-    doubleClickZoom: require('./handler/dblclick_zoom'),
-    touchZoomRotate: require('./handler/touch_zoom_rotate')
-};
-
-var DOM = require('../util/dom'),
-    util = require('../util/util'),
-    Point = require('point-geometry');
-
-module.exports = Interaction;
-
-function Interaction(map) {
-    this._map = map;
-    this._el = map.getCanvasContainer();
-
-    for (var name in handlers) {
-        map[name] = new handlers[name](map);
-    }
-
-    util.bindHandlers(this);
-}
-
-Interaction.prototype = {
-    enable: function () {
-        var options = this._map.options,
-            el = this._el;
-
-        for (var name in handlers) {
-            if (options[name]) this._map[name].enable();
-        }
-
-        el.addEventListener('mousedown', this._onMouseDown, false);
-        el.addEventListener('mouseup', this._onMouseUp, false);
-        el.addEventListener('mousemove', this._onMouseMove, false);
-        el.addEventListener('touchstart', this._onTouchStart, false);
-        el.addEventListener('touchend', this._onTouchEnd, false);
-        el.addEventListener('touchmove', this._onTouchMove, false);
-        el.addEventListener('touchcancel', this._onTouchCancel, false);
-        el.addEventListener('click', this._onClick, false);
-        el.addEventListener('dblclick', this._onDblClick, false);
-        el.addEventListener('contextmenu', this._onContextMenu, false);
-    },
-
-    disable: function () {
-        var options = this._map.options,
-            el = this._el;
-
-        for (var name in handlers) {
-            if (options[name]) this._map[name].disable();
-        }
-
-        el.removeEventListener('mousedown', this._onMouseDown);
-        el.removeEventListener('mouseup', this._onMouseUp);
-        el.removeEventListener('mousemove', this._onMouseMove);
-        el.removeEventListener('touchstart', this._onTouchStart);
-        el.removeEventListener('touchend', this._onTouchEnd);
-        el.removeEventListener('touchmove', this._onTouchMove);
-        el.removeEventListener('touchcancel', this._onTouchCancel);
-        el.removeEventListener('click', this._onClick);
-        el.removeEventListener('dblclick', this._onDblClick);
-        el.removeEventListener('contextmenu', this._onContextMenu);
-    },
-
-    _onMouseDown: function (e) {
-        this._map.stop();
-        this._startPos = DOM.mousePos(this._el, e);
-        this._fireMouseEvent('mousedown', e);
-    },
-
-    _onMouseUp: function (e) {
-        var map = this._map,
-            rotating = map.dragRotate && map.dragRotate.isActive();
-
-        if (this._contextMenuEvent && !rotating) {
-            this._fireMouseEvent('contextmenu', this._contextMenuEvent);
-        }
-
-        this._contextMenuEvent = null;
-        this._fireMouseEvent('mouseup', e);
-    },
-
-    _onMouseMove: function (e) {
-        var map = this._map,
-            el = this._el;
-
-        if (map.dragPan && map.dragPan.isActive()) return;
-        if (map.dragRotate && map.dragRotate.isActive()) return;
-
-        var target = e.toElement || e.target;
-        while (target && target !== el) target = target.parentNode;
-        if (target !== el) return;
-
-        this._fireMouseEvent('mousemove', e);
-    },
-
-    _onTouchStart: function (e) {
-        this._map.stop();
-        this._fireTouchEvent('touchstart', e);
-
-        if (!e.touches || e.touches.length > 1) return;
-
-        if (!this._tapped) {
-            this._tapped = setTimeout(this._onTouchTimeout, 300);
-
-        } else {
-            clearTimeout(this._tapped);
-            this._tapped = null;
-            this._fireMouseEvent('dblclick', e);
-        }
-    },
-
-    _onTouchMove: function (e) {
-        this._fireTouchEvent('touchmove', e);
-    },
-
-    _onTouchEnd: function (e) {
-        this._fireTouchEvent('touchend', e);
-    },
-
-    _onTouchCancel: function (e) {
-        this._fireTouchEvent('touchcancel', e);
-    },
-
-    _onTouchTimeout: function () {
-        this._tapped = null;
-    },
-
-    _onClick: function (e) {
-        var pos = DOM.mousePos(this._el, e);
-
-        if (pos.equals(this._startPos)) {
-            this._fireMouseEvent('click', e);
-        }
-    },
-
-    _onDblClick: function (e) {
-        this._fireMouseEvent('dblclick', e);
-        e.preventDefault();
-    },
-
-    _onContextMenu: function (e) {
-        this._contextMenuEvent = e;
-        e.preventDefault();
-    },
-
-    _fireMouseEvent: function (type, e) {
-        var pos = DOM.mousePos(this._el, e);
-
-        return this._map.fire(type, {
-            lngLat: this._map.unproject(pos),
-            point: pos,
-            originalEvent: e
-        });
-    },
-
-    _fireTouchEvent: function (type, e) {
-        var touches = DOM.touchPos(this._el, e),
-            singular = touches.reduce(function (prev, curr, i, arr) {
-                return prev.add(curr.div(arr.length));
-            }, new Point(0, 0));
-
-        return this._map.fire(type, {
-            lngLat: this._map.unproject(singular),
-            point: singular,
-            lngLats: touches.map(function(t) { return this._map.unproject(t); }, this),
-            points: touches,
-            originalEvent: e
-        });
-    }
-};
-
-
-/**
- * When an event {@link Evented.fire fires} as a result of a
- * user interaction, the event will be called with an EventData
- * object containing the original DOM event along with coordinates of
- * the event target.
- *
- * @typedef {Object} EventData
- * @property {Event} originalEvent The original DOM event
- * @property {Point} point The pixel location of the event
- * @property {LngLat} lngLat The geographic location of the event
- * @example
- * map.on('click', function(data) {
- *   var e = data && data.originalEvent;
- *   console.log('got click ' + (e ? 'button = ' + e.button : ''));
- * });
- */
-
-/**
- * Mouse down event.
- *
- * @event mousedown
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [mousedown event](https://developer.mozilla.org/en-US/docs/Web/Events/mousedown)
- */
-
-/**
- * Mouse up event.
- *
- * @event mouseup
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [mouseup event](https://developer.mozilla.org/en-US/docs/Web/Events/mouseup)
- */
-
-/**
- * Mouse move event.
- *
- * @event mousemove
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [mousemouse event](https://developer.mozilla.org/en-US/docs/Web/Events/mousemove)
- */
-
-/**
- * Touch start event.
- *
- * @event touchstart
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [touchstart event](https://developer.mozilla.org/en-US/docs/Web/Events/touchstart).
- */
-
-/**
- * Touch end event.
- *
- * @event touchend
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [touchcancel event](https://developer.mozilla.org/en-US/docs/Web/Events/touchcancel).
- */
-
-/**
- * Touch move event.
- *
- * @event touchmove
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [touchmove event](https://developer.mozilla.org/en-US/docs/Web/Events/touchmove).
- */
-
-/**
- * Touch cancel event.
- *
- * @event touchcancel
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [touchcancel event](https://developer.mozilla.org/en-US/docs/Web/Events/touchcancel).
- */
-
-/**
- * Click event.
- *
- * @event click
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data: a [click event](https://developer.mozilla.org/en-US/docs/Web/Events/click)
- */
-
-/**
- * Double click event.
- *
- * @event dblclick
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data
- */
-
-/**
- * Context menu event.
- *
- * @event contextmenu
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data, if available
- */
-
-/**
- * Load event. This event is emitted immediately after all necessary resources have been downloaded
- * and the first visually complete rendering has occurred.
- *
- * @event load
- * @memberof Map
- * @instance
- * @type {Object}
- */
-
-/**
- * Move start event. This event is emitted just before the map begins a transition from one
- * view to another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
- *
- * @event movestart
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data, if fired interactively
- */
-
-/**
- * Move event. This event is emitted repeatedly during animated transitions from one view to
- * another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
- *
- * @event move
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data, if fired interactively
- */
-
-/**
- * Move end event. This event is emitted just after the map completes a transition from one
- * view to another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
- *
- * @event moveend
- * @memberof Map
- * @instance
- * @property {EventData} data Original event data, if fired interactively
- */

js/ui/map.js
@@ -13,7 +13,7 @@ var Painter = require('../render/painter');
 var Transform = require('../geo/transform');
 var Hash = require('./hash');
 
-var Interaction = require('./interaction');
+var bindHandlers = require('./bind_handlers');
 
 var Camera = require('./camera');
 var LngLat = require('../geo/lng_lat');
@@ -23,6 +23,34 @@ var Attribution = require('./control/attribution');
 
 var defaultMinZoom = 0;
 var defaultMaxZoom = 20;
+var defaultOptions = {
+    center: [0, 0],
+    zoom: 0,
+    bearing: 0,
+    pitch: 0,
+
+    minZoom: defaultMinZoom,
+    maxZoom: defaultMaxZoom,
+
+    interactive: true,
+
+    scrollZoom: true,
+    boxZoom: true,
+    dragRotate: true,
+    dragPan: true,
+    keyboard: true,
+    doubleClickZoom: true,
+    touchZoomRotate: true,
+
+    bearingSnap: 7,
+
+    hash: false,
+
+    attributionControl: true,
+
+    failIfMajorPerformanceCaveat: false,
+    preserveDrawingBuffer: false
+};
 
 /**
  * Creates a map instance. This is usually the beginning of your map:
@@ -67,7 +95,7 @@ var defaultMaxZoom = 20;
  */
 var Map = module.exports = function(options) {
 
-    options = util.inherit(this.options, options);
+    options = util.extend({}, defaultOptions, options);
     this._interactive = options.interactive;
     this._failIfMajorPerformanceCaveat = options.failIfMajorPerformanceCaveat;
     this._preserveDrawingBuffer = options.preserveDrawingBuffer;
@@ -117,11 +145,15 @@ var Map = module.exports = function(options) {
         window.addEventListener('resize', this._onWindowResize, false);
     }
 
-    this.interaction = new Interaction(this);
-
-    if (options.interactive) {
-        this.interaction.enable();
-    }
+    bindHandlers(this, {
+        scrollZoom: options.interactive && options.scrollZoom,
+        boxZoom: options.interactive && options.boxZoom,
+        dragRotate: options.interactive && options.dragRotate,
+        dragPan: options.interactive && options.dragPan,
+        keyboard: options.interactive && options.keyboard,
+        doubleClickZoom: options.interactive && options.doubleClickZoom,
+        touchZoomRotate: options.interactive && options.touchZoomRotate
+    });
 
     this._hash = options.hash && (new Hash()).addTo(this);
     // don't set position from options if set through hash
@@ -148,35 +180,6 @@ var Map = module.exports = function(options) {
 util.extend(Map.prototype, Evented);
 util.extend(Map.prototype, Camera.prototype);
 util.extend(Map.prototype, /** @lends Map.prototype */{
-
-    options: {
-        center: [0, 0],
-        zoom: 0,
-        bearing: 0,
-        pitch: 0,
-
-        minZoom: defaultMinZoom,
-        maxZoom: defaultMaxZoom,
-
-        interactive: true,
-
-        scrollZoom: true,
-        boxZoom: true,
-        dragRotate: true,
-        dragPan: true,
-        keyboard: true,
-        doubleClickZoom: true,
-        touchZoomRotate: true,
-
-        bearingSnap: 7,
-
-        hash: false,
-
-        attributionControl: true,
-
-        failIfMajorPerformanceCaveat: false,
-        preserveDrawingBuffer: false
-    },
 
     /**
      * Adds a control to the map, calling `control.addTo(this)`.
@@ -1091,3 +1094,151 @@ function removeNode(node) {
   * @memberof Map
   * @instance
   */
+
+
+  /**
+   * When an event {@link Evented.fire fires} as a result of a
+   * user interaction, the event will be called with an EventData
+   * object containing the original DOM event along with coordinates of
+   * the event target.
+   *
+   * @typedef {Object} EventData
+   * @property {Event} originalEvent The original DOM event
+   * @property {Point} point The pixel location of the event
+   * @property {LngLat} lngLat The geographic location of the event
+   * @example
+   * map.on('click', function(data) {
+   *   var e = data && data.originalEvent;
+   *   console.log('got click ' + (e ? 'button = ' + e.button : ''));
+   * });
+   */
+
+  /**
+   * Mouse down event.
+   *
+   * @event mousedown
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [mousedown event](https://developer.mozilla.org/en-US/docs/Web/Events/mousedown)
+   */
+
+  /**
+   * Mouse up event.
+   *
+   * @event mouseup
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [mouseup event](https://developer.mozilla.org/en-US/docs/Web/Events/mouseup)
+   */
+
+  /**
+   * Mouse move event.
+   *
+   * @event mousemove
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [mousemouse event](https://developer.mozilla.org/en-US/docs/Web/Events/mousemove)
+   */
+
+  /**
+   * Touch start event.
+   *
+   * @event touchstart
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [touchstart event](https://developer.mozilla.org/en-US/docs/Web/Events/touchstart).
+   */
+
+  /**
+   * Touch end event.
+   *
+   * @event touchend
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [touchcancel event](https://developer.mozilla.org/en-US/docs/Web/Events/touchcancel).
+   */
+
+  /**
+   * Touch move event.
+   *
+   * @event touchmove
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [touchmove event](https://developer.mozilla.org/en-US/docs/Web/Events/touchmove).
+   */
+
+  /**
+   * Touch cancel event.
+   *
+   * @event touchcancel
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [touchcancel event](https://developer.mozilla.org/en-US/docs/Web/Events/touchcancel).
+   */
+
+  /**
+   * Click event.
+   *
+   * @event click
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data: a [click event](https://developer.mozilla.org/en-US/docs/Web/Events/click)
+   */
+
+  /**
+   * Double click event.
+   *
+   * @event dblclick
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data
+   */
+
+  /**
+   * Context menu event.
+   *
+   * @event contextmenu
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data, if available
+   */
+
+  /**
+   * Load event. This event is emitted immediately after all necessary resources have been downloaded
+   * and the first visually complete rendering has occurred.
+   *
+   * @event load
+   * @memberof Map
+   * @instance
+   * @type {Object}
+   */
+
+  /**
+   * Move start event. This event is emitted just before the map begins a transition from one
+   * view to another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
+   *
+   * @event movestart
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data, if fired interactively
+   */
+
+  /**
+   * Move event. This event is emitted repeatedly during animated transitions from one view to
+   * another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
+   *
+   * @event move
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data, if fired interactively
+   */
+
+  /**
+   * Move end event. This event is emitted just after the map completes a transition from one
+   * view to another, either as a result of user interaction or the use of methods such as `Map#jumpTo`.
+   *
+   * @event moveend
+   * @memberof Map
+   * @instance
+   * @property {EventData} data Original event data, if fired interactively
+   */

commit 9bcc8b32c293f40c21f80b9cbd1f05c3aa12d968

fix broken interactivity
Interactivity was broken by #2583 due to a typo.

js/ui/bind_handlers.js
@@ -21,7 +21,7 @@ module.exports = function bindHandlers(map, options) {
 
     for (var name in handlers) {
         map[name] = new handlers[name](map);
-        if (options.name) {
+        if (options[name]) {
             map[name].enable();
         }
     }

js/util/dom.js
@@ -5,6 +5,9 @@ exports.create = function (tagName, className, container) {
         offsetWidth: container.offsetWidth,
         offsetHeight: container.offsetHeight,
         remove: function () {},
-        addEventListener: function() {}
+        addEventListener: function() {},
+        classList: {
+            add: function () {}
+        }
     };
 };

test/js/ui/map.test.js
@@ -27,8 +27,15 @@ test('Map', function(t) {
     }
 
     t.test('constructor', function(t) {
-        var map = createMap();
+        var map = createMap({interactive: true});
         t.ok(map.getContainer());
+        t.ok(map.boxZoom.isEnabled());
+        t.ok(map.doubleClickZoom.isEnabled());
+        t.ok(map.dragPan.isEnabled());
+        t.ok(map.dragRotate.isEnabled());
+        t.ok(map.keyboard.isEnabled());
+        t.ok(map.scrollZoom.isEnabled());
+        t.ok(map.touchZoomRotate.isEnabled());
         t.end();
     });
 