5c9a7c3d1febc0306f5cc68de89c5bda4e9d9356
Lucas Wojciechowski <lucas@mapbox.com>
2016-05-18 15:15:14 -0700

    Implement mapbox-gl-test-suite API testing

--- test/suite_implementation.js
+++ test/suite_implementation.js
@@ -28,36 +28,60 @@ module.exports = function(style, options, callback) {
     var gl = map.painter.gl;
 
     map.once('load', function() {
-        var w = options.width * browser.devicePixelRatio,
-            h = options.height * browser.devicePixelRatio;
+        applyOperations(map, options.operations, function() {
+            var w = options.width * browser.devicePixelRatio;
+            var h = options.height * browser.devicePixelRatio;
 
-        var pixels = new Uint8Array(w * h * 4);
-        gl.readPixels(0, 0, w, h, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+            var pixels = new Uint8Array(w * h * 4);
+            gl.readPixels(0, 0, w, h, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
 
-        var data = new Buffer(pixels);
+            var data = new Buffer(pixels);
 
-        // Flip the scanlines.
-        var stride = w * 4;
-        var tmp = new Buffer(stride);
-        for (var i = 0, j = h - 1; i < j; i++, j--) {
-            var start = i * stride;
-            var end = j * stride;
-            data.copy(tmp, 0, start, start + stride);
-            data.copy(data, start, end, end + stride);
-            tmp.copy(data, end);
-        }
+            // Flip the scanlines.
+            var stride = w * 4;
+            var tmp = new Buffer(stride);
+            for (var i = 0, j = h - 1; i < j; i++, j--) {
+                var start = i * stride;
+                var end = j * stride;
+                data.copy(tmp, 0, start, start + stride);
+                data.copy(data, start, end, end + stride);
+                tmp.copy(data, end);
+            }
 
-        var results = options.queryGeometry ?
-            map.queryRenderedFeatures(options.queryGeometry, options) :
-            [];
+            var results = options.queryGeometry ?
+                map.queryRenderedFeatures(options.queryGeometry, options) :
+                [];
 
-        map.remove();
-        gl.destroy();
+            map.remove();
+            gl.destroy();
 
-        callback(null, data, results.map(function (r) {
-            r = r.toJSON();
-            delete r.layer;
-            return r;
-        }));
+            callback(null, data, results.map(function (feature) {
+                feature = feature.toJSON();
+                delete feature.layer;
+                return feature;
+            }));
+        });
     });
 };
+
+function applyOperations(map, operations, callback) {
+    var operation = operations && operations[0];
+
+    if (!operations || operations.length === 0) {
+        setTimeout(callback, 0);
+
+    } else if (operation[0] === 'wait') {
+        map.once(operation[1], function() {
+            applyOperations(map, operations.slice(1), callback);
+        });
+
+    } else {
+        try {
+            map[operation[0]].apply(map, operation.slice(1));
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+        applyOperations(map, operations.slice(1), callback);
+    }
+}
