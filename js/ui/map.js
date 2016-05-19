f46f70c96c6b29f2cf609975a13f2e215de8145b
Tom MacWright <tom@macwright.org>
2016-05-19 12:38:58 -0400

    Add note about addLayer relationship to the style spec

--- js/ui/map.js
+++ js/ui/map.js
@@ -642,7 +642,11 @@ util.extend(Map.prototype, /** @lends Map.prototype */{
 
     /**
      * Add a layer to the map style. The layer will be inserted before the layer with
-     * ID `before`, or appended if `before` is omitted.
+     * ID `before`, or appended if `before` is omitted. The layer
+     * needs to [align to the Mapbox GL Style Specification definition of a layer](https://www.mapbox.com/mapbox-gl-style-spec/#layers):
+     * it will usually refer to a source from which it pulls data, and provide
+     * instructions for how that data is styled.
+     *
      * @param {StyleLayer|Object} layer
      * @param {string=} before  ID of an existing layer to insert before
      * @fires layer.add
