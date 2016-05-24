121e4d6e16c02080274b1369037ae85e06798e2f
John Firebaugh <john.firebaugh@gmail.com>
2016-05-24 12:53:28 -0700

    Eliminate conditions from debug shader; not currently used by native

--- src/debug.fragment.glsl
+++ src/debug.fragment.glsl
@@ -6,23 +6,8 @@ precision mediump float;
 #define highp
 #endif
 
-#ifndef MAPBOX_GL_JS
-uniform vec4 u_color;
-uniform float u_blur;
-#endif
-
-#ifndef MAPBOX_GL_JS
-void main() {
-    float dist = length(gl_PointCoord - 0.5);
-    float t = smoothstep(0.5, 0.5 - u_blur, dist);
-#else
 uniform lowp vec4 u_color;
-#endif
 
-#ifndef MAPBOX_GL_JS
-    gl_FragColor = u_color * t;
-#else
 void main() {
     gl_FragColor = u_color;
-#endif
 }
--- src/debug.vertex.glsl
+++ src/debug.vertex.glsl
@@ -6,21 +6,10 @@ precision highp float;
 #define highp
 #endif
 
-#ifndef MAPBOX_GL_JS
-uniform mat4 u_matrix;
-uniform float u_size;
-#endif
-
 attribute vec2 a_pos;
 
-#ifndef MAPBOX_GL_JS
-void main(void) {
-    gl_Position = u_matrix * vec4(a_pos, 0, 1);
-    gl_PointSize = u_size;
-#else
 uniform mat4 u_matrix;
 
 void main() {
     gl_Position = u_matrix * vec4(a_pos, step(32767.0, a_pos.x), 1);
-#endif
 }
