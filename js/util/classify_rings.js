3d39142e43690fdcf06004459e610cc9e46424f6
Young Hahn <young@mapbox.com>
2016-05-26 17:00:19 -0400

    Drop smallest inner rings for earcut performance (#2622)
    * Add unit tests for existing classifyRings functionality
    
    * Add maxRings argument to classifyRings
    
    * Winding order fix, set a max ring threshold to 100 (for now)
    
    * Satisfy the eslint beast
    
    * Bump EARCUT_MAX_RINGS to 500
    
    * Use quickselect instead of sort in classifyRings
    
    * Refactoring
    
    * Respond to PR review
    
    Thanks to @yhahn, @jakepruitt and @mourner for all the help on this effort!

--- js/util/classify_rings.js
+++ js/util/classify_rings.js
@@ -1,10 +1,9 @@
 'use strict';
 
-module.exports = classifyRings;
+var quickselect = require('quickselect');
 
 // classifies an array of rings into polygons with outer rings and holes
-
-function classifyRings(rings) {
+module.exports = function classifyRings(rings, maxRings) {
     var len = rings.length;
 
     if (len <= 1) return [rings];
@@ -14,8 +13,10 @@ function classifyRings(rings) {
         ccw;
 
     for (var i = 0; i < len; i++) {
-        var area = signedArea(rings[i]);
+        var area = calculateSignedArea(rings[i]);
         if (area === 0) continue;
+
+        rings[i].area = Math.abs(area);
 
         if (ccw === undefined) ccw = area < 0;
 
@@ -29,10 +30,24 @@ function classifyRings(rings) {
     }
     if (polygon) polygons.push(polygon);
 
+    // Earcut performance degrages with the # of rings in a polygon. For this
+    // reason, we limit strip out all but the `maxRings` largest rings.
+    if (maxRings > 1) {
+        for (var j = 0; j < polygons.length; j++) {
+            if (polygons[j].length <= maxRings) continue;
+            quickselect(polygons[j], maxRings, 1, polygon.length - 1, compareAreas);
+            polygons[j] = polygon.slice(0, maxRings);
+        }
+    }
+
     return polygons;
+};
+
+function compareAreas(a, b) {
+    return b.area - a.area;
 }
 
-function signedArea(ring) {
+function calculateSignedArea(ring) {
     var sum = 0;
     for (var i = 0, len = ring.length, j = len - 1, p1, p2; i < len; j = i++) {
         p1 = ring[i];
