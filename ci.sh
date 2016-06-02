5e4465acad9c5b00e1876b197c9f3ea3a505dcd7
Lucas Wojciechowski <lucas@mapbox.com>
2016-06-02 16:46:49 -0700

    Revert "Remove all code coverage measurement"
    This reverts commit be6353aec120894c820a6259bd4e8ecfce853af4.

--- ci.sh
+++ ci.sh
@@ -6,11 +6,14 @@ set -o pipefail
 # add npm packages to $PATH
 export PATH=$(pwd)/node_modules/.bin:$PATH
 
+# set up code coverage instrumentation
+rm -rf coverage .nyc_output
+
 # run linters
 npm run lint
 
 # run unit tests
-tap --reporter dot test/js/*/*.js
+tap --reporter dot --coverage --no-coverage-report test/js/*/*.js
 
 # allow writing core files for render tests
 ulimit -c unlimited -S
@@ -19,7 +22,8 @@ echo '/proc/sys/kernel/core_pattern: '`cat /proc/sys/kernel/core_pattern`
 sysctl kernel.core_pattern
 
 # run render tests
-node test/render.test.js && node test/query.test.js
+istanbul cover --dir .nyc_output --include-pid --report none --print none test/render.test.js &&
+istanbul cover --dir .nyc_output --include-pid --report none --print none test/query.test.js
 EXIT_CODE=$?
 
 # collect core dumps from render tests
@@ -30,6 +34,10 @@ for DUMP in $(find ./ -maxdepth 1 -name 'core*' -print); do
     gdb `which node` ${DUMP} -ex "thread apply all bt" -ex "set pagination 0" -batch
     rm -rf ${DUMP}
 done
+
+# send coverage report to coveralls
+nyc report --reporter=lcov
+node ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
 
 # return original error code
 if [[ ${EXIT_CODE} != 0 ]]; then
