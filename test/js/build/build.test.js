fa2655d590415e3e893f446c930a2be9ccc4aa0d
Lucas Wojciechowski <lucas@mapbox.com>
2016-06-09 14:46:29 -0700

    Fix build test

--- test/js/build/build.test.js
+++ test/js/build/build.test.js
@@ -4,6 +4,6 @@ var test = require('tap').test;
 var fs = require('fs');
 
 test('production build removes asserts', function(t) {
-    t.assert(fs.readFileSync('dist/mapbox-gl.js', 'utf8').indexOf('assert') === -1);
+    t.assert(fs.readFileSync('dist/mapbox-gl.js', 'utf8').indexOf('assert(') === -1);
     t.end();
 });
