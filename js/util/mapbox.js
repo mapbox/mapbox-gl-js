b28e66fa1dccd9c1cf1256bb64d9c0e52359c450
xrwang <xiaowei@mapbox.com>
2016-06-09 12:14:26 -0700

    parse url for source and style

--- js/util/mapbox.js
+++ js/util/mapbox.js
@@ -12,14 +12,14 @@ function normalizeURL(inputUrl, pathPrefix, accessToken) {
         throw new Error('An API access token is required to use Mapbox GL. ' +
             'See https://www.mapbox.com/developers/api/#access-tokens');
     }
-    console.log('------------')
-    console.log(inputUrl);
-    console.log('------------')
+    // console.log('------------')
+    // console.log(inputUrl);
+    // console.log('------------')
     var httpsUrl = inputUrl.replace(/^mapbox:\/\//, config.API_URL + pathPrefix);
     httpsUrl += httpsUrl.indexOf('?') !== -1 ? '&access_token=' : '?access_token=';
-    console.log('========')
-    console.log(httpsUrl);
-    console.log('========')
+    // console.log('========')
+    // console.log(httpsUrl);
+    // console.log('========')
     if (config.REQUIRE_ACCESS_TOKEN) {
         if (accessToken[0] === 's') {
             throw new Error('Use a public access token (pk.*) with Mapbox GL JS, not a secret access token (sk.*). ' +
@@ -36,9 +36,23 @@ module.exports.normalizeStyleURL = function(inputUrl, accessToken) {
     if (!inputUrl.match(/^mapbox:\/\/styles\//))
         return inputUrl;
 
-    var parsedUrl = url.parse(inputUrl, true);
-    //out put something like this: mapbox://styles/v1/user/style?fresh=true
-    return normalizeURL(parsedUrl, '/styles/v1', accessToken);
+    var urlObject = url.parse(inputUrl, true);
+    var formattedQuery = '';
+
+    if (Object.getOwnPropertyNames(urlObject.query).length !== 0) {
+      formattedQuery = '?' + querystring.stringify(urlObject.query);
+    }
+
+    // var formattedQuery = '?' + querystring.stringify(urlObject.query);
+    var parsedUrl = urlObject.protocol + '/' + urlObject.pathname + formattedQuery;
+    // https://api.mapbox.com/styles/v1/user/style
+    // mapbox://user/style/draft?fresh=true
+    // console.log(urlObject)
+    // console.log('^^^^^^^^^^^')
+    // console.log(parsedUrl)
+    // console.log('^^^^^^^^^^^')
+    // THE OLD return normalizeURL('mapbox://' + user + '/' + style + draft, '/styles/v1/', accessToken);
+    return normalizeURL(parsedUrl, '/styles/v1/', accessToken);
 };
 
 module.exports.normalizeSourceURL = function(inputUrl, accessToken) {
@@ -46,10 +60,13 @@ module.exports.normalizeSourceURL = function(inputUrl, accessToken) {
         return inputUrl;
 
     var inputUrlJson = inputUrl + '.json';
-    var parsedUrl = url.parse(inputUrlJson);
+    var urlObject = url.parse(inputUrlJson);
 
-    parsedUrl.pathname = parsedUrl.hostname;
-
+    urlObject.pathname = urlObject.hostname;
+    // THE OLD return normalizeURL(url + '.json', '/v4/', accessToken) + '&secure';
+    // console.log(urlObject)
+    // console.log('^^^^^^^^^^^')
+    var parsedUrl = urlObject.protocol + '//' + urlObject.pathname;
     // TileJSON requests need a secure flag appended to their URLs so
     // that the server knows to send SSL-ified resource references.
     return normalizeURL(parsedUrl, '/v4/', accessToken) + '&secure';
@@ -60,7 +77,7 @@ module.exports.normalizeGlyphsURL = function(inputUrl, accessToken) {
         return inputUrl;
 
     var parsedUrl = url.parse(inputUrl, true);
-
+    // THE OLD return normalizeURL('mapbox://' + user + '/{fontstack}/{range}.pbf', '/fonts/v1/', accessToken);
     return normalizeURL(parsedUrl, '/fonts/v1', accessToken);
 };
 
@@ -71,6 +88,7 @@ module.exports.normalizeSpriteURL = function(inputUrl, format, ext, accessToken)
     var parsedUrl = url.parse(inputUrl, true);
 
     parsedUrl.pathname = parsedUrl.pathname + '/sprite' + format + ext;
+    //THE OLD return normalizeURL('mapbox://' + user + '/' + style + draft + '/sprite' + format + ext, '/styles/v1/', accessToken);
 
     return normalizeURL(parsedUrl, '/styles/v1', accessToken);
 };
