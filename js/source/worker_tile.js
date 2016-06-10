2e27d07bda4d6802c6724e2e75f00e3b4251cca5
Anand Thakker <vestibule@anandthakker.net>
2016-06-10 18:31:47 -0400

    Remove unused var

--- js/source/worker_tile.js
+++ js/source/worker_tile.js
@@ -182,9 +182,7 @@ WorkerTile.prototype.parse = function(data, layerFamilies, actor, rawTileData, c
     }
 
     function parseBucket(tile, bucket) {
-        var now = Date.now();
         bucket.populateArrays(collisionTile, stacks, icons);
-        var time = Date.now() - now;
 
 
         if (bucket.type !== 'symbol') {
