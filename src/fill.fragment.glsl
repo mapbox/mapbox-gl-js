03f799b19d27efd40eafafc857077ac6eb34b307
Lucas Wojciechowski <lucas@mapbox.com>
2016-06-13 17:15:07 -0700

    Use pragmas for fill & fill-outline opacity

--- src/fill.fragment.glsl
+++ src/fill.fragment.glsl
@@ -7,12 +7,13 @@ precision mediump float;
 #endif
 
 #pragma mapbox: define lowp vec4 color
-uniform lowp float u_opacity;
+#pragma mapbox: define lowp float opacity
 
 void main() {
     #pragma mapbox: initialize lowp vec4 color
+    #pragma mapbox: initialize lowp float opacity
 
-    gl_FragColor = color * u_opacity;
+    gl_FragColor = color * opacity;
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
