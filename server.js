57bb65648aad65bda8df24a56716fa77bca8b271
Lucas Wojciechowski <lucas@lucaswoj.com>
2016-06-14 11:51:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function downloadBenchData(callback) {
             http.get('http://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_land.geojson', function(response) {
                 response.pipe(file);
                 response.on('end', function() {
-                    console.log('done downloading bench data');
+                    console.log('done downloading benchmark data');
                     callback();
                 });
             });
