d9f135b1101ac54ad0c39580d086b911afdc9588
Anand Thakker <vestibule@anandthakker.net>
2016-06-28 13:30:33 -0400

    Add internal comments in Bucket and StructArray

--- js/data/bucket.js
+++ js/data/bucket.js
@@ -122,6 +122,20 @@ Bucket.prototype.populateBuffers = function() {
  * Check if there is enough space available in the current element group for
  * `vertexLength` vertices. If not, append a new elementGroup. Should be called
  * by `populateBuffers` and its callees.
+ *
+ * Element groups are added to this.arrayGroups[programName].
+ * An individual element group looks like:
+ * {
+ *     index: number,
+ *     layout: {
+ *         layout: VertexArrayType,
+ *         ?element: ElementArrayType,
+ *         ?element2: ElementArrayType
+ *     },
+ *     paint: { [layerName]: PaintVertexArrayType, ...  }
+ * }
+ *
+ *
  * @private
  * @param {string} programName the name of the program associated with the buffer that will receive the vertices
  * @param {number} vertexLength The number of vertices that will be inserted to the buffer.
@@ -163,6 +177,23 @@ Bucket.prototype.makeRoomFor = function(programName, numVertices) {
 /**
  * Start using a new shared `buffers` object and recreate instances of `Buffer`
  * as necessary.
+ *
+ * Sets up `this.arrayTypes` as:
+ * {
+ *     [programName]: {
+ *         layout: {
+ *             vertex: ArrayTypeType,
+ *             ?element: ArrayTypeType,
+ *             ?element2: ArrayTypeType
+ *         },
+ *         paint: { [layerName]: ArrayTypeType, ... }
+ *     },
+ *     ...
+ * }
+ *
+ * And `this.arrayGroups` as { [programName]: [], ... }; these get populated
+ * with array group structure over in `makeRoomFor`.
+ *
  * @private
  */
 Bucket.prototype.createArrays = function() {
