27029d8ef06821aa93aca93e04d88c2707232b0f
Lucas Wojciechowski <lucas@mapbox.com>
2016-07-14 14:58:36 -0700

    Move mapboxgl docs to mapbox-gl.js

--- js/ui/map.js
+++ js/ui/map.js
@@ -1218,34 +1218,6 @@ function removeNode(node) {
 }
 
 /**
- * Gets and sets the map's [access token](https://www.mapbox.com/help/define-access-token/).
- *
- * @var {string} accessToken
- * @example
- * mapboxgl.accessToken = myAccessToken;
- */
-
-/**
- * The version of Mapbox GL JS in use as specified in `package.json`,
- * `CHANGELOG.md`, and the GitHub release.
- *
- * @var {string} version
- */
-
-/**
- * Returns a Boolean indicating whether the browser [supports Mapbox GL JS](https://www.mapbox.com/help/mapbox-browser-support/#mapbox-gl-js).
- *
- * @function supported
- * @param {Object} options
- * @param {boolean} [options.failIfMajorPerformanceCaveat=false] If `true`,
- *   the function will return `false` if the performance of Mapbox GL JS would
- *   be dramatically worse than expected (i.e. a software renderer would be used).
- * @return {boolean}
- * @example
- * mapboxgl.supported() // = true
- */
-
-/**
  * A [`LngLat`](#LngLat) object or an array of two numbers representing longitude and latitude.
  *
  * @typedef {(LngLat | Array<number>)} LngLatLike
