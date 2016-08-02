e601b81c4ad0d122eff402fbcedd8f95c3141f73
Zack Mully <zmully@users.noreply.github.com>
2016-08-02 14:52:26 -0400

    deploy to CN regions (#2915)

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,19 @@ if [ -z $TAG ]; then
 fi
 
 function upload {
-    aws s3 cp --acl public-read --content-type $2 dist/$1 s3://mapbox-gl-js/$TAG/$1
-    echo "upload: dist/$1 to s3://mapbox-gl-js/$TAG/$1"
+  aws s3 cp --acl public-read --content-type $2 dist/$1 s3://mapbox-gl-js/$TAG/$1
+  echo "upload: dist/$1 to s3://mapbox-gl-js/$TAG/$1"
+}
+
+cnregions="
+cn-north-1
+"
+
+function cn_upload {
+  for region in cnregions; do
+    aws s3 cp --region $region --acl public-read --content-type $2 dist/$1 s3://mapbox-gl-js-$region/$TAG/$1
+    echo "upload: dist/$1 to s3://mapbox-gl-js-$region/$TAG/$1"
+  done
 }
 
 npm run build-dev
@@ -21,3 +32,10 @@ upload mapbox-gl.js     application/javascript
 upload mapbox-gl.js.map application/octet-stream
 upload mapbox-gl-dev.js application/javascript
 upload mapbox-gl.css    text/css
+
+export AWS_ACCESS_KEY_ID=$AWSCN_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWSCN_SECRET_ACCESS_KEY
+cn_upload mapbox-gl.js     application/javascript
+cn_upload mapbox-gl.js.map application/octet-stream
+cn_upload mapbox-gl-dev.js application/javascript
+cn_upload mapbox-gl.css    text/css
