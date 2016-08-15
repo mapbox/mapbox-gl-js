b63fa24aec057cd2f66a7b689b1c718f88eff5ea
xrwang <xiaowei@mapbox.com>
2016-08-15 10:40:22 -0700

    no unused vars

--- js/symbol/shaping.js
+++ js/symbol/shaping.js
@@ -114,10 +114,6 @@ function linewrapLong(shaping, glyphs, lineHeight, maxWidth, horizontalAlign, ve
 
     if (maxWidth) {
 
-        var wordLength = positionedGlyphs.length;
-
-        // lastSafeBreak = Math.round(wordLength/2);
-
         for (var i = 0; i < positionedGlyphs.length; i++) {
             var positionedGlyph = positionedGlyphs[i];
 
@@ -183,8 +179,6 @@ function linewrap(shaping, glyphs, lineHeight, maxWidth, horizontalAlign, vertic
     if (maxWidth) {
 
         var wordLength = positionedGlyphs.length;
-
-        // lastSafeBreak = Math.round(wordLength/2);
 
         for (var i = 0; i < positionedGlyphs.length; i++) {
             var positionedGlyph = positionedGlyphs[i];
