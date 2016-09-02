af0c73e39788ee706fc31ecfbe55d335dc9c33ef
Lucas Wojciechowski <lucas@mapbox.com>
2016-09-02 16:36:52 -0700

    Rebase cleanup in window :(

--- js/util/window.js
+++ js/util/window.js
@@ -5,42 +5,30 @@ var gl = require('gl');
 var sinon = require('sinon');
 var util = require('./util');
 
-var window = module.exports;
-
 function restore() {
-    window.close();
 
+    // Remove previous window from module.exports
+    var previousWindow = module.exports;
+    if (previousWindow.close) previousWindow.close();
+    for (var key in previousWindow) if (previousWindow.hasOwnProperty(key)) delete previousWindow[key];
+
+    // Create new window and inject into module.exports
     var window = jsdom.jsdom(undefined, {
         // Send jsdom console output to the node console object.
         virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }).defaultView;
+    util.extend(module.exports, window);
 
-    for (var key in window) if (window.hasOwnProperty(key)) delete window[key];
-    util.extend(window, jsdom.jsdom().defaultView);
+    window.devicePixelRatio = 1;
 
     window.requestAnimationFrame = function(callback) {
         return setImmediate(callback, 0);
     };
-
     window.cancelAnimationFrame = clearImmediate;
 
-    window.devicePixelRatio = 1;
-
-    sinon.xhr.supportsCORS = true;
-    window.server = sinon.fakeServer.create();
-    window.XMLHttpRequest = window.server.xhr;
-
-    window.restore = restore;
-
     // Stub some CSSOM-related properties that jsdom doesn't implement.
-    Object.defineProperties(window.HTMLElement.prototype, {
-        clientLeft: {
-            get: function() { return 0; }
-        },
-        clientTop: {
-            get: function() { return 0; }
-        }
-    });
+    window.HTMLElement.prototype.clientLeft = 0;
+    window.HTMLElement.prototype.clientTop = 0;
 
     window.HTMLCanvasElement.prototype.getContext = function(type, attributes) {
         if (!this._webGLContext) {
@@ -57,6 +45,7 @@ function restore() {
 
     window.restore = restore;
 
+    return window;
 }
 
-restore();
+module.exports = restore();
