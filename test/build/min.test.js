61e1dd622a6233f8794f5f2da86246e32add4967
Étienne Tétreault-Pinard <etienne.t.pinard@gmail.com>
2016-09-12 15:19:24 -0400

    use package-json-versionify to trim package.json in min bundle

--- test/build/min.test.js
+++ test/build/min.test.js
@@ -2,8 +2,16 @@
 
 var test = require('tap').test;
 var fs = require('fs');
+var pkg = require('../../package.json');
+
+var minBundle = fs.readFileSync('dist/mapbox-gl.js', 'utf8');
 
 test('production build removes asserts', function(t) {
-    t.assert(fs.readFileSync('dist/mapbox-gl.js', 'utf8').indexOf('assert(') === -1);
+    t.assert(minBundle.indexOf('assert(') === -1);
+    t.end();
+});
+
+test('trims package.json assets', function(t) {
+    t.assert(minBundle.indexOf('module.exports={"version":"' + pkg.version + '"}') !== -1);
     t.end();
 });
