6e707873a6e308d439ddbf325072b722d7f8e759
John Firebaugh <john.firebaugh@gmail.com>
2016-09-19 11:07:07 -0700

    Generate markdown with remark

--- docs/_generate/generate.js
+++ docs/_generate/generate.js
@@ -2,20 +2,29 @@
 
 // GL style reference generator
 
-var fs = require('fs'),
-    path = require('path'),
-    ref = require('../..').latest,
-    _ = require('underscore');
+var fs = require('fs');
+var path = require('path');
+var ref = require('../..').latest;
+var _ = require('lodash');
+var remark = require('remark');
+var html = require('remark-html');
 
-function tmpl(x) {
-  return _.template(fs.readFileSync(path.join(__dirname, x), 'utf-8'));
+function tmpl(x, options) {
+  return _.template(fs.readFileSync(path.join(__dirname, x), 'utf-8'), options);
 }
 
-var index = tmpl('index.html');
-var item = tmpl('item.html');
+var index = tmpl('index.html', {
+  imports: {
+    _: _,
+    item: tmpl('item.html', {
+      imports: {
+        _: _,
+        md: function(markdown) {
+          return remark().use(html).process(markdown);
+        }
+      }
+    })
+  }
+});
 
-fs.writeFileSync(path.join(__dirname, '../index.html'), index({
-  ref: ref,
-  item: item,
-  _: _
-}));
+fs.writeFileSync(path.join(__dirname, '../index.html'), index({ ref: ref }));
