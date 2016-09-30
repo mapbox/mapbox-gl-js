e5d8f5fc79914a55f5ac0557817843c452bee1ca
Vladimir Agafonkin <agafonkin@gmail.com>
2016-10-01 00:32:45 +0300

    Fix text fields with EOL at the end (#3281)
    * fix text fields with EOL at the end
    
    * better test for text field trimming

--- test/js/symbol/shaping.test.js
+++ test/js/symbol/shaping.test.js
@@ -57,5 +57,10 @@ test('shaping', function(t) {
     shaped = shaping.shapeText(String.fromCharCode(0), glyphs, 15 * oneEm, oneEm, 0.5, 0.5, 0.5, 0 * oneEm, [0, 0]);
     t.equal(false, shaped);
 
+    // https://github.com/mapbox/mapbox-gl-js/issues/3254
+    shaped = shaping.shapeText('   foo bar\n', glyphs, 15 * oneEm, oneEm, 0.5, 0.5, 0.5, 0 * oneEm, [0, 0]);
+    var shaped2 = shaping.shapeText('foo bar', glyphs, 15 * oneEm, oneEm, 0.5, 0.5, 0.5, 0 * oneEm, [0, 0]);
+    t.same(shaped.positionedGlyphs, shaped2.positionedGlyphs);
+
     t.end();
 });
