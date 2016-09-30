7f62a4fc9f21e619824d68abbc4b03cbc1685572
Justin R. Miller <incanus@users.noreply.github.com>
2016-09-30 14:51:28 -0700

    fixes #497: document enum values (#510)
    Adds `doc` property to all enum `values`, moving them from JSON arrays to objects. 
    
    Downstream mobile SDK work in mapbox/mapbox-gl-native#6508.
    Downstream Studio work in mapbox/studio#7765. 
    
    Fixes a typo in `icon-text-fit`. 
    
    Also updates validation, docs generation, and tests.

--- test/spec.js
+++ test/spec.js
@@ -71,15 +71,30 @@ function validSchema(k, t, obj, ref) {
     // schema type must be js native, 'color', or present in ref root object.
     t.ok(types.indexOf(obj.type) !== -1, k + '.type (' + obj.type + ')');
 
-    // schema type is an enum, it must have 'values' and they must be scalars.
+    // schema type is an enum, it must have 'values' and they must be
+    // objects (>=v8) or scalars (<=v7). If objects, check that doc key
+    // (if present) is a string.
     if (obj.type === 'enum') {
-      t.ok(Array.isArray(obj.values) && obj.values.every(function(v) {
+      var values = (ref.$version >= 8 ? Object.keys(obj.values) : obj.values);
+      t.ok(Array.isArray(values) && values.every(function(v) {
         return scalar.indexOf(typeof v) !== -1;
-      }), k + '.values [' + obj.values +']');
+      }), k + '.values [' + values +']');
+      if (ref.$version >= 8) {
+        for (var v in obj.values) {
+          if (Array.isArray(obj.values) === false) { // skips $root.version
+            if (obj.values[v].doc !== undefined) {
+              t.equal('string', typeof obj.values[v].doc, k + '.doc (string)');
+              if (minified) t.fail('minified file should not have ' + k + '.doc');
+            } else {
+              if (t.name === 'latest') t.fail('doc missing for ' + k);
+            }
+          }
+        }
+      }
     }
 
     // schema type is array, it must have 'value' and it must be a type.
-    if (obj.value !== undefined)
+    if (obj.value !== undefined) {
       if (Array.isArray(obj.value)) {
         obj.value.forEach(function(i) {
           t.ok(types.indexOf(i) !== -1, k + '.value (' + i + ')');
@@ -89,31 +104,44 @@ function validSchema(k, t, obj, ref) {
       } else {
         t.ok(types.indexOf(obj.value) !== -1, k + '.value (' + obj.value + ')');
       }
+    }
 
-      // schema key type checks
-      if (obj.doc !== undefined) {
-        t.equal('string', typeof obj.doc, k + '.doc (string)');
-        if (minified) t.fail('minified file should not have ' + k + '.doc');
+    // schema key doc checks
+    if (obj.doc !== undefined) {
+      t.equal('string', typeof obj.doc, k + '.doc (string)');
+      if (minified) t.fail('minified file should not have ' + k + '.doc');
+    } else {
+      if (t.name === 'latest') t.fail('doc missing for ' + k);
+    }
+
+    // schema key example checks
+    if (minified && obj.example !== undefined) {
+      t.fail('minified file should not have ' + k + '.example');
+    }
+
+    // schema key function checks
+    if (obj.function !== undefined) {
+      if (ref.$version >= 7) {
+        t.equal(true, ['interpolated', 'piecewise-constant'].indexOf(obj.function) >= 0, 'function: ' + obj.function);
       } else {
-        if (t.name === 'latest') t.fail('doc missing for ' + k);
+        t.equal('boolean', typeof obj.function, k + '.required (boolean)');
       }
-      if (minified && obj.example !== undefined) {
-        t.fail('minified file should not have ' + k + '.example');
-      }
-      if (obj.function !== undefined) {
-        if (ref.$version >= 7) {
-          t.equal(true, ['interpolated', 'piecewise-constant'].indexOf(obj.function) >= 0, 'function: ' + obj.function);
-        } else {
-          t.equal('boolean', typeof obj.function, k + '.required (boolean)');
-        }
-      }
-      if (obj.required !== undefined)
-        t.equal('boolean', typeof obj.required, k + '.required (boolean)');
-      if (obj.transition !== undefined)
-        t.equal('boolean', typeof obj.transition, k + '.transition (boolean)');
-      if (obj.requires !== undefined)
-        t.equal(true, Array.isArray(obj.requires), k + '.requires (array)')
-      // Array of schema objects or references.
+    }
+
+    // schema key required checks
+    if (obj.required !== undefined) {
+      t.equal('boolean', typeof obj.required, k + '.required (boolean)');
+    }
+
+    // schema key transition checks
+    if (obj.transition !== undefined) {
+      t.equal('boolean', typeof obj.transition, k + '.transition (boolean)');
+    }
+
+    // schema key requires checks
+    if (obj.requires !== undefined) {
+      t.equal(true, Array.isArray(obj.requires), k + '.requires (array)');
+    }
   } else if (Array.isArray(obj)) {
     obj.forEach(function(child, j) {
       if (typeof child === 'string' && scalar.indexOf(child) !== -1) return;
