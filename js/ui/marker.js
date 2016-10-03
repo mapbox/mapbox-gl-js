ab97029e48b7c519039dbb8c61b7d0769a285d41
Molly Lloyd <molly@mapbox.com>
2016-10-03 16:59:42 -0700

    fix bug causing error to be thrown when calling Marker#setLngLat on a marker thats already on the map. Add tests that would have caught the bug.

--- js/ui/marker.js
+++ js/ui/marker.js
@@ -43,9 +43,9 @@ Marker.prototype = {
         this.remove();
         this._map = map;
         map.getCanvasContainer().appendChild(this._element);
-        map.on('move', this._update.bind(this));
-        map.on('moveend', this._update.bind(this, {roundPos: true}));
-        this._update({roundPos: true});
+        map.on('move', this._update);
+        map.on('moveend', this._update);
+        this._update();
 
         // If we attached the `click` listener to the marker element, the popup
         // would close once the event propogated to `map` due to the
@@ -66,6 +66,7 @@ Marker.prototype = {
         if (this._map) {
             this._map.off('click', this._onMapClick);
             this._map.off('move', this._update);
+            this._map.on('moveend', this._update);
             this._map = null;
         }
         DOM.remove(this._element);
@@ -149,10 +150,13 @@ Marker.prototype = {
         else popup.addTo(this._map);
     },
 
-    _update: function (options) {
+    _update: function (e) {
         if (!this._map) return;
         var pos = this._map.project(this._lngLat)._add(this._offset);
-        if (options.roundPos) pos = pos.round();
+        // because rouding the coordinates at every `move` event causes stuttered zooming
+        // we only round them when _update is called with `moveend` or when its called with 
+        // no arguments (when the Marker is initialized or Marker#setLngLat is invoked).
+        if (!e || e.type === "moveend") pos = pos.round();
         DOM.setTransform(this._element, 'translate(' + pos.x + 'px,' + pos.y + 'px)');
     }
 };
