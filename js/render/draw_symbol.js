53b45358a577398907904640a0178399b2655e11
Young Hahn <young@mapbox.com>
2016-10-04 20:33:00 -0400

    Implement symbol depth testing for symbols with pitch-alignment: map (#3243)
    * Sketch of symbol depth test implementation in GL JS modeled after GL native
    
    * Reduce setDepthSublayer calls
    
    * Bump test suite

--- js/render/draw_symbol.js
+++ js/render/draw_symbol.js
@@ -26,37 +26,33 @@ function drawSymbols(painter, sourceCache, layer, coords) {
         gl.enable(gl.STENCIL_TEST);
     }
 
-    painter.setDepthSublayer(0);
-    painter.depthMask(false);
-    gl.disable(gl.DEPTH_TEST);
-
     drawLayerSymbols(painter, sourceCache, layer, coords, false,
-            layer.paint['icon-translate'],
-            layer.paint['icon-translate-anchor'],
-            layer.layout['icon-rotation-alignment'],
-            // icon-pitch-alignment is not yet implemented
-            // and we simply inherit the rotation alignment
-            layer.layout['icon-rotation-alignment'],
-            layer.layout['icon-size'],
-            layer.paint['icon-halo-width'],
-            layer.paint['icon-halo-color'],
-            layer.paint['icon-halo-blur'],
-            layer.paint['icon-opacity'],
-            layer.paint['icon-color']);
+        layer.paint['icon-translate'],
+        layer.paint['icon-translate-anchor'],
+        layer.layout['icon-rotation-alignment'],
+        // icon-pitch-alignment is not yet implemented
+        // and we simply inherit the rotation alignment
+        layer.layout['icon-rotation-alignment'],
+        layer.layout['icon-size'],
+        layer.paint['icon-halo-width'],
+        layer.paint['icon-halo-color'],
+        layer.paint['icon-halo-blur'],
+        layer.paint['icon-opacity'],
+        layer.paint['icon-color']
+    );
 
     drawLayerSymbols(painter, sourceCache, layer, coords, true,
-            layer.paint['text-translate'],
-            layer.paint['text-translate-anchor'],
-            layer.layout['text-rotation-alignment'],
-            layer.layout['text-pitch-alignment'],
-            layer.layout['text-size'],
-            layer.paint['text-halo-width'],
-            layer.paint['text-halo-color'],
-            layer.paint['text-halo-blur'],
-            layer.paint['text-opacity'],
-            layer.paint['text-color']);
-
-    gl.enable(gl.DEPTH_TEST);
+        layer.paint['text-translate'],
+        layer.paint['text-translate-anchor'],
+        layer.layout['text-rotation-alignment'],
+        layer.layout['text-pitch-alignment'],
+        layer.layout['text-size'],
+        layer.paint['text-halo-width'],
+        layer.paint['text-halo-color'],
+        layer.paint['text-halo-blur'],
+        layer.paint['text-opacity'],
+        layer.paint['text-color']
+    );
 
     if (sourceCache.map.showCollisionBoxes) {
         drawCollisionDebug(painter, sourceCache, layer, coords);
@@ -74,6 +70,15 @@ function drawLayerSymbols(painter, sourceCache, layer, coords, isText,
         haloBlur,
         opacity,
         color) {
+
+    var gl = painter.gl;
+    painter.setDepthSublayer(0);
+    painter.depthMask(false);
+    if (pitchAlignment === 'map') {
+        gl.enable(gl.DEPTH_TEST);
+    } else {
+        gl.disable(gl.DEPTH_TEST);
+    }
 
     for (var j = 0; j < coords.length; j++) {
         var tile = sourceCache.getTile(coords[j]);
@@ -98,6 +103,8 @@ function drawLayerSymbols(painter, sourceCache, layer, coords, isText,
                 opacity,
                 color);
     }
+
+    gl.enable(gl.DEPTH_TEST);
 }
 
 function drawSymbol(painter, layer, posMatrix, tile, bucket, bufferGroups, isText, sdf, iconsNeedLinear, adjustedSize, fontstack,
