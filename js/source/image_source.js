caa12163349d9a5ef31d055453a381dd0b1d2a7e
Vladimir Agafonkin <agafonkin@gmail.com>
2016-10-13 01:40:50 +0300

    DRY up VideoSource code (#3353)

--- js/source/image_source.js
+++ js/source/image_source.js
@@ -131,23 +131,23 @@ ImageSource.prototype = util.inherit(Evented, /** @lends ImageSource.prototype *
     },
 
     prepare: function() {
-        if (!this._loaded || !this.image || !this.image.complete) return;
-        if (!this.tile) return;
+        if (!this.tile || !this._loaded || !this.image || !this.image.complete) return;
+        this._prepareImage(this.map.painter.gl, this.image);
+    },
 
-        var painter = this.map.painter;
-        var gl = painter.gl;
-
+    _prepareImage: function (gl, image) {
         if (!this._prepared) {
+            this._prepared = true;
             this.tile.texture = gl.createTexture();
             gl.bindTexture(gl.TEXTURE_2D, this.tile.texture);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
         } else {
             gl.bindTexture(gl.TEXTURE_2D, this.tile.texture);
-            gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+            gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
         }
     },
 
