4e1d93c465eb7a9e302c7461a1c3e9a156fde8a5
Vladimir Agafonkin <agafonkin@gmail.com>
2016-10-19 18:56:41 +0300

    convert SourceCache, Tile, TileCoord, WorkerTile to ES6 class syntax

--- js/source/tile_coord.js
+++ js/source/tile_coord.js
@@ -4,110 +4,90 @@ const assert = require('assert');
 const WhooTS = require('whoots-js');
 const Coordinate = require('../geo/coordinate');
 
-module.exports = TileCoord;
+class TileCoord {
+    constructor(z, x, y, w) {
+        assert(!isNaN(z) && z >= 0 && z % 1 === 0);
+        assert(!isNaN(x) && x >= 0 && x % 1 === 0);
+        assert(!isNaN(y) && y >= 0 && y % 1 === 0);
 
-function TileCoord(z, x, y, w) {
-    assert(!isNaN(z) && z >= 0 && z % 1 === 0);
-    assert(!isNaN(x) && x >= 0 && x % 1 === 0);
-    assert(!isNaN(y) && y >= 0 && y % 1 === 0);
+        if (isNaN(w)) w = 0;
 
-    if (isNaN(w)) w = 0;
+        this.z = +z;
+        this.x = +x;
+        this.y = +y;
+        this.w = +w;
 
-    this.z = +z;
-    this.x = +x;
-    this.y = +y;
-    this.w = +w;
+        // calculate id
+        w *= 2;
+        if (w < 0) w = w * -1 - 1;
+        const dim = 1 << this.z;
+        this.id = ((dim * dim * w + dim * this.y + this.x) * 32) + this.z;
 
-    // calculate id
-    w *= 2;
-    if (w < 0) w = w * -1 - 1;
-    const dim = 1 << this.z;
-    this.id = ((dim * dim * w + dim * this.y + this.x) * 32) + this.z;
+        // for caching pos matrix calculation when rendering
+        this.posMatrix = null;
+    }
 
-    // for caching pos matrix calculation when rendering
-    this.posMatrix = null;
+    toString() {
+        return `${this.z}/${this.x}/${this.y}`;
+    }
+
+    toCoordinate(sourceMaxZoom) {
+        const zoom = Math.min(this.z, sourceMaxZoom);
+        const tileScale = Math.pow(2, zoom);
+        const row = this.y;
+        const column = this.x + tileScale * this.w;
+        return new Coordinate(column, row, zoom);
+    }
+
+    // given a list of urls, choose a url template and return a tile URL
+    url(urls, sourceMaxZoom, scheme) {
+        const bbox = WhooTS.getTileBBox(this.x, this.y, this.z);
+        const quadkey = getQuadkey(this.z, this.x, this.y);
+
+        return urls[(this.x + this.y) % urls.length]
+            .replace('{prefix}', (this.x % 16).toString(16) + (this.y % 16).toString(16))
+            .replace('{z}', Math.min(this.z, sourceMaxZoom || this.z))
+            .replace('{x}', this.x)
+            .replace('{y}', scheme === 'tms' ? (Math.pow(2, this.z) - this.y - 1) : this.y)
+            .replace('{quadkey}', quadkey)
+            .replace('{bbox-epsg-3857}', bbox);
+    }
+
+    // Return the coordinate of the parent tile
+    parent(sourceMaxZoom) {
+        if (this.z === 0) return null;
+
+        // the id represents an overscaled tile, return the same coordinates with a lower z
+        if (this.z > sourceMaxZoom) {
+            return new TileCoord(this.z - 1, this.x, this.y, this.w);
+        }
+
+        return new TileCoord(this.z - 1, Math.floor(this.x / 2), Math.floor(this.y / 2), this.w);
+    }
+
+    wrapped() {
+        return new TileCoord(this.z, this.x, this.y, 0);
+    }
+
+    // Return the coordinates of the tile's children
+    children(sourceMaxZoom) {
+
+        if (this.z >= sourceMaxZoom) {
+            // return a single tile coord representing a an overscaled tile
+            return [new TileCoord(this.z + 1, this.x, this.y, this.w)];
+        }
+
+        const z = this.z + 1;
+        const x = this.x * 2;
+        const y = this.y * 2;
+        return [
+            new TileCoord(z, x, y, this.w),
+            new TileCoord(z, x + 1, y, this.w),
+            new TileCoord(z, x, y + 1, this.w),
+            new TileCoord(z, x + 1, y + 1, this.w)
+        ];
+    }
 }
-
-TileCoord.prototype.toString = function() {
-    return `${this.z}/${this.x}/${this.y}`;
-};
-
-TileCoord.prototype.toCoordinate = function(sourceMaxZoom) {
-    const zoom = Math.min(this.z, sourceMaxZoom);
-    const tileScale = Math.pow(2, zoom);
-    const row = this.y;
-    const column = this.x + tileScale * this.w;
-    return new Coordinate(column, row, zoom);
-};
-
-// Parse a packed integer id into a TileCoord object
-TileCoord.fromID = function(id) {
-    const z = id % 32, dim = 1 << z;
-    const xy = ((id - z) / 32);
-    const x = xy % dim, y = ((xy - x) / dim) % dim;
-    let w = Math.floor(xy / (dim * dim));
-    if (w % 2 !== 0) w = w * -1 - 1;
-    w /= 2;
-    return new TileCoord(z, x, y, w);
-};
-
-function getQuadkey(z, x, y) {
-    let quadkey = '', mask;
-    for (let i = z; i > 0; i--) {
-        mask = 1 << (i - 1);
-        quadkey += ((x & mask ? 1 : 0) + (y & mask ? 2 : 0));
-    }
-    return quadkey;
-}
-
-// given a list of urls, choose a url template and return a tile URL
-TileCoord.prototype.url = function(urls, sourceMaxZoom, scheme) {
-    const bbox = WhooTS.getTileBBox(this.x, this.y, this.z);
-    const quadkey = getQuadkey(this.z, this.x, this.y);
-
-    return urls[(this.x + this.y) % urls.length]
-        .replace('{prefix}', (this.x % 16).toString(16) + (this.y % 16).toString(16))
-        .replace('{z}', Math.min(this.z, sourceMaxZoom || this.z))
-        .replace('{x}', this.x)
-        .replace('{y}', scheme === 'tms' ? (Math.pow(2, this.z) - this.y - 1) : this.y)
-        .replace('{quadkey}', quadkey)
-        .replace('{bbox-epsg-3857}', bbox);
-};
-
-// Return the coordinate of the parent tile
-TileCoord.prototype.parent = function(sourceMaxZoom) {
-    if (this.z === 0) return null;
-
-    // the id represents an overscaled tile, return the same coordinates with a lower z
-    if (this.z > sourceMaxZoom) {
-        return new TileCoord(this.z - 1, this.x, this.y, this.w);
-    }
-
-    return new TileCoord(this.z - 1, Math.floor(this.x / 2), Math.floor(this.y / 2), this.w);
-};
-
-TileCoord.prototype.wrapped = function() {
-    return new TileCoord(this.z, this.x, this.y, 0);
-};
-
-// Return the coordinates of the tile's children
-TileCoord.prototype.children = function(sourceMaxZoom) {
-
-    if (this.z >= sourceMaxZoom) {
-        // return a single tile coord representing a an overscaled tile
-        return [new TileCoord(this.z + 1, this.x, this.y, this.w)];
-    }
-
-    const z = this.z + 1;
-    const x = this.x * 2;
-    const y = this.y * 2;
-    return [
-        new TileCoord(z, x, y, this.w),
-        new TileCoord(z, x + 1, y, this.w),
-        new TileCoord(z, x, y + 1, this.w),
-        new TileCoord(z, x + 1, y + 1, this.w)
-    ];
-};
 
 // Taken from polymaps src/Layer.js
 // https://github.com/simplegeo/polymaps/blob/master/src/Layer.js#L333-L383
@@ -190,3 +170,25 @@ TileCoord.cover = function(z, bounds, actualZ) {
         return t[id];
     });
 };
+
+// Parse a packed integer id into a TileCoord object
+TileCoord.fromID = function(id) {
+    const z = id % 32, dim = 1 << z;
+    const xy = ((id - z) / 32);
+    const x = xy % dim, y = ((xy - x) / dim) % dim;
+    let w = Math.floor(xy / (dim * dim));
+    if (w % 2 !== 0) w = w * -1 - 1;
+    w /= 2;
+    return new TileCoord(z, x, y, w);
+};
+
+function getQuadkey(z, x, y) {
+    let quadkey = '', mask;
+    for (let i = z; i > 0; i--) {
+        mask = 1 << (i - 1);
+        quadkey += ((x & mask ? 1 : 0) + (y & mask ? 2 : 0));
+    }
+    return quadkey;
+}
+
+module.exports = TileCoord;
