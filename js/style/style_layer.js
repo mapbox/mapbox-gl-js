1f6f728516901ff660555ed82afffd528f4149c0
John Firebaugh <john.firebaugh@gmail.com>
2016-10-24 12:31:26 -0700

    Add CircleStyleLayer

--- js/style/style_layer.js
+++ js/style/style_layer.js
@@ -332,6 +332,7 @@ class StyleLayer extends Evented {
 module.exports = StyleLayer;
 
 const subclasses = {
+    'circle': require('./style_layer/circle_style_layer'),
     'fill': require('./style_layer/fill_style_layer'),
     'line': require('./style_layer/line_style_layer'),
     'symbol': require('./style_layer/symbol_style_layer')
---
+++ js/style/style_layer/circle_style_layer.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const StyleLayer = require('../style_layer');
+
+class CircleStyleLayer extends StyleLayer {
+}
+
+module.exports = CircleStyleLayer;
