5257a32d2528bd4660cceb1928ff8e20cb17687f
Lucas Wojciechowski <lucas@mapbox.com>
2016-10-25 15:02:55 -0700

    Switch from regex to nested "if" statements

--- js/util/script_detection.js
+++ js/util/script_detection.js
@@ -1,18 +1,45 @@
 'use strict';
 
-const ideographicBreakingRegExp = new RegExp(`^(${[
-    '[一-鿌]',
-    '[㐀-䶵]',
-    // eslint-disable-next-line no-irregular-whitespace
-    '[　-〿]',
-    '\uD840[\uDC00-\uFFFF]|[\uD841-\uD872]|\uD873[\u0000-\uDEAF]', // '[𠀀-𬺯]'
-    '[！-￮]',
-    '[ぁ-ゟ]',
-    '[゠-ヿ]',
-    '[ㇰ-ㇿ]',
-    '[ꀀ-꓆]'
-].join('|')})+$`);
-
 module.exports.allowsIdeographicBreaking = function(input) {
-    return input.search(ideographicBreakingRegExp) !== -1;
+    for (let i = 0; i < input.length; i++) {
+        if (!charAllowsIdeographicBreaking(input.charCodeAt(i), input.charCodeAt(i + 1))) {
+            return false;
+        }
+    }
+    return true;
 };
+
+
+function charAllowsIdeographicBreaking(char, nextChar) {
+    // "一" to "鿌"
+    if (char >= 0x4E00 && char <= 0x9FCC) return true;
+
+    // "㐀" to "䶵"
+    if (char >= 0x3400 && char <= 0x4DB5) return true;
+
+    // eslint-disable-next-line no-irregular-whitespace
+    // "　" to "〿"
+    if (char >= 0x3000 && char <= 0x303F) return true;
+
+    // "𠀀" to "𬺯"
+    if (char === 0xD840 && nextChar >= 0xDC00) return true;
+    if (char >= 0xD841 && char <= 0xD872) return true;
+    if (char === 0xD873 && nextChar <= 0xDEAF) return true;
+
+    // "！" to "￮"
+    if (char >= 0xFF01 && char <= 0xFFEE) return true;
+
+    // "ぁ" to "ゟ"
+    if (char >= 0x3041 && char <= 0x309F) return true;
+
+    // "゠" to "ヿ"
+    if (char >= 0x30A0 && char <= 0x30FF) return true;
+
+    // "ㇰ" to "ㇿ"
+    if (char >= 0x31F0 && char <= 0x31FF) return true;
+
+    // "ꀀ" to "꓆"
+    if (char >= 0xA000 && char <= 0xA4C6) return true;
+
+    return false;
+}
