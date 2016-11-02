af8445388b988f9cb79821c053446ca35fe59525
Vladimir Agafonkin <agafonkin@gmail.com>
2016-11-02 14:28:44 -0700

    switch to pure uglify for smaller build

--- test/build/min.test.js
+++ test/build/min.test.js
@@ -3,8 +3,9 @@
 const test = require('tap').test;
 const fs = require('fs');
 const pkg = require('../../package.json');
+const path = require('path');
 
-const minBundle = fs.readFileSync('dist/mapbox-gl.js', 'utf8');
+const minBundle = fs.readFileSync(path.join(__dirname, '../../dist/mapbox-gl.js'), 'utf8');
 
 test('production build removes asserts', (t) => {
     t.assert(minBundle.indexOf('assert(') === -1);
@@ -12,6 +13,6 @@ test('production build removes asserts', (t) => {
 });
 
 test('trims package.json assets', (t) => {
-    t.assert(minBundle.indexOf(`module.exports={"version":"${pkg.version}"}`) !== -1);
+    t.assert(minBundle.indexOf(`.exports={version:"${pkg.version}"}`) !== -1);
     t.end();
 });
