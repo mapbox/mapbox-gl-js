c08b084248f6ac1045bbe4362c23cb9552e754ae
Vladimir Agafonkin <agafonkin@gmail.com>
2016-11-07 16:40:02 -0800

    ignore fill-extrusion-color alpha

--- js/style/style_layer/fill_extrusion_style_layer.js
+++ js/style/style_layer/fill_extrusion_style_layer.js
@@ -4,6 +4,15 @@ const StyleLayer = require('../style_layer');
 const FillExtrusionBucket = require('../../data/bucket/fill_extrusion_bucket');
 
 class FillExtrusionStyleLayer extends StyleLayer {
+
+    getPaintValue(name, globalProperties, featureProperties) {
+        const value = super.getPaintValue(name, globalProperties, featureProperties);
+        if (name === 'fill-extrusion-color') {
+            value[3] = 1;
+        }
+        return value;
+    }
+
     createBucket(options) {
         return new FillExtrusionBucket(options);
     }
