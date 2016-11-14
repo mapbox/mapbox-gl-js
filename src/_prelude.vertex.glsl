ec891ce5360e488d81f60991f95d2038b83c4e3c
Bruno de Oliveira Abinader <bruno@mapbox.com>
2016-11-14 14:02:23 +0100

    Avoid redefining precision macros

--- src/_prelude.vertex.glsl
+++ src/_prelude.vertex.glsl
@@ -1,9 +1,19 @@
 #ifdef GL_ES
 precision highp float;
 #else
+
+#if !defined(lowp)
 #define lowp
+#endif
+
+#if !defined(mediump)
 #define mediump
+#endif
+
+#if !defined(highp)
 #define highp
+#endif
+
 #endif
 
 float evaluate_zoom_function_1(const vec4 values, const float t) {
