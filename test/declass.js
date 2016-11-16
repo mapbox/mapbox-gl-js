59f86923f67b1a9b694334260dbb6a5cbfb57962
Anand Thakker <anandthakker@users.noreply.github.com>
2016-11-16 13:58:03 -0800

    Add declass module to preprocess a style's paint classes (#573)
    * Add declass module
    
    * Fix lint
    
    * Add tests confirming that input isn't mutated
    
    * Strip paint.CLASS definitions
    
    * Dedupe extend()

---
+++ test/declass.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var t = require('tape'),
+    declass = require('../lib/declass');
+
+t('declass a style, one class', function (t) {
+    var style = {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': { base: 2, stops: [[0, 'red'], [22, 'yellow']] },
+                'fill-outline-color': 'green'
+            },
+            'paint.one': {
+                'fill-color': { base: 1 },
+                'fill-opacity': 0.5
+            }
+        }]
+    };
+
+    var declassed = declass(style, ['one']);
+
+    t.notEqual(declassed, style, 'returns a new style object');
+    t.notEqual(declassed.layers, style.layers, 'makes new style.layers array');
+    t.notEqual(declassed.layers[0], style.layers[0], 'makes new layer object');
+    t.notEqual(declassed.layers[0].paint, style.layers[0].paint, 'makes new paint object');
+
+    t.deepEqual(declassed, {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': { base: 1 },
+                'fill-outline-color': 'green',
+                'fill-opacity': 0.5
+            }
+        }]
+    });
+
+    t.end();
+});
+
+t('declass a style, missing class ==> noop', function (t) {
+    var style = {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'red',
+                'fill-outline-color': 'green'
+            }
+        }]
+    };
+
+    t.deepEqual(declass(style, ['one']), {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'red',
+                'fill-outline-color': 'green'
+            }
+        }]
+    });
+
+    t.end();
+});
+
+t('declass a style, multiple classes', function (t) {
+    var style = {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'red',
+                'fill-outline-color': 'green'
+            },
+            'paint.one': {
+                'fill-color': 'blue',
+                'fill-opacity': 0.5
+            },
+            'paint.two': {
+                'fill-opacity': 0.75,
+                'fill-something-else': true
+            }
+        }]
+    };
+
+    t.deepEqual(declass(style, ['one', 'two']), {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'blue',
+                'fill-outline-color': 'green',
+                'fill-opacity': 0.75,
+                'fill-something-else': true
+            }
+        }]
+    });
+
+    t.end();
+});
+
+t('declassing a style removes paint.CLASS definitions, whether or not they are applied', function (t) {
+    var style = {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'red',
+                'fill-outline-color': 'green'
+            },
+            'paint.one': {}
+        }]
+    };
+
+    t.deepEqual(declass(style, ['one']), {
+        layers: [{
+            id: 'a',
+            paint: {
+                'fill-color': 'red',
+                'fill-outline-color': 'green'
+            }
+        }]
+    });
+
+    t.end();
+});
+
