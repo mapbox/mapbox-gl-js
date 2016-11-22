4cacc0e339e929127caaa517f7103b0e3c20f862
Lauren Budorick <lauren@mapbox.com>
2016-11-22 15:06:20 -0500

    Fix race condition causing SymbolBucket isEmpty errors

--- js/data/bucket/symbol_bucket.js
+++ js/data/bucket/symbol_bucket.js
@@ -182,9 +182,10 @@ class SymbolBucket {
     }
 
     isEmpty() {
-        return this.arrays.icon.isEmpty() &&
+        return !this.arrays || (
+            this.arrays.icon.isEmpty() &&
             this.arrays.glyph.isEmpty() &&
-            this.arrays.collisionBox.isEmpty();
+            this.arrays.collisionBox.isEmpty());
     }
 
     serialize(transferables) {
