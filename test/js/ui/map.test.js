b2d40e3f780c61756334770234ac3ce0bfb19a91
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2016-12-21 18:14:21 +0300

    Issue an error when map canvas is larger than allowed by gl.MAX_RENDERBUFFER_SIZE #2893 (#3841)
    * Issue an error when map canvas is larger than allowed by gl.MAX_RENDERBUFFER_SIZE #2893
    
    * add test case for #2893
    
    * mention canvas size and gl.MAX_RENDERBUFFER_SIZE halving factor in the error message #2893

--- test/js/ui/map.test.js
+++ test/js/ui/map.test.js
@@ -62,6 +62,17 @@ test('Map', (t) => {
                 container: 'anElementIdWhichDoesNotExistInTheDocument'
             });
         }, new Error("Container 'anElementIdWhichDoesNotExistInTheDocument' not found"), 'throws on invalid map container id');
+
+        const largeContainer = window.document.createElement('div');
+        largeContainer.offsetWidth = 10000;
+        largeContainer.offsetHeight = 10000;
+        t.throws(() => {
+            new Map({
+                container: largeContainer
+            });
+        }, /Map canvas \(\d+x\d+\) is larger than half of gl.MAX_RENDERBUFFER_SIZE \(\d+\)/,
+        'throws on then map canvas is larger than allowed by gl.MAX_RENDERBUFFER_SIZE');
+
         t.end();
     });
 
