6fefdbe141f7a1d1cf262b80eef1e129cbea979e
Colleen <colleen.j.mcginnis@gmail.com>
2017-01-23 18:52:20 -0800

    change icon on esc

---
+++ js/ui/control/fullscreen_control.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const DOM = require('../../util/dom');
+const util = require('../../util/util');
+const window = require('../../util/window');
+
+class FullscreenControl {
+
+    constructor() {
+        this._fullscreen = false;
+        util.bindAll([
+            '_isFullscreen',
+            '_onClickFullscreen',
+            '_onKeyDown',
+            '_changeIcon'
+        ], this);
+    }
+
+    onAdd(map) {
+        const className = 'mapboxgl-ctrl';
+        const container = this._container = DOM.create('div', `${className} mapboxgl-ctrl-group`);
+        const button = this._fullscreenButton = DOM.create('button', (`${className}-icon ${className}-fullscreen`), this._container);
+        button.setAttribute("aria-label", "Toggle fullscreen");
+        this._fullscreenButton.addEventListener('click', this._onClickFullscreen);
+        this._mapContainer = map.getContainer();
+        window.document.addEventListener('webkitfullscreenchange', this._changeIcon);
+        return container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = null;
+    }
+
+    _isFullscreen() {
+        return this._fullscreen;
+    }
+
+    _changeIcon() {
+        this._fullscreen = !this._fullscreen;
+        const className = 'mapboxgl-ctrl';
+        this._fullscreenButton.classList.toggle(`${className}-shrink`);
+        this._fullscreenButton.classList.toggle(`${className}-fullscreen`);
+    }
+
+    _onClickFullscreen() {
+        if (this._isFullscreen()) {
+            if (window.document.exitFullscreen) {
+                window.document.exitFullscreen();
+            } else if (window.document.mozCancelFullScreen) {
+                window.document.mozCancelFullScreen();
+            } else {
+                window.document.webkitCancelFullScreen();
+            }
+        } else if (this._mapContainer.requestFullscreen) {
+            this._mapContainer.requestFullscreen();
+        } else if (this._mapContainer.mozRequestFullScreen) {
+            this._mapContainer.mozRequestFullScreen();
+        } else {
+            this._mapContainer.webkitRequestFullscreen();
+        }
+    }
+}
+
+module.exports = FullscreenControl;
--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ mapboxgl.NavigationControl = require('./ui/control/navigation_control');
 mapboxgl.GeolocateControl = require('./ui/control/geolocate_control');
 mapboxgl.AttributionControl = require('./ui/control/attribution_control');
 mapboxgl.ScaleControl = require('./ui/control/scale_control');
+mapboxgl.FullscreenControl = require('./ui/control/fullscreen_control');
 mapboxgl.Popup = require('./ui/popup');
 mapboxgl.Marker = require('./ui/marker');
-mapboxgl.Fullscreen = require('./ui/control/fullscreen');
 
 mapboxgl.Style = require('./style/style');
 
