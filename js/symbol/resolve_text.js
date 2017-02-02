884618d2919b4e768e1e62824efa0d19a1baa36e
Anand Thakker <anandthakker@users.noreply.github.com>
2017-02-02 14:06:48 -0500

    Enable property functions for text-field, text-transform (#4074)
    * Enable property functions for text-field, text-transform
    
    * Do not perform text-field token replacement on property functions
    
    * Add test for text-field token replacement
    
    * Skip text-{field,transform} property function tests on native
    
    * Update resolve_text test
    
    * Simplify render tests

--- js/symbol/resolve_text.js
+++ js/symbol/resolve_text.js
@@ -2,14 +2,17 @@
 
 const resolveTokens = require('../util/token');
 
-module.exports = function resolveText(feature, layout) {
-    let text = resolveTokens(feature.properties, layout['text-field']);
+module.exports = function resolveText(layer, globalProperties, featureProperties) {
+    let text = layer.getLayoutValue('text-field', globalProperties, featureProperties);
+    if (layer.isLayoutValueFeatureConstant('text-field')) {
+        text = resolveTokens(featureProperties, text);
+    }
     if (!text) {
         return;
     }
     text = text.toString();
 
-    const transform = layout['text-transform'];
+    const transform = layer.getLayoutValue('text-transform', globalProperties, featureProperties);
     if (transform === 'uppercase') {
         text = text.toLocaleUpperCase();
     } else if (transform === 'lowercase') {
