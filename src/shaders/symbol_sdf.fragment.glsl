7427148e6712fa23ea826445f78043dbf71531d6
Anand Thakker <anandthakker@users.noreply.github.com>
2017-02-07 19:17:12 -0500

    Very slightly demistify a couple of constants

--- src/shaders/symbol_sdf.fragment.glsl
+++ src/shaders/symbol_sdf.fragment.glsl
@@ -1,7 +1,5 @@
 #define SDF_PX 8.0
-#define BLUR_OFFSET 1.19
-#define HALO_OFFSET 6.0
-#define GAMMA 0.105/DEVICE_PIXEL_RATIO
+#define EDGE_GAMMA 0.105/DEVICE_PIXEL_RATIO
 
 uniform bool u_is_halo;
 #pragma mapbox: define lowp vec4 fill_color
@@ -27,12 +25,12 @@ void main() {
     #pragma mapbox: initialize lowp float halo_blur
 
     lowp vec4 color = fill_color;
-    lowp float gamma = GAMMA / u_gamma_scale;
+    lowp float gamma = EDGE_GAMMA / u_gamma_scale;
     lowp float buff = (256.0 - 64.0) / 256.0;
     if (u_is_halo) {
         color = halo_color;
-        gamma = (halo_blur * BLUR_OFFSET / SDF_PX + GAMMA) / u_gamma_scale;
-        buff = (HALO_OFFSET - halo_width / u_font_scale) / SDF_PX;
+        gamma = (halo_blur * 1.19 / SDF_PX + EDGE_GAMMA) / u_gamma_scale;
+        buff = (6.0 - halo_width / u_font_scale) / SDF_PX;
     }
 
     lowp float dist = texture2D(u_texture, v_tex).a;
