5fa31ef1a1eb8692f764625471bffb9fbfc10ae3
Lauren Budorick <lauren@mapbox.com>
2017-03-07 18:35:03 -0800

    Don't clamp crossfade in the event of fadeout (#4379)
    * Don't clamp crossfade in the event of fadeout

--- src/render/draw_raster.js
+++ src/render/draw_raster.js
@@ -122,7 +122,7 @@ function getFadeValues(tile, parentTile, layer, transform) {
         // if no parent or parent is older, fade in; if parent is younger, fade out
         const fadeIn = !parentTile || Math.abs(parentTile.coord.z - idealZ) > Math.abs(tile.coord.z - idealZ);
 
-        const childOpacity = tile.refreshedUponExpiration ? 1 : util.clamp(fadeIn ? sinceTile : 1 - sinceParent, 0, 1);
+        const childOpacity = (fadeIn && tile.refreshedUponExpiration) ? 1 : util.clamp(fadeIn ? sinceTile : 1 - sinceParent, 0, 1);
 
         // we don't crossfade tiles that were just refreshed upon expiring:
         // once they're old enough to pass the crossfading threshold
