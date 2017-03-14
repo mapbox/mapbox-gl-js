f2bfe7632ed00ca9c797b3c638fdca17235ae8ab
Anand Thakker <github@anandthakker.net>
2017-03-14 11:26:13 -0400

    Refactor byte pair unpacking into separate shader function

--- src/shaders/_prelude.vertex.glsl
+++ src/shaders/_prelude.vertex.glsl
@@ -35,17 +35,26 @@ vec4 evaluate_zoom_function_4(const vec4 value0, const vec4 value1, const vec4 v
     }
 }
 
+// Unpack a pair of values that have been packed into a single float.
+// The packed values are assumed to be 8-bit unsigned integers, and are
+// packed like so:
+// packedValue = floor(input[0]) * 256 + input[1],
+vec2 unpack_float(const float packedValue) {
+    int packedIntValue = int(packedValue);
+    int v0 = packedIntValue / 256;
+    return vec2(v0, packedIntValue - v0 * 256);
+}
+
 
 // To minimize the number of attributes needed in the mapbox-gl-native shaders,
 // we encode a 4-component color into a pair of floats (i.e. a vec2) as follows:
 // [ floor(color.r * 255) * 256 + color.g * 255,
 //   floor(color.b * 255) * 256 + color.g * 255 ]
 vec4 decode_color(const vec2 encodedColor) {
-    float r = floor(encodedColor[0]/256.0)/255.0;
-    float g = (encodedColor[0] - r*256.0*255.0)/255.0;
-    float b = floor(encodedColor[1]/256.0)/255.0;
-    float a = (encodedColor[1] - b*256.0*255.0)/255.0;
-    return vec4(r, g, b, a);
+    return vec4(
+        unpack_float(encodedColor[0]) / 255.0,
+        unpack_float(encodedColor[1]) / 255.0
+    );
 }
 
 // Unpack a pair of paint values and interpolate between them.
