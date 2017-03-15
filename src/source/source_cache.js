a7fc5743230f55980adc5d6cd8b152b2f8ad7aef
Molly Lloyd <mollymerp@users.noreply.github.com>
2017-03-15 11:49:43 -0700

    ensure erroring tiles dont block map from firing load events (#4426)
    * make sure previousState is bound to _tileLoaded
    
    * add a test

--- src/source/source_cache.js
+++ src/source/source_cache.js
@@ -431,7 +431,7 @@ class SourceCache extends Evented {
             const zoom = coord.z;
             const overscaling = zoom > this._source.maxzoom ? Math.pow(2, zoom - this._source.maxzoom) : 1;
             tile = new Tile(wrapped, this._source.tileSize * overscaling, this._source.maxzoom);
-            this.loadTile(tile, this._tileLoaded.bind(this, tile, coord.id));
+            this.loadTile(tile, this._tileLoaded.bind(this, tile, coord.id, tile.state));
         }
 
         tile.uses++;
