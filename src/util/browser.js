9be6e8f8134a2d61185421a414052644ba4d3ba7
Andy Moreland <amoreland@palantir.com>
2017-04-02 16:05:25 -0700

    Respect img height/width in addImage api
    When the user passes an image tag with a height and width
    that differ from the native height and width of the src,
    mapbox should respect the user's choice and draw the scaled
    image.

--- src/util/browser.js
+++ src/util/browser.js
@@ -69,7 +69,7 @@ exports.getImageData = function (img) {
     const context = canvas.getContext('2d');
     canvas.width = img.width;
     canvas.height = img.height;
-    context.drawImage(img, 0, 0);
+    context.drawImage(img, 0, 0, img.width, img.height);
     return context.getImageData(0, 0, img.width, img.height).data;
 };
 
