b615368aef8e8d198fc61b31cf80642cf66a2d21
Andrew Harvey <andrew@alantgeo.com.au>
2017-04-17 22:15:32 +1000

    ensure gl-style-composite throws error on conflicting source layer names

--- src/style-spec/composite.js
+++ src/style-spec/composite.js
@@ -3,6 +3,7 @@
 module.exports = function (style) {
     const styleIDs = [];
     const sourceIDs = [];
+    const compositedSourceLayers = [];
 
     for (const id in style.sources) {
         const source = style.sources[id];
@@ -35,6 +36,14 @@ module.exports = function (style) {
     style.layers.forEach((layer) => {
         if (styleIDs.indexOf(layer.source) >= 0) {
             layer.source = compositeID;
+
+            if ('source-layer' in layer) {
+                if (compositedSourceLayers.indexOf(layer['source-layer']) >= 0) {
+                    throw new Error('Conflicting source layer names');
+                } else {
+                    compositedSourceLayers.push(layer['source-layer']);
+                }
+            }
         }
     });
 
