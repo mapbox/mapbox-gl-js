ffb62e1599d3d43327fcfe973fbd2f27270a71f6
Chris Loer <chris.loer@gmail.com>
2017-04-25 12:50:43 -0700

    Make collision debug more legible for us colorblindies.

--- src/shaders/collision_box.fragment.glsl
+++ src/shaders/collision_box.fragment.glsl
@@ -6,19 +6,24 @@ varying float v_placement_zoom;
 
 void main() {
 
-    float alpha = 0.5;
+    float alpha = 1.0;
 
-    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0) * alpha;
+    // White = no collisions, label is showing
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0) * alpha;
 
+    // Yellow = collision, label hidden
     if (v_placement_zoom > u_zoom) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0) * alpha;
+        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0) * alpha;
     }
 
+    // Black = this collision box is not used at this zoom (for curved labels)
     if (u_zoom >= v_max_zoom) {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0) * alpha * 0.25;
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0) * alpha;
     }
 
     if (v_placement_zoom >= u_maxzoom) {
-        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0) * alpha * 0.2;
+    // Blue = this collision box will not be placed at tile zoom + 1
+    //  e.g. The only way to show this is overzooming
+        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0) * alpha;
     }
 }
