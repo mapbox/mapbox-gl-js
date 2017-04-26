0495a31120a5134eec9f1a934fdc7fa2cef6f44b
Chris Loer <chris.loer@gmail.com>
2017-04-26 16:40:32 -0700

    CollisionTile#minimumPitchScaling needs to be serialized since it's used  at render time.

--- src/symbol/collision_tile.js
+++ src/symbol/collision_tile.js
@@ -22,11 +22,13 @@ class CollisionTile {
             pitch = serialized.pitch;
             cameraToCenterDistance = serialized.cameraToCenterDistance;
             cameraToTileDistance = serialized.cameraToTileDistance;
+            this.minimumPitchScaling = serialized.minimumPitchScaling;
             this.grid = new Grid(serialized.grid);
             this.ignoredGrid = new Grid(serialized.ignoredGrid);
         } else {
             this.grid = new Grid(EXTENT, 12, 6);
             this.ignoredGrid = new Grid(EXTENT, 12, 0);
+            this.minimumPitchScaling = 1;
         }
 
         this.perspectiveRatio = 1 +
@@ -40,8 +42,6 @@ class CollisionTile {
 
         this.angle = angle;
         this.pitch = pitch;
-
-        this.minimumPitchScaling = 1;
 
         const sin = Math.sin(angle),
             cos = Math.cos(angle);
@@ -103,6 +103,9 @@ class CollisionTile {
         return {
             angle: this.angle,
             pitch: this.pitch,
+            cameraToTileDistance: this.cameraToTileDistance,
+            cameraToCenterDistance: this.cameraToCenterDistance,
+            minimumPitchScaling: this.minimumPitchScaling,
             grid: grid,
             ignoredGrid: ignoredGrid
         };
