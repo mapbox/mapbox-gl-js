4adf6bc78afeddffbcb8aa06fc067151713ac29a
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-06-21 11:17:43 +0300

    check that Fullscreen control gets added then fullscreen is enabled #4838

--- test/unit/ui/control/fullscreen.test.js
+++ test/unit/ui/control/fullscreen.test.js
@@ -17,11 +17,24 @@ function createMap() {
     });
 }
 
-test('FullscreenControl with no options', (t) => {
-    t.plan(0);
+test('FullscreenControl appears then fullscreen enabled', (t) => {
+    window.document.fullscreenEnabled = true;
 
     const map = createMap();
     const fullscreen = new FullscreenControl();
     map.addControl(fullscreen);
+
+    t.equal(map.getContainer().querySelectorAll('.mapboxgl-ctrl-fullscreen').length, 1);
+    t.end();
+});
+
+test('FullscreenControl does not appears then fullscreen is not enabled', (t) => {
+    window.document.fullscreenEnabled = false;
+
+    const map = createMap();
+    const fullscreen = new FullscreenControl();
+    map.addControl(fullscreen);
+
+    t.equal(map.getContainer().querySelectorAll('.mapboxgl-ctrl-fullscreen').length, 0);
     t.end();
 });
