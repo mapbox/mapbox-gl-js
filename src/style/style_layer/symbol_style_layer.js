ec306ba5ed38cc03231105509a92b885399a28b5
Chris Loer <chris.loer@gmail.com>
2017-06-21 11:01:58 -0700

    Implement icon-pitch-alignment following model of text-pitch-alignment.

--- src/style/style_layer/symbol_style_layer.js
+++ src/style/style_layer/symbol_style_layer.js
@@ -17,6 +17,8 @@ class SymbolStyleLayer extends StyleLayer {
             return this.getLayoutValue('symbol-placement', globalProperties, featureProperties) === 'line' ? 'map' : 'viewport';
         case 'text-pitch-alignment':
             return this.getLayoutValue('text-rotation-alignment', globalProperties, featureProperties);
+        case 'icon-pitch-alignment':
+            return this.getLayoutValue('icon-rotation-alignment', globalProperties, featureProperties);
         default:
             return value;
         }
