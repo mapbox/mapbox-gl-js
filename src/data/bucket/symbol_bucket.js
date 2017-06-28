2eaab976b9dd9abeb2fb4532abe26b3ee1b73baa
bolollo <bolosig@gmail.com>
2017-06-28 10:29:01 +0200

    add text-anchor data-driven styling

--- src/data/bucket/symbol_bucket.js
+++ src/data/bucket/symbol_bucket.js
@@ -350,32 +350,6 @@ class SymbolBucket {
         let horizontalAlign = 0.5,
             verticalAlign = 0.5;
 
-        switch (layout['text-anchor']) {
-        case 'right':
-        case 'top-right':
-        case 'bottom-right':
-            horizontalAlign = 1;
-            break;
-        case 'left':
-        case 'top-left':
-        case 'bottom-left':
-            horizontalAlign = 0;
-            break;
-        }
-
-        switch (layout['text-anchor']) {
-        case 'bottom':
-        case 'bottom-right':
-        case 'bottom-left':
-            verticalAlign = 1;
-            break;
-        case 'top':
-        case 'top-right':
-        case 'top-left':
-            verticalAlign = 0;
-            break;
-        }
-
         const justify = layout['text-justify'] === 'right' ? 1 :
             layout['text-justify'] === 'left' ? 0 :
             0.5;
@@ -390,6 +364,34 @@ class SymbolBucket {
 
             let shapedTextOrientations;
             if (feature.text) {
+                let textAnchor = this.layers[0].getLayoutValue('text-anchor', {zoom: this.zoom}, feature.properties);
+                
+                switch (textAnchor) {
+                case 'right':
+                case 'top-right':
+                case 'bottom-right':
+                    horizontalAlign = 1;
+                    break;
+                case 'left':
+                case 'top-left':
+                case 'bottom-left':
+                    horizontalAlign = 0;
+                    break;
+                }
+
+                switch (textAnchor) {
+                case 'bottom':
+                case 'bottom-right':
+                case 'bottom-left':
+                    verticalAlign = 1;
+                    break;
+                case 'top':
+                case 'top-right':
+                case 'top-left':
+                    verticalAlign = 0;
+                    break;
+                }
+                
                 const allowsVerticalWritingMode = scriptDetection.allowsVerticalWritingMode(feature.text);
                 const textOffset = this.layers[0].getLayoutValue('text-offset', {zoom: this.zoom}, feature.properties).map((t)=> t * oneEm);
                 const spacing = this.layers[0].getLayoutValue('text-letter-spacing', {zoom: this.zoom}, feature.properties) * oneEm;
