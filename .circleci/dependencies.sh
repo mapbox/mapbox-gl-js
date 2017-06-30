267a3313d3bcdd726a4720d5c2f75bbcab642b4d
Molly Lloyd <molly@mapbox.com>
2017-06-30 12:32:57 -0700

    attempt to migrate to ci 2.0
    delete old circle config
    
    increase allowable diff for failing tests
    
    test-tag v0.38.0-dev

--- .circleci/dependencies.sh
+++ .circleci/dependencies.sh
@@ -1,15 +1,6 @@
 # disable spotlight to ensure we waste no CPU on needless file indexing
 if [[ $(uname -s) == 'Darwin' ]]; then sudo mdutil -i off /; fi;
 
-if [[ ! -d ./nvm ]]; then
-    git clone --depth 1 https://github.com/creationix/nvm.git ./nvm
-fi
-
-source ./nvm/nvm.sh
-
-nvm install ${NODE_VERSION}
-nvm use ${NODE_VERSION}
-
 if [[ ! -d ~/.yarn ]]; then
     curl -o- -L https://yarnpkg.com/install.sh | bash
 fi
