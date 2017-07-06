b32fc07a0beacb0eb1e68c345aeec9aeef772ee3
Molly Lloyd <molly@mapbox.com>
2017-07-06 14:39:55 -0700

    switch to gl-js specific image

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -11,16 +11,16 @@ PATH=$(python -m site --user-base)/bin:${PATH}
 rm -rf coverage .nyc_output
 
 # run linters
-npm run lint
-npm run lint-docs
-npm run lint-css
+yarn run lint
+yarn run lint-docs
+yarn run lint-css
 
 # build and run build tests
-npm run build-min
-npm run build-dev
+yarn run build-min
+yarn run build-dev
 
 # run flow to check types
-npm run test-flow
+yarn run test-flow
 
 # run unit, render & query tests with coverage
 xvfb-run --server-args="-screen 0 1024x768x24" npm run test-cov
@@ -34,6 +34,6 @@ fi
 
 # upload benchmarks
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-    npm run build-benchmarks
+    yarn run build-benchmarks
     aws s3 cp --acl public-read --content-type application/javascript bench/benchmarks_generated.js s3://mapbox-gl-js/master/benchmarks.js
 fi
