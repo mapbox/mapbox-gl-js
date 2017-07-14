617a5445e611819d43c7e2358887675795b2f517
Chris Loer <chris.loer@gmail.com>
2017-07-14 10:10:53 -0700

    Remove unused serialization code from experimental grid index

--- src/symbol/grid_index_experimental.js
+++ src/symbol/grid_index_experimental.js
@@ -7,36 +7,12 @@ var NUM_PARAMS = 3;
 function GridIndex(extent, n, padding) {
     var cells = this.cells = [];
 
-    if (extent instanceof ArrayBuffer) {
-        this.arrayBuffer = extent;
-        var array = new Int32Array(this.arrayBuffer);
-        extent = array[0];
-        n = array[1];
-        padding = array[2];
-
-        this.d = n + 2 * padding;
-        for (var k = 0; k < this.d * this.d; k++) {
-            var start = array[NUM_PARAMS + k];
-            var end = array[NUM_PARAMS + k + 1];
-            cells.push(start === end ?
-                    null :
-                    array.subarray(start, end));
-        }
-        var keysOffset = array[NUM_PARAMS + cells.length];
-        var bboxesOffset = array[NUM_PARAMS + cells.length + 1];
-        this.keys = array.subarray(keysOffset, bboxesOffset);
-        this.bboxes = array.subarray(bboxesOffset);
-
-        this.insert = this._insertReadonly;
-
-    } else {
-        this.d = n + 2 * padding;
-        for (var i = 0; i < this.d * this.d; i++) {
-            cells.push([]);
-        }
-        this.keys = [];
-        this.bboxes = [];
+    this.d = n + 2 * padding;
+    for (var i = 0; i < this.d * this.d; i++) {
+        cells.push([]);
     }
+    this.keys = [];
+    this.bboxes = [];
 
     this.n = n;
     this.extent = extent;
@@ -57,10 +33,6 @@ GridIndex.prototype.insert = function(key, x1, y1, x2, y2) {
     this.bboxes.push(y1);
     this.bboxes.push(x2);
     this.bboxes.push(y2);
-};
-
-GridIndex.prototype._insertReadonly = function() {
-    throw 'Cannot insert into a GridIndex created from an ArrayBuffer.';
 };
 
 GridIndex.prototype._insertCell = function(x1, y1, x2, y2, cellIndex, uid) {
@@ -163,39 +135,4 @@ GridIndex.prototype._forEachCell = function(x1, y1, x2, y2, fn, arg1, arg2) {
 
 GridIndex.prototype._convertToCellCoord = function(x) {
     return Math.max(0, Math.min(this.d - 1, Math.floor(x * this.scale) + this.padding));
-};
-
-GridIndex.prototype.toArrayBuffer = function() {
-    if (this.arrayBuffer) return this.arrayBuffer;
-
-    var cells = this.cells;
-
-    var metadataLength = NUM_PARAMS + this.cells.length + 1 + 1;
-    var totalCellLength = 0;
-    for (var i = 0; i < this.cells.length; i++) {
-        totalCellLength += this.cells[i].length;
-    }
-
-    var array = new Int32Array(metadataLength + totalCellLength + this.keys.length + this.bboxes.length);
-    array[0] = this.extent;
-    array[1] = this.n;
-    array[2] = this.padding;
-
-    var offset = metadataLength;
-    for (var k = 0; k < cells.length; k++) {
-        var cell = cells[k];
-        array[NUM_PARAMS + k] = offset;
-        array.set(cell, offset);
-        offset += cell.length;
-    }
-
-    array[NUM_PARAMS + cells.length] = offset;
-    array.set(this.keys, offset);
-    offset += this.keys.length;
-
-    array[NUM_PARAMS + cells.length + 1] = offset;
-    array.set(this.bboxes, offset);
-    offset += this.bboxes.length;
-
-    return array.buffer;
 };
