4d34d7db957595d4ef9ff4867acf4d57a9d48321
Durran Jordan <durran@gmail.com>
2017-07-19 21:08:53 +0200

    Handle configured API_URLs that have a path:
    For the case when a user has modified mapboxgl.config.API_URL that
    includes a path such as http://test.example.com/api.mapbox.com the
    path was getting removed when building the full URL as it was being
    overwritten by the original path from the request.
    
    This pull request checks if the generation of the apiUrlObject has a
    path other than '/' (which is the default) we need to prepend it to the
    original urlObject path.
    
    This fixes the case where users using a reverse proxy to mapbox have
    filtered on the specific path set in their API_URL, and when it has been
    removed the lack of a match causes the proxy to not forward the requests
    .

--- src/util/mapbox.js
+++ src/util/mapbox.js
@@ -17,6 +17,10 @@ function makeAPIURL(urlObject: UrlObject, accessToken: string | null | void): st
     urlObject.protocol = apiUrlObject.protocol;
     urlObject.authority = apiUrlObject.authority;
 
+    if (apiUrlObject.path !== '/') {
+        urlObject.authority = `${urlObject.authority}${apiUrlObject.path}`;
+    }
+
     if (!config.REQUIRE_ACCESS_TOKEN) return formatUrl(urlObject);
 
     accessToken = accessToken || config.ACCESS_TOKEN;
