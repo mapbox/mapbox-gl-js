6b460304b5de1d6df847143ea83b618b1917e881
John Firebaugh <john.firebaugh@gmail.com>
2017-08-08 16:07:40 -0700

    Try packing normals into a single component

--- src/shaders/line.vertex.glsl
+++ src/shaders/line.vertex.glsl
@@ -12,7 +12,7 @@
 // #define scale 63.0
 #define scale 0.015873016
 
-attribute vec4 a_pos_normal;
+attribute vec3 a_pos_normal;
 attribute vec4 a_data;
 
 uniform mat4 u_matrix;
@@ -42,7 +42,12 @@ void main() {
     float a_direction = mod(a_data.z, 4.0) - 1.0;
 
     vec2 pos = a_pos_normal.xy;
-    vec2 normal = a_pos_normal.zw;
+
+    // transform y normal so that 0 => -1 and 1 => 1
+    // In the texture normal, x is 0 if the normal points straight up/down and 1 if it's a round cap
+    // y is 1 if the normal points up, and -1 if it points down
+    mediump vec2 normal = unpack_float(a_pos_normal.z);
+    normal.y = sign(normal.y - 0.5);
 
     v_normal = normal;
 
--- src/shaders/line_pattern.vertex.glsl
+++ src/shaders/line_pattern.vertex.glsl
@@ -14,7 +14,7 @@
 // Retina devices need a smaller distance to avoid aliasing.
 #define ANTIALIASING 1.0 / DEVICE_PIXEL_RATIO / 2.0
 
-attribute vec4 a_pos_normal;
+attribute vec3 a_pos_normal;
 attribute vec4 a_data;
 
 uniform mat4 u_matrix;
@@ -44,7 +44,12 @@ void main() {
     float a_linesofar = (floor(a_data.z / 4.0) + a_data.w * 64.0) * LINE_DISTANCE_SCALE;
 
     vec2 pos = a_pos_normal.xy;
-    vec2 normal = a_pos_normal.zw;
+
+    // transform y normal so that 0 => -1 and 1 => 1
+    // In the texture normal, x is 0 if the normal points straight up/down and 1 if it's a round cap
+    // y is 1 if the normal points up, and -1 if it points down
+    mediump vec2 normal = unpack_float(a_pos_normal.z);
+    normal.y = sign(normal.y - 0.5);
 
     v_normal = normal;
 
