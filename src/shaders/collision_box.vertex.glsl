b9ae7ce170ff24e6649bec94349515c3570f19c8
Chris Loer <chris.loer@gmail.com>
2017-08-11 13:59:45 -0700

    Standardize perspective ratio formula.

--- src/shaders/collision_box.vertex.glsl
+++ src/shaders/collision_box.vertex.glsl
@@ -13,7 +13,7 @@ varying float v_notUsed;
 void main() {
     vec4 projectedPoint = u_matrix * vec4(a_anchor_pos, 0, 1);
     highp float camera_to_anchor_distance = projectedPoint.w;
-    highp float collision_perspective_ratio = 1.0 + 0.5 * ((camera_to_anchor_distance / u_camera_to_center_distance) - 1.0);
+    highp float collision_perspective_ratio = 0.5 + 0.5 * (camera_to_anchor_distance / u_camera_to_center_distance);
 
     gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);
     gl_Position.xy += a_extrude * u_extrude_scale * gl_Position.w / collision_perspective_ratio;
