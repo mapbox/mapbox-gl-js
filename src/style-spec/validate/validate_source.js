1e1635aa8e02e94002bd0e93664ed66435896821
Molly Lloyd <molly@mapbox.com>
2017-08-31 18:46:59 -0700

    add raster-dem source type and hillshade layer type

--- src/style-spec/validate/validate_source.js
+++ src/style-spec/validate/validate_source.js
@@ -20,6 +20,7 @@ module.exports = function validateSource(options) {
     switch (type) {
     case 'vector':
     case 'raster':
+    case 'raster-dem':
         errors = errors.concat(validateObject({
             key: key,
             value: value,
@@ -76,7 +77,7 @@ module.exports = function validateSource(options) {
         return validateEnum({
             key: `${key}.type`,
             value: value.type,
-            valueSpec: {values: ['vector', 'raster', 'geojson', 'video', 'image', 'canvas']},
+            valueSpec: {values: ['vector', 'raster', 'raster-dem', 'geojson', 'video', 'image', 'canvas']},
             style: style,
             styleSpec: styleSpec
         });
