dda058fc89d07b2aac3e400da6b572839fcae049
Vladimir Agafonkin <agafonkin@gmail.com>
2017-09-07 15:55:03 +0300

    remove redundant heatmap attributes

--- src/shaders/heatmap.vertex.glsl
+++ src/shaders/heatmap.vertex.glsl
@@ -1,25 +1,21 @@
+#pragma mapbox: define highp float weight
+
 uniform mat4 u_matrix;
 uniform float u_extrude_scale;
+uniform float u_radius;
+uniform float u_opacity;
 
 attribute vec2 a_pos;
-
-#pragma mapbox: define highp vec4 color
-#pragma mapbox: define mediump float radius
-#pragma mapbox: define lowp float opacity
-#pragma mapbox: define highp float weight
 
 varying vec2 v_extrude;
 
 void main(void) {
-    #pragma mapbox: initialize highp vec4 color
-    #pragma mapbox: initialize mediump float radius
-    #pragma mapbox: initialize lowp float opacity
     #pragma mapbox: initialize highp float weight
 
     // unencode the extrusion vector that we snuck into the a_pos vector
     v_extrude = vec2(mod(a_pos, 2.0) * 2.0 - 1.0);
 
-    vec2 extrude = v_extrude * radius * u_extrude_scale;
+    vec2 extrude = v_extrude * u_radius * u_extrude_scale;
 
     // multiply a_pos by 0.5, since we had it * 2 in order to sneak
     // in extrusion data
