460745dee5b4192dd94536856138ea2efca1248d
Chris Loer <chris.loer@gmail.com>
2017-09-14 11:13:57 -0700

    Update CollisionBox comments.

--- src/symbol/collision_box.js
+++ src/symbol/collision_box.js
@@ -26,31 +26,10 @@ export type CollisionBox = {
  * represent all the area covered by a single label. They are used to
  * prevent collisions between labels.
  *
- * A collision box actually represents a 3d volume. The first two dimensions,
- * x and y, are specified with `anchor` along with `x1`, `y1`, `x2`, `y2`.
- * The third dimension, zoom, is limited by `maxScale` which determines
- * how far in the z dimensions the box extends.
- *
- * As you zoom in on a map, all points on the map get further and further apart
- * but labels stay roughly the same size. Labels cover less real world area on
- * the map at higher zoom levels than they do at lower zoom levels. This is why
- * areas are are represented with an anchor point and offsets from that point
- * instead of just using four absolute points.
- *
- * Line labels are represented by a set of these boxes spaced out along a line.
- * When you zoom in, line labels cover less real world distance along the line
- * than they used to. Collision boxes near the edges that used to cover label
- * no longer do. If a box doesn't cover the label anymore it should be ignored
- * when doing collision checks. `maxScale` is how much you can scale the map
- * before the label isn't within the box anymore.
- * For example
- * lower zoom:
- * https://cloud.githubusercontent.com/assets/1421652/8060094/4d975f76-0e91-11e5-84b1-4edeb30a5875.png
- * slightly higher zoom:
- * https://cloud.githubusercontent.com/assets/1421652/8060061/26ae1c38-0e91-11e5-8c5a-9f380bf29f0a.png
- * In the zoomed in image the two grey boxes on either side don't cover the
- * label anymore. Their maxScale is smaller than the current scale.
- *
+ * Line labels are represented by a set of these boxes spaced out along the
+ * line. When we calculate collision geometries, we use the circle inscribed
+ * in the box, rather than the box itself. This makes collision detection more
+ * stable during rotation.
  *
  * @class CollisionBoxArray
  * @private
