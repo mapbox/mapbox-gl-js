816fc155e763f24b6ca77658cb02e64ad9a69309
John Firebaugh <john.firebaugh@gmail.com>
2017-09-14 10:05:52 -0700

    Merge check_subtype.js into types.js

--- src/style-spec/function/types.js
+++ src/style-spec/function/types.js
@@ -54,6 +54,44 @@ function toString(type: Type): string {
     }
 }
 
+const valueMemberTypes = [
+    NullType,
+    NumberType,
+    StringType,
+    BooleanType,
+    ColorType,
+    ObjectType,
+    array(ValueType)
+];
+
+/**
+ * Returns null if `t` is a subtype of `expected`; otherwise returns an
+ * error message.
+ * * @private
+ */
+function checkSubtype(expected: Type, t: Type): ?string {
+    if (t.kind === 'Error') {
+        // Error is a subtype of every type
+        return null;
+    } else if (expected.kind === 'Array') {
+        if (t.kind === 'Array' &&
+            !checkSubtype(expected.itemType, t.itemType) &&
+            (typeof expected.N !== 'number' || expected.N === t.N)) {
+            return null;
+        }
+    } else if (expected.kind === t.kind) {
+        return null;
+    } else if (expected.kind === 'Value') {
+        for (const memberType of valueMemberTypes) {
+            if (!checkSubtype(memberType, t)) {
+                return null;
+            }
+        }
+    }
+
+    return `Expected ${toString(expected)} but found ${toString(t)} instead.`;
+}
+
 module.exports = {
     NullType,
     NumberType,
@@ -64,5 +102,6 @@ module.exports = {
     ValueType,
     array,
     ErrorType,
-    toString
+    toString,
+    checkSubtype
 };
