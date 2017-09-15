ca061a2ac2b6a45dfd40ba4f9705503d1efa6cbb
John Firebaugh <john.firebaugh@gmail.com>
2017-09-15 11:24:21 -0700

    WIP

--- bench/benchmarks_view.js
+++ bench/benchmarks_view.js
@@ -1,199 +1,93 @@
 'use strict';
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "BenchmarksView|clipboard" }]*/
 
-const Clipboard = require('clipboard');
-
-const BenchmarksView = React.createClass({
+class BenchmarkRow extends React.Component {
     render() {
-        return <div style={{width: 960, paddingBottom: window.innerHeight, margin: '0 auto'}}>
-            <h1 className="space-bottom">
-                Benchmarks
-                <a
-                    className={[
-                        'fr',
-                        'icon',
-                        'clipboard',
-                        'button',
-                        (this.getStatus() === 'ended' ? '' : 'disabled')
-                    ].join(' ')}
-                    data-clipboard-text={this.renderTextBenchmarks()}>
-                    Copy Results
-                </a>
-            </h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Benchmark</th>
-                        {this.versions().map((v) => <th key={v}>{v}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(this.state.results).map(this.renderBenchmark)}
-                </tbody>
-            </table>
-        </div>;
-    },
-
-    renderBenchmark(name) {
-        return <tr key={name}>
-            <th><a href={`#${name}`} onClick={this.reload}>{name}</a></th>
-            {Object.keys(this.state.results[name]).map(this.renderBenchmarkVersion.bind(this, name))}
-        </tr>;
-    },
-
-    renderBenchmarkVersion(name, version) {
-        const results = this.state.results[name][version];
-        const sampleData = results.regression ? results.regression.map(row => row.join(',')).join('\n') : null;
         return (
-            <td id={name + version}
-                key={version}
-                className={results.status === 'waiting' ? 'quiet' : ''}>
-                {results.logs.map((log, index) => {
-                    return <div key={index} className={`pad1 dark fill-${log.color}`}>{log.message}</div>;
-                })}
-                {sampleData ? (
-                    <details>
-                        <summary className='pad1 dark fill-green'>
-                            Sample Data
-                            <a className='icon clipboard'
-                                data-clipboard-text={sampleData}>Copy</a>
-                        </summary>
-                        <pre>{sampleData}</pre>
-                    </details>
-                ) : ''}
-            </td>
+            <tr>
+                <th><a href={`#${this.props.name}`} onClick={this.reload}>{this.props.name}</a></th>
+                {this.props.versions.map(version => (
+                    <td key={version.name} className={version.status === 'waiting' ? 'quiet' : ''}>{version.message}</td>
+                ))}
+            </tr>
         );
-    },
-
-    versions() {
-        const versions = [];
-        for (const name in this.state.results) {
-            for (const version in this.state.results[name]) {
-                if (versions.indexOf(version) < 0) {
-                    versions.push(version);
-                }
-            }
-        }
-        return versions;
-    },
-
-    renderTextBenchmarks() {
-        const versions = this.versions();
-        let output = `benchmark | ${versions.join(' | ')}\n---`;
-        for (let i = 0; i < versions.length; i++) {
-            output += ' | ---';
-        }
-        output += '\n';
-
-        for (const name in this.state.results) {
-            output += `**${name}**`;
-            for (const version of versions) {
-                const result = this.state.results[name][version];
-                output += ` | ${result && result.message || 'n\/a'} `;
-            }
-            output += '\n';
-        }
-        return output;
-    },
-
-    getInitialState() {
-        const results = {};
-
-        for (const name in this.props.benchmarks) {
-            for (const version in this.props.benchmarks[name]) {
-                if (!this.props.benchmarkFilter || this.props.benchmarkFilter(name, version)) {
-                    results[name] = results[name] || {};
-                    results[name][version] = {
-                        status: 'waiting',
-                        logs: []
-                    };
-                }
-            }
-        }
-
-        return { results };
-    },
-
-    componentDidMount() {
-        let promise = Promise.resolve();
-
-        for (const name of Object.keys(this.state.results)) {
-            for (const version of Object.keys(this.state.results[name])) {
-                promise = promise.then(() => {
-                    return this.runBenchmark(
-                        this.props.benchmarks[name][version],
-                        this.state.results[name][version]);
-                });
-            }
-        }
-
-        return promise;
-    },
-
-    runBenchmark(benchmark, results) {
-        results.status = 'running';
-
-        const log = (color, message) => {
-            results.logs.push({
-                color: color || 'blue',
-                message: message
-            });
-            this.forceUpdate();
-        }
-
-        return benchmark.run()
-            .then((res) => {
-                results.status = 'ended';
-                results.samples = res.samples;
-                results.regression = res.regression;
-                log('green', res.elapsed);
-            })
-            .catch((error) => {
-                results.status = 'errored';
-                log('red', error.message);
-            });
-    },
-
-    getBenchmarkVersionStatus(name, version) {
-        return this.state.results[name][version].status;
-    },
-
-    getBenchmarkStatus(name) {
-        return reduceStatuses(Object.keys(this.state.results[name]).map((version) => {
-            return this.getBenchmarkVersionStatus(name, version);
-        }));
-    },
-
-    getStatus() {
-        return reduceStatuses(Object.keys(this.state.results).map((name) => {
-            return this.getBenchmarkStatus(name);
-        }));
-    },
+    }
 
     reload() {
         location.reload();
     }
-});
+}
 
-function reduceStatuses(statuses) {
-    if (statuses.indexOf('running') !== -1) {
-        return 'running';
-    } else if (statuses.indexOf('waiting') !== -1) {
-        return 'waiting';
-    } else {
-        return 'ended';
+class BenchmarksTable extends React.Component {
+    render() {
+        return (
+            <table style={{width: 960, margin: '2em auto'}}>
+                <caption className="space-bottom1"><h1>Benchmarks</h1></caption>
+                <thead>
+                    <tr>
+                        <th>Benchmark</th>
+                        {this.props.versions.map(version => <th key={version}>{version}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.benchmarks.map(benchmark => <BenchmarkRow key={benchmark.name} {...benchmark}/>)}
+                </tbody>
+            </table>
+        );
     }
 }
 
-const clipboard = new Clipboard('.clipboard');
+const versions = window.mapboxglVersions;
+const benchmarks = [];
+const filter = window.location.hash.substr(1);
 
-ReactDOM.render(
-    <BenchmarksView
-        benchmarks={window.mapboxglBenchmarks}
-        benchmarkFilter={function(name) {
-            const nameFilter = window.location.hash.substr(1);
-            return !nameFilter || name === nameFilter;
-        }}
-    />,
-    document.getElementById('benchmarks')
-);
+let finished = false;
+let promise = Promise.resolve();
+
+for (const name in window.mapboxglBenchmarks) {
+    if (filter && name !== filter)
+        continue;
+
+    const benchmark = { name, versions: [] };
+    benchmarks.push(benchmark);
+
+    for (const ver in window.mapboxglBenchmarks[name]) {
+        const version = {
+            name: ver,
+            status: 'waiting',
+            logs: []
+        };
+
+        benchmark.versions.push(version);
+
+        promise = promise.then(() => {
+            version.status = 'running';
+            update();
+
+            return window.mapboxglBenchmarks[name][ver].run()
+                .then(result => {
+                    version.status = 'ended';
+                    version.message = result.elapsed;
+                    version.samples = result.samples;
+                    version.regression = result.regression;
+                    update();
+                })
+                .catch(error => {
+                    version.status = 'errored';
+                    version.message = error.message;
+                    update();
+                });
+        });
+    }
+}
+
+promise = promise.then(() => {
+    finished = true;
+    update();
+})
+
+function update() {
+    ReactDOM.render(
+        <BenchmarksTable versions={versions} benchmarks={benchmarks} finished={finished}/>,
+        document.getElementById('benchmarks')
+    );
+}
