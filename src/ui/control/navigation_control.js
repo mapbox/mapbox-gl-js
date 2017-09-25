e16d3dedd69b8a2d5cb8e67f8a3b1273e85c4764
matijs <matijs@gmail.com>
2017-09-25 17:36:35 +0200

    Make compass optional
    The compass button of the navigation control is now optional. Creating a
    new navigation control with `{showCompass: false}` will leave out the
    compass button. Defaults to showing the compass button.

--- src/ui/control/navigation_control.js
+++ src/ui/control/navigation_control.js
@@ -6,6 +6,10 @@ const DragRotateHandler = require('../handler/drag_rotate');
 
 import type Map from '../map';
 
+const defaultOptions = {
+    showCompass: true
+};
+
 /**
  * A `NavigationControl` control contains zoom buttons and a compass.
  *
@@ -18,6 +22,7 @@ import type Map from '../map';
  */
 class NavigationControl {
     _map: Map;
+    options: any;
     _container: HTMLElement;
     _zoomInButton: HTMLElement;
     _zoomOutButton: HTMLElement;
@@ -25,7 +30,8 @@ class NavigationControl {
     _compassArrow: HTMLElement;
     _handler: DragRotateHandler;
 
-    constructor() {
+    constructor(options: any) {
+        this.options = util.extend({}, defaultOptions, options);
         util.bindAll([
             '_rotateCompassArrow'
         ], this);
@@ -35,13 +41,18 @@ class NavigationControl {
 
         this._zoomInButton = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-in', 'Zoom In', () => this._map.zoomIn());
         this._zoomOutButton = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-out', 'Zoom Out', () => this._map.zoomOut());
-        this._compass = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-compass', 'Reset North', () => this._map.resetNorth());
-        this._compassArrow = DOM.create('span', 'mapboxgl-ctrl-compass-arrow', this._compass);
+
+        if (this.options.showCompass) {
+            this._compass = this._createButton('mapboxgl-ctrl-icon mapboxgl-ctrl-compass', 'Reset North', () => this._map.resetNorth());
+            this._compassArrow = DOM.create('span', 'mapboxgl-ctrl-compass-arrow', this._compass);
+        }
     }
 
     _rotateCompassArrow() {
-        const rotate = `rotate(${this._map.transform.angle * (180 / Math.PI)}deg)`;
-        this._compassArrow.style.transform = rotate;
+        if (this.options.showCompass) {
+            const rotate = `rotate(${this._map.transform.angle * (180 / Math.PI)}deg)`;
+            this._compassArrow.style.transform = rotate;
+        }
     }
 
     onAdd(map: Map) {
