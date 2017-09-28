282f4e13d55cd0c1ebc62a7dad30e237a16fdfe7
John Firebaugh <john.firebaugh@gmail.com>
2017-09-28 15:40:43 -0700

    Move deref into benchmark setup

--- bench/benchmarks/style_layer_create.js
+++ bench/benchmarks/style_layer_create.js
@@ -9,11 +9,11 @@ module.exports = class StyleLayerCreate extends Benchmark {
     setup() {
         return fetch(`https://api.mapbox.com/styles/v1/mapbox/streets-v9?access_token=${accessToken}`)
             .then(response => response.json())
-            .then(json => { this.json = json; });
+            .then(json => { this.layers = deref(json.layers); });
     }
 
     bench() {
-        for (const layer of deref(this.json.layers)) {
+        for (const layer of this.layers) {
             StyleLayer.create(layer);
         }
     }
