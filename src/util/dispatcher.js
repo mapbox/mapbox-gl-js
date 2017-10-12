1634951524e6b0d091b22eb531fee13ad0c23ca1
Erik Simmler <esimmler@foreflight.com>
2017-10-12 10:52:14 -0400

    Don't broadcast using asyncAll if no callback is required

--- src/util/dispatcher.js
+++ src/util/dispatcher.js
@@ -36,10 +36,15 @@ class Dispatcher {
      * Broadcast a message to all Workers.
      */
     broadcast(type: string, data: mixed, cb?: Function) {
-        cb = cb || function () {};
-        util.asyncAll(this.actors, (actor, done) => {
-            actor.send(type, data, done);
-        }, cb);
+        if (cb) {
+            util.asyncAll(this.actors, (actor, done) => {
+                actor.send(type, data, done);
+            }, cb);
+        } else {
+            this.actors.forEach(actor => {
+                actor.send(type, data);
+            });
+        }
     }
 
     /**
