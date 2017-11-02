05ac753289a37f463873d869d7b11f990f11526a
Anand Thakker <github@anandthakker.net>
2017-11-01 22:29:20 -0400

    Respect ignores.json in expression tests

--- test/integration/lib/expression.js
+++ test/integration/lib/expression.js
@@ -55,8 +55,8 @@ function deepEqual(a, b) {
  * @param {string} implementation - identify the implementation under test; used to
  * deal with implementation-specific test exclusions and fudge-factors
  * @param {Object} options
- * @param {Array<string>} [options.tests] - array of test names to run; tests not in the
- * array will be skipped
+ * @param {Array<string>} [options.tests] - array of test names to run; tests not in the array will be skipped
+ * @param {Array<string>} [options.ignores] - array of test names to ignore.
  * @param {} runExpressionTest - a function that runs a single expression test fixture
  * @returns {undefined} terminates the process when testing is complete
  */
