069ed187521e9ffcafa362aa8119589c55692ad7
Molly Lloyd <molly@mapbox.com>
2017-11-01 18:51:23 -0700

    update shaders+spec based on nicki's recs

--- src/shaders/hillshade_prepare.fragment.glsl
+++ src/shaders/hillshade_prepare.fragment.glsl
@@ -45,13 +45,27 @@ void main() {
     float h = getElevation(v_pos + vec2(0, epsilon.y), 0.0);
     float i = getElevation(v_pos + vec2(epsilon.x, epsilon.y), 0.0);
 
+
+
+    // here we divide the x and y slopes by 8 * pixel size
+    // where pixel size (aka meters/pixel) is:
+    // circumference of the world / (pixels per tile * number of tiles)
+    // which is equivalent to: 8 * 40075016.6855785 / (512 * pow(2, u_zoom))
+    // which can be reduced to: pow(2, 19.25619978527 - u_zoom)
+    // we want to vertically exaggerate the hillshading though, because otherwise
+    // it is barely noticeable at low zooms. to do this, we multiply this by some
+    // scale factor pow(2, (u_zoom - 14) * a) where a is an arbitrary value and 14 is the
+    // maxzoom of the tile source. here we use a=0.3 which works out to the
+    // expression below. see nickidlugash's awesome breakdown for more info
+    // https://github.com/mapbox/mapbox-gl-js/pull/5286#discussion_r148419556
+    float exaggeration = u_zoom < 4.5 ? 0.35 : 0.3;
+
     vec2 deriv = vec2(
         (c + f + f + i) - (a + d + d + g),
         (g + h + h + i) - (a + b + b + c)
-        // decrease the value u_zoom is subtracted from to intensify terrain appearance
-    ) /  pow(2.0, 14.5 - u_zoom * 0.5);
+    ) /  pow(2.0, (u_zoom - 14.0) * exaggeration + 19.2562 - u_zoom);
 
-
+// (u_zoom > 11.0 ? pow(2.0, 13.5 - u_zoom * 0.5) : pow(2.0, 14.5 - u_zoom * 0.5))
     gl_FragColor = clamp(vec4(
         deriv.x / 2.0 + 0.5,
         deriv.y / 2.0 + 0.5,
