afa79ecc3fb245b5fa35b379875167835fee472a
jingsam <jing-sam@qq.com>
2017-11-10 13:02:53 +0800

    Update map.js

--- src/ui/map.js
+++ src/ui/map.js
@@ -197,7 +197,7 @@ const defaultOptions = {
  * @example
  * var map = new mapboxgl.Map({
  *   container: 'map',
- *   center: [-122.4679, 37.772537],
+ *   center: [-122.420679, 37.772537],
  *   zoom: 13,
  *   style: style_object,
  *   hash: true,
