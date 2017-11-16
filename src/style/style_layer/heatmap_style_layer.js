fd3045f15e7cc1f772212b5b51c308b63651c6f0
John Firebaugh <john.firebaugh@gmail.com>
2017-11-15 16:31:38 -0800

    Fix heatmap layer performance regression

--- src/style/style_layer/heatmap_style_layer.js
+++ src/style/style_layer/heatmap_style_layer.js
@@ -31,11 +31,32 @@ class HeatmapStyleLayer extends StyleLayer {
 
     constructor(layer: LayerSpecification) {
         super(layer, properties);
+
+        // make sure color ramp texture is generated for default heatmap color too
+        this._updateColorRamp();
     }
 
-    updatePaintTransitions(options: {transition?: boolean}, transition: TransitionSpecification) {
-        super.updatePaintTransitions(options, transition);
-        this.colorRamp = this._transitioningPaint._values['heatmap-color'].possiblyEvaluate(({zoom: 0}: any));
+    setPaintProperty(name: string, value: mixed, options: {validate: boolean}) {
+        super.setPaintProperty(name, value, options);
+        if (name === 'heatmap-color') {
+            this._updateColorRamp();
+        }
+    }
+
+    _updateColorRamp() {
+        const expression = this._transitionablePaint._values['heatmap-color'].value.expression;
+        const colorRampData = new Uint8Array(256 * 4);
+        const len = colorRampData.length;
+        for (let i = 4; i < len; i += 4) {
+            const pxColor = expression.evaluate(({heatmapDensity: i / len}: any));
+            // the colors are being unpremultiplied because Color uses
+            // premultiplied values, and the Texture class expects unpremultiplied ones
+            colorRampData[i + 0] = Math.floor(pxColor.r * 255 / pxColor.a);
+            colorRampData[i + 1] = Math.floor(pxColor.g * 255 / pxColor.a);
+            colorRampData[i + 2] = Math.floor(pxColor.b * 255 / pxColor.a);
+            colorRampData[i + 3] = Math.floor(pxColor.a * 255);
+        }
+        this.colorRamp = RGBAImage.create({width: 256, height: 1}, colorRampData);
         this.colorRampTexture = null;
     }
 
