89a1d63b0bb2f493b5ee861bc113c3a1e86734b4
John Firebaugh <john.firebaugh@gmail.com>
2017-11-17 12:41:19 -0800

    High level description of type system, assertions vs. conversion

--- docs/pages/style-spec.js
+++ docs/pages/style-spec.js
@@ -1197,7 +1197,8 @@ export default class extends React.Component {
                                     }`)}
                             </div>
 
-                            <p>This example uses the <a href="#expressions-interpolate"><code>interpolate</code></a>
+                            <p>This example uses the <a
+                                href="#expressions-interpolate"><code>interpolate</code></a>
                                 operator to define a linear relationship between zoom level and circle size using a set
                                 of input-output pairs. In this case, the expression indicates that the circle radius should
                                 be 1 pixel when the zoom level is 5 or below, and 5 pixels when the zoom is 10 or above.
@@ -1240,9 +1241,9 @@ export default class extends React.Component {
                             </div>
 
                             <p>That is, in layout or paint properties, <code>["zoom"]</code> may appear only as the
-                                input to an outer <a href="#expressions-interpolate"><code>interpolate</code></a> or
-                                <a href="#expressions-step"><code>step</code></a> expression, or such an expression within a
-                                <a href="#expressions-let"><code>let</code></a> expression.</p>
+                                input to an outer <a href="#expressions-interpolate"><code>interpolate</code></a> or <a
+                                    href="#expressions-step"><code>step</code></a> expression, or such an expression within
+                                a <a href="#expressions-let"><code>let</code></a> expression.</p>
 
                             <p>There is an important difference between layout and paint properties in
                                 the timing of camera expression evaluation. Paint property camera expressions are
@@ -1274,6 +1275,60 @@ export default class extends React.Component {
 
                             <p>An expression that uses both data and camera operators is considered both a data expression
                                 and a camera expression, and must adhere to the restrictions described above for both.</p>
+
+                            <h3>Type system</h3>
+                            <p>The input arguments to expressions, and their result values, use the same set of <a
+                                href="#types">types</a> as the rest of the style specification: boolean, string,
+                                number, color, and arrays of these types. Furthermore, expressions are <em>type safe</em>:
+                                each use of an expression has a known result type and required argument types, and the
+                                SDKs verify that the result type of an expression is appropriate for the context in
+                                which it is used. For example, the result type of an expression in the <a
+                                href="#layer-filter"><code>filter</code></a> property must be <a
+                                href="#types-boolean">boolean</a>, and the arguments to the <a
+                                href="#expressions-+"><code>+</code></a> operator must be <a
+                                href="#types-number">numbers</a>.
+                            </p>
+
+                            <p>When working with feature data, the type of a feature property value is typically not known
+                                ahead of time by the SDK. In order to preserve type safety, when evaluating a data
+                                expression, the SDK will check that the property value is appropriate for the context.
+                                For example, if you use the expression <code>["get", "feature-color"]</code> for the <a
+                                    href="#paint-circle-circle-color"><code>circle-color</code></a> property, the SDK
+                                will verify that the <code>feature-color</code> value of each feature is a string
+                                identifying a valid <a href="#types-color">color</a>. If this check fails, an error will
+                                be indicated in an SDK-specific way (typically a log message), and the default value for
+                                the property will be used instead.
+                            </p>
+
+                            <p>In most cases, this verification will occur automatically wherever it is needed. However,
+                                in certain situations, the SDK may be unable to automatically determine the expected
+                                result type of a data expression from surrounding context. For example, it is not clear
+                                whether the expression <code>["&lt;", ["get", "a"], ["get", "b"]]</code> is attempting
+                                to compare strings or numbers. In situations like this, you can use one of
+                                the <em>type assertion</em> expression operators to indicate the expected type of a
+                                data expression: <code>["&lt;", ["number", ["get", "a"]], ["number", ["get", "b"]]]</code>.
+                                A type assertion checks that the feature data actually matches the expected type of the
+                                data expression. If this check fails, it produces an error and causes the whole
+                                expression to fall back to the default value for the property being defined. The
+                                assertion operators are <a
+                                    href="#expressions-types-array"><code>array</code></a>, <a
+                                    href="#expressions-types-boolean"><code>boolean</code></a>, <a
+                                    href="#expressions-types-number"><code>number</code></a>, and <a
+                                    href="#expressions-types-string"><code>string</code></a>.
+                            </p>
+
+                            <p>Expressions perform only one kind of implicit type conversion: a data expression used in
+                                a context where a <a href="#types-color">color</a> is expected will convert a string
+                                representation of a color to a color value. In all other cases, if you want to convert
+                                between types, you must use one of the <em>type conversion</em> expression operators: <a
+                                    href="#expressions-types-to-boolean"><code>to-boolean</code></a>, <a
+                                    href="#expressions-types-to-number"><code>to-number</code></a>, <a
+                                    href="#expressions-types-to-string"><code>to-string</code></a>, or <a
+                                    href="#expressions-types-to-color"><code>to-color</code></a>. For example, if you
+                                have a feature property that stores numeric values in string format, and you want to use
+                                those values as numbers rather than strings, you can use an expression such
+                                as <code>["to-number", ["get", "property-name"]]</code>.
+                            </p>
 
                             <h3>SDK Support</h3>
                             <p>Support for expressions was introduced to Mapbox GL JS in version 0.41.0. Support in other SDKs
