ca01a1edfc4b0a1b10b85db1a722cc2aa4f6163a
Andrew Harvey <andrew@alantgeo.com.au>
2017-11-30 12:10:42 +1100

    in Style's addLayer and moveLayer the before param is optional

--- src/style/style.js
+++ src/style/style.js
@@ -510,7 +510,7 @@ class Style extends Evented {
     /**
      * Add a layer to the map style. The layer will be inserted before the layer with
      * ID `before`, or appended if `before` is omitted.
-     * @param {string} before  ID of an existing layer to insert before
+     * @param {string} [before] ID of an existing layer to insert before
      */
     addLayer(layerObject: LayerSpecification, before?: string, options?: {validate?: boolean}) {
         this._checkLoaded();
@@ -568,7 +568,7 @@ class Style extends Evented {
      * Moves a layer to a different z-position. The layer will be inserted before the layer with
      * ID `before`, or appended if `before` is omitted.
      * @param {string} id  ID of the layer to move
-     * @param {string} before  ID of an existing layer to insert before
+     * @param {string} [before] ID of an existing layer to insert before
      */
     moveLayer(id: string, before?: string) {
         this._checkLoaded();
