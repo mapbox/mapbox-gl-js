1490f2cf7cc53d03b9b6bbe1021b7c53dce56d14
John Firebaugh <john.firebaugh@gmail.com>
2018-01-04 11:30:10 -0800

    Fix serialization of Equals/NotEquals expressions

--- src/style-spec/expression/definitions/index.js
+++ src/style-spec/expression/definitions/index.js
@@ -27,14 +27,14 @@ const Case = require('./case');
 const Step = require('./step');
 const Interpolate = require('./interpolate');
 const Coalesce = require('./coalesce');
-const Equals = require('./equals');
+const {Equals, NotEquals} = require('./equals');
 
 import type { Expression } from '../expression';
 
 const expressions: { [string]: Class<Expression> } = {
     // special forms
-    '!=': Equals,
     '==': Equals,
+    '!=': NotEquals,
     'array': ArrayAssertion,
     'at': At,
     'boolean': Assertion,
