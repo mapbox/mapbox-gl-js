579abbad9fc8b83a2a2c1de114b2a77472bee52d
Tom Lee <thomas.j.lee@gmail.com>
2018-01-22 10:30:44 -0500

    Expose Resource Timing API (#5991)
    * expose resource timing API on vt load events
    
    [ci skip] with JSON.parse(JSON.stringify()) the serialization handling can be simpler
    
    add resourceTiming to tile when available
    
    * clean up linting and flow errors
    
    * move collectResourceTiming to request object per @anandthakker's suggestion
    
    * get unit tests passing
    
    * test for presence of collectResourceTiming on vt source dispatch event
    
    * vt worker src tests; wrapper for performance.getEntriesByName()
    
    * resolve linting error related to unused var
    
    * add resourceTiming collection to GeoJSON sources
    
    * working geoJSON resource timing events
    
    * resolve flow error
    
    * test for presence of collectResourceTiming on geojson source dispatch event
    
    * geojson worker src tests
    
    * missing dep
    
    * update jsdoc to reflect geoJSON worker capabilities as well
    
    * simplify geojson source resourceTiming property type
    
    * cleaner syntax for result object
    
    * fix open obj formatting

--- src/style/style.js
+++ src/style/style.js
@@ -447,6 +447,7 @@ class Style extends Evented {
         const shouldValidate = builtIns.indexOf(source.type) >= 0;
         if (shouldValidate && this._validate(validateStyle.source, `sources.${id}`, source, null, options)) return;
 
+        if (this.map && this.map._collectResourceTiming) (source: any).collectResourceTiming = true;
         const sourceCache = this.sourceCaches[id] = new SourceCache(id, source, this.dispatcher);
         sourceCache.style = this;
         sourceCache.setEventedParent(this, () => ({
