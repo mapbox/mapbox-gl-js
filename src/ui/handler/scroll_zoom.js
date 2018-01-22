b3b278356d2978f8da9d35c08cebf62796849ab3
Anand Thakker <github@anandthakker.net>
2018-01-22 15:54:10 -0500

    Fix ScrollZoomHandler to cooperate with render-loop-driven camera easing
    Fixes #5985

--- src/ui/handler/scroll_zoom.js
+++ src/ui/handler/scroll_zoom.js
@@ -4,9 +4,12 @@ const DOM = require('../../util/dom');
 const util = require('../../util/util');
 const browser = require('../../util/browser');
 const window = require('../../util/window');
+const interpolate = require('../../style-spec/util/interpolate').number;
+const LngLat = require('../../geo/lng_lat');
 
 import type Map from '../map';
 import type Point from '@mapbox/point-geometry';
+import type Transform from '../../geo/transform';
 
 const ua = window.navigator.userAgent.toLowerCase(),
     firefox = ua.indexOf('firefox') !== -1,
@@ -21,20 +24,35 @@ class ScrollZoomHandler {
     _map: Map;
     _el: HTMLElement;
     _enabled: boolean;
+    _active: boolean;
     _aroundCenter: boolean;
-    _time: number;
-    _pos: Point;
+    _around: Point;
+    _aroundPoint: Point;
     _type: 'wheel' | 'trackpad' | null;
     _lastValue: number;
-    _timeout: ?number;
+    _timeout: ?number; // used for delayed-handling of a single wheel movement
+    _finishTimeout: ?number; // used to delay final '{move,zoom}end' events
+
+    _lastWheelEvent: any;
+    _lastWheelEventTime: number;
+
+    _startZoom: number;
+    _targetZoom: number;
+    _delta: number;
+    _easing: (number) => number;
+    _prevEase: {start: number, duration: number, easing: (number) => number};
 
     constructor(map: Map) {
         this._map = map;
         this._el = map.getCanvasContainer();
 
+        this._delta = 0;
+
         util.bindAll([
             '_onWheel',
-            '_onTimeout'
+            '_onTimeout',
+            '_onScrollFrame',
+            '_onScrollFinished'
         ], this);
     }
 
@@ -45,6 +63,10 @@ class ScrollZoomHandler {
      */
     isEnabled() {
         return !!this._enabled;
+    }
+
+    isActive() {
+        return !!this._active;
     }
 
     /**
@@ -95,10 +117,9 @@ class ScrollZoomHandler {
         }
 
         const now = browser.now(),
-            timeDelta = now - (this._time || 0);
+            timeDelta = now - (this._lastWheelEventTime || 0);
 
-        this._pos = DOM.mousePos(this._el, e);
-        this._time = now;
+        this._lastWheelEventTime = now;
 
         if (value !== 0 && (value % 4.000244140625) === 0) {
             // This one is definitely a mouse wheel event.
@@ -135,33 +156,106 @@ class ScrollZoomHandler {
         if (e.shiftKey && value) value = value / 4;
 
         // Only fire the callback if we actually know what type of scrolling device the user uses.
-        if (this._type) this._zoom(-value, e);
+        if (this._type) {
+            if (!this.isActive()) {
+                this._active = true;
+                this._map.moving = true;
+                this._map.zooming = true;
+                this._map.fire('movestart', {originalEvent: e});
+                this._map.fire('zoomstart', {originalEvent: e});
+                clearTimeout(this._finishTimeout);
+
+                const pos = DOM.mousePos(this._el, e);
+
+                this._around = LngLat.convert(this._aroundCenter ? this._map.getCenter() : this._map.unproject(pos));
+                this._aroundPoint = this._map.transform.locationPoint(this._around);
+                this._map._startAnimation(this._onScrollFrame, this._onScrollFinished);
+            }
+
+            this._lastWheelEvent = e;
+            this._delta -= value;
+        }
 
         e.preventDefault();
     }
 
     _onTimeout() {
         this._type = 'wheel';
-        this._zoom(-this._lastValue);
+        this._delta -= this._lastValue;
     }
 
-    _zoom(delta: number, e?: Event) {
-        if (delta === 0) return;
-        const map = this._map;
+    _onScrollFrame(tr: Transform) {
+        if (!this.isActive()) return;
 
-        // Scale by sigmoid of scroll wheel delta.
-        let scale = 2 / (1 + Math.exp(-Math.abs(delta / 100)));
-        if (delta < 0 && scale !== 0) scale = 1 / scale;
+        // if we've had scroll events since the last render frame, consume the
+        // accumulated delta, and update the target zoom level accordingly
+        if (this._delta !== 0) {
+            // Scale by sigmoid of scroll wheel delta.
+            let scale = 2 / (1 + Math.exp(-Math.abs(this._delta / 100)));
+            if (this._delta < 0 && scale !== 0) scale = 1 / scale;
+            const fromScale = tr.scale;
+            this._targetZoom = tr.scaleZoom(fromScale * scale);
 
-        const fromScale = map.ease ? (map.ease: any).to : map.transform.scale,
-            targetZoom = map.transform.scaleZoom(fromScale * scale);
+            // if this is a mouse wheel, refresh the starting zoom and easing
+            // function we're using to smooth out the zooming between wheel
+            // events
+            if (this._type === 'wheel') {
+                this._startZoom = tr.zoom;
+                this._easing = this._smoothOutEasing(200);
+            }
 
-        map.zoomTo(targetZoom, {
-            duration: this._type === 'wheel' ? 200 : 0,
-            around: this._aroundCenter ? map.getCenter() : map.unproject(this._pos),
-            delayEndEvents: 200,
-            smoothEasing: true
-        }, { originalEvent: e });
+            this._delta = 0;
+        }
+
+        if (this._type === 'wheel') {
+            const t = Math.min((browser.now() - this._lastWheelEventTime) / 200, 1);
+            const k = this._easing(t);
+            tr.zoom = interpolate(this._startZoom, this._targetZoom, k);
+            if (t === 1) this._map.stop();
+        } else {
+            tr.zoom = this._targetZoom;
+            this._map.stop();
+        }
+
+        tr.setLocationAtPoint(this._around, this._aroundPoint);
+
+        this._map.fire('move', {originalEvent: this._lastWheelEvent});
+        this._map.fire('zoom', {originalEvent: this._lastWheelEvent});
+    }
+
+    _onScrollFinished() {
+        if (!this.isActive()) return;
+        this._active = false;
+        this._finishTimeout = setTimeout(() => {
+            this._map.moving = false;
+            this._map.zooming = false;
+            this._map.fire('zoomend');
+            this._map.fire('moveend');
+        }, 200);
+    }
+
+    _smoothOutEasing(duration: number) {
+        let easing = util.ease;
+
+        if (this._prevEase) {
+            const ease = this._prevEase,
+                t = (browser.now() - ease.start) / ease.duration,
+                speed = ease.easing(t + 0.01) - ease.easing(t),
+
+                // Quick hack to make new bezier that is continuous with last
+                x = 0.27 / Math.sqrt(speed * speed + 0.0001) * 0.01,
+                y = Math.sqrt(0.27 * 0.27 - x * x);
+
+            easing = util.bezier(x, y, 0.25, 1);
+        }
+
+        this._prevEase = {
+            start: browser.now(),
+            duration: duration,
+            easing: easing
+        };
+
+        return easing;
     }
 }
 
