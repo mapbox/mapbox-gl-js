c7cfa01a999216062bdaedd6756802b8f368e9e5
Chris Loer <chris.loer@gmail.com>
2018-01-25 12:05:36 -0800

    WorkerSource per-source-type -> per-source
    - Instead of making WorkerSource implementations responsible for tracking which source's messages they're processing, instantiate a different WorkerSource for each source, and make Worker responsible for forwarding messages to the correct instance.
    
    - Simplifies GeoJSONWorkerSource's coalescing logic.
    
    - Introduces a somewhat subtle requirement for Source implementers: any Source that sends a 'removeSource' message is responsible for sending no further messages. To re-add a Source with the same ID, create a new Source object and start sending messages. A new WorkerSource will be instantiated with matching state.

--- test/unit/source/geojson_worker_source.test.js
+++ test/unit/source/geojson_worker_source.test.js
@@ -6,54 +6,6 @@ const StyleLayerIndex = require('../../../src/style/style_layer_index');
 const OverscaledTileID = require('../../../src/source/tile_id').OverscaledTileID;
 const perf = require('../../../src/util/performance');
 
-test('removeSource', (t) => {
-    t.test('removes the source from _geoJSONIndexes', (t) => {
-        const source = new GeoJSONWorkerSource(null, new StyleLayerIndex());
-        const geoJson = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [0, 0]
-            }
-        };
-
-        function addData(callback) {
-            source.loadData({ source: 'source', data: JSON.stringify(geoJson) }, (err) => {
-                t.equal(err, null);
-                callback();
-            });
-        }
-
-        function loadTile(callback) {
-            const loadVectorDataOpts = {
-                source: 'source',
-                tileID: new OverscaledTileID(0, 0, 0, 0, 0),
-                maxZoom: 10
-            };
-            source.loadVectorData(loadVectorDataOpts, (err, vectorTile) => {
-                t.equal(err, null);
-                callback(vectorTile);
-            });
-        }
-
-        addData(() => {
-            loadTile((vectorTile) => {
-                t.notEqual(vectorTile, null);
-                source.removeSource({ source: 'source' }, (err, res) => {
-                    t.false(err);
-                    t.false(res);
-                });
-                loadTile((vectorTile) => {
-                    t.equal(vectorTile, null);
-                    t.end();
-                });
-            });
-        });
-    });
-
-    t.end();
-});
-
 test('reloadTile', (t) => {
     t.test('does not rebuild vector data unless data has changed', (t) => {
         const layers = [
@@ -259,56 +211,28 @@ test('loadData', (t) => {
         });
     });
 
-    t.test('does not mix coalesce state between sources', (t) => {
-        // Expect first and second calls to run independently,
-        // and third call should run in response to coalesce
-        // from first call.
+    t.test('removeSource aborts callbacks', (t) => {
+        // Expect:
+        // First loadData starts running before removeSource arrives
+        // Second loadData is pending when removeSource arrives, gets cancelled
+        // removeSource is executed immediately
+        // First loadData finishes running, sends results back to foreground
         const worker = createWorker();
-        worker.loadData({ source: 'source1', data: JSON.stringify(geoJson) }, (err, abandoned) => {
-            t.equal(err, null);
-            t.notOk(abandoned);
-            worker.coalesce({ source: 'source1' });
-        });
-
-        worker.loadData({ source: 'source2', data: JSON.stringify(geoJson) }, (err, abandoned) => {
-            t.equal(err, null);
-            t.notOk(abandoned);
-        });
-
         worker.loadData({ source: 'source1', data: JSON.stringify(geoJson) }, (err, abandoned) => {
             t.equal(err, null);
             t.notOk(abandoned);
             t.end();
         });
-    });
-
-    t.test('does not mix stored callbacks between sources', (t) => {
-        // Two loadData calls per source means no calls should
-        // be abandoned.
-        const worker = createWorker();
-        worker.loadData({ source: 'source1', data: JSON.stringify(geoJson) }, (err, abandoned) => {
-            t.equal(err, null);
-            t.notOk(abandoned);
-            worker.coalesce({ source: 'source1' });
-        });
-
-        worker.loadData({ source: 'source2', data: JSON.stringify(geoJson) }, (err, abandoned) => {
-            t.equal(err, null);
-            t.notOk(abandoned);
-            worker.coalesce({ source: 'source2' });
-        });
-
-        worker.loadData({ source: 'source2', data: JSON.stringify(geoJson) }, (err, abandoned) => {
-            t.equal(err, null);
-            t.notOk(abandoned);
-            // test ends here because source2 has the last coalesce call
-            t.end();
-        });
 
         worker.loadData({ source: 'source1', data: JSON.stringify(geoJson) }, (err, abandoned) => {
             t.equal(err, null);
-            t.notOk(abandoned);
+            t.ok(abandoned);
         });
+
+        worker.removeSource({ source: 'source1' }, (err) => {
+            t.notOk(err);
+        });
+
     });
 
     t.end();
