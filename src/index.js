0ff014ede0280d5553d235deb58e88cde3fac69b
Chris Loer <chris.loer@gmail.com>
2018-02-15 15:32:22 -0800

    Update rtl-text plugin to 0.1.2 in example/doc/test.

--- src/index.js
+++ src/index.js
@@ -87,6 +87,6 @@ module.exports = {
  * @param {string} pluginURL URL pointing to the Mapbox RTL text plugin source.
  * @param {Function} callback Called with an error argument if there is an error.
  * @example
- * mapboxgl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.1/mapbox-gl-rtl-text.js');
+ * mapboxgl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.2/mapbox-gl-rtl-text.js');
  * @see [Add support for right-to-left scripts](https://www.mapbox.com/mapbox-gl-js/example/mapbox-gl-rtl-text/)
  */
