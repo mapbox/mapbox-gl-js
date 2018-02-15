c7a841ec4d84d6c0a1e7b8cc956700fd320eae80
John Firebaugh <john.firebaugh@gmail.com>
2018-02-14 16:40:15 -0800

    Add more DragPanHandler tests

--- test/unit/ui/handler/drag_pan.test.js
+++ test/unit/ui/handler/drag_pan.test.js
@@ -10,6 +10,110 @@ function createMap() {
     return new Map({ container: DOM.create('div', '', window.document.body) });
 }
 
+test('DragPanHandler fires dragstart, drag, and dragend events at appropriate times in response to a mouse-triggered drag', (t) => {
+    const map = createMap();
+
+    const dragstart = t.spy();
+    const drag      = t.spy();
+    const dragend   = t.spy();
+
+    map.on('dragstart', dragstart);
+    map.on('drag',      drag);
+    map.on('dragend',   dragend);
+
+    simulate.mousedown(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 0);
+    t.equal(drag.callCount, 0);
+    t.equal(dragend.callCount, 0);
+
+    simulate.mousemove(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 0);
+
+    simulate.mouseup(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
+test('DragPanHandler fires dragstart, drag, and dragend events at appropriate times in response to a touch-triggered drag', (t) => {
+    const map = createMap();
+
+    const dragstart = t.spy();
+    const drag      = t.spy();
+    const dragend   = t.spy();
+
+    map.on('dragstart', dragstart);
+    map.on('drag',      drag);
+    map.on('dragend',   dragend);
+
+    simulate.touchstart(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 0);
+    t.equal(drag.callCount, 0);
+    t.equal(dragend.callCount, 0);
+
+    simulate.touchmove(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 0);
+
+    simulate.touchend(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
+test('DragPanHandler ends a mouse-triggered drag if the window blurs', (t) => {
+    const map = createMap();
+
+    const dragend = t.spy();
+    map.on('dragend', dragend);
+
+    simulate.mousedown(map.getCanvas());
+    map._updateCamera();
+
+    simulate.mousemove(map.getCanvas());
+    map._updateCamera();
+
+    simulate.blur(window);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
+test('DragPanHandler ends a touch-triggered drag if the window blurs', (t) => {
+    const map = createMap();
+
+    const dragend = t.spy();
+    map.on('dragend', dragend);
+
+    simulate.touchstart(map.getCanvas());
+    map._updateCamera();
+
+    simulate.touchmove(map.getCanvas());
+    map._updateCamera();
+
+    simulate.blur(window);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
 test('DragPanHandler requests a new render frame after each mousemove event', (t) => {
     const map = createMap();
     const update = t.spy(map, '_update');
