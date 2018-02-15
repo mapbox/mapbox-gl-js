b7fb6fdfafa571f1dcae797d367901042c91a8b3
Chris Loer <chris.loer@gmail.com>
2018-02-14 17:04:16 -0800

    Don't create RTL text plugin blob on foreground. Avoids CSP script-src constraints. Fixes issue #6169.

--- src/source/rtl_text_plugin.js
+++ src/source/rtl_text_plugin.js
@@ -1,51 +1,38 @@
 // @flow
-
-const ajax = require('../util/ajax');
 const Evented = require('../util/evented');
-const window = require('../util/window');
 
 let pluginRequested = false;
-let pluginBlobURL = null;
+let pluginURL = null;
 
 module.exports.evented = new Evented();
 
 type ErrorCallback = (error: Error) => void;
 
 module.exports.registerForPluginAvailability = function(
-    callback: (args: {pluginBlobURL: string, errorCallback: ErrorCallback}) => void
+    callback: (args: {pluginURL: string, errorCallback: ErrorCallback}) => void
 ) {
-    if (pluginBlobURL) {
-        callback({ pluginBlobURL: pluginBlobURL, errorCallback: module.exports.errorCallback});
+    if (pluginURL) {
+        callback({ pluginURL: pluginURL, errorCallback: module.exports.errorCallback});
     } else {
         module.exports.evented.once('pluginAvailable', callback);
     }
     return callback;
 };
 
-// Exposed so it can be stubbed out by tests
-module.exports.createBlobURL = function(response: Object) {
-    return window.URL.createObjectURL(new window.Blob([response.data], {type: "text/javascript"}));
-};
 // Only exposed for tests
 module.exports.clearRTLTextPlugin = function() {
     pluginRequested = false;
-    pluginBlobURL = null;
+    pluginURL = null;
 };
 
-module.exports.setRTLTextPlugin = function(pluginURL: string, callback: ErrorCallback) {
+module.exports.setRTLTextPlugin = function(url: string, callback: ErrorCallback) {
     if (pluginRequested) {
         throw new Error('setRTLTextPlugin cannot be called multiple times.');
     }
     pluginRequested = true;
+    pluginURL = url;
     module.exports.errorCallback = callback;
-    ajax.getArrayBuffer({ url: pluginURL }, (err, response) => {
-        if (err) {
-            callback(err);
-        } else if (response) {
-            pluginBlobURL = module.exports.createBlobURL(response);
-            module.exports.evented.fire('pluginAvailable', { pluginBlobURL: pluginBlobURL, errorCallback: callback });
-        }
-    });
+    module.exports.evented.fire('pluginAvailable', { pluginURL: pluginURL, errorCallback: callback });
 };
 
 module.exports.applyArabicShaping = (null: ?Function);
