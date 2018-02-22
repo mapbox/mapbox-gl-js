13af776c8a2c711756d4107cb5fb04d2a4a3b5f1
John Firebaugh <john.firebaugh@gmail.com>
2018-02-21 17:33:28 -0800

    Add more tests

---
+++ test/unit/ui/handler/box_zoom.test.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const test = require('mapbox-gl-js-test').test;
+const window = require('../../../../src/util/window');
+const Map = require('../../../../src/ui/map');
+const DOM = require('../../../../src/util/dom');
+const simulate = require('mapbox-gl-js-test/simulate_interaction');
+
+function createMap() {
+    return new Map({ container: DOM.create('div', '', window.document.body) });
+}
+
+test('BoxZoomHandler fires boxzoomstart and boxzoomend events at appropriate times', (t) => {
+    const map = createMap();
+
+    const boxzoomstart = t.spy();
+    const boxzoomend   = t.spy();
+
+    map.on('boxzoomstart', boxzoomstart);
+    map.on('boxzoomend',   boxzoomend);
+
+    simulate.mousedown(map.getCanvas(), {shiftKey: true, clientX: 0, clientY: 0});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 0);
+    t.equal(boxzoomend.callCount, 0);
+
+    simulate.mousemove(map.getCanvas(), {shiftKey: true, clientX: 5, clientY: 5});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 1);
+    t.equal(boxzoomend.callCount, 0);
+
+    simulate.mouseup(map.getCanvas(), {shiftKey: true, clientX: 5, clientY: 5});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 1);
+    t.equal(boxzoomend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
+test('BoxZoomHandler avoids conflicts with DragPanHandler when disabled and reenabled (#2237)', (t) => {
+    const map = createMap();
+
+    map.boxZoom.disable();
+    map.boxZoom.enable();
+
+    const boxzoomstart = t.spy();
+    const boxzoomend   = t.spy();
+
+    map.on('boxzoomstart', boxzoomstart);
+    map.on('boxzoomend',   boxzoomend);
+
+    const dragstart = t.spy();
+    const drag      = t.spy();
+    const dragend   = t.spy();
+
+    map.on('dragstart', dragstart);
+    map.on('drag',      drag);
+    map.on('dragend',   dragend);
+
+    simulate.mousedown(map.getCanvas(), {shiftKey: true, clientX: 0, clientY: 0});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 0);
+    t.equal(boxzoomend.callCount, 0);
+
+    simulate.mousemove(map.getCanvas(), {shiftKey: true, clientX: 5, clientY: 5});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 1);
+    t.equal(boxzoomend.callCount, 0);
+
+    simulate.mouseup(map.getCanvas(), {shiftKey: true, clientX: 5, clientY: 5});
+    map._updateCamera();
+    t.equal(boxzoomstart.callCount, 1);
+    t.equal(boxzoomend.callCount, 1);
+
+    t.equal(dragstart.callCount, 0);
+    t.equal(drag.callCount, 0);
+    t.equal(dragend.callCount, 0);
+
+    map.remove();
+    t.end();
+});
--- test/unit/ui/handler/drag_pan.test.js
+++ test/unit/ui/handler/drag_pan.test.js
@@ -43,6 +43,39 @@ test('DragPanHandler fires dragstart, drag, and dragend events at appropriate ti
     t.end();
 });
 
+test('DragPanHandler captures mousemove events during a mouse-triggered drag (receives them even if they occur outside the map)', (t) => {
+    const map = createMap();
+
+    const dragstart = t.spy();
+    const drag      = t.spy();
+    const dragend   = t.spy();
+
+    map.on('dragstart', dragstart);
+    map.on('drag',      drag);
+    map.on('dragend',   dragend);
+
+    simulate.mousedown(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 0);
+    t.equal(drag.callCount, 0);
+    t.equal(dragend.callCount, 0);
+
+    simulate.mousemove(window.document.body);
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 0);
+
+    simulate.mouseup(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
 test('DragPanHandler fires dragstart, drag, and dragend events at appropriate times in response to a touch-triggered drag', (t) => {
     const map = createMap();
 
@@ -61,6 +94,39 @@ test('DragPanHandler fires dragstart, drag, and dragend events at appropriate ti
     t.equal(dragend.callCount, 0);
 
     simulate.touchmove(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 0);
+
+    simulate.touchend(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 1);
+    t.equal(drag.callCount, 1);
+    t.equal(dragend.callCount, 1);
+
+    map.remove();
+    t.end();
+});
+
+test('DragPanHandler captures touchmove events during a mouse-triggered drag (receives them even if they occur outside the map)', (t) => {
+    const map = createMap();
+
+    const dragstart = t.spy();
+    const drag      = t.spy();
+    const dragend   = t.spy();
+
+    map.on('dragstart', dragstart);
+    map.on('drag',      drag);
+    map.on('dragend',   dragend);
+
+    simulate.touchstart(map.getCanvas());
+    map._updateCamera();
+    t.equal(dragstart.callCount, 0);
+    t.equal(drag.callCount, 0);
+    t.equal(dragend.callCount, 0);
+
+    simulate.touchmove(window.document.body);
     map._updateCamera();
     t.equal(dragstart.callCount, 1);
     t.equal(drag.callCount, 1);
