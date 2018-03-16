72266413c1ea0616832c4e607293341822c10ae8
Anand Thakker <anandthakker@users.noreply.github.com>
2018-03-16 13:03:19 -0400

    Fix lint in shaders/index.js
    eslint-disable directive needed to be above the `require('fs')`

--- src/shaders/index.js
+++ src/shaders/index.js
@@ -1,11 +1,11 @@
 // @flow
 
-const fs = require('fs');
-
 // We use brfs, a browserify transform, to inline shader sources during bundling. As a result:
 // - readFileSync calls must be written out long-form
 // - this module must use CommonJS rather than ES2015 syntax
 /* eslint-disable prefer-template, no-path-concat, import/unambiguous */
+
+const fs = require('fs');
 
 const shaders: {[string]: {fragmentSource: string, vertexSource: string}} = {
     prelude: {
