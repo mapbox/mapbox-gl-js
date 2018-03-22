7fcb6af63233ef26c9216de7e9378590b3adb7e0
Chris Loer <chris.loer@gmail.com>
2018-03-22 14:12:30 -0700

    Add 'collator' expression for locale-aware string comparison: ['collator',   <case-sensitive>,   <diacritic-sensitive>,   (optional) <IETF language tag>]
    Add 'resolved-locale' to test if requested collation locale is available:
    ['resolved-locale', <collator>]
    
    '==','!=','<','<=,'>','>=' now take an optional 'collator' argument when they are used with 'string' arguments.

--- src/style-spec/expression/definitions/equals.js
+++ src/style-spec/expression/definitions/equals.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { toString, ValueType, BooleanType } from '../types';
+import { toString, ValueType, BooleanType, CollatorType } from '../types';
 
 import type { Expression } from '../expression';
 import type EvaluationContext from '../evaluation_context';
@@ -28,21 +28,23 @@ function isComparableType(type: Type) {
  *
  * @private
  */
-function makeComparison(compare) {
+function makeComparison(negate) {
     return class Comparison implements Expression {
         type: Type;
         lhs: Expression;
         rhs: Expression;
+        collator: Expression | null;
 
-        constructor(lhs: Expression, rhs: Expression) {
+        constructor(lhs: Expression, rhs: Expression, collator: Expression | null) {
             this.type = BooleanType;
             this.lhs = lhs;
             this.rhs = rhs;
+            this.collator = collator;
         }
 
         static parse(args: Array<mixed>, context: ParsingContext): ?Expression {
-            if (args.length !== 3)
-                return context.error(`Expected two arguments.`);
+            if (args.length !== 3 && args.length !== 4)
+                return context.error(`Expected two or three arguments.`);
 
             const lhs = context.parse(args[1], 1, ValueType);
             if (!lhs) return null;
@@ -57,16 +59,32 @@ function makeComparison(compare) {
                 return context.error(`Cannot compare ${toString(lhs.type)} and ${toString(rhs.type)}.`);
             }
 
-            return new Comparison(lhs, rhs);
+            let collator = null;
+            if (args.length === 4) {
+                if (lhs.type.kind !== 'string' && rhs.type.kind !== 'string') {
+                    return context.error(`Cannot use collator to compare non-string types.`);
+                }
+                collator = context.parse(args[3], 3, CollatorType);
+                if (!collator) return null;
+            }
+
+            return new Comparison(lhs, rhs, collator);
         }
 
         evaluate(ctx: EvaluationContext) {
-            return compare(this.lhs.evaluate(ctx), this.rhs.evaluate(ctx));
+            const equal = this.collator ?
+                this.collator.evaluate(ctx).compare(this.lhs.evaluate(ctx), this.rhs.evaluate(ctx)) === 0 :
+                this.lhs.evaluate(ctx) === this.rhs.evaluate(ctx);
+
+            return negate ? !equal : equal;
         }
 
         eachChild(fn: (Expression) => void) {
             fn(this.lhs);
             fn(this.rhs);
+            if (this.collator) {
+                fn(this.collator);
+            }
         }
 
         possibleOutputs() {
@@ -75,5 +93,5 @@ function makeComparison(compare) {
     };
 }
 
-export const Equals = makeComparison((lhs, rhs) => lhs === rhs);
-export const NotEquals = makeComparison((lhs, rhs) => lhs !== rhs);
+export const Equals = makeComparison(false);
+export const NotEquals = makeComparison(true);
