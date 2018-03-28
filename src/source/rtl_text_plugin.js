f5fc4dc9acaf6183db838d30a980c50e901f1f8e
Chris Loer <chris.loer@gmail.com>
2018-03-28 09:28:58 -0700

    RTLTextPlugin changes to support 'is-renderable' expression: - Pass completion callback back to foreground so it can answer whether plugin has loaded - Add `isLoaded()` method that can be checked from foreground or background - Serialize error message w/ `toString()` before passing to callback, since we don't have a serialization for `Error#message`

--- src/source/rtl_text_plugin.js
+++ src/source/rtl_text_plugin.js
@@ -4,25 +4,26 @@ import { Event, Evented } from '../util/evented';
 
 let pluginRequested = false;
 let pluginURL = null;
+let foregroundLoadComplete = false;
 
 export const evented = new Evented();
 
+type CompletionCallback = (error?: Error) => void;
 type ErrorCallback = (error: Error) => void;
 
-let _errorCallback;
+let _completionCallback;
 
 export const registerForPluginAvailability = function(
-    callback: (args: {pluginURL: string, errorCallback: ErrorCallback}) => void
+    callback: (args: {pluginURL: string, completionCallback: CompletionCallback}) => void
 ) {
     if (pluginURL) {
-        callback({ pluginURL: pluginURL, errorCallback: _errorCallback});
+        callback({ pluginURL: pluginURL, completionCallback: _completionCallback});
     } else {
         evented.once('pluginAvailable', callback);
     }
     return callback;
 };
 
-// Only exposed for tests
 export const clearRTLTextPlugin = function() {
     pluginRequested = false;
     pluginURL = null;
@@ -34,14 +35,30 @@ export const setRTLTextPlugin = function(url: string, callback: ErrorCallback) {
     }
     pluginRequested = true;
     pluginURL = url;
-    evented.fire(new Event('pluginAvailable', { pluginURL: pluginURL, errorCallback: callback }));
-    _errorCallback = callback;
+    _completionCallback = (error?: Error) => {
+        if (error) {
+            // Clear loaded state to allow retries
+            clearRTLTextPlugin();
+            if (callback) {
+                callback(error);
+            }
+        } else {
+            // Called once for each worker
+            foregroundLoadComplete = true;
+        }
+    };
+    evented.fire(new Event('pluginAvailable', { pluginURL: pluginURL, completionCallback: _completionCallback }));
 };
 
 export const plugin: {
     applyArabicShaping: ?Function,
-    processBidirectionalText: ?(string, Array<number>) => Array<string>
+    processBidirectionalText: ?(string, Array<number>) => Array<string>,
+    isLoaded: () => boolean
 } = {
     applyArabicShaping: null,
-    processBidirectionalText: null
+    processBidirectionalText: null,
+    isLoaded: function() {
+        return foregroundLoadComplete ||       // Foreground: loaded if the completion callback returned successfully
+            plugin.applyArabicShaping != null; // Background: loaded if the plugin functions have been compiled
+    }
 };
