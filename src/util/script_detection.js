f3df91f39100f1853b7b7bd0db23e7a87c2e12f7
Chris Loer <chris.loer@gmail.com>
2018-03-28 09:28:58 -0700

    Add `isStringInSupportedScript` function. Heuristically evaluates string as "likely to render correctly" based on an audit of current rendering support.

--- src/util/script_detection.js
+++ src/util/script_detection.js
@@ -266,3 +266,41 @@ export function charHasRotatedVerticalOrientation(char: number) {
     return !(charHasUprightVerticalOrientation(char) ||
              charHasNeutralVerticalOrientation(char));
 }
+
+export function charInSupportedScript(char: number, canRenderRTL: boolean) {
+    // This is a rough heuristic: whether we "can render" a script
+    // actually depends on the properties of the font being used
+    // and whether differences from the ideal rendering are considered
+    // semantically significant.
+
+    // Even in Latin script, we "can't render" combinations such as the fi
+    // ligature, but we don't consider that semantically significant.
+    if (!canRenderRTL &&
+        ((char >= 0x0590 && char <= 0x08FF) ||
+         isChar['Arabic Presentation Forms-A'](char) ||
+         isChar['Arabic Presentation Forms-B'](char))) {
+        // Main blocks for Hebrew, Arabic, Thaana and other RTL scripts
+        return false;
+    }
+    if ((char >= 0x0900 && char <= 0x0DFF) ||
+        // Main blocks for Indic scripts and Sinhala
+        (char >= 0x0E80 && char <= 0x109F) ||
+        // Main blocks for Lao, Tibetan, and Myanmar
+        isChar['Khmer'](char)) {
+        // These blocks cover common scripts that require
+        // complex text shaping, based on unicode script metadata:
+        // http://www.unicode.org/repos/cldr/trunk/common/properties/scriptMetadata.txt
+        // where "Web Rank <= 32" "Shaping Required = YES"
+        return false;
+    }
+    return true;
+}
+
+export function isStringInSupportedScript(chars: string, canRenderRTL: boolean) {
+    for (const char of chars) {
+        if (!charInSupportedScript(char.charCodeAt(0), canRenderRTL)) {
+            return false;
+        }
+    }
+    return true;
+}
