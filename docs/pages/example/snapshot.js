2800ee50b5bc7fd128357882564d46042c082d13
Sam Murphy <samsammurphy@gmail.com>
2018-04-16 14:00:48 +0100

    snapshot (basic outline)

---
+++ docs/pages/example/snapshot.js
@@ -0,0 +1,10 @@
+/*---
+title: Snapshot
+description: Capture a snapshot image of a map
+tags:
+  - user-interaction
+pathname: /mapbox-gl-js/example/snapshot/
+---*/
+import Example from '../../components/example';
+import html from './snapshot.html';
+export default Example(html);
