fa6410f3322a4ca6665af165a74b02253aefe9e6
Andrew Harvey <andrew@alantgeo.com.au>
2018-05-01 09:04:37 +1000

    move point-from-geocoder-result into geocoder examples group (#6571)

--- docs/pages/example/point-from-geocoder-result.js
+++ docs/pages/example/point-from-geocoder-result.js
@@ -4,7 +4,7 @@ description: >-
   Listen to the `geocoder.input` event from the [Geocoder plugin](https://github.com/mapbox/mapbox-gl-geocoder) and
   place a point on the coordinate results.
 tags:
-  - controls-and-overlays
+  - geocoder
 pathname: /mapbox-gl-js/example/point-from-geocoder-result/
 ---*/
 import Example from '../../components/example';
