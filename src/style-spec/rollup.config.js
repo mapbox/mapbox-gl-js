3f43d809da2b93ba7b9a6385000f0a73ef4da027
Anand Thakker <anandthakker@users.noreply.github.com>
2018-04-30 15:52:54 +0200

    Fix style-spec build (#6575)

--- src/style-spec/rollup.config.js
+++ src/style-spec/rollup.config.js
@@ -1,5 +1,11 @@
 import replace from 'rollup-plugin-replace';
-import {plugins} from '../../build/rollup_plugins';
+import buble from 'rollup-plugin-buble';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import unassert from 'rollup-plugin-unassert';
+import json from 'rollup-plugin-json';
+import {flow} from '../../build/rollup_plugins';
+
 const config = [{
     input: `${__dirname}/style-spec.js`,
     output: {
@@ -16,7 +22,16 @@ const config = [{
             values: {
                 '_token_stack:': ''
             }
-        })
-    ].concat(plugins())
+        }),
+        flow(),
+        json(),
+        buble({transforms: {dangerousForOf: true}, objectAssign: "Object.assign"}),
+        unassert(),
+        resolve({
+            browser: true,
+            preferBuiltins: false
+        }),
+        commonjs()
+    ]
 }];
 export default config;
