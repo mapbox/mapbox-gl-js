9cb86dfef95ba195a973a35aa13a5cebd9629d07
Anand Thakker <github@anandthakker.net>
2018-05-01 16:25:05 -0400

    Ensure style spec is published with build
    Closes #6433
    Closes #6574

--- test/build/style-spec.test.js
+++ test/build/style-spec.test.js
@@ -4,7 +4,6 @@
 const test = require('mapbox-gl-js-test').test;
 const fs = require('fs');
 const path = require('path');
-const exec = require('child_process').exec;
 const isBuiltin = require('is-builtin-module');
 
 const Linter = require('eslint').Linter;
@@ -15,55 +14,43 @@ import rollupConfig from '../../src/style-spec/rollup.config';
 // some paths
 const styleSpecDirectory = path.join(__dirname, '../../src/style-spec');
 const styleSpecPackage = require('../../src/style-spec/package.json');
-const styleSpecDistBundle = path.join(styleSpecDirectory, styleSpecPackage.main);
+const styleSpecDistBundle = fs.readFileSync(path.join(__dirname, '../../dist/style-spec/index.js'), 'utf-8');
 
 test('@mapbox/mapbox-gl-style-spec npm package', (t) => {
-    // simulate npm install of @mapbox/mapbox-gl-style-spec
     t.test('build plain ES5 bundle in prepublish', (t) => {
-        t.tearDown(() => {
-            fs.unlink(styleSpecDistBundle, (error) => {
-                if (error) console.error(error);
-            });
-        });
+        const linter = new Linter();
+        const messages = linter.verify(styleSpecDistBundle, {
+            parserOptions: {
+                ecmaVersion: 5
+            },
+            rules: {},
+            env: {
+                node: true
+            }
+        }).map(message => `${message.line}:${message.column}: ${message.message}`);
+        t.deepEqual(messages, [], 'distributed bundle is plain ES5 code');
 
-        exec(`rm -f ${styleSpecDistBundle} && npm run build`, { cwd: styleSpecDirectory }, (error) => {
-            t.error(error);
-            t.ok(fs.existsSync(styleSpecDistBundle), 'dist bundle exists');
-
-            const linter = new Linter();
-            const messages = linter.verify(fs.readFileSync(styleSpecDistBundle, 'utf-8'), {
-                parserOptions: {
-                    ecmaVersion: 5
-                },
-                rules: {},
-                env: {
-                    node: true
-                }
-            }).map(message => `${message.line}:${message.column}: ${message.message}`);
-            t.deepEqual(messages, [], 'distributed bundle is plain ES5 code');
-
-            t.stub(console, 'warn');
-            rollup.rollup({
-                input: `${styleSpecDirectory}/style-spec.js`,
-                plugins: [{
-                    resolveId: (id, importer) => {
-                        if (
-                            /^[\/\.]/.test(id) ||
-                            isBuiltin(id) ||
-                            /node_modules/.test(importer)
-                        ) {
-                            return null;
-                        }
-
-                        t.ok(styleSpecPackage.dependencies[id], `External dependency ${id} (imported from ${importer}) declared in style-spec's package.json`);
-                        return false;
+        t.stub(console, 'warn');
+        rollup.rollup({
+            input: `${styleSpecDirectory}/style-spec.js`,
+            plugins: [{
+                resolveId: (id, importer) => {
+                    if (
+                        /^[\/\.]/.test(id) ||
+                        isBuiltin(id) ||
+                        /node_modules/.test(importer)
+                    ) {
+                        return null;
                     }
-                }].concat(rollupConfig[0].plugins)
-            }).then(() => {
-                t.end();
-            }).catch(e => {
-                t.error(e);
-            });
+
+                    t.ok(styleSpecPackage.dependencies[id], `External dependency ${id} (imported from ${importer}) declared in style-spec's package.json`);
+                    return false;
+                }
+            }].concat(rollupConfig[0].plugins)
+        }).then(() => {
+            t.end();
+        }).catch(e => {
+            t.error(e);
         });
     });
 
