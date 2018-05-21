90039a05cc633b54826065a280678d08ddc01cbf
ryanhamley <ryan.hamley@mapbox.com>
2018-05-21 14:26:37 -0700

    Add unit tests to ensure docs list all available expressions

--- test/unit/style-spec/expression.test.js
+++ test/unit/style-spec/expression.test.js
@@ -1,5 +1,26 @@
 import { test } from 'mapbox-gl-js-test';
 import { createPropertyExpression } from '../../../src/style-spec/expression';
+import definitions from '../../../src/style-spec/expression/definitions';
+import v8 from '../../../src/style-spec/reference/v8';
+import {expressions as definitionMetadata} from '../../../docs/components/expression-metadata';
+
+// filter out interal "error" and "filter-*" expressions from definition list
+const filterExpressionRegex = /filter-/;
+const definitionList = Object.keys(definitions).filter((expression) => {
+    return expression !== 'error' && !filterExpressionRegex.exec(expression);
+}).sort();
+
+test('v8.json includes all definitions from style-spec', (t) => {
+    const v8List = Object.keys(v8.expression_name.values);
+    t.deepEquals(definitionList, v8List.sort());
+    t.end();
+});
+
+test('expression metadata includes all definitions from style-spec', (t) => {
+    const definitionMetadataList = Object.keys(definitionMetadata);
+    t.deepEquals(definitionList, definitionMetadataList.sort());
+    t.end();
+});
 
 test('createPropertyExpression', (t) => {
     test('prohibits non-interpolable properties from using an "interpolate" expression', (t) => {
