cc3120c4d7bc0c65b8fb0e9f574665895b09ebd1
Andrew Harvey <andrew@alantgeo.com.au>
2018-06-20 22:37:36 +1000

    flow

--- src/ui/marker.js
+++ src/ui/marker.js
@@ -290,7 +290,7 @@ export default class Marker extends Evented {
      * @returns {Marker} `this`
      */
     setPopup(popup: ?Popup) {
-        const keypressListener = (e) => {
+        const keypressListener = (e: Event) => {
             const code = e.charCode || e.keyCode;
 
             if ((code === 32) || (code === 13)) { // space or enter
