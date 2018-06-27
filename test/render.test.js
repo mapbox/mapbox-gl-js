7e3bd7f465f96dd9773669f641e736b2f10ffeaf
John Firebaugh <john.firebaugh@gmail.com>
2018-06-26 20:59:08 -0700

    Upgrade test/integration to ES6 modules

--- test/render.test.js
+++ test/render.test.js
@@ -4,7 +4,7 @@ require('./stub_loader');
 require('@mapbox/flow-remove-types/register');
 require = require("esm")(module, true);
 
-const suite = require('./integration').render;
+const suite = require('./integration/lib/render');
 const suiteImplementation = require('./suite_implementation');
 const ignores = require('./ignores.json');
 
