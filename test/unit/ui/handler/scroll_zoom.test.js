1cc797a3006af4d88474c4836b6bf7b003e6c888
Anand Thakker <github@anandthakker.net>
2018-07-06 13:08:58 -0400

    Add failing test for https://github.com/mapbox/mapbox-gl-js/issues/6782

--- test/unit/ui/handler/scroll_zoom.test.js
+++ test/unit/ui/handler/scroll_zoom.test.js
@@ -113,7 +113,25 @@ test('ScrollZoomHandler', (t) => {
         t.end();
     });
 
-    test('does not zoom if preventDefault is called on the wheel event', (t) => {
+    t.test('Gracefully handle wheel events that cancel each other out before the first scroll frame', (t) => {
+        // See also https://github.com/mapbox/mapbox-gl-js/issues/6782
+        const map = createMap(t);
+        map._renderTaskQueue.run();
+
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -1});
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -1});
+        now += 1;
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: 2});
+
+        map._renderTaskQueue.run();
+
+        now += 400;
+        map._renderTaskQueue.run();
+
+        t.end();
+    });
+
+    t.test('does not zoom if preventDefault is called on the wheel event', (t) => {
         const map = createMap(t);
 
         map.on('wheel', e => e.preventDefault());
