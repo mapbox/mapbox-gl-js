561b0bda2e452fb1cc0d6f08db733af64d27ceec
Asheem Mamoowala <asheem.mamoowala@mapbox.com>
2018-07-24 14:47:39 -0700

    Upgrade supercluster and geojson-vt. Document and add support for geojson-vt generateId

--- src/source/geojson_source.js
+++ src/source/geojson_source.js
@@ -128,7 +128,8 @@ class GeoJSONSource extends Evented implements Source {
                 tolerance: (options.tolerance !== undefined ? options.tolerance : 0.375) * scale,
                 extent: EXTENT,
                 maxZoom: this.maxzoom,
-                lineMetrics: options.lineMetrics || false
+                lineMetrics: options.lineMetrics || false,
+                generateId: options.generateId || false
             },
             superclusterOptions: {
                 maxZoom: options.clusterMaxZoom !== undefined ?
