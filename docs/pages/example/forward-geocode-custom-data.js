88dd20d1b78f8eded241c4ffc89ccb55a6990ca3
kbauhaus <kayla.bauhaus@mapbox.com>
2018-08-03 11:59:16 -0700

    make separate example for forward geocoding custom data

--- docs/pages/example/forward-geocode-custom-data.js
+++ docs/pages/example/forward-geocode-custom-data.js
@@ -1,14 +1,14 @@
 /*---
-title: Supplement reverse geocoding search results from another data source
+title: Supplement forward geocoding search results from another data source
 description: >-
   Use the [mapbox-gl-geocoder](https://github.com/mapbox/mapbox-gl-geocoder)
   control to search for places using Mapbox Geocoding API, supplementing
   results from a local data source or function, in this case to match
-  geographic coordinates provided as input.
+  Chicago Park names with text input.
 tags:
   - geocoder
-pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-local-geocoder/
+pathname: /mapbox-gl-js/example/forward-geocode-custom-data/
 ---*/
 import Example from '../../components/example';
-import html from './mapbox-gl-geocoder-local-geocoder.html';
+import html from './forward-geocode-custom-data.html';
 export default Example(html);
