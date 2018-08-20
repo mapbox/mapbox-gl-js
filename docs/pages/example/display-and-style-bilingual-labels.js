7c4bd16227212e23d183220ecfdf8765121370dd
Mal Wood-Santoro <mallorywood413@gmail.com>
2018-08-20 14:10:10 -0700

    Adds example demonstrating the format expression  (#7151)
    * adds an example utilizing the format expression
    
    * fix indentation
    
    * fix tags
    
    * upate style to streets-10
    
    * add country-label-md and country-label-sm
    
    * fix lint error

---
+++ docs/pages/example/display-and-style-bilingual-labels.js
@@ -0,0 +1,10 @@
+/*---
+title: Display and style bilingual labels
+description: Uses the [format expression](/mapbox-gl-js/style-spec#expressions-types-format) to display country labels in both English and in the local language.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/display-and-style-bilingual-labels/
+---*/
+import Example from '../../components/example';
+import html from './display-and-style-bilingual-labels.html';
+export default Example(html);
