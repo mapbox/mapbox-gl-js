4a1e7c2e08e3f5d0ca6e077c19a89a5045ef8904
linzhongpu <42532869+linzhongpu@users.noreply.github.com>
2018-08-23 21:26:26 +0800

    translate down 100%
    line 2 to line 3

--- docs/pages/example/custom-marker-icons.js
+++ docs/pages/example/custom-marker-icons.js
@@ -1,6 +1,6 @@
 /*---
-title: Add custom icons with Markers
-description: Use [`Marker`](/mapbox-gl-js/api/#marker) to add custom icons to your map.
+title: 用马克笔（Marker）添加自定义图标
+description: 用 [`Marker`](/mapbox-gl-js/api/#marker) 在你的地图上添加自定义图标.
 tags:
   - controls-and-overlays
 pathname: /mapbox-gl-js/example/custom-marker-icons/
