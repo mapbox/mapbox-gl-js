309e866d8463728d2ee8961d7f77b43b980f9a52
lychee <gisllj@qq.com>
2018-08-23 14:45:30 +0800

    translate down 100%

--- docs/pages/example/mapbox-gl-rtl-text.js
+++ docs/pages/example/mapbox-gl-rtl-text.js
@@ -1,7 +1,9 @@
 /*---
 title: 添加从右到左文字的支持
 description: >-
-  使用[mapbox-gl-rtl-text](https://github.com/mapbox/mapbox-gl-rtl-text)插件来支持从右到左（RTL）布局的文字（例如阿拉伯文或希伯来文）。请注意，Mapbox Studio 尚未支持 RTL 文本.
+  使用[mapbox-gl-rtl-text](https://github.com/mapbox/mapbox-gl-rtl-text)插件来支持
+  从右到左（RTL）布局的文字（例如阿拉伯文或希伯来文）。
+  请注意，Mapbox Studio 尚未支持 RTL 文本.
 tags:
   - internationalization
 pathname: /mapbox-gl-js/example/mapbox-gl-rtl-text/
