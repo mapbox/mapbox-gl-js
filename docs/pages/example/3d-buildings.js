e41aa05c75834b15217a6486a6c739ef6eccff79
Jing <jing.liu@mapbox.com>
2018-08-24 12:08:42 +0800

    Update 3d-buildings.js

--- docs/pages/example/3d-buildings.js
+++ docs/pages/example/3d-buildings.js
@@ -1,6 +1,6 @@
 /*---
 title: 以3D形式显示建筑
-description: 使用 [突出层](/mapbox-gl-js/style-spec#layers-fill-extrusion) 以3D的形式显示建筑的高度。
+description: 使用 [extrusions](/mapbox-gl-js/style-spec#layers-fill-extrusion) 以3D的形式显示建筑的高度。
 tags:
   - layers
 pathname: /mapbox-gl-js/example/3d-buildings/
