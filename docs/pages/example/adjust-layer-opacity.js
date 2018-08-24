a2ed16815887d324c1562ac9fbe90127ccb2df0b
Jing <jing.liu@mapbox.com>
2018-08-24 12:10:29 +0800

    Update adjust-layer-opacity.js

--- docs/pages/example/adjust-layer-opacity.js
+++ docs/pages/example/adjust-layer-opacity.js
@@ -1,6 +1,6 @@
 /*---
 title: 调整一个图层的透明度
-description: 拖动滑动条来调整地图顶部一个 [栅格图层](/mapbox-gl-js/style-spec/#layers-raster)的透明度。
+description: 拖动滑动条来调整地图顶部一个 [raster layer](/mapbox-gl-js/style-spec/#layers-raster)的透明度。
 tags:
   - layers
   - user-interaction
