f11cc700f5d85d7b239d5f20e6050f621a2453cc
何秦 <Will.He@outlook.com>
2018-08-26 15:26:11 +0800

    translate 100%

--- docs/pages/example/queryrenderedfeatures-around-point.js
+++ docs/pages/example/queryrenderedfeatures-around-point.js
@@ -1,6 +1,6 @@
 /*---
-title: Select features around a clicked point
-description: Click on the map to query features using [`queryRenderedFeatures`](/mapbox-gl-js/api#map#queryrenderedfeatures).
+title: 识别单击点周围的要素
+description: 通过[`queryRenderedFeatures`](/mapbox-gl-js/api#map#queryrenderedfeatures)使得在地图上单击以显示要素信息。
 tags:
   - user-interaction
 pathname: /mapbox-gl-js/example/queryrenderedfeatures-around-point/
