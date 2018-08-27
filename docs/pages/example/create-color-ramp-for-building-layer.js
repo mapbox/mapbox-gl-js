f59f1850961fc9b1720d8af811f43e77b6131af0
malwoodsantoro <mallorywood413@gmail.com>
2018-08-27 16:27:11 -0700

    add example utilizing eponential interpolation

---
+++ docs/pages/example/create-color-ramp-for-building-layer.js
@@ -0,0 +1,10 @@
+/*---
+title: Create color and opacity ramp for building layer
+description: Uses exponential interpolation to ease-in the building layer and smoothly fade from one color to the next.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/create-color-ramp-for-building-layer/
+---*/
+import Example from '../../components/example';
+import html from './create-color-ramp-for-building-layer.html';
+export default Example(html);
