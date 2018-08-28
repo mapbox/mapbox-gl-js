d5491607f9f7e8227541842cc256cbabc114a496
malwoodsantoro <mallorywood413@gmail.com>
2018-08-28 16:48:27 -0700

    small styling and naming changes

---
+++ docs/pages/example/change-building-color-based-on-zoom-level.js
@@ -0,0 +1,10 @@
+/*---
+title: Change building color based on zoom level
+description: Uses the [interpolate expression](/mapbox-gl-js/style-spec#expressions-interpolate) to ease-in the building layer and smoothly fade from one color to the next.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/change-building-color-based-on-zoom-level/
+---*/
+import Example from '../../components/example';
+import html from './change-building-color-based-on-zoom-level.html';
+export default Example(html);
