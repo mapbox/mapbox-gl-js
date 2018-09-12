bdff645a059b68e1898efd43925cd6d1ae36e8d1
Ansis Brammanis <ansis@mapbox.com>
2018-09-12 12:49:55 -0400

    fix test

--- test/unit/geo/transform.test.js
+++ test/unit/geo/transform.test.js
@@ -247,7 +247,7 @@ test('transform', (t) => {
         transform.center = { lng: -170.01, lat: 0.01 };
 
         let unwrappedCoords = transform.getVisibleUnwrappedCoordinates(new CanonicalTileID(0, 0, 0));
-        t.equal(unwrappedCoords.length, 2);
+        t.equal(unwrappedCoords.length, 4);
 
         //getVisibleUnwrappedCoordinates should honor _renderWorldCopies
         transform._renderWorldCopies = false;
