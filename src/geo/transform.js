9e37467f45ff195243836c1dc9318d8fdea0a365
Ansis Brammanis <ansis.brammanis@gmail.com>
2018-09-12 14:28:10 -0400

    fix missing repeats of CanvasSource when it crosses the antimeridian (#7273)
    fix #7271

--- src/geo/transform.js
+++ src/geo/transform.js
@@ -201,13 +201,21 @@ class Transform {
      * @private
      */
     getVisibleUnwrappedCoordinates(tileID: CanonicalTileID) {
-        const ul = this.pointCoordinate(new Point(0, 0), 0);
-        const ur = this.pointCoordinate(new Point(this.width, 0), 0);
-        const w0 = Math.floor(ul.column);
-        const w1 = Math.floor(ur.column);
         const result = [new UnwrappedTileID(0, tileID)];
         if (this._renderWorldCopies) {
-            for (let w = w0; w <= w1; w++) {
+            const utl = this.pointCoordinate(new Point(0, 0), 0);
+            const utr = this.pointCoordinate(new Point(this.width, 0), 0);
+            const ubl = this.pointCoordinate(new Point(this.width, this.height), 0);
+            const ubr = this.pointCoordinate(new Point(0, this.height), 0);
+            const w0 = Math.floor(Math.min(utl.column, utr.column, ubl.column, ubr.column));
+            const w1 = Math.floor(Math.max(utl.column, utr.column, ubl.column, ubr.column));
+
+            // Add an extra copy of the world on each side to properly render ImageSources and CanvasSources.
+            // Both sources draw outside the tile boundaries of the tile that "contains them" so we need
+            // to add extra copies on both sides in case offscreen tiles need to draw into on-screen ones.
+            const extraWorldCopy = 1;
+
+            for (let w = w0 - extraWorldCopy; w <= w1 + extraWorldCopy; w++) {
                 if (w === 0) continue;
                 result.push(new UnwrappedTileID(w, tileID));
             }
