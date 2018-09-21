ed75ec540668ebb284d1d62be5f3ef1301f5c4aa
malwoodsantoro <mallorywood413@gmail.com>
2018-09-20 21:12:59 -0700

    add example with raw geojson

---
+++ docs/pages/example/visualize-population-density.js
@@ -0,0 +1,10 @@
+/*---
+title: Visualize population density
+description: Using a [`varible binding expression`](/mapbox-gl-js/api/#map#flyto) to calculate and display population density.
+tags:
+  - camera
+pathname: /mapbox-gl-js/example/visualize-population-density/
+---*/
+import Example from '../../components/example';
+import html from './visualize-population-density.html';
+export default Example(html);
