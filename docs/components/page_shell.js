d8815e487cb7487b77582bd70f6b114ed8a7abd6
danswick <dan.swick@gmail.com>
2018-09-21 14:14:11 -0700

    update page shell and add new sentry config

--- docs/components/page_shell.js
+++ docs/components/page_shell.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import ReactPageShell from '../../vendor/dotcom-page-shell/react-page-shell.js';
 
-class PageShell extends React.Component {
-    componentDidMount() {
-        // initialize analytics
-        if (window && window.initializeMapboxAnalytics) {
-            window.initializeMapboxAnalytics();
-        }
+// initialize analytics
+if (typeof window !== 'undefined' && window.initializeMapboxAnalytics) {
+    const isProduction = /\.?mapbox\.com/.test(window.location.hostname);
+
+    let sentryInit = {};
+    if (isProduction) {
+        sentryInit = { sentryDsn: 'https://581913e6cd0845d785f5b551a4986b61@sentry.io/11290' };
+    } else {
+        sentryInit = false;
     }
+    window.initializeMapboxAnalytics({
+        sentry: sentryInit
+    });
+}
+
+class PageShell extends React.Component {
 
     render() {
         return (
