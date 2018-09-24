9032f69d8b51a12d53c9f7b50029222deb93c2b5
malwoodsantoro <mallorywood413@gmail.com>
2018-09-24 12:33:17 -0700

    make small edits

--- docs/pages/example/visualize-population-density.js
+++ docs/pages/example/visualize-population-density.js
@@ -1,6 +1,6 @@
 /*---
 title: Visualize population density
-description: Using a [`varible binding expression`](/mapbox-gl-js/style-spec#expressions-variable-binding) to calculate and display population density.
+description: Using a [`variable binding expression`](/mapbox-gl-js/style-spec#expressions-variable-binding) to calculate and display population density.
 tags:
   - layers
 pathname: /mapbox-gl-js/example/visualize-population-density/
