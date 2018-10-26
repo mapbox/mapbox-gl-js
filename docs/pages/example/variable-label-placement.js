ead961f7c6b903bb5bbfbc3f128362bb46d421eb
Molly Lloyd <molly@mapbox.com>
2018-10-25 20:42:13 -0700

    Variable label placement example.

---
+++ docs/pages/example/variable-label-placement.js
@@ -0,0 +1,10 @@
+/*---
+title: Variable label placement
+description: Use `text-variable-anchor` to allow high priority labels to shift position to stay on the map
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/variable-label-placement/
+---*/
+import Example from '../../components/example';
+import html from './variable-label-placement.html';
+export default Example(html);
