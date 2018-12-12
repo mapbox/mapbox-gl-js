cd8a4cf587ece5bcca66da104cebaf46567a4244
Molly Lloyd <molly@mapbox.com>
2018-12-12 12:44:43 -0800

    make texture pos epsilon highp for gl native

--- src/shaders/hillshade_prepare.vertex.glsl
+++ src/shaders/hillshade_prepare.vertex.glsl
@@ -9,7 +9,7 @@ varying vec2 v_pos;
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
 
-    vec2 epsilon = 1.0 / u_dimension;
+    highp vec2 epsilon = 1.0 / u_dimension;
     float scale = (u_dimension.x - 2.0) / u_dimension.x;
     v_pos = (a_texture_pos / 8192.0) * scale + epsilon;
 }
