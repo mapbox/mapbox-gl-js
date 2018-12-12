d85f0824ec0231daaaddf1344bdf0e2bd9ecfaae
peterqliu <peterqliu@users.noreply.github.com>
2018-12-12 13:55:52 -0800

    update moving state

--- src/ui/map.js
+++ src/ui/map.js
@@ -1683,6 +1683,7 @@ class Map extends Camera {
             showOverdrawInspector: this._showOverdrawInspector,
             rotating: this.isRotating(),
             zooming: this.isZooming(),
+            moving: this.isMoving(),
             fadeDuration: this._fadeDuration
         });
 
