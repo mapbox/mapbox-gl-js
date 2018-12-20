807f042d081c802438a3182ac49663d09ba6442e
malwoodsantoro <mallorywood413@gmail.com>
2018-12-19 16:23:27 -0800

    add example demonstrating how to adjust administrative boundaries based on the worldview feature property in streets v8

---
+++ docs/pages/example/toggle-worldviews.js
@@ -0,0 +1,11 @@
+/*---
+title: Change worldview of administrative boundaries
+description: Uses the worldview value to adjust administrative boundaries based on the map's audience.
+tags:
+  - layers
+  - user interaction
+pathname: /mapbox-gl-js/example/toggle-worldviews/
+---*/
+import Example from '../../components/example';
+import html from './toggle-worldviews.html';
+export default Example(html);
