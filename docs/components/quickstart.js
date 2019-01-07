80ef0c98dff36afc90090661d552bd10d6abc808
Katy DeCorah <decorah@mapbox.com>
2019-01-07 16:28:00 -0500

    adds spaces

--- docs/components/quickstart.js
+++ docs/components/quickstart.js
@@ -80,11 +80,8 @@ export default class extends React.Component {
                 </div>
                 <div className='prose space-bottom2'>
                     <p className='space-bottom2 pad1y'>
-                        Mapbox GL JS is a JavaScript library that uses WebGL to render interactive maps from
-                        <a href='https://www.mapbox.com/help/define-vector-tiles'>vector tiles</a> and
-                        <a href={prefixUrl('/style-spec')}>Mapbox styles</a>.
-                        It is part of the Mapbox GL ecosystem, which includes
-                        <a href='https://www.mapbox.com/mobile/'>Mapbox Mobile</a>,
+                        Mapbox GL JS is a JavaScript library that uses WebGL to render interactive maps from{' '}<a href='https://www.mapbox.com/help/define-vector-tiles'>vector tiles</a> and{' '}<a href={prefixUrl('/style-spec')}>Mapbox styles</a>.
+                        It is part of the Mapbox GL ecosystem, which includes{' '}<a href='https://www.mapbox.com/mobile/'>Mapbox Mobile</a>,
                         a compatible renderer written in C++ with bindings
                         for desktop and mobile platforms. To see what new features our team is working on,
                         take a look at our <a href={prefixUrl('/roadmap')}>roadmap</a>.
