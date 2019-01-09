df7e1c01f6a970f123b5c29bc1b06f5cdf472155
Asheem Mamoowala <asheem.mamoowala@mapbox.com>
2019-01-09 11:47:45 -0800

    Update feedback url and related test

--- src/ui/control/attribution_control.js
+++ src/ui/control/attribution_control.js
@@ -103,7 +103,7 @@ class AttributionControl {
                 }
                 return acc;
             }, `?`);
-            editLink.href = `https://www.mapbox.com/feedback/${paramString}${this._map._hash ? this._map._hash.getHashString(true) : ''}`;
+            editLink.href = `${config.FEEDBACK_URL}/${paramString}${this._map._hash ? this._map._hash.getHashString(true) : ''}`;
         }
     }
 
