9ba0926fd167f927380a335b77271c3227cbb482
Katy DeCorah <decorah@mapbox.com>
2019-01-25 11:05:35 -0500

    adds unit tests for examples

---
+++ test/unit/docs/examples.test.js
@@ -0,0 +1,58 @@
+import { test } from 'mapbox-gl-js-test';
+import fs from 'fs';
+import path from 'path';
+import jsyaml from 'js-yaml';
+import { tags } from '../../../docs/data/tags.js';
+
+const listTags = Object.keys(tags);
+
+const readPost = filename => {
+    const buffer = fs.readFileSync(filename),
+        file = buffer.toString('utf8');
+
+    try {
+        const parts = file.split(/---\s*[\*\n^]/g),
+            frontmatter = parts[1];
+        return {
+            name: filename,
+            file,
+            metadata: frontmatter ? jsyaml.safeLoad(frontmatter) : null,
+            content: parts[2]
+        };
+    } catch (err) {
+        return new Error(
+      `\nCould not read metadata, check the syntax of the metadata and front matter: ${
+          filename}`
+        );
+    }
+};
+
+const listExamples = dir => {
+    const files = fs.readdirSync(`${dir}`);
+    return files.reduce((arr, file) => {
+        if (path.extname(file) === '.js') {
+            arr.push(`${dir}${file}`);
+        }
+        return arr;
+    }, []);
+};
+
+listExamples('./docs/pages/example/').forEach((example) => {
+    const file = readPost(example);
+    const metadata = file.metadata;
+
+    if (metadata) {
+        test(`Example: ${example}`, (t) => {
+            t.ok(metadata.title, 'has title');
+            t.ok(metadata.description, 'has description');
+            t.equal(metadata.description.substr(metadata.description.length - 1), '.', 'description must end with a period');
+            t.ok(metadata.pathname, 'has pathname');
+            t.ok(metadata.pathname.startsWith('/mapbox-gl-js/'), 'pathname starts with /mapbox-gl-js/');
+            t.ok(metadata.tags, 'has tags');
+            metadata.tags.forEach(tag => {
+                t.notEqual(listTags.indexOf(tag), -1, `tag "${tag}" must match an item in docs/data/tags.js: ${listTags.join(', ')}`);
+            });
+            t.end();
+        });
+    }
+});
