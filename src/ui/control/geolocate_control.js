5e6628124f1a4258ea24f960d98f8120b2abc953
andycalder <28855024+andycalder@users.noreply.github.com>
2019-01-29 03:54:19 +1000

    Retain bearing on geolocate

--- src/ui/control/geolocate_control.js
+++ src/ui/control/geolocate_control.js
@@ -187,8 +187,10 @@ class GeolocateControl extends Evented {
     _updateCamera(position: Position) {
         const center = new LngLat(position.coords.longitude, position.coords.latitude);
         const radius = position.coords.accuracy;
+        const bearing = this._map.getBearing();
+        const options = extend({bearing}, this.options.fitBoundsOptions);
 
-        this._map.fitBounds(center.toBounds(radius), this.options.fitBoundsOptions, {
+        this._map.fitBounds(center.toBounds(radius), options, {
             geolocateSource: true // tag this camera change so it won't cause the control to change to background state
         });
     }
