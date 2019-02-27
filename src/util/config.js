760eabe342a0e5ae4d95c44ede1b91875f9adfcb
Arni Sumarlidason <arni.sumarlidason@mapbox.com>
2019-02-26 21:08:10 -0500

    bug: enhance API_URL logic
    fixes #7944

--- src/util/config.js
+++ src/util/config.js
@@ -2,7 +2,7 @@
 
 type Config = {|
   API_URL: string,
-  EVENTS_URL: string,
+  EVENTS_URL: ?string,
   FEEDBACK_URL: string,
   REQUIRE_ACCESS_TOKEN: boolean,
   ACCESS_TOKEN: ?string,
@@ -12,10 +12,13 @@ type Config = {|
 const config: Config = {
     API_URL: 'https://api.mapbox.com',
     get EVENTS_URL() {
+        if (!this.API_URL) { return null; }
         if (this.API_URL.indexOf('https://api.mapbox.cn') === 0) {
             return 'https://events.mapbox.cn/events/v2';
-        } else {
+        } else if (this.API_URL.indexOf('https://api.mapbox.com') === 0) {
             return 'https://events.mapbox.com/events/v2';
+        } else {
+            return null;
         }
     },
     FEEDBACK_URL: 'https://apps.mapbox.com/feedback',
