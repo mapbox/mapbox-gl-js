81cf1bd8ec9094c683204f9f82ec6516656dc979
Mickaël Allonneau <mickael.allonneau@gmail.com>
2019-03-11 18:12:18 +0100

    Example for 'fill-pattern'

---
+++ docs/pages/example/fill-pattern.js
@@ -0,0 +1,10 @@
+/*---
+title: Add a pattern to a polygon
+description: Load an image and use it as a repeating pattern to draw a polygon.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/fill-pattern/
+---*/
+import Example from '../../components/example';
+import html from './fill-pattern.html';
+export default Example(html);
