15d6fe30481af90e921ae3f32a1fbc1016d5c1b9
Vladimir Agafonkin <agafonkin@gmail.com>
2019-03-11 18:15:58 +0200

    add HTML cluster markers + prop aggregation example

---
+++ docs/pages/example/cluster-html.js
@@ -0,0 +1,10 @@
+/*---
+title: Use clusters with HTML markers and aggregated properties
+description: Use Mapbox GL JS' built-in functions to visualize points as clusters.
+tags:
+  - layers
+pathname: /mapbox-gl-js/example/cluster-html/
+---*/
+import Example from '../../components/example';
+import html from './cluster-html.html';
+export default Example(html);
