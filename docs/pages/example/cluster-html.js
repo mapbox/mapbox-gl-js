b517b72b2eb7fcea347f0fcb6983432404ff5bcb
Vladimir Agafonkin <agafonkin@gmail.com>
2019-03-12 20:16:48 +0200

    update cluster html example description

--- docs/pages/example/cluster-html.js
+++ docs/pages/example/cluster-html.js
@@ -1,6 +1,6 @@
 /*---
 title: Display HTML clusters with custom properties
-description: An advanced example of using Mapbox GL JS clustering with HTML markers and custom property expressions. To be able to use HTML or SVG for clusters in place of a Mapbox GL layer, we have to manually synchronize the clustered source with a pool of marker objects that we update continuously while the map view changes.
+description: This advanced example uses Mapbox GL JS clustering with HTML markers and custom property expressions. To use HTML or SVG for clusters in place of a Mapbox GL layer, you have to manually synchronize the clustered source with a pool of marker objects that updates continuously while the map view changes.
 tags:
   - layers
 pathname: /mapbox-gl-js/example/cluster-html/
