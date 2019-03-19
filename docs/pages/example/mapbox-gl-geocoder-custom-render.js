8c6b7a60ada390c5f5d56dd3c086fbbe37f2ce19
Scott Farley <scott.farley@mapbox.com>
2019-03-18 18:14:56 -0700

    add geocoder custom render example

---
+++ docs/pages/example/mapbox-gl-geocoder-custom-render.js
@@ -0,0 +1,13 @@
+/*---
+title: Add a custom render function to a geocoder
+description: >-
+  Use the [mapbox-gl-geocoder](https://github.com/mapbox/mapbox-gl-geocoder)
+  control to search for places using Mapbox Geocoding API, and render the matching 
+  places in a customized way.
+tags:
+  - geocoder
+pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-custom-render/
+---*/
+import Example from '../../components/example';
+import html from './mapbox-gl-geocoder-custom-render.html';
+export default Example(html);
