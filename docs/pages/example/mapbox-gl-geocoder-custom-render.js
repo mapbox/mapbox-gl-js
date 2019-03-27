3760ff9a74c600cd0240b2392cc4596f85cbb24a
Scott Farley <scott.farley@mapbox.com>
2019-03-27 15:25:44 -0700

    lint new examples

--- docs/pages/example/mapbox-gl-geocoder-custom-render.js
+++ docs/pages/example/mapbox-gl-geocoder-custom-render.js
@@ -2,7 +2,7 @@
 title: Use a custom render function with a geocoder 
 description: >-
   Use a custom html rendering function with the [mapbox-gl-geocoder](https://github.com/mapbox/mapbox-gl-geocoder)
-  to customize how the dropdown menu is displayed
+  to customize how the dropdown menu is displayed.
 tags:
   - geocoder
 pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-custom-render/
--- docs/pages/example/mapbox-gl-geocoder-with-flyto.js
+++ docs/pages/example/mapbox-gl-geocoder-with-flyto.js
@@ -2,7 +2,7 @@
 title: Use a custom camera animation with a geocoder
 description: >-
   Use camera animation options with the [mapbox-gl-geocoder](https://github.com/mapbox/mapbox-gl-geocoder)
-  to create a custom animation when a result is selected
+  to create a custom animation when a result is selected.
 tags:
   - geocoder
 pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-with-flyto/
