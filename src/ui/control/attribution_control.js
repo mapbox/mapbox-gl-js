79c3b98a4b45b488d610fc2e8eaf3fb200a4893b
poletani <poletani@gmail.com>
2019-03-29 11:03:07 +0100

    Update attribution_control.js (#8082)
    * Update attribution_control.js
    
    fix https://github.com/mapbox/mapbox-gl-js/issues/8047
    
    Update attribution_control.js
    
    optimalization of fix
    
    * Update attribution_control.js
    
    change variable names String -> HTML
    fix linter errors
    
    * fix line endings lint errors
    
    * fix fix fix!
    
    fix === linter error, remove unwanted flow monitoring added by linter fix

--- src/ui/control/attribution_control.js
+++ src/ui/control/attribution_control.js
@@ -30,6 +30,7 @@ class AttributionControl {
     _container: HTMLElement;
     _innerContainer: HTMLElement;
     _editLink: ?HTMLAnchorElement;
+    _attribHTML: string;
     styleId: string;
     styleOwner: string;
 
@@ -157,8 +158,15 @@ class AttributionControl {
             }
             return true;
         });
+
+        // check if attribution string is different to minimize DOM changes
+        const attribHTML = attributions.join(' | ');
+        if (attribHTML === this._attribHTML) return;
+
+        this._attribHTML = attribHTML;
+
         if (attributions.length) {
-            this._innerContainer.innerHTML = attributions.join(' | ');
+            this._innerContainer.innerHTML = attribHTML;
             this._container.classList.remove('mapboxgl-attrib-empty');
         } else {
             this._container.classList.add('mapboxgl-attrib-empty');
