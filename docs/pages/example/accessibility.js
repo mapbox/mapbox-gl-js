c96ae22a02f4baa7e0b983a7233ff3444ae944a9
peterqliu <peterqliu@users.noreply.github.com>
2019-04-18 14:34:35 -0700

    add example

---
+++ docs/pages/example/accessibility.js
@@ -0,0 +1,10 @@
+/*---
+title: Accessibility plugin
+description: Add accessibility controls.
+tags:
+  - styles
+pathname: /mapbox-gl-js/example/accessibility/
+---*/
+import Example from '../../components/example';
+import html from './accessibility.html';
+export default Example(html);
