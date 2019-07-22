a3f95a64e4f98b764c130cbc78e1abb9bed83eb6
Katy DeCorah <decorah@mapbox.com>
2019-07-22 14:48:46 -0400

    [docs] adds feedback component (#8507)
    * [docs] add feedback component
    
    * Update dr-ui
    
    * eslint
    
    * update dr-ui, fixes https://github.com/mapbox/dr-ui/issues/157

--- docs/components/markdown-page-shell.js
+++ docs/components/markdown-page-shell.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import PageShell from './page_shell';
+import Feedback from '@mapbox/dr-ui/feedback';
+import constants from '../constants';
 
 class MarkdownPageshell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: undefined
+        };
+    }
+
+    componentDidMount() {
+        MapboxPageShell.afterUserCheck(() => {
+            // fetches username so we can identify them in segment
+            this.setState({
+                userName: MapboxPageShell.getUser() ?
+                    MapboxPageShell.getUser().id :
+                    undefined
+            });
+        });
+    }
     render() {
         const { frontMatter, location } = this.props;
         const meta = this.props.meta || {};
@@ -21,6 +40,14 @@ class MarkdownPageshell extends React.Component {
             <PageShell meta={meta} {...this.props}>
                 <div className="prose">
                     {this.props.children}
+                </div>
+                <div className="mt18">
+                    <Feedback
+                        site="Mapbox GL JS"
+                        location={this.props.location}
+                        userName={this.state.userName}
+                        webhook={constants.FORWARD_EVENT_WEBHOOK}
+                    />
                 </div>
             </PageShell>
         );
