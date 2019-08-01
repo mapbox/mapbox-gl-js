cb20ec05258e2f19a4bf66b2e8025b6285514c9b
Vladimir Agafonkin <agafonkin@gmail.com>
2019-08-01 18:50:03 +0300

    more useful gl perf stats

--- bench/gl-stats.js
+++ bench/gl-stats.js
@@ -3,43 +3,9 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const mapboxGlSrc = fs.readFileSync('dist/mapbox-gl.js', 'utf8');
+const benchSrc = fs.readFileSync('bench/gl-stats.html', 'utf8');
 
-const benchHTML = `<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<script>${mapboxGlSrc}</script>
-<link rel="stylesheet" href="dist/mapbox-gl.css">
-</head>
-<body style="margin: 0; padding: 0"><div id="map" style="width: 500px; height: 500px"></div></body>
-<script>
-mapboxgl.accessToken = 'pk.eyJ1IjoibW91cm5lciIsImEiOiJWWnRiWG1VIn0.j6eccFHpE3Q04XPLI7JxbA';
-mapboxgl.workerCount = 1;
-
-var map = new mapboxgl.Map({
-    container: document.getElementById('map'),
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-77.07842066675323, 38.890315130853566],
-    zoom: 11.1
-});
-
-const gl = map.painter.context && map.painter.context.gl || map.painter.gl;
-
-let bufferDataTotal = 0;
-
-const originalBufferData = gl.bufferData;
-gl.bufferData = function (target, data, usage) {
-    bufferDataTotal += data.byteLength;
-    originalBufferData.call(gl, target, data, usage);
-};
-
-map.on('render', () => {
-    if (!map._sourcesDirty && !map._repaint && !map._styleDirty && !map._placementDirty && map.loaded()) {
-        console.log(JSON.stringify({'buffer-data': bufferDataTotal}));
-    }
-});
-</script>
-</html>`;
+const benchHTML = benchSrc.replace(/<script src="..\/dist\/mapbox-gl.js"><\/script>/, `<script>${mapboxGlSrc}</script>`);
 
 function waitForConsole(page) {
     return new Promise((resolve) => {
