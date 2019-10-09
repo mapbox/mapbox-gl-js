99442455ec9ea2127175782238f80436d8abb483
Arindam Bose <arindam.bose@mapbox.com>
2019-10-09 15:13:24 -0700

    Ensure tile mask geometry buffers. are cleared when tile is unloaded

--- src/source/tile.js
+++ src/source/tile.js
@@ -331,8 +331,8 @@ class Tile {
         // don't redo buffer work if the mask is the same;
         if (deepEqual(this.mask, mask)) return;
 
-        this.mask = mask;
         this.clearMask();
+        this.mask = mask;
 
         // We want to render the full tile, and keeping the segments/vertices/indices empty means
         // using the global shared buffers for covering the entire tile.
