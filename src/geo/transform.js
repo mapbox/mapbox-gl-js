487e08df8981fbdb10eed10cd90881d46211c45b
Andrew Harvey <andrew@alantgeo.com.au>
2019-10-11 17:45:13 +1100

    allow maxPitch: 0

--- src/geo/transform.js
+++ src/geo/transform.js
@@ -61,8 +61,8 @@ class Transform {
         this._minZoom = minZoom || 0;
         this._maxZoom = maxZoom || 22;
 
-        this._minPitch = minPitch || 0;
-        this._maxPitch = maxPitch || 60;
+        this._minPitch = typeof minPitch === "undefined" ? 0 : minPitch;
+        this._maxPitch = typeof maxPitch === "undefined" ? 60 : maxPitch;
 
         this.setMaxBounds();
 
