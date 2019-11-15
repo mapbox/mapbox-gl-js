cc1131529bbdf89010e86ec6f1cffe849c70005f
Arindam Bose <arindam.bose@mapbox.com>
2019-11-14 17:21:58 -0800

    Up runs to 15

--- bench/lib/metrics-harness.js
+++ bench/lib/metrics-harness.js
@@ -7,7 +7,7 @@ import {applyOperations} from '../../test/integration/lib/operation-handlers';
 //Used to warm-up the browser cache for consistent tile-load timings
 const NUM_WARMUP_RUNS = 5;
 
-const NUM_ACTUAL_RUNS = 10;
+const NUM_ACTUAL_RUNS = 15;
 
 // This namespace ised to store functions/data that can be accessed and invoked by pupeteer.
 window.mbglMetrics = {};
