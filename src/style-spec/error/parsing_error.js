55761a1de42d6082d68199eea43d9aea32751520
Konstantin Käfer <mail@kkaefer.com>
2019-12-05 16:29:25 +0100

    Fix validation error messages in the browser
    We converted the ParsingError and ValidationError classes to inherit from the built-in Error class in #9025. This works fine in ES6, so our unit tests were happy. However, transpiling it down to ES5 leads to an absence of the message property, essentially removing the text of parsing and validation errors.

--- src/style-spec/error/parsing_error.js
+++ src/style-spec/error/parsing_error.js
@@ -1,12 +1,15 @@
 // @flow
 
-export default class ParsingError extends Error {
+// Note: Do not inherit from Error. It breaks when transpiling to ES5.
+
+export default class ParsingError {
+    message: string;
     error: Error;
     line: number;
 
     constructor(error: Error) {
-        super(error.message);
         this.error = error;
+        this.message = error.message;
         const match = error.message.match(/line (\d+)/);
         this.line = match ? parseInt(match[1], 10) : 0;
     }
