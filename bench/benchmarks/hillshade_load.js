84119b99efe38a6e644f724648873ee48a28ee0c
Arindam Bose <arindam.bose@mapbox.com>
2019-12-05 13:55:15 -0800

    Add HillshadeLoad benchmark

---
+++ bench/benchmarks/hillshade_load.js
@@ -0,0 +1,54 @@
+// @flow
+
+import Benchmark from '../lib/benchmark';
+import createMap from '../lib/create_map';
+import type {StyleSpecification} from '../../src/style-spec/types';
+
+export default class HillshadeLoad extends Benchmark {
+    style: StyleSpecification;
+
+    constructor(style: string) {
+        super();
+        this.style = {
+            "version": 8,
+            "name": "Hillshade-only",
+            "center": [-112.81596278901452, 37.251160384573595],
+            "zoom": 11.560975632435424,
+            "bearing": 0,
+            "pitch": 0,
+            "sources": {
+                "mapbox://mapbox.terrain-rgb": {
+                    "url": "mapbox://mapbox.terrain-rgb",
+                    "type": "raster-dem",
+                    "tileSize": 256
+                }
+            },
+            "layers": [
+                {
+                    "id": "mapbox-terrain-rgb",
+                    "type": "hillshade",
+                    "source": "mapbox://mapbox.terrain-rgb",
+                    "layout": {},
+                    "paint": {}
+                }
+            ]
+        };
+    }
+
+    bench() {
+        return createMap({
+            width: 1024,
+            height: 1024,
+            style: this.style,
+            stubRender: false,
+            showMap: true
+        }).then((map) => {
+            return new Promise((resolve, reject) => {
+                map.once('idle', () => {
+                    resolve()
+                    map.remove();
+                });
+            });
+        });
+    }
+}
--- bench/lib/create_map.js
+++ bench/lib/create_map.js
@@ -4,11 +4,19 @@ import Map from '../../src/ui/map';
 
 export default function (options: any): Promise<Map> {
     return new Promise((resolve, reject) => {
+        if(options){
+            options.stubRender = options.stubRender == null ? true: options.stubRender;
+            options.showMap = options.showMap == null ? false: options.showMap;
+        }
+
         const container = document.createElement('div');
         container.style.width = `${options.width || 512}px`;
         container.style.height = `${options.width || 512}px`;
         container.style.margin = '0 auto';
-        container.style.display = 'none';
+
+        if(!options.showMap){
+            container.style.display = 'none';
+        }
         (document.body: any).appendChild(container);
 
         const map = new Map(Object.assign({
@@ -18,15 +26,16 @@ export default function (options: any): Promise<Map> {
 
         map
             .on('load', () => {
-                // Stub out `_rerender`; benchmarks need to be the only trigger of `_render` from here on out.
-                map._rerender = () => {};
+                if(options.stubRender){
+                    // Stub out `_rerender`; benchmarks need to be the only trigger of `_render` from here on out.
+                    map._rerender = () => {};
 
-                // If there's a pending rerender, cancel it.
-                if (map._frame) {
-                    map._frame.cancel();
-                    map._frame = null;
+                    // If there's a pending rerender, cancel it.
+                    if (map._frame) {
+                        map._frame.cancel();
+                        map._frame = null;
+                    }
                 }
-
                 resolve(map);
             })
             .on('error', (e) => reject(e.error))
