9dce947e0e41eb0b707769ce3ce06c6e06550729
Anjana Vakil <anjana.vakil@mapbox.com>
2019-12-11 17:59:11 -0800

    [WIP] working on TouchZoomHandler

--- src/ui/handler/touch.js
+++ src/ui/handler/touch.js
@@ -1,9 +1,11 @@
 // @flow
 
 import Handler from './handler';
-import DOM from '../../util/dom';
-
 import Inertia from './inertia';
+import DOM from '../../util/dom';
+import window from '../../util/window';
+import browser from '../../util/browser';
+import { bindAll } from '../../util/util';
 
 import type Map from '../map';
 import type Point from '@mapbox/point-geometry';
@@ -12,45 +14,100 @@ import type Point from '@mapbox/point-geometry';
 class TouchHandler extends Handler {
   _el: HTMLElement;
   _startTouchEvent: ?TouchEvent;
+  _startTouchData: ?Object;
   _startTime: ?number;
+  _lastTouchEvent: ?TouchEvent;
+  _lastTouchData: ?Object;
 
   /**
    * @private
    */
-  constructor(map: Map, options: ?Object) {
+  constructor(map: Map, options?: Object) {
     super(map, options);
-    this._el = this.map.getCanvasContainer();
+    this._el = this._map.getCanvasContainer();
+
+    bindAll([
+        '_onTouchStart',
+        '_onTouchMove',
+        '_onTouchEnd',
+        '_onTouchCancel'
+    ], this);
 
     // Bind touchstart and touchmove with passive: false because, even though
     // they only fire a map events and therefore could theoretically be
     // passive, binding with passive: true causes iOS not to respect
     // e.preventDefault() in _other_ handlers, even if they are non-passive
     // (see https://bugs.webkit.org/show_bug.cgi?id=184251)
-    DOM.addEventListener(el, 'touchstart', this._onTouchStart, {passive: false});
-    DOM.addEventListener(el, 'touchmove', this._onTouchMove, {passive: false});
-    DOM.addEventListener(el, 'touchend', this._onTouchEnd);
-    DOM.addEventListener(el, 'touchcancel', this._onTouchCancel);
+    DOM.addEventListener(this._el, 'touchstart', this._onTouchStart, {passive: false});
+    DOM.addEventListener(this._el, 'touchmove', this._onTouchMove, {passive: false});
+    DOM.addEventListener(this._el, 'touchend', this._onTouchEnd);
+    DOM.addEventListener(this._el, 'touchcancel', this._onTouchCancel);
 
   }
 
   _getTouchEventData(e: TouchEvent) {
-      const multiTouch = e.touches.length > 1;
+      const isMultiTouch = e.touches.length > 1;
       const points = e.touches.map(touch => DOM.mousePos(this._el, touch));
-      const vec = multiTouch ? points[0].sub(points[1]) : null ;
-
-      return { multiTouch, points, vec };
+      const center = isMultiTouch ? points[0].add(points[1]).div(2) : points[0];
+      const vector = isMultiTouch ? points[0].sub(points[1]) : null ;
+      return { isMultiTouch, points, center, vector };
   }
 
 
   _onTouchStart(e: TouchEvent) {
     if (!this.isEnabled()) return;
     this._startTouchEvent = e;
+    this._startTouchData = this._getTouchEventData(e);
     this._startTime = browser.now();
-    this._state = 'pending';
+    // this._state = 'pending';
   };
+
+  // _onTouchMove(e: TouchEvent) {
+  //
+  // }
+  //
+  // _onTouchEnd(e: TouchEvent) {
+  //
+  // }
+  //
+  // _onTouchCancel(e: TouchEvent) {
+  //
+  // }
 
 
 }
 
+class TouchZoomHandler extends TouchHandler {
+    significantScaleThreshold: number;
+    _startScale: ?number;
 
-export default TouchHandler;
+    constructor(map: Map, options?: Object) {
+      super(map, options);
+      this.significantScaleThreshold = 0.15;
+    }
+
+    _onTouchStart(e: TouchEvent) {
+      super._onTouchStart(e);
+      if (!this._startTouchData.isMultiTouch) return;
+      this._state = 'pending';
+      this._startScale = this._map.transform.scale;
+    }
+
+    _onTouchMove(e: TouchEvent) {
+      console.log('TouchZoomHandler._onTouchMove');
+      if (!(this._state === 'pending' || this._state === 'active')) return;
+      this._lastTouchEvent = e;
+      this._lastTouchData = this._getTouchEventData(e);
+      if (!this._lastTouchData.isMultiTouch) return;
+      // TODO check time vs. start time to prevent responding to spurious events (vs. tap)
+      const scale = this._lastTouchData.vector.mag() / this._startTouchData.vector.mag();
+      const scalingSignificantly = Math.abs(1 - scale) > this.significantScaleThreshold;
+      if (scalingSignificantly) {
+        this._state = 'active';
+        // TODO request update
+      }
+    }
+}
+
+
+export { TouchHandler, TouchZoomHandler };
--- test/unit/ui/handler/touch_handler.test.js
+++ test/unit/ui/handler/touch_handler.test.js
@@ -1,17 +1,60 @@
 import {test} from '../../../util/test';
-import TouchHandler from '../../../../src/ui/handler/touch';
-import window from '../../../../src/util/window';
+import { TouchHandler, TouchZoomHandler } from '../../../../src/ui/handler/touch';
 import Map from '../../../../src/ui/map';
 import DOM from '../../../../src/util/dom';
+import window from '../../../../src/util/window';
+import simulate from '../../../util/simulate_interaction';
+import {extend} from '../../../../src/util/util';
 
-
-function createMap(t) {
+function createMap(t, options) {
     t.stub(Map.prototype, '_detectMissingCSS');
-    return new Map({
+    return new Map(extend({
         container: DOM.create('div', '', window.document.body),
-    });
+    }, options));
 }
 
+test('New TouchZoomHandler is in "enabled" state', (t) => {
+    const map = createMap(t);
+    const h = new TouchZoomHandler(map);
+    t.ok(h.isEnabled());
+    t.equal(h._state, 'enabled');
+    t.end();
+});
+
+test('New TouchZoomHandler listens for touchstart events', (t) => {
+    const map = createMap(t, {zoom: 5});
+    const h = new TouchZoomHandler(map);
+
+    simulate.touchstart(map.getCanvas(), {touches: [{clientX: 1, clientY: 1}]});
+    t.ok(h._startTouchEvent);
+    t.equal(h._startTouchEvent.touches.length, 1);
+    t.notOk(h._startTouchData.isMultiTouch);
+    t.equal(h._state, 'enabled', 'single-touch event does not trigger "pending" state');
+
+    simulate.touchstart(map.getCanvas(), {touches: [{clientX: 0, clientY: 0}, {clientX: 3, clientY: 4}]});
+    t.equal(h._startTouchEvent.touches.length, 2);
+    t.ok(h._startTouchData.isMultiTouch);
+    t.equal(h._startTouchData.vector.mag(), 5);
+    t.equal(h._state, 'pending', 'single-touch event triggers "pending" state');
+    t.equal(h._startScale, 32); // z5
+    t.end();
+});
+
+test('New TouchZoomHandler listens for touchmove events', (t) => {
+    const map = createMap(t, {zoom: 5});
+    const h = new TouchZoomHandler(map);
+
+    t.stub(console, 'error').callsFake(console.log);
+    simulate.touchstart(map.getCanvas(), {touches: [{clientX: 1, clientY: 1}]});
+    simulate.touchmove(map.getCanvas(), {touches: [{clientX: 1, clientY: 2}]});
+    t.equal(h._state, 'enabled', 'single-touch event does not trigger anything');
+
+    simulate.touchstart(map.getCanvas(), {touches: [{clientX: 0, clientY: 0}, {clientX: 3, clientY: 4}]});
+    simulate.touchmove(map.getCanvas(), {touches: [{clientX: 0, clientY: 0}, {clientX: 6, clientY: 8}]});
+    t.equal(h._state, 'active', 'multi-touch event activates the handler');
+    t.equal(h._startScale, 32); // z5
+    t.end();
+});
 
 // test('Methods .enable(), .disable(), and .isEnabled() work as expected', (t) => {
 //     const map = createMap(t);
