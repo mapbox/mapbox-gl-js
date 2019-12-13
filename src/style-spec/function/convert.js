f6a45948b1acc0d0796c16225e1a8697ef1d1749
Jo IE <joie.software@gmail.com>
2019-12-13 14:47:38 +0100

    Edit convert functions to output linear interpolation

--- src/style-spec/function/convert.js
+++ src/style-spec/function/convert.js
@@ -153,7 +153,18 @@ function convertPropertyFunction(parameters, propertySpec, stops) {
         ];
     } else if (type === 'exponential') {
         const base = parameters.base !== undefined ? parameters.base : 1;
-        const expression = [getInterpolateOperator(parameters), ['exponential', base], ['number', get]];
+        const expression =
+            base === 1 ?
+                [
+                    getInterpolateOperator(parameters),
+                    ["linear"],
+                    ["number", get]
+                ] :
+                [
+                    getInterpolateOperator(parameters),
+                    ["exponential", base],
+                    ["number", get]
+                ];
         for (const stop of stops) {
             appendStopPair(expression, stop[0], stop[1], false);
         }
@@ -177,7 +188,14 @@ function convertZoomFunction(parameters, propertySpec, stops, input = ['zoom']) 
         isStep = true;
     } else if (type === 'exponential') {
         const base = parameters.base !== undefined ? parameters.base : 1;
-        expression = [getInterpolateOperator(parameters), ['exponential', base], input];
+        expression =
+            base === 1 ?
+                [getInterpolateOperator(parameters), ["linear"], input] :
+                [
+                    getInterpolateOperator(parameters),
+                    ["exponential", base],
+                    input
+                ];
     } else {
         throw new Error(`Unknown zoom function type "${type}"`);
     }
