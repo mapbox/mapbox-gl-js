9c3b2b7c74d4f7263b7e914d186294461dba3124
Michael Holroyd <meekohi@gmail.com>
2020-01-17 14:25:47 -0500

    refactor distance calculations

--- src/geo/mercator_coordinate.js
+++ src/geo/mercator_coordinate.js
@@ -4,15 +4,16 @@ import LngLat from '../geo/lng_lat';
 import type {LngLatLike} from '../geo/lng_lat';
 
 /*
- * The circumference of the world in meters at the equator.
+ * The average circumference of the world in meters.
  */
-const circumferenceAtEquator = 2 * Math.PI * 6378137;
+const earthRadius = LngLat.earthRadius(); // meters
+const earthCircumfrence = 2 * Math.PI * earthRadius;
 
 /*
- * The circumference of the world in meters at the given latitude.
+ * The circumference at a line of latitude in meters.
  */
 function circumferenceAtLatitude(latitude: number) {
-    return circumferenceAtEquator * Math.cos(latitude * Math.PI / 180);
+    return earthCircumfrence * Math.cos(latitude * Math.PI / 180);
 }
 
 export function mercatorXfromLng(lng: number) {
@@ -142,7 +143,7 @@ class MercatorCoordinate {
      */
     meterInMercatorCoordinateUnits() {
         // 1 meter / circumference at equator in meters * Mercator projection scale factor at this latitude
-        return 1 / circumferenceAtEquator * mercatorScale(latFromMercatorY(this.y));
+        return 1 / earthCircumfrence * mercatorScale(latFromMercatorY(this.y));
     }
 
 }
