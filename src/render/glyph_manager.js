e21bbecced165ed92407b87047a425ef996eecab
Olivier Terral <olivier.terral@geops.de>
2020-03-04 16:44:44 +0100

    lint

--- src/render/glyph_manager.js
+++ src/render/glyph_manager.js
@@ -53,7 +53,7 @@ class GlyphManager {
                 entry = this.entries[stack] = {
                     glyphs: {},
                     requests: {},
-                    ranges: {},
+                    ranges: {}
                 };
             }
 
