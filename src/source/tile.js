f0daf2316e60a38faac29121fed75c4ab18c737e
Nick Mann <nick_j_mann@uk.ibm.com>
2020-03-18 11:49:41 +0000

    add an extra check for paint member of style layer

--- src/source/tile.js
+++ src/source/tile.js
@@ -404,7 +404,8 @@ class Tile {
             if (!sourceLayer || !sourceLayerStates || Object.keys(sourceLayerStates).length === 0) continue;
 
             bucket.update(sourceLayerStates, sourceLayer, this.imageAtlas && this.imageAtlas.patternPositions || {});
-            if (painter && painter.style) {
+
+            if (painter && painter.style && painter.style.getLayer(id).paint) {
                 this.queryPadding = Math.max(this.queryPadding, painter.style.getLayer(id).queryRadius(bucket));
             }
         }
