74a2b6827df811e8fbb76120a22be6bb556e23ab
Luke Butler <lukepbutler@gmail.com>
2020-03-22 23:44:24 -0400

    Implement "index-of" expression

--- src/style-spec/expression/definitions/index.js
+++ src/style-spec/expression/definitions/index.js
@@ -24,6 +24,7 @@ import Assertion from './assertion';
 import Coercion from './coercion';
 import At from './at';
 import In from './in';
+import IndexOf from './index_of';
 import Match from './match';
 import Case from './case';
 import Slice from './slice';
@@ -65,6 +66,7 @@ const expressions: ExpressionRegistry = {
     'format': FormatExpression,
     'image': ImageExpression,
     'in': In,
+    'index-of': IndexOf,
     'interpolate': Interpolate,
     'interpolate-hcl': Interpolate,
     'interpolate-lab': Interpolate,
