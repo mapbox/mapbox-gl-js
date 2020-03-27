394dd2730a60a23000a40f7dd258b62955c60fe2
Luke Butler <lukepbutler@gmail.com>
2020-03-27 14:35:25 -0400

    Revert "Merged in & index-of expressions to the same class"
    This reverts commit 4b2893bfdb543540c3f397c5019db13a32d2aa9f.

--- src/style-spec/expression/definitions/index_of.js
+++ src/style-spec/expression/definitions/index_of.js
@@ -1,6 +1,6 @@
 // @flow
 
-import {ValueType, BooleanType, toString} from '../types';
+import {ValueType, toString, NumberType} from '../types';
 import RuntimeError from '../runtime_error';
 import {typeOf} from '../values';
 
@@ -21,7 +21,8 @@ function isComparableType(type: Type) {
 function isComparableRuntimeValue(needle: boolean | string | number | null) {
     return typeof needle === 'boolean' ||
            typeof needle === 'string' ||
-           typeof needle === 'number';
+           typeof needle === 'number' ||
+           needle ===  null;
 }
 
 function isSearchableRuntimeValue(haystack: Array<Value> | string) {
@@ -29,20 +30,22 @@ function isSearchableRuntimeValue(haystack: Array<Value> | string) {
            typeof haystack === 'string';
 }
 
-class In implements Expression {
+class IndexOf implements Expression {
     type: Type;
     needle: Expression;
     haystack: Expression;
+    fromIndex: ?Expression;
 
-    constructor(needle: Expression, haystack: Expression) {
-        this.type = BooleanType;
+    constructor(needle: Expression, haystack: Expression, fromIndex?: Expression) {
+        this.type = NumberType;
         this.needle = needle;
         this.haystack = haystack;
+        this.fromIndex = fromIndex;
     }
 
     static parse(args: $ReadOnlyArray<mixed>, context: ParsingContext) {
-        if (args.length !== 3) {
-            return context.error(`Expected 2 arguments, but found ${args.length - 1} instead.`);
+        if (args.length <= 2 ||  args.length >= 5) {
+            return context.error(`Expected 3 or 4 arguments, but found ${args.length - 1} instead.`);
         }
 
         const needle = context.parse(args[1], 1, ValueType);
@@ -50,19 +53,22 @@ class In implements Expression {
         const haystack = context.parse(args[2], 2, ValueType);
 
         if (!needle || !haystack) return null;
-
         if (!isComparableType(needle.type)) {
             return context.error(`Expected first argument to be of type boolean, string, number or null, but found ${toString(needle.type)} instead`);
         }
 
-        return new In(needle, haystack);
+        if (args.length === 4) {
+            const fromIndex = context.parse(args[3], 3, NumberType);
+            if (!fromIndex) return null;
+            return new IndexOf(needle, haystack, fromIndex);
+        } else {
+            return new IndexOf(needle, haystack);
+        }
     }
 
     evaluate(ctx: EvaluationContext) {
         const needle = (this.needle.evaluate(ctx): any);
         const haystack = (this.haystack.evaluate(ctx): any);
-
-        if (needle == null || !haystack) return false;
 
         if (!isComparableRuntimeValue(needle)) {
             throw new RuntimeError(`Expected first argument to be of type boolean, string or number, but found ${toString(typeOf(needle))} instead.`);
@@ -72,21 +78,33 @@ class In implements Expression {
             throw new RuntimeError(`Expected second argument to be of type array or string, but found ${toString(typeOf(haystack))} instead.`);
         }
 
-        return haystack.indexOf(needle) >= 0;
+        if (this.fromIndex) {
+            const fromIndex = (this.fromIndex.evaluate(ctx): number);
+            return haystack.indexOf(needle, fromIndex);
+        }
+
+        return haystack.indexOf(needle);
     }
 
     eachChild(fn: (_: Expression) => void) {
         fn(this.needle);
         fn(this.haystack);
+        if (this.fromIndex) {
+            fn(this.fromIndex);
+        }
     }
 
     outputDefined() {
-        return true;
+        return false;
     }
 
     serialize() {
-        return ["in", this.needle.serialize(), this.haystack.serialize()];
+        if (this.fromIndex != null && this.fromIndex !== undefined) {
+            const fromIndex = this.fromIndex.serialize();
+            return ["index-of", this.needle.serialize(), this.haystack.serialize(), fromIndex];
+        }
+        return ["index-of", this.needle.serialize(), this.haystack.serialize()];
     }
 }
 
-export default In;
+export default IndexOf;
