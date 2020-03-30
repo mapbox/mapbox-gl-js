2fe7f2ce2272843ade7d6efe5d886e4dc59f9c2a
Karim Naaji <karim.naaji@mapbox.com>
2020-03-30 15:38:18 -0700

    Fix flow

--- src/data/program_configuration.js
+++ src/data/program_configuration.js
@@ -7,7 +7,7 @@ import {register} from '../util/web_worker_transfer';
 import {PossiblyEvaluatedPropertyValue} from '../style/properties';
 import {StructArrayLayout1f4, StructArrayLayout2f8, StructArrayLayout4f16, PatternLayoutArray} from './array_types';
 import {clamp} from '../util/util';
-import {patternAttributes} from './bucket/pattern_attributes';
+import patternAttributes from './bucket/pattern_attributes';
 import EvaluationParameters from '../style/evaluation_parameters';
 import FeaturePositionMap from './feature_position_map';
 import {
