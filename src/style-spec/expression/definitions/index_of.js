274d215be3b7bb1af15a0bfa3c28ad7822f3251b
Luke Butler <lukepbutler@gmail.com>
2020-04-02 19:18:06 -0400

    Add `slice` and `index-of` expressions (#9450)

--- src/style-spec/expression/definitions/index_of.js
+++ src/style-spec/expression/definitions/index_of.js
@@ -9,20 +9,22 @@ import type ParsingContext from '../parsing_context';
 import type EvaluationContext from '../evaluation_context';
 import type {Type} from '../types';
 
-class In implements Expression {
+class IndexOf implements Expression {
     type: Type;
     needle: Expression;
     haystack: Expression;
+    fromIndex: ?Expression;
 
-    constructor(needle: Expression, haystack: Expression) {
-        this.type = BooleanType;
+    constructor(needle: Expression, haystack: Expression, fromIndex?: Expression) {
+        this.type = NumberType;
         this.needle = needle;
         this.haystack = haystack;
+        this.fromIndex = fromIndex;
     }
 
     static parse(args: $ReadOnlyArray<mixed>, context: ParsingContext) {
-        if (args.length !== 3) {
-            return context.error(`Expected 2 arguments, but found ${args.length - 1} instead.`);
+        if (args.length <= 2 ||  args.length >= 5) {
+            return context.error(`Expected 3 or 4 arguments, but found ${args.length - 1} instead.`);
         }
 
         const needle = context.parse(args[1], 1, ValueType);
@@ -30,19 +32,22 @@ class In implements Expression {
         const haystack = context.parse(args[2], 2, ValueType);
 
         if (!needle || !haystack) return null;
-
         if (!isValidType(needle.type, [BooleanType, StringType, NumberType, NullType, ValueType])) {
             return context.error(`Expected first argument to be of type boolean, string, number or null, but found ${toString(needle.type)} instead`);
         }
 
-        return new In(needle, haystack);
+        if (args.length === 4) {
+            const fromIndex = context.parse(args[3], 3, NumberType);
+            if (!fromIndex) return null;
+            return new IndexOf(needle, haystack, fromIndex);
+        } else {
+            return new IndexOf(needle, haystack);
+        }
     }
 
     evaluate(ctx: EvaluationContext) {
         const needle = (this.needle.evaluate(ctx): any);
         const haystack = (this.haystack.evaluate(ctx): any);
-
-        if (!haystack) return false;
 
         if (!isValidNativeType(needle, ['boolean', 'string', 'number', 'null'])) {
             throw new RuntimeError(`Expected first argument to be of type boolean, string, number or null, but found ${toString(typeOf(needle))} instead.`);
@@ -52,21 +57,33 @@ class In implements Expression {
             throw new RuntimeError(`Expected second argument to be of type array or string, but found ${toString(typeOf(haystack))} instead.`);
         }
 
-        return haystack.indexOf(needle) >= 0;
+        if (this.fromIndex) {
+            const fromIndex = (this.fromIndex.evaluate(ctx): number);
+            return haystack.indexOf(needle, fromIndex);
+        }
+
+        return haystack.indexOf(needle);
     }
 
     eachChild(fn: (_: Expression) => void) {
         fn(this.needle);
         fn(this.haystack);
+        if (this.fromIndex) {
+            fn(this.fromIndex);
+        }
     }
 
     outputDefined() {
-        return true;
+        return false;
     }
 
     serialize() {
-        return ["in", this.needle.serialize(), this.haystack.serialize()];
+        if (this.fromIndex != null && this.fromIndex !== undefined) {
+            const fromIndex = this.fromIndex.serialize();
+            return ["index-of", this.needle.serialize(), this.haystack.serialize(), fromIndex];
+        }
+        return ["index-of", this.needle.serialize(), this.haystack.serialize()];
     }
 }
 
-export default In;
+export default IndexOf;
