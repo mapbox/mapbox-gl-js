71420185c2564904d80d46901c3569f4b02bf8d7
Arindam Bose <arindam.bose@mapbox.com>
2020-04-08 21:10:40 -0700

    Fix lint errors

--- src/style-spec/rollup.config.js
+++ src/style-spec/rollup.config.js
@@ -32,10 +32,10 @@ const config = [{
                 // Some users reference modules within style-spec package directly, instead of the bundle
                 // This means that files within the style-spec package should NOT import files from the parent mapbox-gl-js tree.
                 // This check will cause the build the fail on CI allowing these issues to be caught.
-                if(importer && !importer.includes('node_modules')){
+                if (importer && !importer.includes('node_modules')) {
                     const resolvedPath = path.join(importer, source);
                     const fromRoot = path.relative(ROOT_DIR, resolvedPath);
-                    if(fromRoot.length > 2 && fromRoot.slice(0, 2) == '..'){
+                    if (fromRoot.length > 2 && fromRoot.slice(0, 2) === '..') {
                         throw new Error(`Module ${importer} imports ${source} from outside the style-spec package root directory.`);
                     }
                 }
