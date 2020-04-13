bae89c0b157c9b77e287ad159d92bdee9f904dbd
Sam Fader <sam.fader@mapbox.com>
2020-04-13 15:45:29 -0700

    missed a tab

--- src/ui/control/geolocate_control.js
+++ src/ui/control/geolocate_control.js
@@ -412,7 +412,7 @@ class GeolocateControl extends Evented {
     * @example
     * var geolocate = new mapboxgl.GeolocateControl({
     *  positionOptions: {
-    *  enableHighAccuracy: true
+    *    enableHighAccuracy: true
     *  },
     *  
     *  trackUserLocation: true
