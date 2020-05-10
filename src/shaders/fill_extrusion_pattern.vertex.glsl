cef95aa0241e748b396236f1269fbb8270f31565
Aleksandar Stojiljković <aleksandar.stojiljkovic@mapbox.com>
2020-05-10 21:33:03 +0300

    Reduce memory used for fill extrusion pos and normal

--- src/shaders/fill_extrusion_pattern.vertex.glsl
+++ src/shaders/fill_extrusion_pattern.vertex.glsl
@@ -10,8 +10,7 @@ uniform vec3 u_lightcolor;
 uniform lowp vec3 u_lightpos;
 uniform lowp float u_lightintensity;
 
-attribute vec2 a_pos;
-attribute vec4 a_normal_ed;
+attribute vec4 a_pos_normal_ed;
 
 varying vec2 v_pos_a;
 varying vec2 v_pos_b;
@@ -41,8 +40,16 @@ void main() {
     float fromScale = u_scale.y;
     float toScale = u_scale.z;
 
-    vec3 normal = a_normal_ed.xyz;
-    float edgedistance = a_normal_ed.w;
+    vec3 pos_nx = floor(a_pos_normal_ed.xyz * 0.5);
+    // The least significant bits of a_pos_normal_ed.xy hold:
+    // x is 1 if it's on top, 0 for ground.
+    // y is 1 if the normal points up, and 0 if it points to side.
+    // z is sign of ny: 1 for positive, 0 for values <= 0.
+    mediump vec3 top_up_ny = a_pos_normal_ed.xyz - 2.0 * pos_nx;
+
+    float x_normal = pos_nx.z / 8192.0;
+    vec3 normal = top_up_ny.y == 1.0 ? vec3(0.0, 0.0, 1.0) : normalize(vec3(x_normal, (2.0 * top_up_ny.z - 1.0) * (1.0 - abs(x_normal)), 0.0));
+    float edgedistance = a_pos_normal_ed.w;
 
     vec2 display_size_a = (pattern_br_a - pattern_tl_a) / pixel_ratio_from;
     vec2 display_size_b = (pattern_br_b - pattern_tl_b) / pixel_ratio_to;
@@ -50,20 +57,20 @@ void main() {
     base = max(0.0, base);
     height = max(0.0, height);
 
-    float t = mod(normal.x, 2.0);
+    float t = top_up_ny.x;
     float z = t > 0.0 ? height : base;
 
-    gl_Position = u_matrix * vec4(a_pos, z, 1);
+    gl_Position = u_matrix * vec4(pos_nx.xy, z, 1);
 
-    vec2 pos = normal.x == 1.0 && normal.y == 0.0 && normal.z == 16384.0
-        ? a_pos // extrusion top
+    vec2 pos = normal.z == 1.0
+        ? pos_nx.xy // extrusion top
         : vec2(edgedistance, z * u_height_factor); // extrusion side
 
     v_pos_a = get_pattern_pos(u_pixel_coord_upper, u_pixel_coord_lower, fromScale * display_size_a, tileRatio, pos);
     v_pos_b = get_pattern_pos(u_pixel_coord_upper, u_pixel_coord_lower, toScale * display_size_b, tileRatio, pos);
 
     v_lighting = vec4(0.0, 0.0, 0.0, 1.0);
-    float directional = clamp(dot(normal / 16383.0, u_lightpos), 0.0, 1.0);
+    float directional = clamp(dot(normal, u_lightpos), 0.0, 1.0);
     directional = mix((1.0 - u_lightintensity), max((0.5 + u_lightintensity), 1.0), directional);
 
     if (normal.y != 0.0) {
