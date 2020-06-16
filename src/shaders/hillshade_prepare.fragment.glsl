842ea2e08f57dbc4c70537dc9988512439d05f9d
Brendan Ward <bcward@astutespruce.com>
2020-06-16 13:57:20 -0700

    Decouple hillshade exaggeration from raster-dem maxzoom (#9789)

--- src/shaders/hillshade_prepare.fragment.glsl
+++ src/shaders/hillshade_prepare.fragment.glsl
@@ -6,7 +6,6 @@ uniform sampler2D u_image;
 varying vec2 v_pos;
 uniform vec2 u_dimension;
 uniform float u_zoom;
-uniform float u_maxzoom;
 uniform vec4 u_unpack;
 
 float getElevation(vec2 coord, float bias) {
@@ -44,23 +43,25 @@ void main() {
     float h = getElevation(v_pos + vec2(0, epsilon.y), 0.0);
     float i = getElevation(v_pos + vec2(epsilon.x, epsilon.y), 0.0);
 
-    // here we divide the x and y slopes by 8 * pixel size
+    // Here we divide the x and y slopes by 8 * pixel size
     // where pixel size (aka meters/pixel) is:
     // circumference of the world / (pixels per tile * number of tiles)
     // which is equivalent to: 8 * 40075016.6855785 / (512 * pow(2, u_zoom))
-    // which can be reduced to: pow(2, 19.25619978527 - u_zoom)
-    // we want to vertically exaggerate the hillshading though, because otherwise
-    // it is barely noticeable at low zooms. to do this, we multiply this by some
-    // scale factor pow(2, (u_zoom - u_maxzoom) * a) where a is an arbitrary value
-    // Here we use a=0.3 which works out to the expression below. see 
-    // nickidlugash's awesome breakdown for more info
+    // which can be reduced to: pow(2, 19.25619978527 - u_zoom).
+    // We want to vertically exaggerate the hillshading because otherwise
+    // it is barely noticeable at low zooms. To do this, we multiply this by
+    // a scale factor that is a function of zooms below 15, which is an arbitrary
+    // that corresponds to the max zoom level of Mapbox terrain-RGB tiles.
+    // See nickidlugash's awesome breakdown for more info:
     // https://github.com/mapbox/mapbox-gl-js/pull/5286#discussion_r148419556
-    float exaggeration = u_zoom < 2.0 ? 0.4 : u_zoom < 4.5 ? 0.35 : 0.3;
+
+    float exaggerationFactor = u_zoom < 2.0 ? 0.4 : u_zoom < 4.5 ? 0.35 : 0.3;
+    float exaggeration = u_zoom < 15.0 ? (u_zoom - 15.0) * exaggerationFactor : 0.0;
 
     vec2 deriv = vec2(
         (c + f + f + i) - (a + d + d + g),
         (g + h + h + i) - (a + b + b + c)
-    ) /  pow(2.0, (u_zoom - u_maxzoom) * exaggeration + 19.2562 - u_zoom);
+    ) / pow(2.0, exaggeration + (19.2562 - u_zoom));
 
     gl_FragColor = clamp(vec4(
         deriv.x / 2.0 + 0.5,
