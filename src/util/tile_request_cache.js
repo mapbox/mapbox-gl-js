d202f4db6e9a4a2b9c00b4f8b960ddf4f2e89ec3
Anatolij Terentjev <7015263@mail.ru>
2020-07-16 13:09:30 +0300

    Fixing issue when working in Blazor debugger #9145

--- src/util/tile_request_cache.js
+++ src/util/tile_request_cache.js
@@ -151,6 +151,7 @@ export function enforceCacheSizeLimit(limit: number) {
 
     sharedCache
         .then(cache => {
+         if (!сache) return;
             cache.keys().then(keys => {
                 for (let i = 0; i < keys.length - limit; i++) {
                     cache.delete(keys[i]);
