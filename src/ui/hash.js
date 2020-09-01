4fae6b6f321bc52cbd2570d17cf611327d623bff
Nikola Pavlovic <npavlovic101@gmail.com>
2020-09-01 13:21:41 +0200

    use null in place of document.title
    null will default to document.title

--- src/ui/hash.js
+++ src/ui/hash.js
@@ -135,7 +135,7 @@ class Hash {
         // Replace if already present, else append the updated hash string
         const location = window.location.href.replace(/(#.+)?$/, this.getHashString());
         try {
-            window.history.replaceState(window.history.state, document.title, location);
+            window.history.replaceState(window.history.state, null, location);
         } catch (SecurityError) {
             // IE11 does not allow this if the page is within an iframe created
             // with iframe.contentWindow.document.write(...).
