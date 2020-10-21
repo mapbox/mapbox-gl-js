2311a03e7a101f2bd09d8a0d00d404166ad2a113
Tristen Brown <tristen@users.noreply.github.com>
2020-10-20 20:30:48 -0400

    Clear out the content container rather than deleting it (#10036)

--- src/ui/popup.js
+++ src/ui/popup.js
@@ -411,7 +411,17 @@ export default class Popup extends Evented {
      *   .addTo(map);
      */
     setDOMContent(htmlNode: Node) {
-        this._createContent();
+        if (this._content) {
+            // Clear out children first.
+            while (this._content.hasChildNodes()) {
+                if (this._content.firstChild) {
+                    this._content.removeChild(this._content.firstChild);
+                }
+            }
+        } else {
+            this._content = DOM.create('div', 'mapboxgl-popup-content', this._container);
+        }
+
         // The close button should be the last tabbable element inside the popup for a good keyboard UX.
         this._content.appendChild(htmlNode);
         this._createCloseButton();
@@ -477,14 +487,6 @@ export default class Popup extends Evented {
         if (this._container) {
             return this._container.classList.toggle(className);
         }
-    }
-
-    _createContent() {
-        if (this._content) {
-            DOM.remove(this._content);
-        }
-
-        this._content = DOM.create('div', 'mapboxgl-popup-content', this._container);
     }
 
     _createCloseButton() {
