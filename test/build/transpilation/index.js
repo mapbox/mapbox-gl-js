72f0c501c15b1bce9005f2d25ecc80b9c59b8f29
Arindam Bose <arindam.bose@mapbox.com>
2020-12-16 21:01:24 -0800

    First try webpack build test on CI

---
+++ test/build/transpilation/index.js
@@ -0,0 +1,11 @@
+import mapboxgl from '../../../dist/mapbox-gl-csp.js';
+import MapboxGLWorker from '../../../dist/mapbox-gl-csp-worker.js';
+
+mapboxgl.workerClass = MapboxGLWorker;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
