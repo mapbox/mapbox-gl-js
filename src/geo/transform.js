9f283102019419e52d7f586da479696adc0735a3
Vladimir Agafonkin <agafonkin@gmail.com>
2020-12-17 14:02:36 +0200

    try a different fix (don't clamp if no horizon)

--- src/geo/transform.js
+++ src/geo/transform.js
@@ -902,7 +902,7 @@ class Transform {
     pointCoordinate(p: Point): MercatorCoordinate {
         // For p above horizon, don't return point behind camera but clamp p.y at horizon line.
         const horizonOffset = this.horizonLineFromTop();
-        const clamped = horizonOffset > p.y ? new Point(p.x, horizonOffset) : p;
+        const clamped = horizonOffset > 0 && horizonOffset > p.y ? new Point(p.x, horizonOffset) : p;
 
         return this.rayIntersectionCoordinate(this.pointRayIntersection(clamped));
     }
@@ -1012,7 +1012,7 @@ class Transform {
     }
 
     /**
-     * Returns position of horizon line from the top of the map in pixels.
+     * Returns position of horizon line from the top of the map in pixels. If horizon is not visible, returns 0.
      * @private
      */
     horizonLineFromTop(): number {
@@ -1020,7 +1020,8 @@ class Transform {
         const h = this.height / 2 / Math.tan(this._fov / 2) / Math.tan(Math.max(this._pitch, 0.1)) + this.centerOffset.y;
         // incorporate 3% of the area above center to account for reduced precision.
         const horizonEpsilon = 0.03;
-        return this.height / 2 - h * (1 - horizonEpsilon);
+        const offset = this.height / 2 - h * (1 - horizonEpsilon);
+        return Math.max(0, offset);
     }
 
     /**
