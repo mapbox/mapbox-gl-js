60bada618820e6d9c95bc025809e172bcadd93b3
Arindam Bose <arindam.bose@mapbox.com>
2020-12-17 11:51:29 -0800

    Add support for using third party worker-loader bundler plugins (#10219)

--- src/util/browser/web_worker.js
+++ src/util/browser/web_worker.js
@@ -6,5 +6,5 @@ import mapboxgl from '../../';
 import type {WorkerInterface} from '../web_worker';
 
 export default function (): WorkerInterface {
-    return (new window.Worker(mapboxgl.workerUrl): any);
+    return (mapboxgl.workerClass != null) ? new mapboxgl.workerClass() : (new window.Worker(mapboxgl.workerUrl): any); // eslint-disable-line new-cap
 }
