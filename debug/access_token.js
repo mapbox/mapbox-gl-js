4626adcfe96c613fc94e3d6704d2e495b577c779
Ryan Hamley <rshamley@gmail.com>
2021-01-20 13:11:40 -0800

    Add comment

--- debug/access_token.js
+++ debug/access_token.js
@@ -8,6 +8,7 @@ function getAccessToken() {
         process.env.MAPBOX_ACCESS_TOKEN ||
         getURLParameter('access_token') ||
         localStorage.getItem('accessToken') ||
+        // this token is a fallback for CI and testing. it is domain restricted to localhost
         'pk.eyJ1IjoibWFwYm94LWdsLWpzIiwiYSI6ImNram9ybGI1ajExYjQyeGxlemppb2pwYjIifQ.LGy5UGNIsXUZdYMvfYRiAQ'
     );
     try {
