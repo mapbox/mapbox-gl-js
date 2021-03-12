4d32dbe61128d279821f7f50dc0c1d9d804b7ed4
Karim Naaji <karim.naaji@gmail.com>
2021-03-12 12:11:23 -0800

    Revert 10444 + 10446 (#10462)
    * Revert "Terrain: Don't load unnecessary tiles (#10444)"
    
    This reverts commit b00f90b080034ee2a2bd25e033a81e1dc902d096.
    
    * Revert "Unit test for zoom 22 at Mile High City (#10446)"
    
    This reverts commit 1bacb2b40aa9c357461c5c55ac4bcada0abd662d.

--- src/source/source_cache.js
+++ src/source/source_cache.js
@@ -494,7 +494,7 @@ class SourceCache extends Evented {
                 maxzoom: this._source.maxzoom,
                 roundZoom: this._source.roundZoom && !updateForTerrain,
                 reparseOverscaled: this._source.reparseOverscaled,
-                isTerrainDEM: this.usedForTerrain
+                useElevationData: !!this.transform.elevation && !this.usedForTerrain
             });
 
             if (this._source.hasTile) {
