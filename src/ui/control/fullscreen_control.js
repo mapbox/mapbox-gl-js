ad275c48a662ca7d6ccac18af2437c9fd8b60307
danswick <dan.swick@gmail.com>
2021-03-17 12:01:28 -0700

    add note about fullscreen compatibility with mobile safari

--- src/ui/control/fullscreen_control.js
+++ src/ui/control/fullscreen_control.js
@@ -12,7 +12,7 @@ type Options = {
 };
 
 /**
- * A `FullscreenControl` control contains a button for toggling the map in and out of fullscreen mode.
+ * A `FullscreenControl` control contains a button for toggling the map in and out of fullscreen mode. `FullScreenControl` is [not currently compatible](https://developer.mozilla.org/en-US/docs/Web/API/Element/requestFullScreen#browser_compatibility) with the mobile Safari browser.
  *
  * @implements {IControl}
  * @param {Object} [options]
