4b72f3adde5fa0721402fbfb2eec07be2dadb9ac
Arindam Bose <arindam.bose@mapbox.com>
2021-03-17 11:53:27 -0700

    rename posMatrix to projMatrix

--- src/source/tile_id.js
+++ src/source/tile_id.js
@@ -79,7 +79,7 @@ export class OverscaledTileID {
     wrap: number;
     canonical: CanonicalTileID;
     key: number;
-    posMatrix: Float32Array;
+    projMatrix: Float32Array;
 
     constructor(overscaledZ: number, wrap: number, z: number, x: number, y: number) {
         assert(overscaledZ >= z);
@@ -219,4 +219,4 @@ function getQuadkey(z, x, y) {
 }
 
 register('CanonicalTileID', CanonicalTileID);
-register('OverscaledTileID', OverscaledTileID, {omit: ['posMatrix']});
+register('OverscaledTileID', OverscaledTileID, {omit: ['projMatrix']});
