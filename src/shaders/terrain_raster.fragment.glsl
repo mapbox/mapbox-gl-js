b5602c715adcecb4783359bb0880c31338319c97
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 12:25:47 -0700

    Apply fog to terrain render

--- src/shaders/terrain_raster.fragment.glsl
+++ src/shaders/terrain_raster.fragment.glsl
@@ -1,11 +1,16 @@
 uniform sampler2D u_image0;
 varying vec2 v_pos0;
 
-void main() {
-    gl_FragColor = texture2D(u_image0, v_pos0);
 #ifdef FOG
-    gl_FragColor = vec4(fog_color(), 1.0);
+varying float v_depth;
 #endif
+
+void main() {
+    vec4 color = texture2D(u_image0, v_pos0);
+#ifdef FOG
+    color.rgb = fog_apply(color.rgb, v_depth);
+#endif
+    gl_FragColor = color;
 #ifdef TERRAIN_WIREFRAME
     gl_FragColor = vec4(1.0, 0.0, 0.0, 0.8);
 #endif
