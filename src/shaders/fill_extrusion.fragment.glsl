361a059a99c5757c3be8f2369970c290aae1e3d3
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 16:33:26 -0700

    Apply fog to fill-extrusion layer

--- src/shaders/fill_extrusion.fragment.glsl
+++ src/shaders/fill_extrusion.fragment.glsl
@@ -1,7 +1,15 @@
 varying vec4 v_color;
 
+#ifdef FOG
+varying float v_depth;
+#endif
+
 void main() {
-    gl_FragColor = v_color;
+    vec4 color = v_color;
+#ifdef FOG
+    color.rgb = fog_apply(color.rgb, v_depth);
+#endif
+    gl_FragColor = color;
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
