fd96bd5c158635731339fdc4c265112773b71a9a
Karim Naaji <karim.naaji@gmail.com>
2021-03-16 17:58:01 -0700

    Inject shader preludes and connect to globally defined fog uniforms
    Fixup

--- src/shaders/_prelude_fog.fragment.glsl
+++ src/shaders/_prelude_fog.fragment.glsl
@@ -1,3 +1,14 @@
 #ifdef FOG
 
+uniform vec2 u_fog_range;
+uniform vec3 u_fog_color;
+uniform float u_fog_opacity;
+uniform float u_fog_sky_blend;
+
+vec3 fogColor() { return u_fog_color; }
+
+#else
+
+vec3 fogColor() { return vec3(0.0); }
+
 #endif
