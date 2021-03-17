7f647da031194416bc8e4ee369cbb3e1bfb24e16
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 16:38:09 -0700

    Apply fog to fill layer

--- src/shaders/fill.fragment.glsl
+++ src/shaders/fill.fragment.glsl
@@ -1,11 +1,21 @@
 #pragma mapbox: define highp vec4 color
 #pragma mapbox: define lowp float opacity
 
+#ifdef FOG
+varying float v_depth;
+#endif
+
 void main() {
     #pragma mapbox: initialize highp vec4 color
     #pragma mapbox: initialize lowp float opacity
 
-    gl_FragColor = color * opacity;
+    vec4 out_color = color * opacity;
+
+#ifdef FOG
+    out_color.rgb = fog_apply(out_color.rgb, v_depth);
+#endif
+
+    gl_FragColor = out_color;
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
