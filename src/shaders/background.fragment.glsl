286d1b184bc0fb06322fdfc81d6f4bd27864be71
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 17:11:14 -0700

    Apply fog to background layer

--- src/shaders/background.fragment.glsl
+++ src/shaders/background.fragment.glsl
@@ -1,8 +1,18 @@
 uniform vec4 u_color;
 uniform float u_opacity;
 
+#ifdef FOG
+varying float v_depth;
+#endif
+
 void main() {
-    gl_FragColor = u_color * u_opacity;
+    vec4 out_color = u_color * u_opacity;
+
+#ifdef FOG
+    out_color.rgb = fog_apply(out_color.rgb, v_depth);
+#endif
+
+    gl_FragColor = out_color;
 
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
