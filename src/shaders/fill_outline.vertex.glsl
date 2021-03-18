b011fcc96098ad4164b104a9fbce4afd9e54f4a1
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 17:08:01 -0700

    Apply fog to fill-outline, fill-outline-pattern, fill-pattern layers

--- src/shaders/fill_outline.vertex.glsl
+++ src/shaders/fill_outline.vertex.glsl
@@ -5,6 +5,10 @@ uniform vec2 u_world;
 
 varying vec2 v_pos;
 
+#ifdef FOG
+varying float v_depth;
+#endif
+
 #pragma mapbox: define highp vec4 outline_color
 #pragma mapbox: define lowp float opacity
 
@@ -14,4 +18,8 @@ void main() {
 
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
     v_pos = (gl_Position.xy / gl_Position.w + 1.0) / 2.0 * u_world;
+
+#ifdef FOG
+    v_depth = length(gl_Position.xyz);
+#endif
 }
