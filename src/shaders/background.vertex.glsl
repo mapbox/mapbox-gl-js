3a7035f793690b6c9bc13663b288136bef7b2d2f
Karim Naaji <karim.naaji@gmail.com>
2021-03-17 17:11:14 -0700

    Apply fog to background layer

--- src/shaders/background.vertex.glsl
+++ src/shaders/background.vertex.glsl
@@ -2,6 +2,14 @@ attribute vec2 a_pos;
 
 uniform mat4 u_matrix;
 
+#ifdef FOG
+varying float v_depth;
+#endif
+
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
+
+#ifdef FOG
+    v_depth = length(gl_Position.xyz);
+#endif
 }
