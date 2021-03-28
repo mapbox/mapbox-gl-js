faeac7786052baf1b167d34061af4464f686020f
Ricky Reusser <ricky.reusser@mapbox.com>
2021-03-28 11:54:07 -0700

    Rework the fog function

--- src/shaders/_prelude_fog.fragment.glsl
+++ src/shaders/_prelude_fog.fragment.glsl
@@ -14,27 +14,41 @@ vec3 fog_apply_sky_gradient(vec3 cubemap_uv, vec3 sky_color) {
     float fog_falloff = clamp(gradient + (1.0 - u_fog_opacity), 0.0, 1.0);
 
     // We may or may not wish to use gamma-correct blending
-    return gamma_mix(u_fog_color, sky_color, fog_falloff);
+    return mix(u_fog_color, sky_color, fog_falloff);
 }
 
-float fog_opacity(vec3 position) {
-    float depth = length(position);
+float fog_opacity(float depth) {
     float start = u_fog_range.x;
     float end = u_fog_range.y;
 
-    // Fog falls off exponentially, but it also doesn't start at an arbitrary
-    // distance. So we opt for a sigmoid function that results in a very smooth
-    // onset. See: https://www.desmos.com/calculator/boumcg1dwo
-    // Fog power puts the fog at about 0.6% and 99.4% at near and far, respectively
-    // The multiplier puts the far limit at 1.0 since clipping depends on that
-    const float fog_pow = 10.0;
-    float fog_falloff = min(1.0, 1.00675 / (1.0 + exp(-fog_pow * ((depth - start) / (end - start) - 0.5))));
+    // The fog is not physically accurate, so we seek an expression which satisfies a
+    // couple basic constraints:
+    //   - opacity should be 0 at the near limit
+    //   - opacity should be 1 at the far limit
+    //   - the onset should have smooth derivatives to avoid a sharp band
+    // To this end, we use an (1 - e^x)^n, where n is set to 3 to ensure the
+    // function is C2 continuous at the onset. The fog is about 99% opaque at
+    // the far limit, so we simply scale it and clip to achieve 100% opacity.
+    // https://www.desmos.com/calculator/3taufutxid
+    const float decay = 5.5;
+    float falloff = max(0.0, 1.0 - exp(-decay * (depth - start) / (end - start)));
 
-    return fog_falloff * u_fog_opacity;
+    // Cube without pow()
+    falloff *= falloff * falloff;
+
+    // Scale and clip to 1 at the far limit
+    falloff = min(1.0, 1.00747 * falloff);
+
+
+    return falloff * u_fog_opacity;
 }
 
-vec3 fog_apply(vec3 color, vec3 position) {
-    return mix(color, u_fog_color, fog_opacity(position));
+vec3 fog_apply(vec3 color, vec3 pos) {
+    // We mix in sRGB color space. sRGB roughly corrects for perceived brightness
+    // so that dark fog and light fog obscure similarly for otherwise identical
+    // parameters. If we gamma-correct, then the parameters to control dark and
+    // light fog are fundamentally different.
+    return mix(color, u_fog_color, fog_opacity(length(pos)));
 }
 
 vec3 fog_dither(vec3 color) {
@@ -47,9 +61,9 @@ vec4 fog_dither(vec4 color) {
 
 // Un-premultiply the alpha, then blend fog, then re-premultiply alpha. For
 // use with colors using premultiplied alpha
-vec4 fog_apply_premultiplied(vec4 color, vec3 position) {
+vec4 fog_apply_premultiplied(vec4 color, vec3 pos) {
     float a = 1e-4 + color.a;
-    return vec4(fog_apply(min(color.rgb / a, vec3(1)), position) * a, color.a);
+    return vec4(fog_apply(min(color.rgb / a, vec3(1)), pos) * a, color.a);
 }
 
 #endif
