930795b04dd29d1012671b3cf67a7decd91a91ad
Dom Brassey <dom.brassey@mapbox.com>
2021-03-30 16:35:40 -0700

    spacing corrections

--- src/ui/events.js
+++ src/ui/events.js
@@ -194,7 +194,8 @@ export class MapTouchEvent extends Event {
  */
 export class MapWheelEvent extends Event {
     /**
-     * The event type (one of {@link Map.event:wheel}.     */
+     * The event type (one of {@link Map.event:wheel}).
+     */
     type: 'wheel';
 
     /**
@@ -813,7 +814,6 @@ export type MapEvent =
      * Fired just after the map completes a transition from one zoom level to another
      * as the result of either user interaction or methods such as {@link Map#flyTo}.
      * The zoom transition will usually end before rendering is finished, so if you
-
      * need to wait for rendering to finish, use the {@link Map#idle} event instead.
      *
      * @event zoomend
