b7b08b3c46dda743e35a047c2f0ed83a7b1eb73d
Arindam Bose <arindam.bose@mapbox.com>
2021-04-01 14:40:51 -0700

    linear and single channel

--- src/shaders/fog_texture.fragment.glsl
+++ src/shaders/fog_texture.fragment.glsl
@@ -1,17 +1,8 @@
 uniform sampler2D u_image;
 varying vec2 v_pos;
 
-
-// Unpack depth from RGBA. A piece of code copied in various libraries and WebGL
-// shadow mapping examples.
-float unpack_depth(vec4 rgba_depth)
-{
-    const vec4 bit_shift = vec4(1.0 / (256.0 * 256.0 * 256.0), 1.0 / (256.0 * 256.0), 1.0 / 256.0, 1.0);
-    return dot(rgba_depth, bit_shift) * 2.0 - 1.0;
-}
-
 void main() {
-    float fog_depth = unpack_depth(texture2D(u_image, v_pos));
+    float fog_depth = texture2D(u_image, v_pos).r;
 
     gl_FragColor = vec4(u_fog_color * fog_depth, fog_depth);
 #ifdef OVERDRAW_INSPECTOR
