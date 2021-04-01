a94c3f15acfe0bbd35aa4df9e6d75daca9a33b15
Ricky Reusser <ricky.reusser@mapbox.com>
2021-04-01 10:05:13 -0700

    Set button type=button to prevent form submit

--- src/ui/control/attribution_control.js
+++ src/ui/control/attribution_control.js
@@ -57,6 +57,7 @@ class AttributionControl {
         this._map = map;
         this._container = DOM.create('div', 'mapboxgl-ctrl mapboxgl-ctrl-attrib');
         this._compactButton = DOM.create('button', 'mapboxgl-ctrl-attrib-button', this._container);
+        this._compactButton.type = 'button';
         this._compactButton.addEventListener('click', this._toggleAttribution);
         this._setElementTitle(this._compactButton, 'ToggleAttribution');
         this._innerContainer = DOM.create('div', 'mapboxgl-ctrl-attrib-inner', this._container);
