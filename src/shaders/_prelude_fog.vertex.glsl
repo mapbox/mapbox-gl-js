dcd0ae18009e7186f0430313b173777dd3ab4166
Ricky Reusser <rreusser@users.noreply.github.com>
2021-04-07 14:15:51 -0700

    Rename cameraMatrix --> fogMatrix (#10544)

--- src/shaders/_prelude_fog.vertex.glsl
+++ src/shaders/_prelude_fog.vertex.glsl
@@ -1,11 +1,11 @@
 #ifdef FOG
 
-uniform mat4 u_cam_matrix;
+uniform mat4 u_fog_matrix;
 
 vec3 fog_position(vec3 pos) {
-    // The following function requires that u_cam_matrix be affine and result in
+    // The following function requires that u_fog_matrix be affine and result in
     // a vector with w = 1. Otherwise we must divide by w.
-    return (u_cam_matrix * vec4(pos, 1)).xyz;
+    return (u_fog_matrix * vec4(pos, 1)).xyz;
 }
 
 // Accept either 2D or 3D positions
