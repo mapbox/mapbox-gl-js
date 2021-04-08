3534fa5ba5825cb326aea7ab22ec7374ff2f823c
Ricky Reusser <rreusser@users.noreply.github.com>
2021-04-08 11:01:48 -0700

    Offload terrain fog to vertex shader (#10549)
    * Offload terrain fog to vertex shader
    
    * Don't set haze uniforms unless haze is present
    
    * Remove haze varying unless haze is used
    
    * Disable fog code path on the 2d case
    
    * Remove redundant uniforms
    
    * Simplify fog render logic
    
    Co-authored-by: Karim Naaji <karim.naaji@gmail.com>

--- src/shaders/_prelude_fog.vertex.glsl
+++ src/shaders/_prelude_fog.vertex.glsl
@@ -1,6 +1,18 @@
 #ifdef FOG
 
 uniform mat4 u_fog_matrix;
+uniform mediump float u_fog_opacity;
+uniform mediump float u_fog_exponent;
+uniform mediump vec2 u_fog_range;
+uniform mediump vec4 u_haze_color_linear;
+
+// This function much match fog_opacity defined in _prelude_fog.fragment.glsl
+float fog_opacity(float t) {
+    const float decay = 6.0;
+    float falloff = 1.0 - min(1.0, exp(-decay * t));
+    falloff *= falloff * falloff;
+    return u_fog_opacity * min(1.0, 1.00747 * falloff);
+}
 
 vec3 fog_position(vec3 pos) {
     // The following function requires that u_fog_matrix be affine and result in
@@ -11,6 +23,28 @@ vec3 fog_position(vec3 pos) {
 // Accept either 2D or 3D positions
 vec3 fog_position(vec2 pos) {
     return fog_position(vec3(pos, 0));
+}
+
+void fog_haze(
+    vec3 pos, out float fog_opac
+#ifdef FOG_HAZE
+    , out vec4 haze
+#endif
+) {
+    // Map [near, far] to [0, 1]
+    float t = (length(pos) - u_fog_range.x) / (u_fog_range.y - u_fog_range.x);
+
+    float haze_opac = fog_opacity(t);
+    fog_opac = haze_opac * pow(smoothstep(0.0, 1.0, t), u_fog_exponent);
+
+#ifdef FOG_HAZE
+    haze.rgb = (haze_opac * u_haze_color_linear.a) * u_haze_color_linear.rgb;
+
+    // The smoothstep fades in tonemapping slightly before the fog layer. This violates
+    // the principle that fog should not have an effect outside the fog layer, but the
+    // effect is hardly noticeable except on pure white glaciers.
+    haze.a = u_fog_opacity * min(1.0, u_haze_color_linear.a) * smoothstep(-0.5, 0.25, t);
+#endif
 }
 
 #endif
