2a73031d58e997d25a2418cd0deb5cd94796ed08
Aleksandar Stojiljković <aleksandar.stojiljkovic@mapbox.com>
2021-04-08 17:39:07 +0300

    Terrain: shader support for sampling DEM elevation from float textures
    Feature is implemented in gl-native first. Since shader code is shared, code here is
    not used by gl-js (`TERRAIN_DEM_FLOAT_FORMAT` is not defined).

--- src/shaders/hillshade_prepare.fragment.glsl
+++ src/shaders/hillshade_prepare.fragment.glsl
@@ -8,11 +8,15 @@ uniform vec2 u_dimension;
 uniform float u_zoom;
 uniform vec4 u_unpack;
 
-float getElevation(vec2 coord, float bias) {
+float getElevation(vec2 coord) {
+#ifdef TERRAIN_DEM_FLOAT_FORMAT
+    return texture2D(u_image, coord).a / 4.0;
+#else
     // Convert encoded elevation value to meters
     vec4 data = texture2D(u_image, coord) * 255.0;
     data.a = -1.0;
     return dot(data, u_unpack) / 4.0;
+#endif
 }
 
 void main() {
@@ -33,15 +37,15 @@ void main() {
     // |   |   |   |
     // +-----------+
 
-    float a = getElevation(v_pos + vec2(-epsilon.x, -epsilon.y), 0.0);
-    float b = getElevation(v_pos + vec2(0, -epsilon.y), 0.0);
-    float c = getElevation(v_pos + vec2(epsilon.x, -epsilon.y), 0.0);
-    float d = getElevation(v_pos + vec2(-epsilon.x, 0), 0.0);
-    float e = getElevation(v_pos, 0.0);
-    float f = getElevation(v_pos + vec2(epsilon.x, 0), 0.0);
-    float g = getElevation(v_pos + vec2(-epsilon.x, epsilon.y), 0.0);
-    float h = getElevation(v_pos + vec2(0, epsilon.y), 0.0);
-    float i = getElevation(v_pos + vec2(epsilon.x, epsilon.y), 0.0);
+    float a = getElevation(v_pos + vec2(-epsilon.x, -epsilon.y));
+    float b = getElevation(v_pos + vec2(0, -epsilon.y));
+    float c = getElevation(v_pos + vec2(epsilon.x, -epsilon.y));
+    float d = getElevation(v_pos + vec2(-epsilon.x, 0));
+    float e = getElevation(v_pos);
+    float f = getElevation(v_pos + vec2(epsilon.x, 0));
+    float g = getElevation(v_pos + vec2(-epsilon.x, epsilon.y));
+    float h = getElevation(v_pos + vec2(0, epsilon.y));
+    float i = getElevation(v_pos + vec2(epsilon.x, epsilon.y));
 
     // Here we divide the x and y slopes by 8 * pixel size
     // where pixel size (aka meters/pixel) is:
