71c71730f8028e7565c7fa9673ea0357aad0c1f9
Christian Oliff <christianoliff@pm.me>
2021-04-09 23:44:53 +0900

    Fix Inaccessibility typo (#10551)

--- src/util/find_pole_of_inaccessibility.js
+++ src/util/find_pole_of_inaccessibility.js
@@ -6,13 +6,13 @@ import Point from '@mapbox/point-geometry';
 import {distToSegmentSquared} from './intersection_tests.js';
 
 /**
- * Finds an approximation of a polygon's Pole Of Inaccessibiliy https://en.wikipedia.org/wiki/Pole_of_inaccessibility
+ * Finds an approximation of a polygon's Pole Of Inaccessibility https://en.wikipedia.org/wiki/Pole_of_inaccessibility
  * This is a copy of http://github.com/mapbox/polylabel adapted to use Points
  *
  * @param polygonRings first item in array is the outer ring followed optionally by the list of holes, should be an element of the result of util/classify_rings
  * @param precision Specified in input coordinate units. If 0 returns after first run, if > 0 repeatedly narrows the search space until the radius of the area searched for the best pole is less than precision
  * @param debug Print some statistics to the console during execution
- * @returns Pole of Inaccessibiliy.
+ * @returns Pole of Inaccessibility.
  * @private
  */
 export default function (polygonRings: Array<Array<Point>>, precision?: number = 1, debug?: boolean = false): Point {
