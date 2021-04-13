269c45a0ce180145aabace7439b708864b4a4738
Colleen McGinnis <colleen.mcginnis@mapbox.com>
2021-04-13 13:10:51 -0500

    document EdgeInsets as an internal class (#10563)

--- src/geo/edge_insets.js
+++ src/geo/edge_insets.js
@@ -4,6 +4,7 @@ import Point  from "@mapbox/point-geometry";
 import {clamp} from "../util/util.js";
 
 /**
+ * @private
  * An `EdgeInset` object represents screen space padding applied to the edges of the viewport.
  * This shifts the apparent center or the vanishing point of the map. This is useful for adding floating UI elements
  * on top of the map and having the vanishing point shift as UI elements resize.
