a228a36ef6d11ff904fc48e234e794cd76c44d9b
Vladimir Agafonkin <agafonkin@gmail.com>
2021-04-21 18:11:57 +0300

    fixup

--- src/data/program_configuration.js
+++ src/data/program_configuration.js
@@ -425,7 +425,7 @@ export default class ProgramConfiguration {
             const propType = value.property.specification['property-type'];
             const isCrossFaded = propType === 'cross-faded' || propType === 'cross-faded-data-driven';
 
-            const sourceException = property === 'line-dasharray' && layer.layout._values['line-cap'].value.kind === 'source';
+            const sourceException = String(property) === 'line-dasharray' && (layer.layout: any).get('line-cap').value.kind === 'source';
 
             if (expression.kind === 'constant' && !sourceException) {
                 this.binders[property] = isCrossFaded ?
@@ -433,7 +433,7 @@ export default class ProgramConfiguration {
                     new ConstantBinder(expression.value, names, type);
                 keys.push(`/u_${property}`);
 
-            } else if (expression.kind === 'source' || isCrossFaded) {
+            } else if (expression.kind === 'source' || sourceException || isCrossFaded) {
                 const StructArrayLayout = layoutType(property, type, 'source');
                 this.binders[property] = isCrossFaded ?
                     new CrossFadedCompositeBinder(expression, names, type, useIntegerZoom, zoom, StructArrayLayout, layer.id) :
