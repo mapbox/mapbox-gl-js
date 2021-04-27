ecc163fe053abd8867bf2e36f25ea3d2a02a21ad
Karim Naaji <karim.naaji@gmail.com>
2021-04-27 13:52:53 -0700

    Revise comments and cleanup shader code for readability - Also removes extraneous branching with min in fog_apply_premultiplied

--- src/shaders/_prelude_fog.vertex.glsl
+++ src/shaders/_prelude_fog.vertex.glsl
@@ -3,14 +3,13 @@
 uniform mat4 u_fog_matrix;
 
 vec3 fog_position(vec3 pos) {
-    // The following function requires that u_fog_matrix be affine and result in
-    // a vector with w = 1. Otherwise we must divide by w.
-    return (u_fog_matrix * vec4(pos, 1)).xyz;
+    // The following function requires that u_fog_matrix be affine and
+    // results in a vector with w = 1. Otherwise we must divide by w.
+    return (u_fog_matrix * vec4(pos, 1.0)).xyz;
 }
 
-// Accept either 2D or 3D positions
 vec3 fog_position(vec2 pos) {
-    return fog_position(vec3(pos, 0));
+    return fog_position(vec3(pos, 0.0));
 }
 
 float fog(vec3 pos) {
