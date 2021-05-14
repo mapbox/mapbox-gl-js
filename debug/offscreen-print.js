96dfdb926984dfa2d7035e71cb0f2d7a5f06ff57
Adriana Babakanian <adriana.babakanian@mapbox.com>
2021-05-14 10:20:06 -0700

    Add form to html page

--- debug/offscreen-print.js
+++ debug/offscreen-print.js
@@ -1,25 +1,25 @@
 importScripts('../dist/mapbox-gl-dev.js');
 importScripts('../debug/access_token_generated.js');
 self.requestAnimationFrame = function(callback) {
-        return setTimeout(callback, 16);
-    };
+  return setTimeout(callback, 16);
+};
 self.cancelAnimationFrame = clearTimeout;
 self.devicePixelRatio = 1;
 
 onmessage = function(e) {
-    const canvas = new OffscreenCanvas(1000, 1000);
-    var map = new mapboxgl.Map({
-        container: canvas ,
-        zoom: 12.5,
-        center: [-122.4194, 37.7749],
-        style: 'mapbox://styles/mapbox/streets-v10',
-        hash: true
-    });
+  const canvas = new OffscreenCanvas(e.data.width, e.data.height);
+  var map = new mapboxgl.Map({
+    container: canvas ,
+    zoom: 12.5,
+    center: [-122.4194, 37.7749],
+    style: 'mapbox://styles/mapbox/streets-v10',
+    hash: true
+  });
 
-    map.on('idle', () => {
-        canvas.convertToBlob({type: 'image/png'}).then((blob) => {
-            console.log(URL.createObjectURL(blob));
-            postMessage({ renderResult: blob });
-        })
+  map.on('idle', () => {
+    canvas.convertToBlob({type: 'image/png'}).then((blob) => {
+        console.log(URL.createObjectURL(blob));
+        postMessage({ renderResult: blob });
     });
+  });
 }
