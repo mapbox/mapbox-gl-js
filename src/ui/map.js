9524b840f2d45545174a42d4121e99a4975aa074
Dom Brassey <dom.brassey@mapbox.com>
2021-06-08 14:57:33 -0700

    add two ?

--- src/ui/map.js
+++ src/ui/map.js
@@ -1156,7 +1156,7 @@ class Map extends Camera {
      * @see [Animate the camera around a point with 3D terrain](https://docs.mapbox.com/mapbox-gl-js/example/free-camera-point/)
      * @see [Play map locations as a slideshow](https://docs.mapbox.com/mapbox-gl-js/example/playback-locations/)
      */
-    once(type: MapEvent, layerId: any, listener: any) {
+    once(type: MapEvent, layerId?: any, listener: any) {
         if (listener === undefined && typeof layerId === 'function') {
             return super.once(type, layerId);
         }
@@ -1195,7 +1195,7 @@ class Map extends Camera {
      * });
      * @see [Create a draggable point](https://docs.mapbox.com/mapbox-gl-js/example/drag-a-point/)
      */
-    off(type: MapEvent, layerId: any, listener: any) {
+    off(type: MapEvent, layerId?: any, listener: any) {
         if (listener === undefined && typeof layerId === 'function') {
             return super.off(type, layerId);
         }
