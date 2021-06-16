f417a7943c7be76db1218cfb6f6d9a232d1f1ade
Vladimir Agafonkin <agafonkin@gmail.com>
2021-06-16 19:55:15 +0300

    skip resampling for mercator

--- src/data/load_geometry.js
+++ src/data/load_geometry.js
@@ -58,8 +58,7 @@ export default function loadGeometry(feature: VectorTileFeature, canonical?: Can
 
     function reproject(p) {
         if (projection && projection.name === 'mercator' || !canonical) {
-            const p_ = new Point(Math.round(p.x * scale), Math.round(p.y * scale));
-            return clampPoint(p_);
+            return new Point(Math.round(p.x * scale), Math.round(p.y * scale));
         } else {
             const lng = lngFromMercatorX((canonical.x + p.x / featureExtent) / z2);
             const lat = latFromMercatorY((canonical.y + p.y / featureExtent) / z2);
@@ -96,7 +95,7 @@ export default function loadGeometry(feature: VectorTileFeature, canonical?: Can
             const pointMerc = ring[i];
             const pointProj = reproject(ring[i]);
 
-            if (prevMerc && prevProj && feature.type !== 1) {
+            if (projection && projection.name !== 'mercator' && prevMerc && prevProj && feature.type !== 1) {
                 addResampled(resampled, prevMerc, pointMerc, prevProj, pointProj);
             } else {
                 resampled.push(clampPoint(pointProj));
