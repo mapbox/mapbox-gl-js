c377a90e768c48b7a422596740e06cae2a55a055
Ricky Reusser <rreusser@users.noreply.github.com>
2021-06-21 12:57:15 -0700

    v2.3.1 additional cherry-picks (#10790)
    * Migrate github size chekcer app and aws creds to mbx-ci (#10719)
    
    * Fix collision boxes for text with variable placement (#10709)
    
    * Add 10709 to changelog
    
    Co-authored-by: Arindam Bose <arindam.bose@mapbox.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ### 🐞 Bug fixes
 
 * Fix fog flickering when the map option `optimizeForTerrain` is set to false ([#10763](https://github.com/mapbox/mapbox-gl-js/pull/10767))
+* Fix collision boxes which were not correctly updated for symbols with `text-variable-anchor` ([#10709](https://github.com/mapbox/mapbox-gl-js/pull/10709))
 
 ## 2.3.0
 
--- src/data/bucket/symbol_attributes.js
+++ src/data/bucket/symbol_attributes.js
@@ -108,7 +108,6 @@ export const symbolInstance = createLayout([
     {type: 'Uint16', name: 'numVerticalIconVertices'},
     {type: 'Uint16', name: 'useRuntimeCollisionCircles'},
     {type: 'Uint32', name: 'crossTileID'},
-    {type: 'Float32', name: 'textBoxScale'},
     {type: 'Float32', components: 2, name: 'textOffset'},
     {type: 'Float32', name: 'collisionCircleDiameter'},
 ]);
