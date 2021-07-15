e407a1beecd40aa57c205fbd754e692d68f811b9
HeyStenson <heather.stenson@mapbox.com>
2021-07-15 15:26:08 -0700

    GL-JS --> Mapbox GL JS

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ const exported = {
      * `mapboxgl.clearPrewarmedResources()`. This is only necessary if your web page remains
      * active but stops using maps altogether.
      *
-     * This is primarily useful when using GL-JS maps in a single page app, wherein a user
+     * This is primarily useful when using Mapbox GL JS maps in a single page app, in which a user
      * would navigate between various views that can cause Map instances to constantly be
      * created and destroyed.
      *
