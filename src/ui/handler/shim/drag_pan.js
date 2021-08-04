34b0c66fb64b08349003062206fb0d53060af73f
Dom Brassey <dom.brassey@mapbox.com>
2021-08-04 14:01:44 -0700

    simplify easing in example, add @see link, clarify that default value uses a dependency

--- src/ui/handler/shim/drag_pan.js
+++ src/ui/handler/shim/drag_pan.js
@@ -38,7 +38,7 @@ export default class DragPanHandler {
      *
      * @param {Object} [options] Options object.
      * @param {number} [options.linearity=0] Factor used to scale the drag velocity.
-     * @param {Function} [options.easing=bezier(0, 0, 0.3, 1)] Easing function applied to {@link Map#panTo} when applying the drag.
+     * @param {Function} [options.easing] Optional easing function applied to {@link Map#panTo} when applying the drag. Defaults to bezier function using [@]mapbox/unitbezier
      * @param {number} [options.maxSpeed=1400] The maximum value of the drag velocity.
      * @param {number} [options.deceleration=2500] The rate at which the speed reduces after the pan ends.
      *
@@ -47,10 +47,13 @@ export default class DragPanHandler {
      * @example
      * map.dragPan.enable({
      *     linearity: 0.3,
-     *     easing: bezier(0, 0, 0.3, 1),
+     *     easing: function(t){
+     *         return t;
+     *     },
      *     maxSpeed: 1400,
      *     deceleration: 2500
      * });
+     * @see [Highlight features within a bounding box](https://docs.mapbox.com/mapbox-gl-js/example/using-box-queryrenderedfeatures/)
      */
     enable(options?: DragPanOptions) {
         this._inertiaOptions = options || {};
