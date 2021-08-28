b519ea4f54affbe317a6db6dbe4af4febd0a5623
Karim Naaji <karim.naaji@gmail.com>
2021-08-27 17:09:36 -0700

    Simplify readability of shader code

--- src/shaders/globe_raster.vertex.glsl
+++ src/shaders/globe_raster.vertex.glsl
@@ -11,7 +11,12 @@ varying vec2 v_pos0;
 
 void main() {
     v_pos0 = a_uv;
-    vec4 globe =  u_globe_matrix * vec4(a_globe_pos + elevationVector(a_uv * 8192.0) * elevation(a_uv * 8192.0), 1.0);
+
+    vec3 height = elevationVector(a_uv * 8192.0) * elevation(a_uv * 8192.0);
+
+    vec4 globe =  u_globe_matrix * vec4(a_globe_pos + height, 1.0);
     vec4 mercator = u_merc_matrix * vec4(a_merc_pos, 1.0);
-    gl_Position = u_proj_matrix * vec4(mix(globe.xyz, mercator.xyz, vec3(u_zoom_transition)), 1.0);
+    vec3 position = mix(globe.xyz, mercator.xyz, vec3(u_zoom_transition));
+
+    gl_Position = u_proj_matrix * vec4(position, 1.0);
 }
