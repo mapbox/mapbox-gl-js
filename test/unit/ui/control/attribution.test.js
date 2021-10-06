13d547f7afca53bf00feea605ee03150a021d1a2
avpeery <anna.peery@mapbox.com>
2021-10-06 16:35:54 -0700

    Change size of container instead of canvas container to trigger resize in unit tests

--- test/unit/ui/control/attribution.test.js
+++ test/unit/ui/control/attribution.test.js
@@ -64,14 +64,14 @@ test('AttributionControl appears in compact mode if compact option is used', (t)
 
 test('AttributionControl appears in compact mode if container is less then 640 pixel wide', (t) => {
     const map = createMap(t);
-    Object.defineProperty(map.getCanvasContainer(), 'offsetWidth', {value: 700, configurable: true});
+    Object.defineProperty(map.getContainer(), 'offsetWidth', {value: 700, configurable: true});
     map.addControl(new AttributionControl());
 
     const container = map.getContainer();
 
     t.equal(container.querySelectorAll('.mapboxgl-ctrl-attrib:not(.mapboxgl-compact)').length, 1);
 
-    Object.defineProperty(map.getCanvasContainer(), 'offsetWidth', {value: 600, configurable: true});
+    Object.defineProperty(map.getContainer(), 'offsetWidth', {value: 600, configurable: true});
     map.resize();
 
     t.equal(container.querySelectorAll('.mapboxgl-ctrl-attrib.mapboxgl-compact').length, 1);
