5aebc241de6d9b4162b48be41b13e72aee38577d
Ansis Brammanis <ansis@mapbox.com>
2021-10-20 12:30:06 -0400

    add projections pages to release testing

--- test/release/index.js
+++ test/release/index.js
@@ -140,7 +140,27 @@ const pages = [
         "key": "canvas-size",
         "title": "Canvas Size",
         "url": "./canvas-size.html"
-    }
+    },
+    {
+        "key": "projections",
+        "title": "Projections",
+        "url": "./projections.html"
+    },
+    {
+        "key": "featurestate",
+        "title": "Feature state",
+        "url": "./featurestate.html"
+    },
+    {
+        "key": "markers",
+        "title": "Markers",
+        "url": "./markers.html"
+    },
+    {
+        "key": "video",
+        "title": "Video",
+        "url": "./video.html"
+    },
 ];
 
 const versions = {
