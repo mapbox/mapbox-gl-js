1e1c07793ee5c3d850e6eef83fd31688b39bd997
Aidan H <aidan.hendrickson@mapbox.com>
2021-11-04 10:07:14 -0600

    Removing double check for terrain

--- src/source/tile.js
+++ src/source/tile.js
@@ -531,15 +531,9 @@ class Tile {
             if (!sourceLayer || !sourceLayerStates || Object.keys(sourceLayerStates).length === 0) continue;
 
             bucket.update(sourceLayerStates, sourceLayer, availableImages, this.imageAtlas && this.imageAtlas.patternPositions || {});
-            if (painter._terrain && painter._terrain.enabled &&
-                (bucket instanceof LineBucket ||
-                bucket instanceof FillBucket ||
-                bucket instanceof FillExtrusionBucket ||
-                bucket instanceof CircleBucket)
-                // Symbol bucket has no programConfigurations. This has to be spelled out for Flow.
-            ) {
+            if (bucket instanceof LineBucket || bucket instanceof FillBucket || bucket instanceof FillExtrusionBucket || bucket instanceof CircleBucket) {
                 const sourceCache = painter.style._getSourceCache(bucket.layers[0].source);
-                if (sourceCache && painter._terrain && bucket.programConfigurations.needsUpload) {
+                if (painter._terrain && painter._terrain.enabled && sourceCache && bucket.programConfigurations.needsUpload) {
                     painter._terrain._clearRenderCacheForTile(sourceCache.id, this.tileID);
                 }
             }
