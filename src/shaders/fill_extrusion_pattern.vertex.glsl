a1e53712e1fbeac252a2f10053fe25850c099120
Mikko Pulkki <mikko.pulkki@mapbox.com>
2021-11-05 10:41:21 +0200

    Add globe view support to fill extrusion shaders

--- src/shaders/fill_extrusion_pattern.vertex.glsl
+++ src/shaders/fill_extrusion_pattern.vertex.glsl
@@ -13,6 +13,17 @@ uniform lowp float u_lightintensity;
 attribute vec4 a_pos_normal_ed;
 attribute vec2 a_centroid_pos;
 
+#ifdef PROJECTION_GLOBE_VIEW
+attribute vec3 a_pos_3;         // Projected position on the globe
+attribute vec3 a_pos_normal_3;  // Surface normal at the position
+
+uniform mat4 u_inv_rot_matrix;
+uniform vec2 u_merc_center;
+uniform vec3 u_tile_id;
+uniform float u_zoom_transition;
+uniform vec3 u_up_dir;
+#endif
+
 varying vec2 v_pos_a;
 varying vec2 v_pos_b;
 varying vec4 v_lighting;
@@ -75,6 +86,14 @@ void main() {
     vec3 p = vec3(pos_nx.xy, h);
 #else
     vec3 p = vec3(pos_nx.xy, z);
+#endif
+
+#ifdef PROJECTION_GLOBE_VIEW
+    vec3 globeNormal = normalize(mix(a_pos_normal_3 / 16384.0, u_up_dir, u_zoom_transition));
+    vec3 globePos = a_pos_3 + globeNormal * u_tile_up_scale * p.z;
+    vec3 mercPos = mercator_tile_position(u_inv_rot_matrix, p.xy, u_tile_id, u_merc_center) + u_up_dir * u_tile_up_scale * p.z;
+
+    p = mix_globe_mercator(globePos, mercPos, u_zoom_transition);
 #endif
 
     float hidden = float(centroid_pos.x == 0.0 && centroid_pos.y == 1.0);
