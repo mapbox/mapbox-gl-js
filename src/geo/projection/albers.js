131c0ecfbcc8cb2d23f7d9864d910295c4c2e4af
Vladimir Agafonkin <agafonkin@gmail.com>
2021-11-05 08:59:07 +0200

    bring back albers wrap correction

--- src/geo/projection/albers.js
+++ src/geo/projection/albers.js
@@ -1,6 +1,6 @@
 // @flow
 import LngLat from '../lng_lat.js';
-import {clamp, degToRad, radToDeg} from '../../util/util.js';
+import {clamp, wrap, degToRad, radToDeg} from '../../util/util.js';
 import {MAX_MERCATOR_LATITUDE} from '../mercator_coordinate.js';
 import {vec2} from 'gl-matrix';
 
@@ -27,34 +27,36 @@ export default {
 
         this.constants = {n, c, r0, parallels: this.parallels};
     },
+
     project(lng: number, lat: number) {
         this.initializeConstants();
 
-        const lng_ = degToRad(lng - this.center[0]);
-        const lat_ = degToRad(lat);
+        const lambda = degToRad(lng - this.center[0]);
+        const phi = degToRad(lat);
 
         const {n, c, r0} = this.constants;
-        const r = Math.sqrt(c - 2 * n * Math.sin(lat_)) / n;
-        const x = r * Math.sin(lng_ * n);
-        const y = r0 - r * Math.cos(lng_ * n);
-        return {x, y: -y};
+        const r = Math.sqrt(c - 2 * n * Math.sin(phi)) / n;
+        const x = r * Math.sin(lambda * n);
+        const y = r * Math.cos(lambda * n) - r0;
+        return {x, y};
     },
+
     unproject(x: number, y: number) {
         this.initializeConstants();
         const {n, c, r0} = this.constants;
 
-        y = -y;
-
-        const r0y = r0 - y;
+        const r0y = r0 + y;
         let l = Math.atan2(x, Math.abs(r0y)) * Math.sign(r0y);
         if (r0y * n < 0) {
             l -= Math.PI * Math.sign(x) * Math.sign(r0y);
         }
-        const lng = l / n;
-        const lat = Math.asin(clamp((c - (x * x + r0y * r0y) * n * n) / (2 * n), -1, 1));
+        const dt = degToRad(this.center[0]) * n;
+        l = wrap(l, -Math.PI - dt, Math.PI - dt);
 
-        return new LngLat(
-            radToDeg(lng) + this.center[0],
-            clamp(radToDeg(lat), -MAX_MERCATOR_LATITUDE, MAX_MERCATOR_LATITUDE));
+        const lng = radToDeg(l / n) + this.center[0];
+        const phi = Math.asin(clamp((c - (x * x + r0y * r0y) * n * n) / (2 * n), -1, 1));
+        const lat = clamp(radToDeg(phi), -MAX_MERCATOR_LATITUDE, MAX_MERCATOR_LATITUDE);
+
+        return new LngLat(lng, lat);
     }
 };
