28e7d4962c6f1a4f2d4d95b7f66c5a7a05ed3fca
avpeery <anna.peery@mapbox.com>
2021-11-23 16:08:18 -0800

    removed comment

--- test/util/index.js
+++ test/util/index.js
@@ -19,11 +19,8 @@ export function createMap(t, options, callback) {
 
     Object.defineProperty(container, 'getBoundingClientRect',
         {value: () => ({height: 200, width: 200}), configurable: true});
-
     Object.defineProperty(container, 'clientWidth', {value: 200, configurable: true});
     Object.defineProperty(container, 'clientHeight', {value: 200, configurable: true});
-
-    // ADD IN CLIENT WIDTH AND CLIENT HEIGHT FOR UNIT TESTS
 
     if (!options || !options.skipCSSStub) t.stub(Map.prototype, '_detectMissingCSS');
     if (options && options.deleteStyle) delete defaultOptions.style;
