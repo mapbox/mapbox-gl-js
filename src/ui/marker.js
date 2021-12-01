6d27042b03d5aff87ec62bee0f2014eeea6a32b9
Vladimir Agafonkin <agafonkin@gmail.com>
2021-12-01 22:37:12 +0200

    svg cleanup

--- src/ui/marker.js
+++ src/ui/marker.js
@@ -127,21 +127,17 @@ export default class Marker extends Evented {
                 viewBox: `0 0 ${defaultWidth} ${defaultHeight}`
             }, this._element);
 
-            const defs = DOM.createSVG('defs', {}, svg);
-            const gradient = DOM.createSVG('radialGradient', {id: 'shadowGradient'}, defs);
+            const gradient = DOM.createSVG('radialGradient', {id: 'shadowGradient'}, DOM.createSVG('defs', {}, svg));
             DOM.createSVG('stop', {offset: '10%', 'stop-opacity': 0.4}, gradient);
             DOM.createSVG('stop', {offset: '100%', 'stop-opacity': 0.05}, gradient);
-
             DOM.createSVG('ellipse', {cx: 13.5, cy: 34.8, rx: 10.5, ry: 5.25, fill: 'url(#shadowGradient)'}, svg); // shadow
 
             DOM.createSVG('path', { // marker shape
                 fill: this._color,
                 d: 'M27,13.5C27,19.07 20.25,27 14.75,34.5C14.02,35.5 12.98,35.5 12.25,34.5C6.75,27 0,19.22 0,13.5C0,6.04 6.04,0 13.5,0C20.96,0 27,6.04 27,13.5Z'
             }, svg);
-
             DOM.createSVG('path', { // border
                 opacity: 0.25,
-                fill: 'black',
                 d: 'M13.5,0C6.04,0 0,6.04 0,13.5C0,19.22 6.75,27 12.25,34.5C13,35.52 14.02,35.5 14.75,34.5C20.25,27 27,19.07 27,13.5C27,6.04 20.96,0 13.5,0ZM13.5,1C20.42,1 26,6.58 26,13.5C26,15.9 24.5,19.18 22.22,22.74C19.95,26.3 16.71,30.14 13.94,33.91C13.74,34.18 13.61,34.32 13.5,34.44C13.39,34.32 13.26,34.18 13.06,33.91C10.28,30.13 7.41,26.31 5.02,22.77C2.62,19.23 1,15.95 1,13.5C1,6.58 6.58,1 13.5,1Z'
             }, svg);
 
