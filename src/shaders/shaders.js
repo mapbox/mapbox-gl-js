c5bb6fd20cd9e4653d3c9f4ef3cf7df64981d4c4
Karim Naaji <karim.naaji@gmail.com>
2021-12-07 14:59:05 -0800

    Disable terrain depth rendering for globe

--- src/shaders/shaders.js
+++ src/shaders/shaders.js
@@ -65,8 +65,6 @@ import skyboxCaptureFrag from './skybox_capture.fragment.glsl';
 import skyboxCaptureVert from './skybox_capture.vertex.glsl';
 import globeFrag from './globe_raster.fragment.glsl';
 import globeVert from './globe_raster.vertex.glsl';
-import globeDepthFrag from './globe_depth.fragment.glsl';
-import globeDepthVert from './globe_depth.vertex.glsl';
 import atmosphereFrag from './globe_atmosphere.fragment.glsl';
 import atmosphereVert from './globe_atmosphere.vertex.glsl';
 
