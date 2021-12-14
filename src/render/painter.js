47bdd211ee6eac78810dd4de83167392965ac507
Daniel Eke <daniel.eke@mapbox.com>
2021-12-14 18:15:43 +0200

    Separate globe atmosphere rendering from sky

--- src/render/painter.js
+++ src/render/painter.js
@@ -40,6 +40,7 @@ import background from './draw_background.js';
 import debug, {drawDebugPadding, drawDebugQueryGeometry} from './draw_debug.js';
 import custom from './draw_custom.js';
 import sky from './draw_sky.js';
+import drawGlobeAtmosphere from './draw_globe_atmosphere.js';
 import {GlobeSharedBuffers} from '../geo/projection/globe.js';
 import {Terrain} from '../terrain/terrain.js';
 import {Debug} from '../util/debug.js';
@@ -580,7 +581,7 @@ class Painter {
         // They are drawn at max depth, they are drawn after opaque and before
         // translucent to fail depth testing and mix with translucent objects.
         this.renderPass = 'sky';
-        if (this.transform.isHorizonVisible() || this.transform.projection.name === 'globe') {
+        if (this.transform.projection.name !== 'globe' && this.transform.isHorizonVisible()) {
             for (this.currentLayer = 0; this.currentLayer < layerIds.length; this.currentLayer++) {
                 const layer = this.style._layers[layerIds[this.currentLayer]];
                 const sourceCache = style._getLayerSourceCache(layer);
@@ -589,6 +590,9 @@ class Painter {
 
                 this.renderLayer(this, sourceCache, layer, coords);
             }
+        }
+        if (this.transform.projection.name === 'globe') {
+            drawGlobeAtmosphere(this);
         }
 
         // Translucent pass ===============================================
