a5ef6196689619c7341d83818f7187d21798f336
Vladimir Agafonkin <agafonkin@gmail.com>
2022-01-14 11:03:59 +0200

    add Flow types for gl-matrix

---
+++ flow-typed/gl-matrix.js
@@ -0,0 +1,90 @@
+
+type VecType = Array<number> | Float32Array | Float64Array;
+
+declare module "gl-matrix" {
+    declare type Vec2 = VecType;
+    declare type Vec3 = VecType;
+    declare type Vec4 = VecType;
+    declare type Quat = VecType;
+    declare type Mat2 = VecType;
+    declare type Mat3 = VecType;
+    declare type Mat4 = VecType;
+
+    declare var vec2: {
+        exactEquals(Vec2, Vec2): boolean
+    };
+
+    declare var vec3: {
+        create(): Float32Array,
+        fromValues(number, number, number): Float32Array,
+        length(Vec3): number,
+        dot(Vec3, Vec3): number,
+        exactEquals(Vec3, Vec3): boolean,
+
+        normalize<T: Vec3>(T, Vec3): T,
+        add<T: Vec3>(T, Vec3, Vec3): T,
+        sub<T: Vec3>(T, Vec3, Vec3): T,
+        negate<T: Vec3>(T, Vec3): T,
+        scale<T: Vec3>(T, Vec3, number): T,
+        scaleAndAdd<T: Vec3>(T, Vec3, Vec3, number): T,
+        multiply<T: Vec3>(T, Vec3, Vec3): T,
+        min<T: Vec3>(T, Vec3, Vec3): T,
+        max<T: Vec3>(T, Vec3, Vec3): T,
+        transformQuat<T: Vec3>(T, Vec3, Quat): T,
+        transformMat3<T: Vec3>(T, Vec3, Mat3): T,
+        transformMat4<T: Vec3>(T, Vec3, Mat4): T
+    };
+
+    declare var vec4: {
+        scale<T: Vec4>(T, Vec4, number): T,
+        mul<T: Vec4>(T, Vec4, Vec4): T,
+        transformMat4<T: Vec4>(T, Vec4, Mat4): T
+    };
+
+    declare var mat2: {
+        create(): Float32Array,
+        rotate<T: Mat2>(T, Mat2, number): T,
+        invert<T: Mat2>(T, Mat2): T,
+        scale<T: Mat2>(T, Mat2, Vec2): T
+    };
+
+    declare var mat3: {
+        create(): Float32Array,
+
+        fromMat4<T: Mat3>(T, Mat4): T,
+        fromRotation<T: Mat3>(T, number): T,
+        mul<T: Mat3>(T, Mat3, Mat3): T,
+        multiply<T: Mat3>(T, Mat3, Mat3): T,
+        adjoint<T: Mat3>(T, Mat3): T,
+        transpose<T: Mat3>(T, Mat3): T
+    };
+
+    declare var mat4: {
+        create(): Float32Array,
+
+        ortho<T: Mat4>(T, number, number, number, number, number, number): T,
+        perspective<T: Mat4>(T, number, number, number, number): T,
+        identity<T: Mat4>(T): T,
+        scale<T: Mat4>(T, Mat4, Vec3): T,
+        mul<T: Mat4>(T, Mat4, Mat4): T,
+        multiply<T: Mat4>(T, Mat4, Mat4): T,
+        rotateX<T: Mat4>(T, Mat4, number): T,
+        rotateY<T: Mat4>(T, Mat4, number): T,
+        rotateZ<T: Mat4>(T, Mat4, number): T,
+        translate<T: Mat4>(T, Mat4, Vec3): T,
+        invert<T: Mat4>(T, Mat4): T,
+        copy<T: Mat4>(T, Mat4): T
+    };
+
+    declare var quat: {
+        create(): Float32Array,
+        length(Quat): number,
+        exactEquals(Quat, Quat): boolean,
+
+        normalize<T: Quat>(T, Quat): T,
+        identity<T: Quat>(T): T,
+        rotateX<T: Quat>(T, Quat, number): T,
+        rotateY<T: Quat>(T, Quat, number): T,
+        rotateZ<T: Quat>(T, Quat, number): T
+    }
+}
