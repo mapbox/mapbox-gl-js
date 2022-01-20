d13ff87d230801a7e582c3d62cb6acc9386d0426
Karim Naaji <karim.naaji@gmail.com>
2022-01-20 09:12:15 -0800

    Remove unused uniform

--- src/shaders/symbol_text_and_icon.vertex.glsl
+++ src/shaders/symbol_text_and_icon.vertex.glsl
@@ -20,7 +20,6 @@ uniform mat4 u_label_plane_matrix;
 uniform mat4 u_coord_matrix;
 uniform bool u_is_text;
 uniform bool u_pitch_with_map;
-uniform highp float u_pitch;
 uniform bool u_rotate_symbol;
 uniform highp float u_aspect_ratio;
 uniform highp float u_camera_to_center_distance;
