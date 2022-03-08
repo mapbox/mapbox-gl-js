05ecb07ba47a85f84ed2e5003a9831a4ab2b02cd
Fouad Valadbeigi <fouad.valadbeigi@mapbox.com>
2022-03-08 16:31:49 +0100

    revert to original wireframe color

--- src/shaders/globe_raster.fragment.glsl
+++ src/shaders/globe_raster.fragment.glsl
@@ -4,7 +4,7 @@ varying vec2 v_pos0;
 void main() {
     gl_FragColor = texture2D(u_image0, v_pos0);
 #ifdef TERRAIN_WIREFRAME
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 0.8);
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.8);
 #endif
 #ifdef OVERDRAW_INSPECTOR
     gl_FragColor = vec4(1.0);
