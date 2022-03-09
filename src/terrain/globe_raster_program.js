abd37b3e95c82c71002a5cbc3428e7369c78fa1a
Karim Naaji <karim.naaji@gmail.com>
2022-03-09 10:49:39 -0800

    Fix star resolution on high screen pixel ratio display

--- src/terrain/globe_raster_program.js
+++ src/terrain/globe_raster_program.js
@@ -8,6 +8,7 @@ import {
     UniformMatrix4f,
     Uniform1f
 } from '../render/uniform_binding.js';
+import browser from '../util/browser.js';
 
 import type Context from '../gl/context.js';
 import type {UniformValues, UniformLocations} from '../render/uniform_binding.js';
@@ -105,7 +106,7 @@ const atmosphereUniformValues = (
     'u_sky_color': skyColor,
     'u_latlon': latlon,
     'u_star_intensity': starIntensity,
-    'u_star_size': 5.0,
+    'u_star_size': 5.0 * browser.devicePixelRatio,
     'u_star_density': 0.0
 });
 
