6066c1be21024266a21454b8ea34502e76eb2cb7
avpeery <anna.peery@mapbox.com>
2022-03-18 12:14:24 -0700

    fix lint issue

--- test/unit/ui/handler/scroll_zoom.test.js
+++ test/unit/ui/handler/scroll_zoom.test.js
@@ -464,7 +464,7 @@ test('When cooperativeGestures option is set to true, scroll zoom is not prevent
 
     simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -simulate.magicWheelZoomDelta});
     map._renderTaskQueue.run();
-    
+
     t.equal(zoomSpy.callCount, 1);
     t.end();
 });
