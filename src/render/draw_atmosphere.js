61578fe1f4d4a3ae03290afceac310237062ab7e
Karim Naaji <karim.naaji@gmail.com>
2022-03-30 16:08:05 -0700

    Simplify `viewMatrix` calculation for atmosphere render

--- src/render/draw_atmosphere.js
+++ src/render/draw_atmosphere.js
@@ -9,7 +9,8 @@ import {atmosphereUniformValues} from '../terrain/globe_raster_program.js';
 import type Painter from './painter.js';
 import type {DynamicDefinesType} from '../render/program/program_uniforms.js';
 import {degToRad, mapValue} from '../util/util.js';
-import {vec3, mat4} from 'gl-matrix';
+import {vec3} from 'gl-matrix';
+import Fog from '../style/fog.js';
 
 export default drawAtmosphere;
 
@@ -17,64 +18,51 @@ function project(point, m) {
     return vec3.transformMat4(point, point, m);
 }
 
-function drawAtmosphere(painter: Painter) {
-    const fog = painter.style.fog;
-
-    if (!fog) {
-        return;
-    }
-
+function drawAtmosphere(painter: Painter, fog: Fog) {
     const context = painter.context;
     const gl = context.gl;
-    const transform = painter.transform;
+    const tr = painter.transform;
     const depthMode = new DepthMode(gl.LEQUAL, DepthMode.ReadOnly, [0, 1]);
-    const defines = transform.projection.name === 'globe' ? ['PROJECTION_GLOBE_VIEW', 'FOG'] : ['FOG'];
+    const defines = tr.projection.name === 'globe' ? ['PROJECTION_GLOBE_VIEW', 'FOG'] : ['FOG'];
     const program = painter.useProgram('globeAtmosphere', null, ((defines: any): DynamicDefinesType[]));
 
     // Render the gradient atmosphere by casting rays from screen pixels and determining their
     // closest distance to the globe. This is done in view space where camera is located in the origo
     // facing -z direction.
-    const offset = transform.centerOffset;
-    const cameraToClip = transform._camera.getCameraToClipPerspective(transform._fov, transform.width / transform.height, transform._nearZ, transform._farZ);
+    const zUnit = tr.projection.zAxisUnit === "meters" ? tr.pixelsPerMeter : 1.0;
+    const viewMatrix = tr._camera.getWorldToCamera(tr.worldSize, zUnit);
 
-    cameraToClip[8] = -offset.x * 2 / transform.width;
-    cameraToClip[9] = offset.y * 2 / transform.height;
-
-    const clipToCamera = mat4.invert([], cameraToClip);
-    const viewMatrix = mat4.mul([], clipToCamera, transform.projMatrix);
-
-    const center = [transform.globeMatrix[12], transform.globeMatrix[13], transform.globeMatrix[14]];
+    const center = [tr.globeMatrix[12], tr.globeMatrix[13], tr.globeMatrix[14]];
     const globeCenterInViewSpace = project(center, viewMatrix);
-    const globeRadius = transform.worldSize / 2.0 / Math.PI - 1.0;
+    const globeRadius = tr.worldSize / 2.0 / Math.PI - 1.0;
 
-    const transitionT = globeToMercatorTransition(transform.zoom);
+    const transitionT = globeToMercatorTransition(tr.zoom);
 
     const fogColor = fog.properties.get('color').toArray01();
     const highColor = fog.properties.get('high-color').toArray01();
     const spaceColor = fog.properties.get('space-color').toArray01PremultipliedAlpha();
 
+    const starIntensity = mapValue(fog.properties.get('star-intensity'), 0.0, 1.0, 0.0, 0.25);
+    // https://www.desmos.com/calculator/oanvvpr36d
+    const horizonBlend = mapValue(fog.properties.get('horizon-blend'), 0.0, 1.0, 0.0, 0.25);
+
     const temporalOffset = (painter.frameCounter / 1000.0) % 1;
     const latlon = [
-        degToRad(transform._center.lat) / (Math.PI * 0.5),
-        degToRad(transform._center.lng) / Math.PI
+        degToRad(tr._center.lat) / (Math.PI * 0.5),
+        degToRad(tr._center.lng) / Math.PI
     ];
-
-    const starIntensity = mapValue(fog.properties.get('star-intensity'), 0.0, 1.0, 0.0, 0.25);
 
     const globeCenterDistance = vec3.length(globeCenterInViewSpace);
     const distanceToHorizon = Math.sqrt(Math.pow(globeCenterDistance, 2.0) - Math.pow(globeRadius, 2.0));
     const horizonAngle = Math.acos(distanceToHorizon / globeCenterDistance);
 
-    // https://www.desmos.com/calculator/oanvvpr36d
-    const horizonBlend = mapValue(fog.properties.get('horizon-blend'), 0.0, 1.0, 0.0, 0.25);
-
     const uniforms = atmosphereUniformValues(
-        transform.frustumCorners.TL,
-        transform.frustumCorners.TR,
-        transform.frustumCorners.BR,
-        transform.frustumCorners.BL,
-        transform.frustumCorners.horizonL,
-        transform.frustumCorners.horizonR,
+        tr.frustumCorners.TL,
+        tr.frustumCorners.TR,
+        tr.frustumCorners.BR,
+        tr.frustumCorners.BL,
+        tr.frustumCorners.horizonL,
+        tr.frustumCorners.horizonR,
         globeCenterInViewSpace,
         globeRadius,
         transitionT,
