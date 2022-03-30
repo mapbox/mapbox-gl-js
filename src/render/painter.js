8fb0df1f4044ee640a1ec87df00fbdef29f4fbef
Karim Naaji <karim.naaji@gmail.com>
2022-03-30 16:08:05 -0700

    Simplify `viewMatrix` calculation for atmosphere render

--- src/render/painter.js
+++ src/render/painter.js
@@ -622,7 +622,9 @@ class Painter {
             }
         }
 
-        drawAtmosphere(this);
+        if (this.style.fog) {
+            drawAtmosphere(this, this.style.fog);
+        }
 
         // Translucent pass ===============================================
         // Draw all other layers bottom-to-top.
