36c1749d6d3bd330d781e2aff9f380c0789592ea
ozero dien <139508+ozero@users.noreply.github.com>
2022-04-05 03:26:48 +0900

    Allow duplicated coordinates in tile request URLs (#11441)

--- src/source/tile_id.js
+++ src/source/tile_id.js
@@ -31,9 +31,9 @@ export class CanonicalTileID {
 
         return urls[(this.x + this.y) % urls.length]
             .replace('{prefix}', (this.x % 16).toString(16) + (this.y % 16).toString(16))
-            .replace('{z}', String(this.z))
-            .replace('{x}', String(this.x))
-            .replace('{y}', String(scheme === 'tms' ? (Math.pow(2, this.z) - this.y - 1) : this.y))
+            .replace(/{z}/g, String(this.z))
+            .replace(/{x}/g, String(this.x))
+            .replace(/{y}/g, String(scheme === 'tms' ? (Math.pow(2, this.z) - this.y - 1) : this.y))
             .replace('{quadkey}', quadkey)
             .replace('{bbox-epsg-3857}', bbox);
     }
