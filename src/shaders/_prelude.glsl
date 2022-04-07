6279391fc65031285db4d427342a1510a5090a3b
Daniel Eke <daniel.eke@mapbox.com>
2022-04-07 10:37:32 +0300

    Move fog properties to stage-specific preludes (#11673)

--- src/shaders/_prelude.glsl
+++ src/shaders/_prelude.glsl
@@ -13,30 +13,24 @@
 
 #ifdef FOG
 
-uniform mediump vec4 u_fog_color;
-uniform mediump vec2 u_fog_range;
-uniform mediump float u_fog_horizon_blend;
-
-varying vec3 v_fog_pos;
-
-float fog_range(float depth) {
+float fog_range(vec2 range, float depth) {
     // Map [near, far] to [0, 1] without clamping
-    return (depth - u_fog_range[0]) / (u_fog_range[1] - u_fog_range[0]);
+    return (depth - range[0]) / (range[1] - range[0]);
 }
 
 // Assumes z up and camera_dir *normalized* (to avoid computing
 // its length multiple times for different functions).
-float fog_horizon_blending(vec3 camera_dir) {
-    float t = max(0.0, camera_dir.z / u_fog_horizon_blend);
+float fog_horizon_blending(vec4 color, float blend, vec3 camera_dir) {
+    float t = max(0.0, camera_dir.z / blend);
     // Factor of 3 chosen to roughly match smoothstep.
     // See: https://www.desmos.com/calculator/pub31lvshf
-    return u_fog_color.a * exp(-3.0 * t * t);
+    return color.a * exp(-3.0 * t * t);
 }
 
 // Compute a ramp for fog opacity
 //   - t: depth, rescaled to 0 at fogStart and 1 at fogEnd
 // See: https://www.desmos.com/calculator/3taufutxid
-float fog_opacity(float t) {
+float fog_opacity(vec4 color, float t) {
     const float decay = 6.0;
     float falloff = 1.0 - min(1.0, exp(-decay * t));
 
@@ -44,7 +38,7 @@ float fog_opacity(float t) {
     falloff *= falloff * falloff;
 
     // Scale and clip to 1 at the far limit
-    return u_fog_color.a * min(1.0, 1.00747 * falloff);
+    return color.a * min(1.0, 1.00747 * falloff);
 }
 
 #endif
