9ef19b60463f0a700719879a9707b67d98959973
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:37:48 +0300

    Add localization for UI strings in Latvian

---
+++ src/ui/locales/lv.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Pārslēgt attiecinājumu",
+    "AttributionControl.MapFeedback": "Atsauksmes par karti",
+    "FullscreenControl.Enter": "Ieiet pilnekrāna režīmā",
+    "FullscreenControl.Exit": "Iziet pilnekrāna režīmā",
+    "GeolocateControl.FindMyLocation": "Atrodi manu atrašanās vietu",
+    "GeolocateControl.LocationNotAvailable": "Atrašanās vieta nav pieejama",
+    "LogoControl.Title": "Mapbox logotips",
+    "Map.Title": "Karte",
+    "NavigationControl.ResetBearing": "Atiestatīt gultni uz ziemeļiem",
+    "NavigationControl.ZoomIn": "Pietuvināt",
+    "NavigationControl.ZoomOut": "Attālināt",
+    "ScaleControl.Feet": "pēdas",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "es",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Izmantojiet ctrl + ritināšanu, lai tuvinātu karti",
+    "ScrollZoomBlocker.CmdMessage": "Izmantojiet ⌘ + ritināšanu, lai tuvinātu karti",
+    "TouchPanBlocker.Message": "Izmantojiet divus pirkstus, lai pārvietotu karti"
+};
+
+export default locale;
