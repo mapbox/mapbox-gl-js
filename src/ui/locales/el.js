fce4bec313963b2b1b025aa4bef3a20575e44289
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:25:47 +0300

    Add localization for UI strings in Greek

---
+++ src/ui/locales/el.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Εναλλαγή απόδοσης",
+    "AttributionControl.MapFeedback": "Σχόλια χάρτη",
+    "FullscreenControl.Enter": "Μπείτε σε πλήρη οθόνη",
+    "FullscreenControl.Exit": "Έξοδος από πλήρη οθόνη",
+    "GeolocateControl.FindMyLocation": "Βρείτε την τοποθεσία μου",
+    "GeolocateControl.LocationNotAvailable": "Η τοποθεσία δεν είναι διαθέσιμη",
+    "LogoControl.Title": "Λογότυπο Mapbox",
+    "Map.Title": "Χάρτης",
+    "NavigationControl.ResetBearing": "Επαναφορά ρουλεμάν προς βορρά",
+    "NavigationControl.ZoomIn": "Μεγέθυνση",
+    "NavigationControl.ZoomOut": "Σμίκρυνση",
+    "ScaleControl.Feet": "πόδια",
+    "ScaleControl.Meters": "Μ",
+    "ScaleControl.Kilometers": "χλμ",
+    "ScaleControl.Miles": "μου",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Χρησιμοποιήστε τα πλήκτρα ctrl + scroll για να μεγεθύνετε τον χάρτη",
+    "ScrollZoomBlocker.CmdMessage": "Χρησιμοποιήστε ⌘ + κύλιση για μεγέθυνση του χάρτη",
+    "TouchPanBlocker.Message": "Χρησιμοποιήστε δύο δάχτυλα για να μετακινήσετε τον χάρτη"
+}
+
+export default locale;
