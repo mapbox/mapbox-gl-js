38f39397b04422759371aaec9ba0253a011fb78b
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:37:57 +0300

    Add localization for UI strings in Malay

---
+++ src/ui/locales/ms.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Togol atribusi",
+    "AttributionControl.MapFeedback": "Maklum balas peta",
+    "FullscreenControl.Enter": "Masukkan skrin penuh",
+    "FullscreenControl.Exit": "Keluar dari skrin penuh",
+    "GeolocateControl.FindMyLocation": "Cari lokasi saya",
+    "GeolocateControl.LocationNotAvailable": "Lokasi tidak tersedia",
+    "LogoControl.Title": "Logo peti peta",
+    "Map.Title": "Peta",
+    "NavigationControl.ResetBearing": "Tetapkan semula galas ke utara",
+    "NavigationControl.ZoomIn": "Mengezum masuk",
+    "NavigationControl.ZoomOut": "Zum keluar",
+    "ScaleControl.Feet": "kaki",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "saya",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Gunakan ctrl + tatal untuk mengezum peta",
+    "ScrollZoomBlocker.CmdMessage": "Gunakan ⌘ + tatal untuk mengezum peta",
+    "TouchPanBlocker.Message": "Gunakan dua jari untuk mengalihkan peta"
+};
+
+export default locale;
