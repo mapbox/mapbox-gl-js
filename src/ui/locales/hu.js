58e5a8a3c7846124bbe6ecdf09f7d4dfb5510847
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:27:14 +0300

    Add localization for UI strings in Hungarian

---
+++ src/ui/locales/hu.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Kapcsolja be a hozzárendelést",
+    "AttributionControl.MapFeedback": "Térkép-visszajelzés",
+    "FullscreenControl.Enter": "Lépjen be a teljes képernyőre",
+    "FullscreenControl.Exit": "Lépjen ki a teljes képernyőről",
+    "GeolocateControl.FindMyLocation": "Keresse meg a helyemet",
+    "GeolocateControl.LocationNotAvailable": "A helyszín nem elérhető",
+    "LogoControl.Title": "Mapbox logó",
+    "Map.Title": "Térkép",
+    "NavigationControl.ResetBearing": "Állítsa vissza a csapágyat északra",
+    "NavigationControl.ZoomIn": "Nagyítás",
+    "NavigationControl.ZoomOut": "Kicsinyítés",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "nekem",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "A térkép nagyításához használja a ctrl + görgetést",
+    "ScrollZoomBlocker.CmdMessage": "A térkép nagyításához használja a ⌘ + görgetést",
+    "TouchPanBlocker.Message": "Két ujjal mozgassa a térképet"
+}
+
+export default locale;
