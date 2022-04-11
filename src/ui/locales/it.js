284fc634a69e362548539453b8dd0cfe73a054ea
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:27:34 +0300

    Add localization for UI strings in Italian

---
+++ src/ui/locales/it.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Attiva/disattiva attribuzione",
+    "AttributionControl.MapFeedback": "Feedback sulla mappa",
+    "FullscreenControl.Enter": "Entra a schermo intero",
+    "FullscreenControl.Exit": "Esci dalla modalità schermo intero",
+    "GeolocateControl.FindMyLocation": "Trova la mia posizione",
+    "GeolocateControl.LocationNotAvailable": "Posizione non disponibile",
+    "LogoControl.Title": "Logo della scatola delle mappe",
+    "Map.Title": "Carta geografica",
+    "NavigationControl.ResetBearing": "Ripristina la direzione verso nord",
+    "NavigationControl.ZoomIn": "Ingrandire",
+    "NavigationControl.ZoomOut": "Rimpicciolisci",
+    "ScaleControl.Feet": "piedi",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "me",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Usa ctrl + scorri per ingrandire la mappa",
+    "ScrollZoomBlocker.CmdMessage": "Usa ⌘ + scorri per ingrandire la mappa",
+    "TouchPanBlocker.Message": "Usa due dita per spostare la mappa"
+}
+
+export default locale;
