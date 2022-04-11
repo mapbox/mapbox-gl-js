6bf2cf24609f0705a51f65b29946facc9e8c3ae2
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:40:36 +0300

    Add localization for UI strings in Serbian

---
+++ src/ui/locales/sr.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Укључите приписивање",
+    "AttributionControl.MapFeedback": "Повратне информације о мапи",
+    "FullscreenControl.Enter": "Унесите цео екран",
+    "FullscreenControl.Exit": "Изађите преко целог екрана",
+    "GeolocateControl.FindMyLocation": "Пронађите моју локацију",
+    "GeolocateControl.LocationNotAvailable": "Локација није доступна",
+    "LogoControl.Title": "Мапбок лого",
+    "Map.Title": "Мапа",
+    "NavigationControl.ResetBearing": "Ресетујте смер на север",
+    "NavigationControl.ZoomIn": "Увеличати",
+    "NavigationControl.ZoomOut": "Умањите",
+    "ScaleControl.Feet": "фт",
+    "ScaleControl.Meters": "м",
+    "ScaleControl.Kilometers": "км",
+    "ScaleControl.Miles": "ја",
+    "ScaleControl.NauticalMiles": "нм",
+    "ScrollZoomBlocker.CtrlMessage": "Користите цтрл + скрол да бисте зумирали мапу",
+    "ScrollZoomBlocker.CmdMessage": "Користите ⌘ + скролујте да бисте зумирали мапу",
+    "TouchPanBlocker.Message": "Користите два прста да померите мапу"
+};
+
+export default locale;
