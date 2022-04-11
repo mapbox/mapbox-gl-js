43908e3e3f67d72f9aa8213739a8cfba5e98e7d8
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:41:50 +0300

    Add localization for UI strings in Traditional Chinese

---
+++ src/ui/locales/zh-Hant.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "切換歸因",
+    "AttributionControl.MapFeedback": "地圖反饋",
+    "FullscreenControl.Enter": "進入全屏",
+    "FullscreenControl.Exit": "退出全屏",
+    "GeolocateControl.FindMyLocation": "查找我的位置",
+    "GeolocateControl.LocationNotAvailable": "位置不可用",
+    "LogoControl.Title": "Mapbox 徽標",
+    "Map.Title": "地圖",
+    "NavigationControl.ResetBearing": "將方位重置為北",
+    "NavigationControl.ZoomIn": "放大",
+    "NavigationControl.ZoomOut": "縮小",
+    "ScaleControl.Feet": "英尺",
+    "ScaleControl.Meters": "米",
+    "ScaleControl.Kilometers": "公里",
+    "ScaleControl.Miles": "我",
+    "ScaleControl.NauticalMiles": "納米",
+    "ScrollZoomBlocker.CtrlMessage": "使用 ctrl + scroll 縮放地圖",
+    "ScrollZoomBlocker.CmdMessage": "使用 ⌘ + 滾動來縮放地圖",
+    "TouchPanBlocker.Message": "用兩根手指移動地圖"
+};
+
+export default locale;
