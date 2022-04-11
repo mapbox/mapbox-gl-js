99a12e6154f5d60347ba1b6a3f6bd8cde354df8d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:41:19 +0300

    Add localization for UI strings in Turkish

---
+++ src/ui/locales/tr.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "İlişkilendirmeyi aç/kapat",
+    "AttributionControl.MapFeedback": "Harita geri bildirimi",
+    "FullscreenControl.Enter": "Tam ekran yap",
+    "FullscreenControl.Exit": "Tam ekrandan çık",
+    "GeolocateControl.FindMyLocation": "Konumumu bul",
+    "GeolocateControl.LocationNotAvailable": "Konum mevcut değil",
+    "LogoControl.Title": "harita kutusu logosu",
+    "Map.Title": "Harita",
+    "NavigationControl.ResetBearing": "Yönü kuzeye sıfırla",
+    "NavigationControl.ZoomIn": "Yakınlaştır",
+    "NavigationControl.ZoomOut": "Uzaklaştır",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "Bende",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Haritayı yakınlaştırmak için ctrl + kaydırma tuşlarını kullanın",
+    "ScrollZoomBlocker.CmdMessage": "Haritayı yakınlaştırmak için ⌘ + kaydırmayı kullanın",
+    "TouchPanBlocker.Message": "Haritayı hareket ettirmek için iki parmağınızı kullanın"
+};
+
+export default locale;
