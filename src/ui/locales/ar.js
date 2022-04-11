e613244f6e0808098bada69c9124540a401e2903
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:20:19 +0300

    Add localization for UI strings in Arabic

---
+++ src/ui/locales/ar.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "تبديل الإحالة",
+    "AttributionControl.MapFeedback": "ملاحظات الخريطة",
+    "FullscreenControl.Enter": "أدخل ملء الشاشة",
+    "FullscreenControl.Exit": "الخروج من وضع ملئ للشاشة",
+    "GeolocateControl.FindMyLocation": "البحث عن موقعي",
+    "GeolocateControl.LocationNotAvailable": "الموقع غير متوفر",
+    "LogoControl.Title": "شعار Mapbox",
+    "Map.Title": "خريطة",
+    "NavigationControl.ResetBearing": "إعادة تعيين اتجاه الشمال",
+    "NavigationControl.ZoomIn": "تكبير",
+    "NavigationControl.ZoomOut": "تصغير",
+    "ScaleControl.Feet": "قدم",
+    "ScaleControl.Meters": "م",
+    "ScaleControl.Kilometers": "كم",
+    "ScaleControl.Miles": "أنا",
+    "ScaleControl.NauticalMiles": "نانومتر",
+    "ScrollZoomBlocker.CtrlMessage": "استخدم ctrl + التمرير لتكبير الخريطة",
+    "ScrollZoomBlocker.CmdMessage": "استخدم ⌘ + مرر لتكبير الخريطة",
+    "TouchPanBlocker.Message": "استخدم إصبعين لتحريك الخريطة"
+}
+
+export default locale;
