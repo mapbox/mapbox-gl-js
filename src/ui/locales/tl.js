b9d072dccf35e192cc3a34f8cebae379ca57324b
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:41:09 +0300

    Add localization for UI strings in Tagalog

---
+++ src/ui/locales/tl.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "I-toggle ang attribution",
+    "AttributionControl.MapFeedback": "Feedback sa mapa",
+    "FullscreenControl.Enter": "Ipasok ang fullscreen",
+    "FullscreenControl.Exit": "Lumabas sa fullscreen",
+    "GeolocateControl.FindMyLocation": "Hanapin ang aking lokasyon",
+    "GeolocateControl.LocationNotAvailable": "Hindi available ang lokasyon",
+    "LogoControl.Title": "Logo ng mapbox",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "I-reset ang bearing sa hilaga",
+    "NavigationControl.ZoomIn": "Palakihin",
+    "NavigationControl.ZoomOut": "Mag-zoom out",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "ako",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Gamitin ang ctrl + scroll para i-zoom ang mapa",
+    "ScrollZoomBlocker.CmdMessage": "Gamitin ang ⌘ + scroll upang i-zoom ang mapa",
+    "TouchPanBlocker.Message": "Gumamit ng dalawang daliri upang ilipat ang mapa"
+};
+
+export default locale;
