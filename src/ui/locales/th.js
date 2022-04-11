73a8033cc6893be958fd00d765547145d4fb9f1d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:40:59 +0300

    Add localization for UI strings in Thai

---
+++ src/ui/locales/th.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "สลับการระบุแหล่งที่มา",
+    "AttributionControl.MapFeedback": "ความคิดเห็นเกี่ยวกับแผนที่",
+    "FullscreenControl.Enter": "เข้าสู่โหมดเต็มหน้าจอ",
+    "FullscreenControl.Exit": "ออกจากโหมดเต็มหน้าจอ",
+    "GeolocateControl.FindMyLocation": "ค้นหาตำแหน่งของฉัน",
+    "GeolocateControl.LocationNotAvailable": "ไม่มีที่ตั้ง",
+    "LogoControl.Title": "โลโก้ Mapbox",
+    "Map.Title": "แผนที่",
+    "NavigationControl.ResetBearing": "รีเซ็ตแบริ่งไปทางทิศเหนือ",
+    "NavigationControl.ZoomIn": "ขยายเข้า",
+    "NavigationControl.ZoomOut": "ซูมออก",
+    "ScaleControl.Feet": "ฟุต",
+    "ScaleControl.Meters": "ม",
+    "ScaleControl.Kilometers": "กม.",
+    "ScaleControl.Miles": "ฉัน",
+    "ScaleControl.NauticalMiles": "นาโนเมตร",
+    "ScrollZoomBlocker.CtrlMessage": "ใช้ ctrl + เลื่อนเพื่อซูมแผนที่",
+    "ScrollZoomBlocker.CmdMessage": "ใช้ ⌘ + เลื่อนเพื่อซูมแผนที่",
+    "TouchPanBlocker.Message": "ใช้สองนิ้วเพื่อเลื่อนแผนที่"
+};
+
+export default locale;
