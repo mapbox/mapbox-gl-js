3b54b464f2ce9709b5a2806aa2168574fa119169
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:27:02 +0300

    Add localization for UI strings in Hebrew

---
+++ src/ui/locales/he.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "החלף את הייחוס",
+    "AttributionControl.MapFeedback": "משוב על המפה",
+    "FullscreenControl.Enter": "היכנס למסך מלא",
+    "FullscreenControl.Exit": "צא ממסך מלא",
+    "GeolocateControl.FindMyLocation": "מצא את המיקום שלי",
+    "GeolocateControl.LocationNotAvailable": "המיקום אינו זמין",
+    "LogoControl.Title": "לוגו Mapbox",
+    "Map.Title": "מַפָּה",
+    "NavigationControl.ResetBearing": "אפס את המיסב לצפון",
+    "NavigationControl.ZoomIn": "לְהִתְמַקֵד",
+    "NavigationControl.ZoomOut": "להקטין את התצוגה",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "M",
+    "ScaleControl.Kilometers": "ק\"מ",
+    "ScaleControl.Miles": "לִי",
+    "ScaleControl.NauticalMiles": "נ\"מ",
+    "ScrollZoomBlocker.CtrlMessage": "השתמש ב-ctrl + גלילה כדי להגדיל את המפה",
+    "ScrollZoomBlocker.CmdMessage": "השתמש ב-⌘ + גלילה כדי להגדיל את המפה",
+    "TouchPanBlocker.Message": "השתמש בשתי אצבעות כדי להזיז את המפה"
+}
+
+export default locale;
