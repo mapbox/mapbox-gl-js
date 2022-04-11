280e4db4c5f7d05ddbd7bc44b0f0f2d4758d4952
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:27:23 +0300

    Add localization for UI strings in Indonesian

---
+++ src/ui/locales/id.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Alihkan atribusi",
+    "AttributionControl.MapFeedback": "Umpan balik peta",
+    "FullscreenControl.Enter": "Memasuki layar penuh",
+    "FullscreenControl.Exit": "Keluar dari layar penuh",
+    "GeolocateControl.FindMyLocation": "Temukan lokasi saya",
+    "GeolocateControl.LocationNotAvailable": "Lokasi tidak tersedia",
+    "LogoControl.Title": "logo kotak peta",
+    "Map.Title": "Peta",
+    "NavigationControl.ResetBearing": "Setel ulang bantalan ke utara",
+    "NavigationControl.ZoomIn": "Perbesar",
+    "NavigationControl.ZoomOut": "Perkecil",
+    "ScaleControl.Feet": "kaki",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "Saya",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Gunakan ctrl + gulir untuk memperbesar peta",
+    "ScrollZoomBlocker.CmdMessage": "Gunakan + gulir untuk memperbesar peta",
+    "TouchPanBlocker.Message": "Gunakan dua jari untuk memindahkan peta"
+}
+
+export default locale;
