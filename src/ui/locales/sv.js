e84cacd6bb651b9d93b31c4bf8a2f01fd21cd1dc
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:40:50 +0300

    Add localization for UI strings in Swedish

---
+++ src/ui/locales/sv.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Växla tillskrivning",
+    "AttributionControl.MapFeedback": "Kartfeedback",
+    "FullscreenControl.Enter": "Öppna helskärm",
+    "FullscreenControl.Exit": "Avsluta helskärm",
+    "GeolocateControl.FindMyLocation": "Hitta min plats",
+    "GeolocateControl.LocationNotAvailable": "Platsen är inte tillgänglig",
+    "LogoControl.Title": "Mapbox-logotyp",
+    "Map.Title": "Karta",
+    "NavigationControl.ResetBearing": "Återställ bäringen till norr",
+    "NavigationControl.ZoomIn": "Zooma in",
+    "NavigationControl.ZoomOut": "Zooma ut",
+    "ScaleControl.Feet": "med",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mig",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Använd ctrl + scroll för att zooma kartan",
+    "ScrollZoomBlocker.CmdMessage": "Använd ⌘ + rulla för att zooma kartan",
+    "TouchPanBlocker.Message": "Använd två fingrar för att flytta kartan"
+};
+
+export default locale;
