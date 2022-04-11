b78049c7acbc0ccc523275ce2553b7fa245e4985
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:37:14 +0300

    Add localization for UI strings in Georgian

---
+++ src/ui/locales/ka.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "ატრიბუტის გადართვა",
+    "AttributionControl.MapFeedback": "რუკის გამოხმაურება",
+    "FullscreenControl.Enter": "შედით სრულ ეკრანზე",
+    "FullscreenControl.Exit": "სრული ეკრანიდან გამოსვლა",
+    "GeolocateControl.FindMyLocation": "იპოვე ჩემი მდებარეობა",
+    "GeolocateControl.LocationNotAvailable": "მდებარეობა მიუწვდომელია",
+    "LogoControl.Title": "Mapbox ლოგო",
+    "Map.Title": "რუკა",
+    "NavigationControl.ResetBearing": "ტარების გადატვირთვა ჩრდილოეთით",
+    "NavigationControl.ZoomIn": "Მიახლოება",
+    "NavigationControl.ZoomOut": "მასშტაბირება",
+    "ScaleControl.Feet": "ფუტი",
+    "ScaleControl.Meters": "მ",
+    "ScaleControl.Kilometers": "კმ",
+    "ScaleControl.Miles": "მე",
+    "ScaleControl.NauticalMiles": "ნმ",
+    "ScrollZoomBlocker.CtrlMessage": "გამოიყენეთ ctrl + გადახვევა რუკის გასადიდებლად",
+    "ScrollZoomBlocker.CmdMessage": "გამოიყენეთ ⌘ + გადახვევა რუკის გასადიდებლად",
+    "TouchPanBlocker.Message": "გამოიყენეთ ორი თითი რუკის გადასატანად"
+};
+
+export default locale;
