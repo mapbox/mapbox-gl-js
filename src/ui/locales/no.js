32457e389ac2807516fc1e1c15814882bbe4eccc
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:38:29 +0300

    Add localization for UI strings in Norwegian

---
+++ src/ui/locales/no.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Slå av/på attribusjon",
+    "AttributionControl.MapFeedback": "Kart tilbakemelding",
+    "FullscreenControl.Enter": "Gå inn i fullskjerm",
+    "FullscreenControl.Exit": "Avslutt fullskjerm",
+    "GeolocateControl.FindMyLocation": "Finn min plassering",
+    "GeolocateControl.LocationNotAvailable": "Plassering er ikke tilgjengelig",
+    "LogoControl.Title": "Mapbox-logo",
+    "Map.Title": "Kart",
+    "NavigationControl.ResetBearing": "Tilbakestill peiling mot nord",
+    "NavigationControl.ZoomIn": "Zoom inn",
+    "NavigationControl.ZoomOut": "Zoome ut",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "meg",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Bruk ctrl + rull for å zoome kartet",
+    "ScrollZoomBlocker.CmdMessage": "Bruk ⌘ + rull for å zoome kartet",
+    "TouchPanBlocker.Message": "Bruk to fingre for å flytte kartet"
+};
+
+export default locale;
