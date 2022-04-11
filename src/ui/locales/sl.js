5379556ed58c0ce576cdd341310a49602745fb21
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:40:25 +0300

    Add localization for UI strings in Slovenian

---
+++ src/ui/locales/sl.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Preklop dodeljevanja",
+    "AttributionControl.MapFeedback": "Povratne informacije o zemljevidu",
+    "FullscreenControl.Enter": "Vstopite v celozaslonski način",
+    "FullscreenControl.Exit": "Zapustite celozaslonski način",
+    "GeolocateControl.FindMyLocation": "Najdi mojo lokacijo",
+    "GeolocateControl.LocationNotAvailable": "Lokacija ni na voljo",
+    "LogoControl.Title": "Logotip Mapbox",
+    "Map.Title": "Zemljevid",
+    "NavigationControl.ResetBearing": "Ponastavite smer proti severu",
+    "NavigationControl.ZoomIn": "Približaj",
+    "NavigationControl.ZoomOut": "Pomanjšanje",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "jaz",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Uporabite ctrl + drsenje za povečavo zemljevida",
+    "ScrollZoomBlocker.CmdMessage": "Uporabite ⌘ + pomikanje, da povečate zemljevid",
+    "TouchPanBlocker.Message": "Uporabite dva prsta za premikanje zemljevida"
+};
+
+export default locale;
