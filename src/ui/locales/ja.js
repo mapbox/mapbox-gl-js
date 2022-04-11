c18312290342ee3aa54c487b01400bcb1f496d3c
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:36:27 +0300

    Add localization for UI strings in Japanese

---
+++ src/ui/locales/ja.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "アトリビューションを切り替える",
+    "AttributionControl.MapFeedback": "マップフィードバック",
+    "FullscreenControl.Enter": "フルスクリーンに入る",
+    "FullscreenControl.Exit": "全画面表示を終了",
+    "GeolocateControl.FindMyLocation": "私の場所を探す",
+    "GeolocateControl.LocationNotAvailable": "場所は利用できません",
+    "LogoControl.Title": "Mapboxのロゴ",
+    "Map.Title": "地図",
+    "NavigationControl.ResetBearing": "ベアリングを北にリセット",
+    "NavigationControl.ZoomIn": "ズームイン",
+    "NavigationControl.ZoomOut": "ズームアウトする",
+    "ScaleControl.Feet": "フィート",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "自分",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Ctrlキーを押しながらスクロールして地図をズームします",
+    "ScrollZoomBlocker.CmdMessage": "⌘+スクロールを使用して地図をズームします",
+    "TouchPanBlocker.Message": "2本の指で地図を移動します"
+};
+
+export default locale;
