10b6c4721e04fbc55c4da3426a5234ccc766b60d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:22:36 +0300

    Add localization for UI strings in Czech

---
+++ src/ui/locales/cs.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Přepnout atribuci",
+    "AttributionControl.MapFeedback": "Zpětná vazba na mapu",
+    "FullscreenControl.Enter": "Vstupte na celou obrazovku",
+    "FullscreenControl.Exit": "Ukončete celou obrazovku",
+    "GeolocateControl.FindMyLocation": "Najít moji polohu",
+    "GeolocateControl.LocationNotAvailable": "Poloha není k dispozici",
+    "LogoControl.Title": "Logo Mapbox",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "Resetujte azimut na sever",
+    "NavigationControl.ZoomIn": "Přiblížit",
+    "NavigationControl.ZoomOut": "Oddálit",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mě",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Pro přiblížení mapy použijte ctrl + scroll",
+    "ScrollZoomBlocker.CmdMessage": "Pomocí ⌘ + posouvání mapu přiblížíte",
+    "TouchPanBlocker.Message": "Pomocí dvou prstů posuňte mapu"
+}
+
+export default locale;
