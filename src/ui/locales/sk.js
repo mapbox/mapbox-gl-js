39b4bee66a89aaa813166db3f4f6d72b1b283276
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-11 16:40:13 +0300

    Add localization for UI strings in Slovak

---
+++ src/ui/locales/sk.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Prepnúť pripisovanie",
+    "AttributionControl.MapFeedback": "Spätná väzba na mapu",
+    "FullscreenControl.Enter": "Prejsť na celú obrazovku",
+    "FullscreenControl.Exit": "Ukončiť celú obrazovku",
+    "GeolocateControl.FindMyLocation": "Nájsť moju polohu",
+    "GeolocateControl.LocationNotAvailable": "Poloha nie je k dispozícii",
+    "LogoControl.Title": "Logo Mapboxu",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "Obnovte azimut na sever",
+    "NavigationControl.ZoomIn": "Priblížiť",
+    "NavigationControl.ZoomOut": "Oddialiť",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "ja",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Na priblíženie mapy použite ctrl + rolovanie",
+    "ScrollZoomBlocker.CmdMessage": "Na priblíženie mapy použite ⌘ + posúvanie",
+    "TouchPanBlocker.Message": "Na posúvanie mapy použite dva prsty"
+};
+
+export default locale;
