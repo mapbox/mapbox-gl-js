ed8b916af36943d5b0a85f715eacb5c1384b7c47
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:55:02 +0300

    Add localization for UI strings in Polish (#11712)
    h/t @LukasPaczos

---
+++ src/ui/locales/pl.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Przełącz atrybucję",
+    "AttributionControl.MapFeedback": "Opinia o mapie",
+    "FullscreenControl.Enter": "Włącz tryb pełnoekranowy",
+    "FullscreenControl.Exit": "Wyłącz tryb pełnoekranowy",
+    "GeolocateControl.FindMyLocation": "Znajdź moją lokalizację",
+    "GeolocateControl.LocationNotAvailable": "Lokalizacja niedostępna",
+    "LogoControl.Title": "Logo Mapbox",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "Zresetuj rotację mapy na północ",
+    "NavigationControl.ZoomIn": "Przybliż",
+    "NavigationControl.ZoomOut": "Oddal",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Użyj ctrl + scroll, aby przybliżyć mapę",
+    "ScrollZoomBlocker.CmdMessage": "Użyj ⌘ + scroll, aby przybliżyć mapę",
+    "TouchPanBlocker.Message": "Użyj dwóch palców, aby przesunąć mapę"
+};
+
+export default locale;
