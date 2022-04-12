8ff22f11d58a021f1cbae5689b59cf84f4bece3f
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 14:46:40 +0300

    Add localization for UI strings in Vietnamese (#11722)
    h/t @1ec5

---
+++ src/ui/locales/vi.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Hiện/ẩn lời ghi công",
+    "AttributionControl.MapFeedback": "Phản hồi về bản đồ",
+    "FullscreenControl.Enter": "Vào chế độ toàn màn hình",
+    "FullscreenControl.Exit": "Thoát chế độ toàn màn hình",
+    "GeolocateControl.FindMyLocation": "Tìm vị trí của tôi",
+    "GeolocateControl.LocationNotAvailable": "Vị trí không có sẵn",
+    "LogoControl.Title": "Biểu trưng Mapbox",
+    "Map.Title": "Bản đồ",
+    "NavigationControl.ResetBearing": "Quay về hướng bắc",
+    "NavigationControl.ZoomIn": "Phóng to",
+    "NavigationControl.ZoomOut": "Thu nhỏ",
+    "ScaleControl.Feet": "bộ",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "dặm",
+    "ScaleControl.NauticalMiles": "hải lý",
+    "ScrollZoomBlocker.CtrlMessage": "Sử dụng ctrl + cuộn để thu phóng bản đồ",
+    "ScrollZoomBlocker.CmdMessage": "Sử dụng ⌘ + cuộn để thu phóng bản đồ",
+    "TouchPanBlocker.Message": "Vuốt bằng hai ngón tay để cuộn bản đồ"
+};
+
+export default locale;
