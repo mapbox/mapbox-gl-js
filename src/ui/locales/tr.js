25f5c07218d565bdd7c1187d283d9dc7b02eaf27
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:16:03 +0300

    Apply suggestions from code review
    Co-authored-by: Bulut Catikoglu <10766699+Catalonium@users.noreply.github.com>

--- src/ui/locales/tr.js
+++ src/ui/locales/tr.js
@@ -6,8 +6,8 @@ const locale = {
     "FullscreenControl.Enter": "Tam ekran yap",
     "FullscreenControl.Exit": "Tam ekrandan çık",
     "GeolocateControl.FindMyLocation": "Konumumu bul",
-    "GeolocateControl.LocationNotAvailable": "Konum mevcut değil",
-    "LogoControl.Title": "harita kutusu logosu",
+    "GeolocateControl.LocationNotAvailable": "Konum bulunamadı",
+    "LogoControl.Title": "Mapbox logosu",
     "Map.Title": "Harita",
     "NavigationControl.ResetBearing": "Yönü kuzeye sıfırla",
     "NavigationControl.ZoomIn": "Yakınlaştır",
@@ -15,11 +15,11 @@ const locale = {
     "ScaleControl.Feet": "ft",
     "ScaleControl.Meters": "m",
     "ScaleControl.Kilometers": "km",
-    "ScaleControl.Miles": "Bende",
+    "ScaleControl.Miles": "mi",
     "ScaleControl.NauticalMiles": "nm",
-    "ScrollZoomBlocker.CtrlMessage": "Haritayı yakınlaştırmak için ctrl + kaydırma tuşlarını kullanın",
-    "ScrollZoomBlocker.CmdMessage": "Haritayı yakınlaştırmak için ⌘ + kaydırmayı kullanın",
-    "TouchPanBlocker.Message": "Haritayı hareket ettirmek için iki parmağınızı kullanın"
+    "ScrollZoomBlocker.CtrlMessage": "Ctrl + mouse orta tuşlarını kullanarak haritayı yakınlaştırıp uzaklaştırabilirsiniz",
+    "ScrollZoomBlocker.CmdMessage": "⌘ + mouse orta tuşlarını kullanarak haritayı yakınlaştırıp uzaklaştırabilirsiniz",
+    "TouchPanBlocker.Message": "İki parmağınızı kullanarak haritayı hareket ettirebilirsiniz"
 };
 
 export default locale;
