5fa3e7979b8f943976704dce4e2e93343bdbb811
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 21:10:12 +0300

    Add localization for UI strings in German (#11694)
    h/t @melanieimfeld @sebrockm @kkaefer

---
+++ src/ui/locales/de.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Attributierung umschalten",
+    "AttributionControl.MapFeedback": "Karten-Feedback",
+    "FullscreenControl.Enter": "Vollbildmodus aktivieren",
+    "FullscreenControl.Exit": "Vollbildmodus beenden",
+    "GeolocateControl.FindMyLocation": "Finde meinen Standort",
+    "GeolocateControl.LocationNotAvailable": "Position nicht verfügbar",
+    "LogoControl.Title": "Mapbox-Logo",
+    "Map.Title": "Karte",
+    "NavigationControl.ResetBearing": "Setze die Orientierung auf Norden zurück",
+    "NavigationControl.ZoomIn": "Hineinzoomen",
+    "NavigationControl.ZoomOut": "Herauszoomen",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Verwenden Sie Strg + Scrollen, um die Karte zu zoomen",
+    "ScrollZoomBlocker.CmdMessage": "Verwenden Sie ⌘ + Scrollen, um die Karte zu zoomen",
+    "TouchPanBlocker.Message": "Verwenden Sie zwei Finger, um die Karte zu verschieben"
+};
+
+export default locale;
