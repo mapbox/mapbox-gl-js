ef15d220a11dbbd3b9efc0562d2186370c6ae7bc
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 19:13:57 +0300

    Add localization for UI strings in French (#11699)
    h/t @karimnaaji

---
+++ src/ui/locales/fr.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Afficher l'attribution",
+    "AttributionControl.MapFeedback": "Améliorer cette carte",
+    "FullscreenControl.Enter": "Passer en mode plein écran",
+    "FullscreenControl.Exit": "Quitter le mode plein écran",
+    "GeolocateControl.FindMyLocation": "Afficher ma position",
+    "GeolocateControl.LocationNotAvailable": "Localisation non disponible",
+    "LogoControl.Title": "Mapbox logo",
+    "Map.Title": "Carte",
+    "NavigationControl.ResetBearing": "Réinitialiser l'orientation vers le nord",
+    "NavigationControl.ZoomIn": "Agrandir",
+    "NavigationControl.ZoomOut": "Dézoomer",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Utilisez ctrl + scroll pour zoomer sur la carte",
+    "ScrollZoomBlocker.CmdMessage": "Utilisez ⌘ + scroll pour agrandir la carte",
+    "TouchPanBlocker.Message": "Utilisez deux doigts pour déplacer la carte"
+};
+
+export default locale;
