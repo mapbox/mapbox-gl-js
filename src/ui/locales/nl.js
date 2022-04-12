dd3283a2b205567dd80c109d3d227c6c26ac8a9d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:57:56 +0300

    Add localization for UI strings in Dutch (#11710)
    h/t @tobrun

---
+++ src/ui/locales/nl.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Attributie in-/uitschakelen",
+    "AttributionControl.MapFeedback": "Kaart feedback",
+    "FullscreenControl.Enter": "Ga naar volledig scherm",
+    "FullscreenControl.Exit": "Verlaat volledig scherm",
+    "GeolocateControl.FindMyLocation": "Vind mijn locatie",
+    "GeolocateControl.LocationNotAvailable": "Locatie niet beschikbaar",
+    "LogoControl.Title": "Mapbox-logo",
+    "Map.Title": "Kaart",
+    "NavigationControl.ResetBearing": "Compass naar het noorden resetten",
+    "NavigationControl.ZoomIn": "Inzoomen",
+    "NavigationControl.ZoomOut": "Uitzoomen",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mijl",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Gebruik ctrl + scroll om in te zoomen op de kaart",
+    "ScrollZoomBlocker.CmdMessage": "Gebruik ⌘ + scroll om in te zoomen op de kaart",
+    "TouchPanBlocker.Message": "Gebruik twee vingers om de kaart te verplaatsen"
+};
+
+export default locale;
