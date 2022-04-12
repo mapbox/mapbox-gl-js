627a47db5e983d84bf96073ce3d8b2f64dd8b258
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:21:33 +0300

    Apply suggestions from code review
    Co-authored-by: Jordi Torres <jordi.torresfabra@mapbox.com>

--- src/ui/locales/es.js
+++ src/ui/locales/es.js
@@ -1,23 +1,23 @@
 // @flow
 
 const locale = {
-    "AttributionControl.ToggleAttribution": "Alternar atribución",
+    "AttributionControl.ToggleAttribution": "Mostrar atribución",
     "AttributionControl.MapFeedback": "Comentarios sobre el mapa",
-    "FullscreenControl.Enter": "Ingrese a pantalla completa",
+    "FullscreenControl.Enter": "Entrar en pantalla completa",
     "FullscreenControl.Exit": "Salir de pantalla completa",
     "GeolocateControl.FindMyLocation": "Encuentra mi ubicación",
     "GeolocateControl.LocationNotAvailable": "Ubicación no disponible",
-    "LogoControl.Title": "logotipo de la caja de mapas",
+    "LogoControl.Title": "Logotipo de Mapbox",
     "Map.Title": "Mapa",
-    "NavigationControl.ResetBearing": "Restablecer rumbo al norte",
-    "NavigationControl.ZoomIn": "Acercarse",
+    "NavigationControl.ResetBearing": "Restablecer orientación al norte",
+    "NavigationControl.ZoomIn": "Ampliar",
     "NavigationControl.ZoomOut": "Disminuir el zoom",
-    "ScaleControl.Feet": "pie",
-    "ScaleControl.Meters": "metro",
-    "ScaleControl.Kilometers": "kilómetros",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
     "ScaleControl.Miles": "mi",
-    "ScaleControl.NauticalMiles": "Nuevo Méjico",
-    "ScrollZoomBlocker.CtrlMessage": "Usa ctrl + scroll para hacer zoom en el mapa",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Usa ctrl + desplazamiento para hacer zoom en el mapa",
     "ScrollZoomBlocker.CmdMessage": "Use ⌘ + desplazamiento para hacer zoom en el mapa",
     "TouchPanBlocker.Message": "Usa dos dedos para mover el mapa"
 };
