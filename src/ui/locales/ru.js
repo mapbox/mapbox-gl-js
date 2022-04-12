e206b6721df3bffe95eb6a8e1ff3a2ed28d34af5
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 13:39:46 +0300

    Add localization for UI strings in Russian (#11688)
    h/t @alexander-kulikovskii

---
+++ src/ui/locales/ru.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Переключить отображение авторских прав",
+    "AttributionControl.MapFeedback": "Оставить отзыв о карте",
+    "FullscreenControl.Enter": "Войти в полноэкранный режим",
+    "FullscreenControl.Exit": "Выйти из полноэкранного режима",
+    "GeolocateControl.FindMyLocation": "Найти моё местоположение",
+    "GeolocateControl.LocationNotAvailable": "Местоположение недоступно",
+    "LogoControl.Title": "Логотип Mapbox",
+    "Map.Title": "Карта",
+    "NavigationControl.ResetBearing": "Сбросить азимут на север",
+    "NavigationControl.ZoomIn": "Приблизить",
+    "NavigationControl.ZoomOut": "Отдалить",
+    "ScaleControl.Feet": "футов",
+    "ScaleControl.Meters": "м",
+    "ScaleControl.Kilometers": "км",
+    "ScaleControl.Miles": "миль",
+    "ScaleControl.NauticalMiles": "морских миль",
+    "ScrollZoomBlocker.CtrlMessage": "Используйте ctrl + прокрутку, чтобы увеличить карту",
+    "ScrollZoomBlocker.CmdMessage": "Используйте ⌘ + прокрутку, чтобы увеличить карту",
+    "TouchPanBlocker.Message": "Используйте два пальца для перемещения карты"
+};
+
+export default locale;
