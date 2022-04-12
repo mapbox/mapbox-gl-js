810b4d4d880cd1ec892216a9c57475c6066f7490
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 14:48:55 +0300

    Apply suggestions from code review
    Co-authored-by: Katia Maeda <katia.maeda@mapbox.com>

--- src/ui/locales/pt.js
+++ src/ui/locales/pt.js
@@ -10,15 +10,15 @@ const locale = {
     "LogoControl.Title": "Logo do Mapbox",
     "Map.Title": "Mapa",
     "NavigationControl.ResetBearing": "Redefinir rumo para o norte",
-    "NavigationControl.ZoomIn": "Mais Zoom",
-    "NavigationControl.ZoomOut": "Reduzir o zoom",
+    "NavigationControl.ZoomIn": "Ampliar tela",
+    "NavigationControl.ZoomOut": "Diminuir tela",
     "ScaleControl.Feet": "pés",
     "ScaleControl.Meters": "m",
     "ScaleControl.Kilometers": "km",
-    "ScaleControl.Miles": "mim",
-    "ScaleControl.NauticalMiles": "nm",
-    "ScrollZoomBlocker.CtrlMessage": "Use ctrl + scroll para ampliar o mapa",
-    "ScrollZoomBlocker.CmdMessage": "Use ⌘ + rolar para ampliar o mapa",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "mn",
+    "ScrollZoomBlocker.CtrlMessage": "Aperte ctrl + rolar para ampliar o mapa",
+    "ScrollZoomBlocker.CmdMessage": "Aperte ⌘ + rolar para ampliar o mapa",
     "TouchPanBlocker.Message": "Use dois dedos para mover o mapa"
 };
 
