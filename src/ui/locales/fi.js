31f2e3b6b111228ef58f80c1ec779b71bbef04b4
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:23:45 +0300

    Apply suggestions from code review
    Co-authored-by: Aleksi Grön <aleksi.gron@mapbox.com>
    Co-authored-by: Oskari Ruutiainen <oskari.ruutiainen@mapbox.com>

--- src/ui/locales/fi.js
+++ src/ui/locales/fi.js
@@ -1,7 +1,7 @@
 // @flow
 
 const locale = {
-    "AttributionControl.ToggleAttribution": "Ota vaikuttavuus käyttöön",
+    "AttributionControl.ToggleAttribution": "Käytä nimeämistä",
     "AttributionControl.MapFeedback": "Karttapalaute",
     "FullscreenControl.Enter": "Siirry koko näytön tilaan",
     "FullscreenControl.Exit": "Poistu koko näytöstä",
@@ -9,16 +9,16 @@ const locale = {
     "GeolocateControl.LocationNotAvailable": "Sijainti ei saatavilla",
     "LogoControl.Title": "Mapboxin logo",
     "Map.Title": "Kartta",
-    "NavigationControl.ResetBearing": "Palauta laakeri pohjoiseen",
+    "NavigationControl.ResetBearing": "Palauta suuntima pohjoiseen",
     "NavigationControl.ZoomIn": "Lähennä",
-    "NavigationControl.ZoomOut": "Loitontaa",
+    "NavigationControl.ZoomOut": "Loitonna",
     "ScaleControl.Feet": "ft",
     "ScaleControl.Meters": "m",
     "ScaleControl.Kilometers": "km",
-    "ScaleControl.Miles": "minä",
+    "ScaleControl.Miles": "mi",
     "ScaleControl.NauticalMiles": "nm",
-    "ScrollZoomBlocker.CtrlMessage": "Käytä ctrl + scrollia zoomataksesi karttaa",
-    "ScrollZoomBlocker.CmdMessage": "Zoomaa karttaa ⌘ + selaamalla",
+    "ScrollZoomBlocker.CtrlMessage": "Käytä ctrl + rullaa zoomataksesi karttaa",
+    "ScrollZoomBlocker.CmdMessage": "Käytä ⌘ + rullaa zoomataksesi karttaa",
     "TouchPanBlocker.Message": "Siirrä karttaa kahdella sormella"
 };
 
