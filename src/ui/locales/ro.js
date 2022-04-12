b3e7a7b17356887e6445c1caf81ebedf7984e646
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 15:59:03 +0300

    Add localization for UI strings in Romanian (#11714)
    h/t @vesh93

---
+++ src/ui/locales/ro.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Comutați atribuirea",
+    "AttributionControl.MapFeedback": "Feedback pe hartă",
+    "FullscreenControl.Enter": "Intrați pe ecran complet",
+    "FullscreenControl.Exit": "Ieșiți din ecranul complet",
+    "GeolocateControl.FindMyLocation": "Găsiți locația mea",
+    "GeolocateControl.LocationNotAvailable": "Locația nu este disponibilă",
+    "LogoControl.Title": "Logo Mapbox",
+    "Map.Title": "Hartă",
+    "NavigationControl.ResetBearing": "Resetați rulmentul spre nord",
+    "NavigationControl.ZoomIn": "A mări",
+    "NavigationControl.ZoomOut": "A micșora",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Folosiți ctrl + derulați pentru a mări harta",
+    "ScrollZoomBlocker.CmdMessage": "Folosiți ⌘ + derulați pentru a mări harta",
+    "TouchPanBlocker.Message": "Folosiți două degete pentru a muta harta"
+};
+
+export default locale;
