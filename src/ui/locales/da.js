54b07580bc151682edec0727178f532df46659bb
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 16:37:06 +0300

    Add localization for UI strings in Danish (#11693)
    h/t @DorkMatter

---
+++ src/ui/locales/da.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Slå attribuering til/fra",
+    "AttributionControl.MapFeedback": "Feedback om kortet",
+    "FullscreenControl.Enter": "Skift til fuld skærm",
+    "FullscreenControl.Exit": "Luk fuld skærm",
+    "GeolocateControl.FindMyLocation": "Find min position",
+    "GeolocateControl.LocationNotAvailable": "Position utilgængelig",
+    "LogoControl.Title": "Mapbox logo",
+    "Map.Title": "Kort",
+    "NavigationControl.ResetBearing": "Nulstil pejling til nord",
+    "NavigationControl.ZoomIn": "Zoom ind",
+    "NavigationControl.ZoomOut": "Zoom ud",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mil",
+    "ScaleControl.NauticalMiles": "sømil",
+    "ScrollZoomBlocker.CtrlMessage": "Brug ctrl + scroll for at zoome ind på kortet",
+    "ScrollZoomBlocker.CmdMessage": "Brug ⌘ + scroll for at zoome ind på kortet",
+    "TouchPanBlocker.Message": "Brug to fingre til at flytte kortet"
+};
+
+export default locale;
