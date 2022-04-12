1dba2467785bd05140f6c4a7d9e911c98df384c9
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-12 18:38:32 +0300

    Add localization for UI strings in Ukrainian (#11689)
    h/t @mourner @Andygol

---
+++ src/ui/locales/uk.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Показати атрибуцію",
+    "AttributionControl.MapFeedback": "Відгук про мапу",
+    "FullscreenControl.Enter": "Перейти у повноекранний режим",
+    "FullscreenControl.Exit": "Вийти з повноекранного режиму",
+    "GeolocateControl.FindMyLocation": "Показати моє місцезнаходження",
+    "GeolocateControl.LocationNotAvailable": "Немає даних про місцезнаходження",
+    "LogoControl.Title": "Логотип Mapbox",
+    "Map.Title": "Мапа",
+    "NavigationControl.ResetBearing": "Орієнтувати мапу на північ",
+    "NavigationControl.ZoomIn": "Наблизити",
+    "NavigationControl.ZoomOut": "Віддалити",
+    "ScaleControl.Feet": "фт",
+    "ScaleControl.Meters": "м",
+    "ScaleControl.Kilometers": "км",
+    "ScaleControl.Miles": "миль",
+    "ScaleControl.NauticalMiles": "м. миль",
+    "ScrollZoomBlocker.CtrlMessage": "Прокрутіть з натиснутим Ctrl для масштабування мапи",
+    "ScrollZoomBlocker.CmdMessage": "Прокрутіть з натиснутим ⌘ для масштабування мапи",
+    "TouchPanBlocker.Message": "Рухайте мапу двома пальцями"
+};
+
+export default locale;
