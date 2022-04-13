b4176b212412894f08f39232231b0a66a6aa4f16
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-13 12:43:40 +0300

    Add localization for UI strings in Finnish (#11698)
    h/t @OzQu and @aleksigron

---
+++ src/ui/locales/fi.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Käytä nimeämistä",
+    "AttributionControl.MapFeedback": "Karttapalaute",
+    "FullscreenControl.Enter": "Siirry koko näytön tilaan",
+    "FullscreenControl.Exit": "Poistu koko näytöstä",
+    "GeolocateControl.FindMyLocation": "Etsi sijaintini",
+    "GeolocateControl.LocationNotAvailable": "Sijainti ei saatavilla",
+    "LogoControl.Title": "Mapboxin logo",
+    "Map.Title": "Kartta",
+    "NavigationControl.ResetBearing": "Palauta suuntima pohjoiseen",
+    "NavigationControl.ZoomIn": "Lähennä",
+    "NavigationControl.ZoomOut": "Loitonna",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Käytä ctrl + rullaa zoomataksesi karttaa",
+    "ScrollZoomBlocker.CmdMessage": "Käytä ⌘ + rullaa zoomataksesi karttaa",
+    "TouchPanBlocker.Message": "Siirrä karttaa kahdella sormella"
+};
+
+export default locale;
