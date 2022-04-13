2357cdc4f43e28f6cc8c1c92bc27750c9c366d57
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-13 14:08:29 +0300

    Add localization for UI strings in Korean (#11706)
    h/t @cs09g

---
+++ src/ui/locales/ko.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "속성 전환",
+    "AttributionControl.MapFeedback": "지도 피드백",
+    "FullscreenControl.Enter": "전체 화면으로 전환",
+    "FullscreenControl.Exit": "전체화면 종료",
+    "GeolocateControl.FindMyLocation": "내 위치 찾기",
+    "GeolocateControl.LocationNotAvailable": "위치를 사용할 수 없음",
+    "LogoControl.Title": "맵박스 로고",
+    "Map.Title": "지도",
+    "NavigationControl.ResetBearing": "방위를 북쪽으로 재설정",
+    "NavigationControl.ZoomIn": "확대",
+    "NavigationControl.ZoomOut": "축소",
+    "ScaleControl.Feet": "피트",
+    "ScaleControl.Meters": "미터",
+    "ScaleControl.Kilometers": "킬로미터",
+    "ScaleControl.Miles": "마일",
+    "ScaleControl.NauticalMiles": "해리",
+    "ScrollZoomBlocker.CtrlMessage": "ctrl + 스크롤을 사용하여 지도 확대/축소",
+    "ScrollZoomBlocker.CmdMessage": "⌘ + 스크롤을 사용하여 지도 확대/축소",
+    "TouchPanBlocker.Message": "두 손가락을 사용하여 지도 이동"
+};
+
+export default locale;
