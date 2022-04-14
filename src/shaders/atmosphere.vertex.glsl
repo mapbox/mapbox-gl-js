a5d6c2577adb915290467a73a5cfea1eb8f5f57c
Karim Naaji <karim.naaji@gmail.com>
2022-04-14 11:06:50 -0700

    Reconstruct horizon direction in shader

--- src/shaders/atmosphere.vertex.glsl
+++ src/shaders/atmosphere.vertex.glsl
@@ -6,17 +6,21 @@ uniform vec3 u_frustum_tl;
 uniform vec3 u_frustum_tr;
 uniform vec3 u_frustum_br;
 uniform vec3 u_frustum_bl;
-uniform vec3 u_horizon_l;
-uniform vec3 u_horizon_r;
+uniform float u_horizon;
 
 varying highp vec3 v_ray_dir;
 varying highp vec3 v_horizon_dir;
 
 void main() {
-    v_ray_dir = mix(mix(u_frustum_tl, u_frustum_tr, a_uv.x),
+    v_ray_dir = mix(
+        mix(u_frustum_tl, u_frustum_tr, a_uv.x),
         mix(u_frustum_bl, u_frustum_br, a_uv.x),
         a_uv.y);
-    v_horizon_dir = mix(u_horizon_l, u_horizon_r, a_uv.x);
+
+    v_horizon_dir = mix(
+        mix(u_frustum_tl, u_frustum_bl, u_horizon),
+        mix(u_frustum_tr, u_frustum_br, u_horizon),
+        a_uv.x);
 
     gl_Position = vec4(a_pos, 1.0);
 }
