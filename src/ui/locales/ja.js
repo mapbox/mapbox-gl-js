ef228061720b4f7096a3fdf1e803c8eb43d8c292
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-14 06:22:39 +0300

    Add localization for UI strings in Japanese (#11704)
    * Add localization for UI strings in Japanese
    
    * Apply suggestions from code review
    
    Co-authored-by: Yoshi Yazawa <54607254+YoshiYazawa@users.noreply.github.com>
    
    Co-authored-by: Yoshi Yazawa <54607254+YoshiYazawa@users.noreply.github.com>

---
+++ src/ui/locales/ja.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "アトリビューションを切り替え",
+    "AttributionControl.MapFeedback": "地図へのフィードバック",
+    "FullscreenControl.Enter": "全画面表示を開始",
+    "FullscreenControl.Exit": "全画面表示を終了",
+    "GeolocateControl.FindMyLocation": "現在地を検索",
+    "GeolocateControl.LocationNotAvailable": "現在地が見つかりません",
+    "LogoControl.Title": "Mapboxのロゴ",
+    "Map.Title": "地図",
+    "NavigationControl.ResetBearing": "北向きに変える",
+    "NavigationControl.ZoomIn": "ズームイン",
+    "NavigationControl.ZoomOut": "ズームアウト",
+    "ScaleControl.Feet": "フィート",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "マイル",
+    "ScaleControl.NauticalMiles": "海里",
+    "ScrollZoomBlocker.CtrlMessage": "ctrlキー+スクロールで地図を縮小/拡大",
+    "ScrollZoomBlocker.CmdMessage": "⌘+スクロールで地図を縮小/拡大",
+    "TouchPanBlocker.Message": "指2本で地図をスクロール"
+};
+
+export default locale;
