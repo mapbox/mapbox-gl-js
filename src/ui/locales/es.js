3cd7990d1ef20c8bc395dd629b190f5e818d3fc9
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-18 16:49:26 +0300

    Add localization for UI strings in Spanish (#11696)
    h/t @jtorresfabra @ThibaudLopez

---
+++ src/ui/locales/es.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Mostrar atribución",
+    "AttributionControl.MapFeedback": "Comentarios sobre el mapa",
+    "FullscreenControl.Enter": "Entrar en pantalla completa",
+    "FullscreenControl.Exit": "Salir de pantalla completa",
+    "GeolocateControl.FindMyLocation": "Encuentra mi ubicación",
+    "GeolocateControl.LocationNotAvailable": "Ubicación no disponible",
+    "LogoControl.Title": "Logotipo de Mapbox",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "Restablecer orientación al norte",
+    "NavigationControl.ZoomIn": "Ampliar",
+    "NavigationControl.ZoomOut": "Disminuir el zoom",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Usa ctrl + desplazamiento para hacer zoom en el mapa",
+    "ScrollZoomBlocker.CmdMessage": "Use ⌘ + desplazamiento para hacer zoom en el mapa",
+    "TouchPanBlocker.Message": "Usa dos dedos para mover el mapa"
+};
+
+export default locale;
