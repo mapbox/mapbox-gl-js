01ad1f24358990831b7a5e2c2320b34d92f1dfd6
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-04-18 21:40:18 +0300

    Add localization for UI strings in Catalan (#11691)
    h/t @jtorresfabra

---
+++ src/ui/locales/ca.js
@@ -0,0 +1,25 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Mostrar atribució",
+    "AttributionControl.MapFeedback": "Comentaris sobre el mapa",
+    "FullscreenControl.Enter": "Entra a pantalla completa",
+    "FullscreenControl.Exit": "Sortir de pantalla completa",
+    "GeolocateControl.FindMyLocation": "Troba la meva ubicació",
+    "GeolocateControl.LocationNotAvailable": "Ubicació no disponible",
+    "LogoControl.Title": "Logotip de Mapbox",
+    "Map.Title": "Mapa",
+    "NavigationControl.ResetBearing": "Restableix l'orientació cap al nord",
+    "NavigationControl.ZoomIn": "Ampliar",
+    "NavigationControl.ZoomOut": "Disminuir el zoom",
+    "ScaleControl.Feet": "ft",
+    "ScaleControl.Meters": "m",
+    "ScaleControl.Kilometers": "km",
+    "ScaleControl.Miles": "mi",
+    "ScaleControl.NauticalMiles": "nm",
+    "ScrollZoomBlocker.CtrlMessage": "Utilitzeu ctrl + desplaçament per ampliar el mapa",
+    "ScrollZoomBlocker.CmdMessage": "Utilitzeu ⌘ + desplaçament per ampliar el mapa",
+    "TouchPanBlocker.Message": "Feu servir dos dits per moure el mapa"
+};
+
+export default locale;
