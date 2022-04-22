8616217a0aa6133a1ab2c8a98fab1dd0348c6455
Aidan H <aidan.hendrickson@mapbox.com>
2022-04-22 17:17:43 -0600

    Adding unit test ensuring `mapboxgl.version` is valid regex (#11810)

--- test/unit/mapbox-gl.js
+++ test/unit/mapbox-gl.js
@@ -4,11 +4,13 @@ import mapboxgl from '../../src/index.js';
 test('mapboxgl', (t) => {
     t.test('version', (t) => {
         t.ok(mapboxgl.version);
+        t.match(mapboxgl.version, /^2\.[0-9]+\.[0-9]+(-dev|-beta\.[1-9])?$/);
         t.end();
     });
 
     t.test('workerCount', (t) => {
         t.ok(typeof mapboxgl.workerCount === 'number');
+        t.same(mapboxgl.workerCount, 2); // Test that workerCount defaults to 2
         t.end();
     });
     t.end();
