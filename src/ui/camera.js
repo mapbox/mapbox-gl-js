d8060094516c01484269ab93de7b51c2e64d465b
Aidan H <aidan.hendrickson@mapbox.com>
2022-05-10 12:15:56 -0600

    A bit more clarity

--- src/ui/camera.js
+++ src/ui/camera.js
@@ -50,8 +50,9 @@ type Required<T> = $ObjMap<T, <V>(v: V) => $NonMaybeType<V>>;
  * measured in degrees with a range between 0 and 85 degrees. For example, pitch: 0 provides the appearance
  * of looking straight down at the map, while pitch: 60 tilts the user's perspective towards the horizon.
  * Increasing the pitch value is often used to display 3D objects.
- * @property {LngLatLike} around The location serving as the origin for a change in `zoom`, `pitch` and/or `bearing`.
- * If this location is on screen, it will remain at the same screen point following the transform.
+ * @property {LngLatLike} around The location serving as the origin for a change in `zoom`, `pitch` and/or `bearing`,
+ * replacing the map center.
+ * This location will remain at the same screen position following the transform.
  * `center` is ignored if `around` is included.
  * @property {PaddingOptions} padding Dimensions in pixels applied on each side of the viewport for shifting the vanishing point.
  * @example
