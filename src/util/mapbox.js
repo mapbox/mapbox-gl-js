14c355e5ce7d7f5bffa91095eecbf32b2cef780f
zjp369 <605146581@qq.com>
2022-05-22 12:15:14 +0800

    Modify sprite can't be written in relative path("sprite": "css/sprite") (#884).

--- src/util/mapbox.js
+++ src/util/mapbox.js
@@ -103,11 +103,10 @@ export class RequestManager {
     }
 
     normalizeSpriteURL(url: string, format: string, extension: string, accessToken?: string): string {
-        const urlObject = parseUrl(url);
         if (!isMapboxURL(url)) {
-            urlObject.path += `${format}${extension}`;
-            return formatUrl(urlObject);
+            return `${url}${format}${extension}`;
         }
+        const urlObject = parseUrl(url);
         urlObject.path = `/styles/v1${urlObject.path}/sprite${format}${extension}`;
         return this._makeAPIURL(urlObject, this._customAccessToken || accessToken);
     }
