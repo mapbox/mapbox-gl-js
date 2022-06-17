0e381b4cc7c9dd36017c26a407be8cabc854cca8
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-06-17 01:14:32 -0700

    conditional order importance

--- src/style-spec/diff.js
+++ src/style-spec/diff.js
@@ -372,7 +372,7 @@ export default function diffStyles(before: StyleSpecification, after: StyleSpeci
         if (!isEqual(before.light, after.light)) {
             commands.push({command: operations.setLight, args: [after.light]});
         }
-        if (after.fog !== undefined && !isEqual(before.fog, after.fog)) {
+        if (!isEqual(before.fog, after.fog && after.fog !== undefined)) {
             commands.push({command: operations.setFog, args: [after.fog]});
         }
         if (!isEqual(before.projection, after.projection)) {
