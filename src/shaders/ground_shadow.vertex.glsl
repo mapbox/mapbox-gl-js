e7b12ef358e0301453f605a319c91b251340770b
Aleksi Grön <aleksi.gron@mapbox.com>
2022-06-29 18:44:35 +0300

    Adjust ground shadow strength based on fog opacity

--- src/shaders/ground_shadow.vertex.glsl
+++ src/shaders/ground_shadow.vertex.glsl
@@ -9,6 +9,10 @@ varying vec4 v_pos_light_view_0;
 varying vec4 v_pos_light_view_1;
 varying float v_depth;
 
+#ifdef FOG
+varying float v_fog_opacity;
+#endif
+
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0.0, 1.0);
 
@@ -16,4 +20,8 @@ void main() {
     v_pos_light_view_1 = u_light_matrix_1 * vec4(a_pos, 0.0, 1.0);
 
     v_depth = gl_Position.w;
+
+#ifdef FOG
+    v_fog_opacity = fog(fog_position(a_pos));
+#endif
 }
