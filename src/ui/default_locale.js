506f8984258dd5300fca663893861c988571871d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-11 16:00:49 +0300

    drop ScaleControl in default_locale

--- src/ui/default_locale.js
+++ src/ui/default_locale.js
@@ -12,11 +12,6 @@ const defaultLocale = {
     'NavigationControl.ResetBearing': 'Reset bearing to north',
     'NavigationControl.ZoomIn': 'Zoom in',
     'NavigationControl.ZoomOut': 'Zoom out',
-    'ScaleControl.Feet': 'ft',
-    'ScaleControl.Meters': 'm',
-    'ScaleControl.Kilometers': 'km',
-    'ScaleControl.Miles': 'mi',
-    'ScaleControl.NauticalMiles': 'nm',
     'ScrollZoomBlocker.CtrlMessage': 'Use ctrl + scroll to zoom the map',
     'ScrollZoomBlocker.CmdMessage': 'Use ⌘ + scroll to zoom the map',
     'TouchPanBlocker.Message': 'Use two fingers to move the map'
