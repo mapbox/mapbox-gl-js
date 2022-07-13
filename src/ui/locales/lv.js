32f0185097da8354791df3d006a69dc778f8b4df
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-13 13:37:39 +0300

    drop ScaleControl strings

--- src/ui/locales/lv.js
+++ src/ui/locales/lv.js
@@ -12,11 +12,6 @@ const locale = {
     "NavigationControl.ResetBearing": "Atiestatīt gultni uz ziemeļiem",
     "NavigationControl.ZoomIn": "Pietuvināt",
     "NavigationControl.ZoomOut": "Attālināt",
-    "ScaleControl.Feet": "pēdas",
-    "ScaleControl.Meters": "m",
-    "ScaleControl.Kilometers": "km",
-    "ScaleControl.Miles": "es",
-    "ScaleControl.NauticalMiles": "nm",
     "ScrollZoomBlocker.CtrlMessage": "Izmantojiet ctrl + ritināšanu, lai tuvinātu karti",
     "ScrollZoomBlocker.CmdMessage": "Izmantojiet ⌘ + ritināšanu, lai tuvinātu karti",
     "TouchPanBlocker.Message": "Izmantojiet divus pirkstus, lai pārvietotu karti"
