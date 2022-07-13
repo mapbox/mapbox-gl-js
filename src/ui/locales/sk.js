ded56fc92207bca09f1089fc24ad6990a68a53db
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-13 13:38:39 +0300

    drop ScaleControl strings

--- src/ui/locales/sk.js
+++ src/ui/locales/sk.js
@@ -12,11 +12,6 @@ const locale = {
     "NavigationControl.ResetBearing": "Obnovte azimut na sever",
     "NavigationControl.ZoomIn": "Priblížiť",
     "NavigationControl.ZoomOut": "Oddialiť",
-    "ScaleControl.Feet": "ft",
-    "ScaleControl.Meters": "m",
-    "ScaleControl.Kilometers": "km",
-    "ScaleControl.Miles": "ja",
-    "ScaleControl.NauticalMiles": "nm",
     "ScrollZoomBlocker.CtrlMessage": "Na priblíženie mapy použite ctrl + rolovanie",
     "ScrollZoomBlocker.CmdMessage": "Na priblíženie mapy použite ⌘ + posúvanie",
     "TouchPanBlocker.Message": "Na posúvanie mapy použite dva prsty"
