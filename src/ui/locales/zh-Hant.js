c8ed6508c7601d1687fdafac4164f2def6b15693
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-13 13:39:16 +0300

    drop ScaleControl strings

--- src/ui/locales/zh-Hant.js
+++ src/ui/locales/zh-Hant.js
@@ -12,11 +12,6 @@ const locale = {
     "NavigationControl.ResetBearing": "將方位重置為北",
     "NavigationControl.ZoomIn": "放大",
     "NavigationControl.ZoomOut": "縮小",
-    "ScaleControl.Feet": "英尺",
-    "ScaleControl.Meters": "米",
-    "ScaleControl.Kilometers": "公里",
-    "ScaleControl.Miles": "我",
-    "ScaleControl.NauticalMiles": "納米",
     "ScrollZoomBlocker.CtrlMessage": "使用 ctrl + scroll 縮放地圖",
     "ScrollZoomBlocker.CmdMessage": "使用 ⌘ + 滾動來縮放地圖",
     "TouchPanBlocker.Message": "用兩根手指移動地圖"
