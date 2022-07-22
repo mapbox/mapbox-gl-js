d5c121e11e42d82f0955976dead4c6775cf9b4f0
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-22 14:31:58 +0300

    Add localization for UI strings in Norwegian Bokmål (#11709) (h/t @felix-mittermeier)
    * Add localization for UI strings in Norwegian Bokmål

---
+++ src/ui/locales/nb.js
@@ -0,0 +1,20 @@
+// @flow
+
+const locale = {
+    "AttributionControl.ToggleAttribution": "Slå av/på attribusjon",
+    "AttributionControl.MapFeedback": "Karttilbakemelding",
+    "FullscreenControl.Enter": "Gå inn i fullskjerm",
+    "FullscreenControl.Exit": "Avslutt fullskjerm",
+    "GeolocateControl.FindMyLocation": "Finn min plassering",
+    "GeolocateControl.LocationNotAvailable": "Plassering er ikke tilgjengelig",
+    "LogoControl.Title": "Mapbox-logo",
+    "Map.Title": "Kart",
+    "NavigationControl.ResetBearing": "Tilbakestill peiling mot nord",
+    "NavigationControl.ZoomIn": "Zoom inn",
+    "NavigationControl.ZoomOut": "Zoom ut",
+    "ScrollZoomBlocker.CtrlMessage": "Bruk ctrl + rull for å zoome kartet",
+    "ScrollZoomBlocker.CmdMessage": "Bruk ⌘ + rull for å zoome kartet",
+    "TouchPanBlocker.Message": "Bruk to fingre for å flytte kartet"
+};
+
+export default locale;
