f64eb119bd455b06c03e5c5dbc6f972cf93ab835
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-07-27 13:04:30 -0700

    specify comment to explain why render environmental variable also needs to be checked

--- test/integration/testem/testem.js
+++ test/integration/testem/testem.js
@@ -138,7 +138,8 @@ if (process.env.CI) {
     Object.assign(testemConfig, ciTestemConfig);
 
 } else if (process.env.RENDER && process.env.USE_ANGLE && ['metal', 'gl', 'vulkan', 'swiftshader', 'gles'].includes(process.env.USE_ANGLE)) {
-    // Allow setting chrome flag `--use-angle` for local development. Search accepted values for `--use-angle` here: https://source.chromium.org/search?q=%22--use-angle%3D%22
+    // Allow setting chrome flag `--use-angle` for local development on render/query tests.
+    // Search accepted values for `--use-angle` here: https://source.chromium.org/search?q=%22--use-angle%3D%22
     const angleTestemConfig = setChromeFlags([ `--use-angle=${process.env.USE_ANGLE}` ]);
     Object.assign(testemConfig, angleTestemConfig);
 }
