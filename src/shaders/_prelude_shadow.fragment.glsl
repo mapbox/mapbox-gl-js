2cf33667efe8225e9c175629b7826772a52b7175
Aleksi Grön <aleksi.gron@mapbox.com>
2022-08-18 11:02:08 +0300

    Use rotated poisson disc sampling for softer shadows
    Set angle to 0 if you want to use non-rotated poisson disc.

--- src/shaders/_prelude_shadow.fragment.glsl
+++ src/shaders/_prelude_shadow.fragment.glsl
@@ -8,6 +8,10 @@ uniform vec2 u_cascade_distances;
 uniform highp vec3 u_shadow_direction;
 uniform highp vec3 u_shadow_bias;
 
+float rand(vec2 co) {
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 highp float shadow_sample_1(highp vec2 uv, highp float compare) {
     return step(unpack_depth(texture2D(u_shadowmap_1, uv)), compare);
 }
@@ -52,38 +56,40 @@ highp float shadow_occlusion_1(highp vec4 pos, highp float bias) {
 }
 
 highp float shadow_occlusion_0(highp vec4 pos, highp float bias) {
+    float disc_radius = 5.0;
+    vec2 poisson_disc[16];
+    poisson_disc[0] = vec2(-0.945846, 0.111362);
+    poisson_disc[1] = vec2(-0.337822, -0.0272108);
+    poisson_disc[2] = vec2(-0.68053, 0.526722);
+    poisson_disc[3] = vec2(-0.733113, -0.344172);
+    poisson_disc[4] = vec2(-0.44441, 0.865449);
+    poisson_disc[5] = vec2(-0.300874, 0.519281);
+    poisson_disc[6] = vec2(-0.0177885, 0.29553);
+    poisson_disc[7] = vec2(0.127917, 0.847511);
+    poisson_disc[8] = vec2(-0.472163, -0.651532);
+    poisson_disc[9] = vec2(-0.0999916, -0.439646);
+    poisson_disc[10] = vec2(0.0576763, -0.104369);
+    poisson_disc[11] = vec2(-0.00980055, -0.804103);
+    poisson_disc[12] = vec2(0.374496, 0.227166);
+    poisson_disc[13] = vec2(0.381638, 0.583732);
+    poisson_disc[14] = vec2(0.354931, -0.457904);
+    poisson_disc[15] = vec2(0.670774, -0.0304093);
+
+    float angle = rand(pos.xy);
+    float cos_a = cos(angle);
+    float sin_a = sin(angle);
+    mat2 rotation = mat2(cos_a, sin_a, -sin_a, cos_a);
+
     pos.xyz /= pos.w;
     pos.xy = pos.xy * 0.5 + 0.5;
     highp float compare0 = min(pos.z, 0.999) - bias;
-    highp vec2 uv = pos.xy;
 
-    highp vec2 texel = uv / u_texel_size - vec2(0.5);
-    highp vec2 f = fract(texel);
+    mediump float accumulate = 0.0;
+    for (int i = 0; i < 16; ++i) {
+        accumulate += shadow_sample_0(pos.xy + (poisson_disc[i] * disc_radius * u_texel_size) * rotation, compare0);
+    }
 
-    highp float s = u_texel_size;
-
-    // Perform brute force percentage-closer filtering with a 4x4 sample grid.
-    // Edge tap smoothing is used to weight each sample based on their contribution in the overall PCF kernel, i.e. `weight = clamp(kernel, texel.bounds).area / texel.area`
-    highp vec2 uv00 = (texel - f + 0.5) * s;
-    highp vec2 uv10 = uv00 + vec2(1.0 * s, 0.0);
-
-    highp vec2 uv01 = uv00 + vec2(0.0, 1.0 * s);
-    highp vec2 uv11 = uv01 + vec2(1.0 * s, 0.0);
-
-    highp float o00 = shadow_sample_0(uv00, compare0);
-    highp float o10 = shadow_sample_0(uv10, compare0);
-
-    highp float o01 = shadow_sample_0(uv01, compare0);
-    highp float o11 = shadow_sample_0(uv11, compare0);
-
-    // Edge tap smoothing
-    highp float value = 
-        (1.0 - f.x) * (1.0 - f.y) * o00 +
-        f.x * (1.0 - f.y) * o10 +
-        (1.0 - f.x) * f.y * o01 +
-        f.x * f.y * o11;
-
-    return clamp(value, 0.0, 1.0);
+    return clamp(accumulate / 16.0, 0.0, 1.0);
 }
 
 float shadow_occlusion(highp vec4 light_view_pos0, highp vec4 light_view_pos1, float view_depth, highp float bias) {
