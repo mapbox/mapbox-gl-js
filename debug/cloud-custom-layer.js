bb9cbe03418d79f2eca0a63beda32da385e2c5d7
Fouad Valadbeigi <fouad.valadbeigi@mapbox.com>
2022-08-22 00:54:35 +0200

    some so so experiments with rendering cloud

--- debug/cloud-custom-layer.js
+++ debug/cloud-custom-layer.js
@@ -10,10 +10,18 @@ const GLOBE_RADIUS_ECEF = GLOBE_CIRCUMFERENCE_ECEF / (2 * Math.PI);
 
 const METERS_TO_ECEF = GLOBE_CIRCUMFERENCE_ECEF / EARTH_CIRCUMFERENCE_METERS;
 
-const CLOUD_TEXTURE_SIZE = 128;
+const CLOUD_TEXTURE_SIZE = 64;
 
-// Transforms a point from ECEF coordinates to Mercator coordinates (which we can expose as a public API).
-function getEcefToMercatorMatrix(transform) {
+function getPixelToMercatorMatrix(transform) {
+    const PIXEL_TO_MERCATOR = 1 / transform.worldSize;
+    const METER_TO_MERCATOR = transform.pixelsPerMeter * PIXEL_TO_MERCATOR;
+    const pixelToMercator = new THREE.Matrix4().scale(
+        new THREE.Vector3(PIXEL_TO_MERCATOR, PIXEL_TO_MERCATOR, METER_TO_MERCATOR)
+    )
+    return pixelToMercator;
+}
+
+function getEcefToPixelMatrix(transform) {
     const {lng, lat} = transform._center;
     
     const ecefToPixelScale = transform.worldSize / GLOBE_CIRCUMFERENCE_ECEF;
@@ -43,138 +51,104 @@ function getEcefToMercatorMatrix(transform) {
     .multiply(rotationX)
     .multiply(rotationY);
 
-    const PIXEL_TO_MERCATOR = 1 / transform.worldSize;
-    const METER_TO_MERCATOR = transform.pixelsPerMeter * PIXEL_TO_MERCATOR;
-    const pixelToMercator = new THREE.Matrix4().scale(
-        new THREE.Vector3(PIXEL_TO_MERCATOR, PIXEL_TO_MERCATOR, METER_TO_MERCATOR)
-    )
-
-    const ecefToMercator = pixelToMercator.multiply(ecefToPixel);
-
-    return ecefToMercator;
+    return ecefToPixel;
 }
 
-function getModelToEcefMatrix(cloudAltitudeMeter = 25000) {
-    const altitudeEcef = Math.max(0, cloudAltitudeMeter) * METERS_TO_ECEF;
-    const elevationEcef = altitudeEcef + GLOBE_RADIUS_ECEF;
+function getModelToEcefMatrix(cloudAltitudeMeter) {
+    const altEcef = Math.max(cloudAltitudeMeter, 0) * METERS_TO_ECEF;
+    const modelToEcefScaler = altEcef + GLOBE_RADIUS_ECEF;
     const modelToEcef = new THREE.Matrix4().scale(
-        new THREE.Vector3(elevationEcef, -elevationEcef, elevationEcef)
+        new THREE.Vector3(modelToEcefScaler, modelToEcefScaler, modelToEcefScaler)
     )
     return modelToEcef;
 }
 
-function generateNoiseTexture(size) {
-    const data = new Uint8Array(size * size * size);
-    const vector = new THREE.Vector3();
-    let i = 0;
-    for (let z = 0; k < size; z++) {
-        for (let y = 0; j < size; y++) {
-            for (let x = 0; i < size; x++) {
-                const dist = vector.set(x, y, z).subScalar(size / 2).divideScalar(size).length();
-                data[i] = 1;
-            }
-        }
-    }
-    const texture = new THREE.Data3DTexture(data, size, size, size);
-    texture.format = THREE.RedFormat;
-    texture.minFilter = THREE.LinearFilter;
-    texture.magFilter = THREE.LinearFilter;
-    texture.unpackAlignment = 1;
-    texture.needsUpdate = true;
-    return texture;
+function getModelToMercatorMatrix(transform, cloudAltitudeMeter) {
+    const modelToEcef = getModelToEcefMatrix(cloudAltitudeMeter);
+    const ecefToPixel = getEcefToPixelMatrix(transform);
+    const pixelToMercator = getPixelToMercatorMatrix(transform);
+    return pixelToMercator.multiply(ecefToPixel).multiply(modelToEcef);
 }
 
 const vertexShader = /* glsl */`
-    in vec3 position;
+    attribute vec3 position;
     uniform mat4 projectionMatrix;
-    out vec3 vPosition;
+    varying vec3 vPosition;
     void main() {
         vPosition = position;
-        gl_Position = projectionMatrix * mvPosition;
+        gl_Position = projectionMatrix * vec4(position, 1.0);
     }
 `;
 
-
-
 const fragmentShader = /* glsl */`
     precision highp float;
-    precision highp sampler3D;
-    uniform mat4 projectionMatrix;
 
-    in vec3 vPosition;
+    float square(float x) { return x * x; }
+    float hash(float p) { p = fract(p * 0.011); p *= p + 7.5; p *= p + p; return fract(p); }
+    float noise(vec3 x) { const vec3 step = vec3(110, 241, 171); vec3 i = floor(x); vec3 f = fract(x); float n = dot(i, step); vec3 u = f * f * (3.0 - 2.0 * f); return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x), mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y), mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x), mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z); }
+    #define DEFINE_FBM(name, OCTAVES) float name(vec3 x) { float v = 0.0; float a = 0.5; vec3 shift = vec3(100); for (int i = 0; i < OCTAVES; ++i) { v += a * noise(x); x = x * 2.0 + shift; a *= 0.5; } return v; }
+    DEFINE_FBM(fbm3, 3)
+    DEFINE_FBM(fbm5, 5)
 
-    uniform vec3 base;
-    uniform sampler3D map;
+    varying vec3 vPosition;
+    uniform mat4 projectionMatrix;    
+    uniform vec3 cloudColor;
     uniform vec3 cameraPos;
+    uniform vec3 lightDir;
     uniform float opacity;
-    uniform float altitude;
-    uniform float height;
     uniform float steps;
-    uniform float frame;
 
-    out vec4 color;
-
-    uint wangHash(uint seed) {
-        seed = (seed ^ 61u) ^ (seed >> 16u);
-        seed *= 9u;
-        seed = seed ^ (seed >> 4u);
-        seed *= 0x27d4eb2du;
-        seed = seed ^ (seed >> 15u);
-        return seed;
+    // Hit unit sphere at origin.
+    vec2 sphereIntersection(vec3 ro, vec3 rd) {
+        float b = dot(ro, rd);
+        float c = dot(ro, ro) - 1.0;
+        float d = b * b - c;
+        if (d < 0.0) discard;
+        d = sqrt(d);
+        float tN = (-b - d);
+        float tF = (-b + d);
+        if (tF < 0.0) discard;
+        return vec2(tN, tF);
     }
 
-    float randomFloat(inout uint seed) {
-        return float(wangHash(seed)) / 4294967296.;
+    float cloudDensity(vec3 p, float t) {
+        return fbm5(p + 1.5 * fbm3(p - t * 0.047) - t * vec3(0.03, 0.01, 0.01)) - 0.42;
     }
 
-    vec2 intersectSphere(vec3 orig, vec3 dir) {
-        return vec2(0.0, 0.0);
+    float cloudDensity2(vec3 p, float t) {
+        return fbm5(p);
     }
 
-    float sample1(vec3 p) {
-        return texture(map, p).r;
-    }
-
-    float shading(vec3 coord) {
-        float step = 0.01;
-        return sample1(coord + vec3(-step)) - sample1(coord + vec3(step));
-    }
+    const float MAX_STEPS = 100.0;
 
     void main() {
-        // vec3 rayDir = normalize(vPosition - cameraPos);
-        // vec2 hit = intersecSphere( cameraPos, rayDir );
+        vec3 rd = normalize(vPosition - cameraPos);
+        vec3 ro = -0.01 * rd + vPosition; // avoids precision issues when camera is too far.
+        vec2 hit = sphereIntersection(ro, rd);
+        
+        hit.x = max(hit.x, 0.0);
+        hit.y = hit.x + (hit.y - hit.x) / 100.0;
+        float stepSize = 1.0 / steps;
 
-        // if (hit.x > hit.y ) discard;
-
-        // hit.x = max(hit.x, 0.0);
-        // vec3 p = cameraPos + hit.x * rayDir;
-        // vec3 inc = 1.0 / abs(rayDir);
-        // float delta = min(inc.x, min(inc.y, inc.z));
-        // delta /= steps;
-
-        // // Jitter
-        // // Nice little seed from
-        // // https://blog.demofox.org/2020/05/25/casual-shadertoy-path-tracing-1-basic-camera-diffuse-emissive/
-        // uint seed = uint(gl_FragCoord.x) * uint(1973) + uint(gl_FragCoord.y) * uint(9277) + uint(frame) * uint(26699);
-        // vec3 size = vec3(textureSize(map, 0));
-        // float randNum = randomFloat(seed) * 2.0 - 1.0;
-        // p += rayDir * randNum * (1.0 / size);
-
-        // vec4 ac = vec4(base, 0.0);
-        // for (float t = hit.x; t < hit.y; t += delta) {
-        //     float d = sample1(p + 0.5);
-
-        //     d = smoothstep(threshold - range, threshold + range, d ) * opacity;
-            
-        //     float col = shading( p + 0.5 ) * 3.0 + ( ( p.x + p.y ) * 0.25 ) + 0.2;
-        //     ac.rgb += ( 1.0 - ac.a ) * d * col;
-        //     ac.a += ( 1.0 - ac.a ) * d;
-        //     if ( ac.a >= 0.95 ) break;
-        //     p += rayDir * delta;
-        // }
-        // color = ac;
-        // if (color.a == 0.0) discard;
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+        vec4 radiance = vec4(0.0);
+        float t = hit.y;
+        for (float i = 0.0; i < MAX_STEPS; i++) {
+            if (t > hit.x) {
+                vec3 p = ro + t * rd;
+                float density = cloudDensity(p, 0.0);
+                if (density > 0.0) {
+                    float shading = clamp(-(cloudDensity(p + lightDir * stepSize, 0.0) - density) / stepSize, -1.0, 1.0) * 0.5 + 0.5;
+                    vec3 Lo = cloudColor * shading * vec3(2.1);
+                    radiance = mix(radiance, vec4(Lo, 1.0), density);
+                    density *= square(1.0 - abs(2.0 * length(p) - (0.85 + 1.0)) * (1.0 / (1.0 - 0.85)));
+                    t += 2.0 * stepSize; 
+                }
+                t -= 3.0 * stepSize;
+            } else {
+                break;
+            }
+        }
+        gl_FragColor = radiance;
     }
 `;
 
@@ -185,18 +159,14 @@ const cloudLayer = {
         this.camera = new THREE.Camera();
         this.scene = new THREE.Scene();
 
-        // const texture = generateNoiseTexture();
-        const geometry = new THREE.OctahedronGeometry(1, 16);
+        const geometry = new THREE.IcosahedronGeometry(1, 8);
         const material = new THREE.RawShaderMaterial({
-            glslVersion: THREE.GLSL3,
             uniforms: {
-                base: {value: new THREE.Color(0x798aa0)},
+                cloudColor: {value: new THREE.Color(0xffffff)},
                 cameraPos: {value: new THREE.Vector3()},
-                opacity: {value: 0.25},
-                altitude: {value: 0.1},
-                height: {value: 0.1},
-                steps: {value: 100},
-                frame: {value: 0}
+                lightDir: {value: new THREE.Vector3(1.0 / 1.7464, 1.3 / 1.7464, 0.6 / 1.7464)},
+                opacity: {value: 0.75},
+                steps: {value: 100}
             },
             vertexShader,
             fragmentShader,
@@ -204,6 +174,7 @@ const cloudLayer = {
             transparent: true
         });
         const mesh = new THREE.Mesh(geometry, material);
+        this.mesh = mesh;
         this.scene.add(mesh);
 
         this.map = map;
@@ -224,11 +195,20 @@ const cloudLayer = {
         // const cloudEndAltNormalized = 1;
 
         if (this.map.transform.projection.name === 'globe') {
-            const projection = new THREE.Matrix4().fromArray(projectionMatrix);
-            const globe = getEcefToMercatorMatrix(this.map.transform);
-            const model = getModelToEcefMatrix();
+            const transform = this.map.transform;
 
-            this.camera.projectionMatrix = projection.multiply(globe).multiply(model);
+            const projection = new THREE.Matrix4().fromArray(projectionMatrix);
+            const cloudAltitudeMeter = 50000;
+            const modelToMercator = getModelToMercatorMatrix(transform, cloudAltitudeMeter);
+            const mercatorToModel = modelToMercator.clone().invert();
+
+            let cameraPos = new THREE.Vector4(transform._camera.position[0],
+                                              transform._camera.position[1],
+                                              transform._camera.position[2]);
+            cameraPos.applyMatrix4(mercatorToModel);
+
+            this.mesh.material.uniforms.cameraPos.value.copy(cameraPos);
+            this.camera.projectionMatrix = projection.multiply(modelToMercator);
 
             this.renderer.resetState();
             this.renderer.render(this.scene, this.camera);
