7a59ea3ced829f1784372f3b7aaeecd1ee9b1aa5
Aleksi Grön <aleksi.gron@mapbox.com>
2022-08-23 16:59:34 +0300

    Use receiver plane depth bias in shadows

--- src/shaders/_prelude_shadow.fragment.glsl
+++ src/shaders/_prelude_shadow.fragment.glsl
@@ -8,6 +8,24 @@ uniform vec2 u_cascade_distances;
 uniform highp vec3 u_shadow_direction;
 uniform highp vec3 u_shadow_bias;
 
+#ifdef USE_STANDARD_DERIVATIVES
+highp vec2 light_space_derivatives(highp vec3 pos) {
+    // Receiver plane depth bias
+    // From https://developer.amd.com/wordpress/media/2012/10/Isidoro-ShadowMapping.pdf
+    // Derivatives of position in light space w.r.t. screen space x and y
+    highp vec3 screen_x_deriv = dFdx(pos);
+    highp vec3 screen_y_deriv = dFdy(pos);
+
+    // Calculate derivatives w.r.t. light space x and y
+    highp vec2 light_deriv;
+    light_deriv.x = (screen_y_deriv.y * screen_x_deriv.z) - (screen_x_deriv.y * screen_y_deriv.z);
+    light_deriv.y = (screen_x_deriv.x * screen_y_deriv.z) - (screen_y_deriv.x * screen_x_deriv.z);
+    light_deriv *= 1.0 / ((screen_x_deriv.x * screen_y_deriv.y) - (screen_x_deriv.y * screen_y_deriv.x));
+
+    return light_deriv;
+}
+#endif
+
 highp float shadow_sample_1(highp vec2 uv, highp float compare) {
     return step(unpack_depth(texture2D(u_shadowmap_1, uv)), compare);
 }
@@ -19,34 +37,46 @@ highp float shadow_sample_0(highp vec2 uv, highp float compare) {
 highp float shadow_occlusion_1(highp vec4 pos, highp float bias) {
     pos.xyz /= pos.w;
     pos.xy = pos.xy * 0.5 + 0.5;
-    highp float compare1 = min(pos.z, 0.999) - bias;
 
     highp vec2 texel = pos.xy / u_texel_size - vec2(0.5);
     highp vec2 f = fract(texel);
+
+    highp vec2 light_deriv;
+#ifdef USE_STANDARD_DERIVATIVES
+    light_deriv = light_space_derivatives(pos.xyz);
+#endif
 
     highp float s = u_texel_size;
 
     // Perform percentage-closer filtering with a 2x2 sample grid.
     // Edge tap smoothing is used to weight each sample based on their contribution in the overall PCF kernel
 
-    highp vec2 uv00 = (texel - f + 0.5) * s;
-    highp vec2 uv10 = uv00 + vec2(1.0 * s, 0.0);
+    float samples[4];
+    highp vec2 origin = (texel - f + 0.5) * u_texel_size;
+    for (float y = 0.0; y < 2.0; y += 1.0) {
+        for (float x = 0.0; x < 2.0; x += 1.0) {
+            vec2 sample_pos = origin + vec2(x * u_texel_size, y * u_texel_size);
 
-    highp vec2 uv01 = uv00 + vec2(0.0, 1.0 * s);
-    highp vec2 uv11 = uv01 + vec2(1.0 * s, 0.0);
+            highp float receiverBias;
 
-    highp float o00 = shadow_sample_1(uv00, compare1);
-    highp float o10 = shadow_sample_1(uv10, compare1);
+#ifdef USE_STANDARD_DERIVATIVES
+            vec2 sample_offset = sample_pos - pos.xy;
+            receiverBias = (light_deriv.x * sample_offset.x) + (light_deriv.y * sample_offset.y);
+#else
+            receiverBias = 0.0;
+#endif
 
-    highp float o01 = shadow_sample_1(uv01, compare1);
-    highp float o11 = shadow_sample_1(uv11, compare1);
+            highp float compare1 = min(pos.z, 0.999) + receiverBias - bias;
+            samples[int(y * 2.0 + x)] = shadow_sample_1(sample_pos, compare1);
+        }
+    }
 
     // Edge tap smoothing
     highp float value = 
-        (1.0 - f.x) * (1.0 - f.y) * o00 +
-        f.x * (1.0 - f.y) * o10 +
-        (1.0 - f.x) * f.y * o01 +
-        f.x * f.y * o11;
+        (1.0 - f.x) * (1.0 - f.y) * samples[0] +
+        f.x * (1.0 - f.y) * samples[1] +
+        (1.0 - f.x) * f.y * samples[2] +
+        f.x * f.y * samples[3];
 
     return clamp(value, 0.0, 1.0);
 }
@@ -54,67 +84,50 @@ highp float shadow_occlusion_1(highp vec4 pos, highp float bias) {
 highp float shadow_occlusion_0(highp vec4 pos, highp float bias) {
     pos.xyz /= pos.w;
     pos.xy = pos.xy * 0.5 + 0.5;
-    highp float compare0 = min(pos.z, 0.999) - bias;
-    highp vec2 uv = pos.xy;
 
-    highp vec2 texel = uv / u_texel_size - vec2(1.5);
+    highp vec2 texel = pos.xy / u_texel_size - vec2(1.5);
     highp vec2 f = fract(texel);
 
-    highp float s = u_texel_size;
+    highp vec2 light_deriv;
+#ifdef USE_STANDARD_DERIVATIVES
+    light_deriv = light_space_derivatives(pos.xyz);
+#endif
 
-    // Perform brute force percentage-closer filtering with a 4x4 sample grid.
-    // Edge tap smoothing is used to weight each sample based on their contribution in the overall PCF kernel, i.e. `weight = clamp(kernel, texel.bounds).area / texel.area`
-    highp vec2 uv00 = (texel - f + 0.5) * s;
-    highp vec2 uv10 = uv00 + vec2(1.0 * s, 0);
-    highp vec2 uv20 = uv00 + vec2(2.0 * s, 0);
-    highp vec2 uv30 = uv00 + vec2(3.0 * s, 0);
+    // Perform shadow filtering with a 4x4 sample grid.
+    // Edge tap smoothing is used to weight each sample based on their contribution in the overall PCF kernel,
+    // i.e. `weight = clamp(kernel, texel.bounds).area / texel.area`
 
-    highp vec2 uv01 = uv00 + vec2(0.0, 1.0 * s);
-    highp vec2 uv11 = uv01 + vec2(1.0 * s, 0);
-    highp vec2 uv21 = uv01 + vec2(2.0 * s, 0);
-    highp vec2 uv31 = uv01 + vec2(3.0 * s, 0);
+    float samples[16];
+    highp vec2 origin = (texel - f + 0.5) * u_texel_size;
+    for (float y = 0.0; y < 4.0; y += 1.0) {
+        for (float x = 0.0; x < 4.0; x += 1.0) {
+            vec2 sample_pos = origin + vec2(x * u_texel_size, y * u_texel_size);
 
-    highp vec2 uv02 = uv01 + vec2(0.0, 1.0 * s);
-    highp vec2 uv12 = uv02 + vec2(1.0 * s, 0);
-    highp vec2 uv22 = uv02 + vec2(2.0 * s, 0);
-    highp vec2 uv32 = uv02 + vec2(3.0 * s, 0);
+            highp float receiverBias;
 
-    highp vec2 uv03 = uv02 + vec2(0.0, 1.0 * s);
-    highp vec2 uv13 = uv03 + vec2(1.0 * s, 0);
-    highp vec2 uv23 = uv03 + vec2(2.0 * s, 0);
-    highp vec2 uv33 = uv03 + vec2(3.0 * s, 0);
+#ifdef USE_STANDARD_DERIVATIVES
+            vec2 sample_offset = sample_pos - pos.xy;
+            receiverBias = (light_deriv.x * sample_offset.x) + (light_deriv.y * sample_offset.y);
+#else
+            receiverBias = 0.0;
+#endif
 
-    highp float o00 = shadow_sample_0(uv00, compare0);
-    highp float o10 = shadow_sample_0(uv10, compare0);
-    highp float o20 = shadow_sample_0(uv20, compare0);
-    highp float o30 = shadow_sample_0(uv30, compare0);
-
-    highp float o01 = shadow_sample_0(uv01, compare0);
-    highp float o11 = shadow_sample_0(uv11, compare0);
-    highp float o21 = shadow_sample_0(uv21, compare0);
-    highp float o31 = shadow_sample_0(uv31, compare0);
-
-    highp float o02 = shadow_sample_0(uv02, compare0);
-    highp float o12 = shadow_sample_0(uv12, compare0);
-    highp float o22 = shadow_sample_0(uv22, compare0);
-    highp float o32 = shadow_sample_0(uv32, compare0);
-
-    highp float o03 = shadow_sample_0(uv03, compare0);
-    highp float o13 = shadow_sample_0(uv13, compare0);
-    highp float o23 = shadow_sample_0(uv23, compare0);
-    highp float o33 = shadow_sample_0(uv33, compare0);
+            highp float compare0 = min(pos.z, 0.999) + receiverBias - bias;
+            samples[int(y * 4.0 + x)] = shadow_sample_0(sample_pos, compare0);
+        }
+    }
 
     // Edge tap smoothing
     highp float value = 
-        (1.0 - f.x) * (1.0 - f.y) * o00 +
-        (1.0 - f.y) * (o10 + o20) +
-        f.x * (1.0 - f.y) * o30 +
-        (1.0 - f.x) * (o01 + o02) +
-        f.x * (o31 + o32) +
-        (1.0 - f.x) * f.y * o03 +
-        f.y * (o13 + o23) +
-        f.x * f.y * o33 +
-        o11 + o21 + o12 + o22;
+        (1.0 - f.x) * (1.0 - f.y) * samples[0] +
+        (1.0 - f.y) * (samples[1] + samples[2]) +
+        f.x * (1.0 - f.y) * samples[3] +
+        (1.0 - f.x) * (samples[4] + samples[8]) +
+        f.x * (samples[7] + samples[11]) +
+        (1.0 - f.x) * f.y * samples[12] +
+        f.y * (samples[13] + samples[14]) +
+        f.x * f.y * samples[15] +
+        samples[5] + samples[6] + samples[9] + samples[10];
 
     return clamp(value / 9.0, 0.0, 1.0);
 }
