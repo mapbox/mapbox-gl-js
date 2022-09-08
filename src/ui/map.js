a4339ab6577b8d9f1248d323b780a2c16b71fce8
Joe Woodhouse <joe@natural-apptitude.co.uk>
2022-09-08 17:00:28 +0100

    Fix resource leak on Safari on `map.remove()` (#12224)
    * Unbind scroll event listener from the map container, and unbind listeners and dereference canvas and other containers to prevent resource leak on Safari
    
    * Fix flow tests
    
    * Add unit test for checking that the same number of event listeners are added as removed from the map container element

--- src/ui/map.js
+++ src/ui/map.js
@@ -3437,10 +3437,21 @@ class Map extends Camera {
 
         const extension = this.painter.context.gl.getExtension('WEBGL_lose_context');
         if (extension) extension.loseContext();
+
+        this._canvas.removeEventListener('webglcontextlost', this._contextLost, false);
+        this._canvas.removeEventListener('webglcontextrestored', this._contextRestored, false);
+
         removeNode(this._canvasContainer);
         removeNode(this._controlContainer);
         removeNode(this._missingCSSCanary);
+
+        this._canvas = (undefined: any);
+        this._canvasContainer = (undefined: any);
+        this._controlContainer = (undefined: any);
+        this._missingCSSCanary = (undefined: any);
+
         this._container.classList.remove('mapboxgl-map');
+        this._container.removeEventListener('scroll', this._onMapScroll, false);
 
         PerformanceUtils.clearMetrics();
         removeAuthState(this.painter.context.gl);
