6c9b93d984f1fc2f7078b16b6977907e232b1e36
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-09-15 23:38:18 -0700

    fix accidental change in unit tests

--- test/unit/ui/handler/scroll_zoom.test.js
+++ test/unit/ui/handler/scroll_zoom.test.js
@@ -65,7 +65,7 @@ test('ScrollZoomHandler', (t) => {
         // Simulate a single 'wheel' event without the magical deltaY value.
         // This requires the handler to briefly wait to see if a subsequent
         // event is coming in order to guess trackpad vs. mouse wheel
-        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: 100});
+        simulate.wheel(map.getCanvas(), {type: 'wheel', deltaY: -20});
         map.on('zoomstart', () => {
             map.remove();
             t.end();
