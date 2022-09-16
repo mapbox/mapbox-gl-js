3ab3a3008bda8b2e9ef99b5091eeca4a1101f130
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-09-16 15:31:41 +0500

    Add HTMLImageElement support, fix JSDoc

--- src/source/custom_source.js
+++ src/source/custom_source.js
@@ -17,8 +17,9 @@ type DataType = 'raster';
 
 function isRaster(data: any): boolean {
     return data instanceof window.ImageData ||
+        data instanceof window.HTMLCanvasElement ||
         data instanceof window.ImageBitmap ||
-        data instanceof window.HTMLCanvasElement;
+        data instanceof window.HTMLImageElement;
 }
 
 /**
@@ -126,7 +127,7 @@ function isRaster(data: any): boolean {
  * @param {{ z: number, x: number, y: number }} tile Tile name to load in the XYZ scheme format.
  * @param {Object} options Options.
  * @param {AbortSignal} options.signal A signal object that communicates when the map cancels the tile loading request.
- * @returns {Promise<TextureImage | undefined | null>} The promise that resolves to the tile image data as an `HTMLImageElement`, `ImageData`, `ImageBitmap` or object with `width`, `height`, and `data`.
+ * @returns {Promise<TextureImage | undefined | null>} The promise that resolves to the tile image data as an `HTMLCanvasElement`, `HTMLImageElement`, `ImageData`, `ImageBitmap` or object with `width`, `height`, and `data`.
  *  If `loadTile` resolves to `undefined`, a map will render an overscaled parent tile in the tile’s space. If `loadTile` resolves to `null`, a map will render nothing in the tile’s space.
  */
 export type CustomSourceInterface<T> = {
