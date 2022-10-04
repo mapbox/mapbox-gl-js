abcc30672ef0c2e214b65a6a6f26384637d89903
Anna Peery <42715836+avpeery@users.noreply.github.com>
2022-10-04 16:41:56 -0700

    only apply epsilon changes if projection is globe

--- src/terrain/terrain.js
+++ src/terrain/terrain.js
@@ -260,7 +260,7 @@ export class Terrain extends Elevation {
         this._sourceTilesOverlap = {};
         this.proxySourceCache = new ProxySourceCache(style.map);
         this.orthoMatrix = mat4.create();
-        const epsilon = .35; // Experimentally the smallest value to avoid rendering artifacts (https://github.com/mapbox/mapbox-gl-js/issues/11975)
+        const epsilon = this.painter.transform.projection.name === 'globe' ?  .35 : 0; // Experimentally the smallest value to avoid rendering artifacts (https://github.com/mapbox/mapbox-gl-js/issues/11975)
         mat4.ortho(this.orthoMatrix, -epsilon, EXTENT + epsilon, -epsilon, EXTENT + epsilon, 0, 1);
         const gl = context.gl;
         this._overlapStencilMode = new StencilMode({func: gl.GEQUAL, mask: 0xFF}, 0, 0xFF, gl.KEEP, gl.KEEP, gl.REPLACE);
