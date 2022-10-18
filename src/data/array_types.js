89d778f79b3cba5d6cffb1b2786caa79a2b10d6f
Aidan H <aidan.hendrickson@mapbox.com>
2022-10-18 13:29:44 -0600

    Generating asserts

--- src/data/array_types.js
+++ src/data/array_types.js
@@ -1013,7 +1013,6 @@ export class FillExtrusionExtArray extends StructArrayLayout6i12 {
     get(index: number): FillExtrusionExtStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new FillExtrusionExtStruct(this, index);
     }
 }
@@ -1053,7 +1052,6 @@ export class CollisionBoxArray extends StructArrayLayout5i4f1i1ul2ui40 {
     get(index: number): CollisionBoxStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new CollisionBoxStruct(this, index);
     }
 }
@@ -1105,7 +1103,6 @@ export class PlacedSymbolArray extends StructArrayLayout3i2f2ui3ul3ui2f3ub1ul1i1
     get(index: number): PlacedSymbolStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new PlacedSymbolStruct(this, index);
     }
 }
@@ -1163,7 +1160,6 @@ export class SymbolInstanceArray extends StructArrayLayout3i2f6i15ui1ul3f76 {
     get(index: number): SymbolInstanceStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new SymbolInstanceStruct(this, index);
     }
 }
@@ -1214,7 +1210,6 @@ export class FeatureIndexArray extends StructArrayLayout1ul3ui12 {
     get(index: number): FeatureIndexStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new FeatureIndexStruct(this, index);
     }
 }
@@ -1243,7 +1238,6 @@ export class FillExtrusionCentroidArray extends StructArrayLayout2ui4 {
     get(index: number): FillExtrusionCentroidStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new FillExtrusionCentroidStruct(this, index);
     }
 }
@@ -1276,7 +1270,6 @@ export class CircleGlobeExtArray extends StructArrayLayout6i12 {
     get(index: number): CircleGlobeExtStruct {
         assert(!this.isTransferred);
         assert(index < this.length);
-        assert(index >= 0);
         return new CircleGlobeExtStruct(this, index);
     }
 }
