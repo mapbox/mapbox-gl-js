94d602522f6eb996a8196677f8de040cad8e82b0
Karim Naaji <karim.naaji@gmail.com>
2022-10-26 12:56:19 -0700

    Add mapbox URL regexs

--- src/util/mapbox.js
+++ src/util/mapbox.js
@@ -230,6 +230,22 @@ export function isMapboxHTTPURL(url: string): boolean {
     return config.API_URL_REGEX.test(url);
 }
 
+export function isMapboxHTTPStyleURL(url: string): boolean {
+    return config.API_STYLE_REGEX.test(url) && !isMapboxHTTPSpriteURL(url);
+}
+
+export function isMapboxHTTPTileJSONURL(url: string): boolean {
+    return config.API_TILEJSON_REGEX.test(url);
+}
+
+export function isMapboxHTTPSpriteURL(url: string): boolean {
+    return config.API_SPRITE_REGEX.test(url);
+}
+
+export function isMapboxHTTPFontsURL(url: string): boolean {
+    return config.API_FONTS_REGEX.test(url);
+}
+
 export function hasCacheDefeatingSku(url: string): boolean {
     return url.indexOf('sku=') > 0 && isMapboxHTTPURL(url);
 }
