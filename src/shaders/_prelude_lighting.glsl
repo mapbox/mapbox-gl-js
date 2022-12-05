850f27d1c826ba3efa482a82e93ab3ce953f8cc4
Mikko Pulkki <mikko.pulkki@mapbox.com>
2022-12-05 15:54:36 +0200

    Add precision qualifiers to shader uniforms

--- src/shaders/_prelude_lighting.glsl
+++ src/shaders/_prelude_lighting.glsl
@@ -1,8 +1,8 @@
 #ifdef LIGHTING_3D_MODE
 
-uniform vec3 u_lighting_ambient_color;
-uniform vec3 u_lighting_directional_dir;        // Direction towards the light source
-uniform vec3 u_lighting_directional_color;
+uniform mediump vec3 u_lighting_ambient_color;
+uniform mediump vec3 u_lighting_directional_dir;        // Direction towards the light source
+uniform mediump vec3 u_lighting_directional_color;
 
 vec3 apply_lighting(vec3 color) {
     float NdotL = u_lighting_directional_dir.z;
