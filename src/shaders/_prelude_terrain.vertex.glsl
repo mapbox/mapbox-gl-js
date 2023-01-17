77757cf67dddfa636ce609f4986bdf91c5a74999
Aleksei Romanov <Aleksei.Romanov@mapbox.com>
2023-01-17 14:41:49 +0200

    [MAPS3D-425] introduce skirts for globe rendering
    Absent skirts lead to holes for hight exaggerated terrain in globe mode.

--- src/shaders/_prelude_terrain.vertex.glsl
+++ src/shaders/_prelude_terrain.vertex.glsl
@@ -24,6 +24,18 @@ vec3 elevationVector(vec2 pos) { return vec3(0, 0, 1); }
 
 #endif
 
+// Handle skirt flag for terrain & globe shaders
+
+const float skirtOffset = 24575.0;
+vec3 decomposeToPosAndSkirt(vec2 posWithComposedSkirt)
+{
+    float skirt = float(posWithComposedSkirt.x >= skirtOffset);
+    vec2 pos = posWithComposedSkirt - vec2(skirt * skirtOffset, 0.0);
+
+    return vec3(pos, skirt);
+}
+
+
 #ifdef TERRAIN
 
 #ifdef TERRAIN_DEM_FLOAT_FORMAT
