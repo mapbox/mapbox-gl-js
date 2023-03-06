86c30fa9fc94064cc3614f05872e2b7a7224aafc
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2023-03-06 18:56:44 +0200

    Enable anisotropic filtering only beyond 20 degrees pitch (#12577)
    * Enable trilinear filtering only beyond 20 degrees pitch
    
    * Add raster-filtering render tests
    
    * Lower the baseline for render tests
    
    * Move the anisotropic filter check to the `drawRaster`

--- test/ignores/linux-chrome.js
+++ test/ignores/linux-chrome.js
@@ -1,9 +1,7 @@
 const todo = [
     // Raster resampling is broken on Linux in Chrome
     // https://github.com/mapbox/mapbox-gl-js/issues/7331
-    "render-tests/globe/globe-transforms/north-pole",
-    "render-tests/raster-resampling/function",
-    "render-tests/raster-resampling/literal",
+    "render-tests/globe/globe-transforms/north-pole"
 ];
 const skip = [];
 
