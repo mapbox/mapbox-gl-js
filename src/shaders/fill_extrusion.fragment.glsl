ea40b4c2f5b28d5dae1df80814c81ade701ff8fa
Fouad Valadbeigi <67509069+akoylasar@users.noreply.github.com>
2023-03-21 18:27:29 +0100

    [MAPS3D-487] Fix subtle difference in AO when flood light is enabled. (internal-500)
    * when flood light is enabled ao was applied before gamma correction and opacity adjustment which is not what other code paths do leading to a slight visual change that doesnt look consistent. this commit fixes it.
    
    * update render tests
    
    * update render test images
    
    * make the fs more compatible for generating metal shader

--- src/shaders/fill_extrusion.fragment.glsl
+++ src/shaders/fill_extrusion.fragment.glsl
@@ -46,32 +46,40 @@ vec4 color;
     color = v_color;
 #endif
 float h = max(0.0, v_height);
+float ao_shade = 1.0;
 #ifdef FAUX_AO
     float intensity = u_ao[0];
     float h_floors = h / (u_ao[1] * u_vertical_scale);
     float y_shade = 1.0 - 0.9 * intensity * min(v_ao.y, 1.0);
-    float shade = (1.0 - 0.08 * intensity) * (y_shade + (1.0 - y_shade) * (1.0 - pow(1.0 - min(h_floors / 16.0, 1.0), 16.0))) + 0.08 * intensity * min(h_floors / 160.0, 1.0);
+    ao_shade = (1.0 - 0.08 * intensity) * (y_shade + (1.0 - y_shade) * (1.0 - pow(1.0 - min(h_floors / 16.0, 1.0), 16.0))) + 0.08 * intensity * min(h_floors / 160.0, 1.0);
     // concave angle
     float concave = v_ao.x * v_ao.x;
 #ifdef ZERO_ROOF_RADIUS
     concave *= (1.0 - z);
 #endif
     float x_shade = mix(1.0, mix(0.6, 0.75, min(h_floors / 30.0, 1.0)), intensity) + 0.1 * intensity * min(h, 1.0);
-    shade *= mix(1.0, x_shade * x_shade * x_shade, concave);
+    ao_shade *= mix(1.0, x_shade * x_shade * x_shade, concave);
 
-#if defined(LIGHTING_3D_MODE) && defined(FLOOD_LIGHT)
-    color.rgb *= mix(shade, 1.0, v_has_floodlight); // flood light and AO are mutually exclusive effects.
+#ifdef LIGHTING_3D_MODE
+#ifndef FLOOD_LIGHT
+    color.rgb *= ao_shade;
 #endif
-#if !defined(LIGHTING_3D_MODE) || !defined(FLOOD_LIGHT)
-    color.rgb *= shade;
-#endif
+#else
+    color.rgb *= ao_shade;
 #endif
 
-#if defined(LIGHTING_3D_MODE) && defined(FLOOD_LIGHT)
+#endif
+
+#ifdef LIGHTING_3D_MODE
+#ifdef FLOOD_LIGHT
     vec3 flood_radiance = u_flood_light_color * (1.0 - min(h / v_flood_radius, 1.0));   
     color.rgb += flood_radiance * v_has_floodlight;
     color.rgb = linearTosRGB(color.rgb);
     color *= u_opacity;
+#ifdef FAUX_AO
+    color.rgb *= mix(ao_shade, 1.0, v_has_floodlight); // flood light and AO are mutually exclusive effects.
+#endif
+#endif
 #endif
 
 #ifdef RENDER_SHADOWS
