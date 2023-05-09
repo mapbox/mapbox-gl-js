5d276d39b763bd2cb0e141f83a68aeba6a54b778
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2023-05-09 20:37:57 +0300

    Add local inference annotations in src/style/style_layer/symbol_style_layer.js

--- src/style-spec/expression/definitions/format.js
+++ src/style-spec/expression/definitions/format.js
@@ -9,7 +9,7 @@ import type EvaluationContext from '../evaluation_context.js';
 import type ParsingContext from '../parsing_context.js';
 import type {Type} from '../types.js';
 
-type FormattedSectionExpression = {
+export type FormattedSectionExpression = {
     // Content of a section may be Image expression or other
     // type of expression that is coercable to 'string'.
     content: Expression,
