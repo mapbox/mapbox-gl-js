e3f480d14597cd6c86d367662e4ce4eaba5d9b36
Aleksi Grön <aleksi.gron@mapbox.com>
2023-05-19 18:29:55 +0300

    Ignore tests that are timing out

--- test/ignores/linux-chrome.js
+++ test/ignores/linux-chrome.js
@@ -1,7 +1,12 @@
 const todo = [
     // Raster resampling is broken on Linux in Chrome
     // https://github.com/mapbox/mapbox-gl-js/issues/7331
-    "render-tests/globe/globe-transforms/north-pole"
+    "render-tests/globe/globe-transforms/north-pole",
+
+    // Timing out
+    // https://mapbox.atlassian.net/browse/GLJS-398
+    "render-tests/model-layer/buildings-trees-shadows-fog-terrain",
+    "render-tests/model-layer/multiple-models-terrain-fog"
 ];
 const skip = [];
 
