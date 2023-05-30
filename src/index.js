4d68d8bc6ceb88abbed688a1eeddebcf5b356a53
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2023-05-30 19:29:20 +0300

    Add `mapboxgl.loadersUrl` to allow overriding loaders bundle URL (#577)
    * Add `mapboxgl.loadersUrl` to allow overriding loaders bundle URL

--- src/index.js
+++ src/index.js
@@ -213,6 +213,31 @@ const exported = {
     workerClass: null,
 
     /**
+     * Provides an interface for loading mapbox-gl's loaders bundle from a self-hosted URL.
+     * This needs to be set only once, and before any call to `new mapboxgl.Map(..)` takes place.
+     * This is useful if your site needs to operate in a strict CSP (Content Security Policy) environment
+     * wherein you are not allowed to load JavaScript code from a [`Blob` URL](https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL), which is default behavior.
+     *
+     * See our documentation on [CSP Directives](https://docs.mapbox.com/mapbox-gl-js/api/#csp-directives) for more details.
+     *
+     * @var {string} loadersUrl
+     * @returns {string} A URL hosting a JavaScript mapbox-gl's loaders bundle.
+     * @example
+     * <script src='https://api.mapbox.com/mapbox-gl-js/v3.0.0/mapbox-gl.js'></script>
+     * <script>
+     * mapboxgl.loadersUrl = "https://api.mapbox.com/mapbox-gl-js/v3.0.0/mapbox-gl-loaders.js";
+     * ...
+     * </script>
+     */
+    get loadersUrl(): string {
+        return config.LOADERS_URL;
+    },
+
+    set loadersUrl(url: string) {
+        config.LOADERS_URL = url;
+    },
+
+    /**
      * Sets the time used by Mapbox GL JS internally for all animations. Useful for generating videos from Mapbox GL JS.
      *
      * @var {number} time
