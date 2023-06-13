b0d5648dc54463ca8eb0c2220760907642915250
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2023-06-13 17:42:29 +0200

    Implement better validation for imports and options

---
+++ src/style-spec/validate/validate_style.js
@@ -0,0 +1,29 @@
+// @flow
+import validate from './validate.js';
+import latestStyleSpec from '../reference/latest.js';
+import validateGlyphsURL from './validate_glyphs_url.js';
+
+import ValidationError from '../error/validation_error.js';
+
+import type {ValidationOptions} from './validate.js';
+import type {StyleSpecification} from '../types.js';
+
+type StyleValidationOptions = {
+    key?: ValidationOptions["key"]
+}
+
+export default function validateStyle(style: StyleSpecification, styleSpec: Object = latestStyleSpec, options: StyleValidationOptions = {}): ValidationError[] {
+    const errors = validate({
+        key: options.key || '',
+        value: style,
+        valueSpec: styleSpec.$root,
+        styleSpec,
+        style,
+        objectElementValidators: {
+            glyphs: validateGlyphsURL,
+            '*': () => []
+        }
+    });
+
+    return errors;
+}
