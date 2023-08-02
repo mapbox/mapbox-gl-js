d527ec15bc55707ecbd8ce287d176c4d012fec0f
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2023-08-02 17:11:19 +0300

    Prepare for the v3.0.0-beta.1 release (internal-671)
    * Prepare for the v3.0.0-beta.1 release

--- test/unit/ui/map.test.js
+++ test/unit/ui/map.test.js
@@ -4084,7 +4084,7 @@ test('Map', (t) => {
         const version = map.version;
         t.test('returns version string', (t) => {
             t.ok(version);
-            t.match(version, /^2\.[0-9]+\.[0-9]+(-dev|-beta\.[1-9])?$/);
+            t.match(version, /^3\.[0-9]+\.[0-9]+(-dev|-beta\.[1-9])?$/);
             t.end();
         });
         t.test('cannot be set', (t) => {
