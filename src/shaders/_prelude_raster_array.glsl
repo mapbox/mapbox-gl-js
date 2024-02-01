4f303e8e155cf5ad322e2c75e069a68e9517cfdf
Mikhail Pozdnyakov <mikhail.pozdnyakov@mapbox.com>
2024-02-01 20:04:36 +0200

    Enforce highp preciseness for raster array prelude function arguments (internal-1090)
    So that rendering in not affected on Android (GLES).

--- src/shaders/_prelude_raster_array.glsl
+++ src/shaders/_prelude_raster_array.glsl
@@ -6,14 +6,14 @@ const vec4 NODATA = vec4(1);
 
 // Decode raster array data and interpolate linearly using nearest neighbor samples
 // Returns: vec2(value, nodata_alpha)
-vec4 _raTexLinearCoord(vec2 texCoord, vec2 texResolution, out vec2 fxy) {
+vec4 _raTexLinearCoord(highp vec2 texCoord, highp vec2 texResolution, out highp vec2 fxy) {
     texCoord = texCoord * texResolution - 0.5;
     fxy = fract(texCoord);
     texCoord -= fxy;
     return (texCoord.xxyy + vec2(1.5, 0.5).xyxy) / texResolution.xxyy;
 }
 
-vec2 _raTexLinearMix(vec2 fxy, vec4 colorMix, float colorOffset, vec4 t00, vec4 t10, vec4 t01, vec4 t11) {
+vec2 _raTexLinearMix(highp vec2 fxy, highp vec4 colorMix, highp float colorOffset, highp vec4 t00, highp vec4 t10, highp vec4 t01, highp vec4 t11) {
     // Interpolate as a vec2: 1) the mixed value, and 2) a binary 0/1 mask.
     vec2 c00 = t00 == NODATA ? vec2(0) : vec2(colorOffset + dot(t00, colorMix), 1);
     vec2 c10 = t10 == NODATA ? vec2(0) : vec2(colorOffset + dot(t10, colorMix), 1);
@@ -24,7 +24,7 @@ vec2 _raTexLinearMix(vec2 fxy, vec4 colorMix, float colorOffset, vec4 t00, vec4 
 
 // Decode raster array data and interpolate linearly using nearest neighbor samples
 // Returns: vec2(value, nodata_alpha)
-vec2 raTexture2D_image0_linear(vec2 texCoord, vec2 texResolution, vec4 colorMix, float colorOffset) {
+vec2 raTexture2D_image0_linear(highp vec2 texCoord, highp vec2 texResolution, highp vec4 colorMix, highp float colorOffset) {
     vec2 fxy;
     vec4 c = _raTexLinearCoord(texCoord, texResolution, fxy);
     return _raTexLinearMix(fxy, colorMix, colorOffset,
@@ -34,7 +34,7 @@ vec2 raTexture2D_image0_linear(vec2 texCoord, vec2 texResolution, vec4 colorMix,
          texture(u_image0, c.xw)
     );
 }
-vec2 raTexture2D_image1_linear(vec2 texCoord, vec2 texResolution, vec4 colorMix, float colorOffset) {
+vec2 raTexture2D_image1_linear(highp vec2 texCoord, highp vec2 texResolution, highp vec4 colorMix, highp float colorOffset) {
     vec2 fxy;
     vec4 c = _raTexLinearCoord(texCoord, texResolution, fxy);
     return _raTexLinearMix(fxy, colorMix, colorOffset,
@@ -47,11 +47,11 @@ vec2 raTexture2D_image1_linear(vec2 texCoord, vec2 texResolution, vec4 colorMix,
 
 // Decode raster array data and return nearest neighbor sample
 // Returns: vec2(value, nodata_alpha)
-vec2 raTexture2D_image0_nearest(vec2 texCoord, vec2 texResolution, vec4 colorMix, float colorOffset) {
+vec2 raTexture2D_image0_nearest(highp vec2 texCoord, highp vec2 texResolution, highp vec4 colorMix, highp float colorOffset) {
     vec4 t = texture(u_image0, texCoord);
     return t == NODATA ? vec2(0) : vec2(colorOffset + dot(t, colorMix), 1);
 }
-vec2 raTexture2D_image1_nearest(vec2 texCoord, vec2 texResolution, vec4 colorMix, float colorOffset) {
+vec2 raTexture2D_image1_nearest(highp vec2 texCoord, highp vec2 texResolution, highp vec4 colorMix, highp float colorOffset) {
     vec4 t = texture(u_image1, texCoord);
     return t == NODATA ? vec2(0) : vec2(colorOffset + dot(t, colorMix), 1);
 }
