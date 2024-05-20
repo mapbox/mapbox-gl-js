fed58b859b6b322b2f147275050a0df4b13acf9b
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-05-20 16:35:48 +0300

    Fix local `start-release` workflow (internal-1420)

--- build/print-release-url.js
+++ build/print-release-url.js
@@ -1,6 +1,12 @@
-import address from 'address';
-import qrcode from 'qrcode-terminal'
+/* eslint-disable flowtype/require-valid-file-annotation */
 
-const url = `http://${address.ip()}:9966/test/release/index.html`;
+import {ip} from 'address';
+import qrcode from 'qrcode-terminal';
+
+let url = `http://${ip()}:9966/test/release/index.html`;
 console.warn(`Scan this QR code or enter ${url}`);
+
+const accessToken = process.env['MAPBOX_ACCESS_TOKEN'];
+if (accessToken) url += `#page=&access_token=${accessToken}`;
+
 qrcode.generate(url);
