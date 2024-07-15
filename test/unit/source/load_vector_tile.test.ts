ce1aeeaa979c6f594e4177e5bd7681b046c6603b
Joe B <joseph@continuum.industries>
2024-07-15 10:20:51 +0100

    add test for queue size being enforced

--- test/unit/source/load_vector_tile.test.ts
+++ test/unit/source/load_vector_tile.test.ts
@@ -8,7 +8,7 @@ const scheduler = {add: () => {}};
 
 test('loadVectorTile does not make array buffer request for duplicate tile requests', () => {
     const deduped = new DedupedRequest(scheduler);
-    const params = {url: 'http://localhost:2900/fake.pbf'};
+    const params = {request: {url: 'http://localhost:2900/fake.pbf'}};
     expect.assertions(1);
     const arrayBufRequester = () => () => {
         expect(true).toBeTruthy();
@@ -19,4 +19,24 @@ test('loadVectorTile does not make array buffer request for duplicate tile reque
 });
 
 // Handles 300 concurrent requests through queue
+
+test('only processes concurrent requests up to the queue limit', () => {
+    vi.useFakeTimers();
+    const deduped = new DedupedRequest(scheduler);
+    expect.assertions(49);
+    const arrayBufRequester = () => {
+        return () => {
+            setTimeout(() => {
+                expect(true).toBeTruthy();
+            }, 1500);
+        };
+    };
+
+    for (let i = 0; i < 300; i++) {
+        loadVectorTile({request: {url: i}}, () => {}, deduped, false, arrayBufRequester);
+    }
+    vi.advanceTimersByTime(1500);
+    vi.useRealTimers();
+});
 // Fails to fetch if concurrent requests get too large when unqueued
+// Some stuff about cancelling within the queue?
