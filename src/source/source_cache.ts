5e8ca3275fa6875aa2a58c38b78112fa63e756de
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-07-26 18:21:11 +0300

    Fix `isSourceLoaded` on `sourcedata` events (internal-1692)

--- src/source/source_cache.ts
+++ src/source/source_cache.ts
@@ -128,7 +128,7 @@ class SourceCache extends Evented {
         if (!this._source.loaded()) { return false; }
         for (const t in this._tiles) {
             const tile = this._tiles[t];
-            if (tile.state !== 'errored' && (tile.state !== 'loaded' || !tile.bucketsLoaded()))
+            if (tile.state !== 'loaded' && tile.state !== 'errored')
                 return false;
         }
         return true;
