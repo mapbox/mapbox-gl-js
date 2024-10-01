63b8d31bcfcaf41bd7fa3eb7f6d687d48f9cdc80
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2024-10-01 13:34:34 +0300

    Fix env substitution in access token generation (internal-1893)
    * Fix ENV substitution in generate-access-token-script.js
    
    * fix CSP urls

--- debug/access_token.js
+++ debug/access_token.js
@@ -4,13 +4,14 @@ mapboxgl.accessToken = getAccessToken();
 
 function getAccessToken() {
     const accessToken = [
-        typeof process !== 'undefined' && process.env.MapboxAccessToken,
-        typeof process !== 'undefined' && process.env.MAPBOX_ACCESS_TOKEN,
+        process.env.MapboxAccessToken,
+        process.env.MAPBOX_ACCESS_TOKEN,
         getURLParameter('access_token'),
         localStorage.getItem('accessToken'),
         // this token is a fallback for CI and testing. it is domain restricted to localhost
         'pk.eyJ1IjoibWFwYm94LWdsLWpzIiwiYSI6ImNram9ybGI1ajExYjQyeGxlemppb2pwYjIifQ.LGy5UGNIsXUZdYMvfYRiAQ'
     ].find(Boolean);
+
     try {
         localStorage.setItem('accessToken', accessToken);
     } catch (_) {
@@ -20,7 +21,6 @@ function getAccessToken() {
 }
 
 function getURLParameter(name) {
-    var regexp = new RegExp('[?&]' + name + '=([^&#]*)', 'i');
-    var output = regexp.exec(window.location.href);
-    return output && output[1];
+    const url = new URL(window.location.href);
+    return url.searchParams.get(name);
 }
