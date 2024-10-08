e57a7e5a019539e6cbc88e32f6ff9c7c0e277185
Danil Ilinykh <danil.ilinykh@mapbox.com>
2024-10-08 21:34:01 +0700

    Added expression tests for image options (internal-1908)

--- test/ignores/all.js
+++ test/ignores/all.js
@@ -37,7 +37,19 @@ const todo = [
 
     // https://mapbox.atlassian.net/browse/GLJS-1005
     "expression-tests/image/two-arguments/available",
-    "render-tests/image-fallback-nested/add-image-after"
+    "render-tests/image-fallback-nested/add-image-after",
+
+    // https://mapbox.atlassian.net/browse/GLJS-873
+    "expression-tests/image/options/one-image",
+    "expression-tests/image/options/options-for-both-images",
+    "expression-tests/image/options/options-for-first-image-only",
+    "expression-tests/image/options/options-for-second-image-only",
+    "expression-tests/image/options/options-with-invalid-params/array",
+    "expression-tests/image/options/options-with-invalid-params/empty-parameter-name",
+    "expression-tests/image/options/options-with-invalid-params/integer-parameter-value",
+    "expression-tests/image/options/options-with-invalid-params/parameter-value-with-unknown-color",
+    "expression-tests/image/options/options-with-invalid-params/string",
+    "expression-tests/image/options/options-without-params"
 ];
 
 const skip = [
