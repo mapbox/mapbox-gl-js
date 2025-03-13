4228794b8c1736fe9f00657255d86ae5c9d4a8b9
Josh Moore <josh.moore@iventis.com>
2025-03-13 15:44:49 +0000

    fixed types

--- src/style-spec/types.ts
+++ src/style-spec/types.ts
@@ -1065,8 +1065,8 @@ export type ModelLayerSpecification = {
     "filter"?: FilterSpecification,
     "layout"?: {
         "visibility"?: "visible" | "none" | ExpressionSpecification,
-        "model-id"?: DataDrivenPropertyValueSpecification<string>
-        "model-allow-density-reduction"?: PropertyValueSpecification<boolean>,
+        "model-id"?: DataDrivenPropertyValueSpecification<string>,
+        "model-allow-density-reduction"?: boolean
     },
     "paint"?: {
         "model-opacity"?: DataDrivenPropertyValueSpecification<number>,
