a929e1e242ec769f9143d24883f2fb22633f5ed3
Aleksandr Shoronov <aleksandr.shoronov@mapbox.com>
2025-03-24 11:50:16 +0200

    [GLJS-1226] Disable flaky render-tests/video/default (internal-2294)

--- test/ignores/all.js
+++ test/ignores/all.js
@@ -162,8 +162,10 @@ const skip = [
     "render-tests/text-variable-anchor/pitched",
 
     // Flaky
+    // https://mapbox.atlassian.net/browse/GLJS-1226
     // https://github.com/mapbox/mapbox-gl-js/issues/11234
     "render-tests/video/projected",
+    "render-tests/video/default",
 
     // Non-deterministic
     "query-tests/terrain/draped/lines/slope-occlusion-box-query",
