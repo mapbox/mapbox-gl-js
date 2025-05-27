c462f9b151d363d3eb347fa2110e6a046e87fa56
Aleksandr Shoronov <aleksandr.shoronov@mapbox.com>
2025-05-27 15:06:00 +0300

    Migrate build tests to Node.js test API (internal-2541)

--- test/build/style-spec.test.js
+++ test/build/style-spec.test.js
@@ -1,27 +1,26 @@
-/* eslint-disable import/extensions */
-import path from 'path';
-import fs from 'fs';
+import {describe, it} from 'node:test';
+import assert from 'node:assert/strict';
+import path from 'node:path';
+import fs from 'node:fs';
+import {fileURLToPath} from 'node:url';
 import isBuiltin from 'is-builtin-module';
+import {createRequire} from 'node:module';
 import {rollup} from 'rollup';
-import {test} from 'tape';
 import rollupConfig from '../../src/style-spec/rollup.config.js';
-// eslint-disable-next-line import/order
-import {createRequire} from 'module';
+
 const require = createRequire(import.meta.url);
 
-import {fileURLToPath} from 'url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const styleSpecDirectory = path.join(__dirname, '../../src/style-spec');
 
 const styleSpecPackage = JSON.parse(fs.readFileSync(path.join(styleSpecDirectory, 'package.json')).toString());
-// import styleSpecPackage from '../../src/style-spec/package.json';
 
-test('@mapbox/mapbox-gl-style-spec npm package', (t) => {
-    t.test('builds self-contained bundle without undeclared dependencies', (t) => {
+describe('@mapbox/mapbox-gl-style-spec npm package', () => {
+    it('builds self-contained bundle without undeclared dependencies', () => {
         const warn = console.warn;
         console.warn = () => {};
-        rollup({
+        return rollup({
             input: `${styleSpecDirectory}/style-spec.ts`,
             plugins: [{
                 name: 'check-dependencies',
@@ -32,7 +31,7 @@ test('@mapbox/mapbox-gl-style-spec npm package', (t) => {
 
                     if (!styleSpecPackage.dependencies[id]) {
                         if (!path.resolve(importer, id).startsWith(styleSpecDirectory)) {
-                            t.fail(`Import outside of the style-spec package: ${id} (imported from ${importer})`);
+                            throw new Error(`Import outside of the style-spec package: ${id} (imported from ${importer})`);
                         }
                     }
 
@@ -40,24 +39,20 @@ test('@mapbox/mapbox-gl-style-spec npm package', (t) => {
                         return null;
                     }
 
-                    t.ok(styleSpecPackage.dependencies[id], `External dependency ${id} (imported from ${importer}) declared in style-spec's package.json`);
+                    assert(styleSpecPackage.dependencies[id], `External dependency ${id} (imported from ${importer}) declared in style-spec's package.json`);
                 }
             }].concat(rollupConfig[0].plugins)
         }).then(() => {
             console.warn = warn;
-            t.end();
         }).catch(e => {
             console.warn = warn;
-            t.error(e);
+            throw e;
         });
     });
 
-    t.test('exports components directly, not behind `default` - https://github.com/mapbox/mapbox-gl-js/issues/6601', (t) => {
+    it('exports components directly, not behind `default` - https://github.com/mapbox/mapbox-gl-js/issues/6601', () => {
         const spec = require('../../dist/style-spec/index.cjs');
-        t.ok(spec.validate);
-        t.notOk(spec.default && spec.default.validate);
-        t.end();
+        assert(!!spec.validate, 'validate function is exported directly');
+        assert(!spec.default || !spec.default.validate, 'validate function is not exported behind default export');
     });
-
-    t.end();
 });
