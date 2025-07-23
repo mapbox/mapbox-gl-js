89e6f998dbb7076a38f7376955607f7955a785fa
Aleksandr Shoronov <aleksandr.shoronov@mapbox.com>
2025-07-23 16:33:08 +0300

    Fix GL JS Publish NPM and style-spec (internal-5399)
    GitOrigin-RevId: af4c2d86bf50a6b04fb953155cf67c0b63a5e775

--- build/publish.sh
+++ build/publish.sh
@@ -24,29 +24,27 @@ git tag --points-at HEAD | while read tag; do
     tag=$(echo $tag | sed 's/^gl-js\///')
 
     if [[ $tag =~ ^style-spec@ ]]; then
-        cd src/style-spec
         spec_version="${tag#style-spec@}"
         echo "Publishing style-spec: $spec_version"
 
-        current_version=$(node -p "require('./package.json').version")
+        current_version=$(node -p "require('./src/style-spec/package.json').version")
         if [[ "$spec_version" != "$current_version" ]]; then
-            echo "npm version $spec_version --no-git-tag-version"
+            echo "npm version $spec_version --no-git-tag-version -w src/style-spec"
             if [[ $dry_run == false ]]; then
-                npm version "$spec_version" --no-git-tag-version
+                npm version "$spec_version" --no-git-tag-version -w src/style-spec
             fi
         else
             echo "Version already set to $spec_version, skipping version update"
         fi
 
         if [[ -z $(npm view .@$spec_version) ]]; then
-            echo "npm publish --tag $disttag"
+            echo "npm publish --tag $disttag -w src/style-spec"
             if [[ $dry_run == false ]]; then
-                npm publish --tag "$disttag"
+                npm publish --tag "$disttag" -w src/style-spec
             fi
         else
             echo "Already published."
         fi
-        cd ../..
 
     elif [[ $tag =~ ^v[0-9] ]]; then
         version="${tag#v}"
