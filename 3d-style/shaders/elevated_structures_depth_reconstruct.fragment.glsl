c6fb1857727ce574bd1a5ca3f4025cb11142bddb
Yang <30654729+longpractice@users.noreply.github.com>
2025-08-15 09:59:44 +0200

    Dithered transparency for lane cutout
    GitOrigin-RevId: d0d4e96bccb9627a4bfa16d6e2e91890b4da0e7c

--- 3d-style/shaders/elevated_structures_depth_reconstruct.fragment.glsl
+++ 3d-style/shaders/elevated_structures_depth_reconstruct.fragment.glsl
@@ -6,7 +6,10 @@ void main() {
 #ifdef DEPTH_RECONSTRUCTION
     if (v_height >= 0.0)
         discard;
+#else
+#ifdef FEATURE_CUTOUT
+    apply_feature_cutout(vec4(0.0, 0.0, 0.0, 1.0), gl_FragCoord);
 #endif
-
+#endif
     glFragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
