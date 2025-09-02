2c1a083fd5be0ae593ac5343c6749c49400394ac
Stepan Kuzmin <533564+stepankuzmin@users.noreply.github.com>
2025-09-02 12:29:55 +0300

    Set `accessToken` on debug pages explicitly (internal-5862)
    GitOrigin-RevId: 9cab9dfcf2c5a1b5c7e29405dc1c2a9002ecc6f0

--- debug/access_token.js
+++ debug/access_token.js
@@ -1,26 +1,11 @@
-'use strict';
-
-mapboxgl.accessToken = getAccessToken();
-
-function getAccessToken() {
-    const accessToken = [
-        process.env.MapboxAccessToken,
-        process.env.MAPBOX_ACCESS_TOKEN,
-        getURLParameter('access_token'),
-        localStorage.getItem('accessToken'),
+export function getAccessToken() {
+    const accessToken =
+        process.env.MAPBOX_ACCESS_TOKEN ||
+        (new URLSearchParams(location.search).get('access_token')) ||
+        localStorage.getItem('accessToken') ||
         // this token is a fallback for CI and testing. it is domain restricted to localhost
         'pk.eyJ1IjoiZ2wtanMtdGVhbSIsImEiOiJjbTV1d3l0d3AwMThnMmpzZ2M5OTNyeDE1In0.2nygBIo7PXbkFCCt6LEBgw'
-    ].find(Boolean);
 
-    try {
-        localStorage.setItem('accessToken', accessToken);
-    } catch (_) {
-        // no-op
-    }
+    localStorage.setItem('accessToken', accessToken);
     return accessToken;
-}
-
-function getURLParameter(name) {
-    const url = new URL(window.location.href);
-    return url.searchParams.get(name);
 }
