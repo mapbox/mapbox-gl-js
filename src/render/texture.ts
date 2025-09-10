339b8254061c11f52bc7121cf1399d496933e01f
Stepan Kuzmin <533564+stepankuzmin@users.noreply.github.com>
2025-09-10 17:08:15 +0300

    Fix `texSubImage2D` call in Texture (internal-6461)
    GitOrigin-RevId: 112262f607ecd659a124a6a571082e55d90cfba3

--- src/render/texture.ts
+++ src/render/texture.ts
@@ -95,8 +95,8 @@ class Texture {
         if (this.size) {
             if (externalImage) {
                 gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, _getLegacyFormat(this.format), _getType(this.format), image);
-            } else {
-                gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, srcWidth, srcHeight, _getLegacyFormat(this.format), _getType(this.format), (image as RGBAImage | AlphaImage | Float32Image | EmptyImage).data);
+            } else if ('data' in image && image.data) {
+                gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, srcWidth, srcHeight, _getLegacyFormat(this.format), _getType(this.format), image.data);
             }
         }
 
@@ -200,7 +200,9 @@ export class Texture3D {
         assert(image.height === height);
         assert(image.width === width * depth);
 
-        gl.texImage3D(gl.TEXTURE_3D, 0, this.format, width, height, depth, 0, _getLegacyFormat(this.format), _getType(this.format), (image as RGBAImage | AlphaImage | Float32Image | EmptyImage).data);
+        if ('data' in image && image.data) {
+            gl.texImage3D(gl.TEXTURE_3D, 0, this.format, width, height, depth, 0, _getLegacyFormat(this.format), _getType(this.format), image.data);
+        }
     }
 
     bind(filter: TextureFilter, wrap: TextureWrap) {
