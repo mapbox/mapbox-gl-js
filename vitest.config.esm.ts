484dd82d35baf1bbafff3081f2cd1a086cd081a9
Aleksandr Shoronov <aleksandr.shoronov@mapbox.com>
2025-11-05 12:08:34 +0200

    Fix ESM test after Vitest update (internal-7839)
    GitOrigin-RevId: 0c5ca3b3415b470f2c454885ff08d881d2d0434c

--- vitest.config.esm.ts
+++ vitest.config.esm.ts
@@ -1,5 +1,6 @@
 import {defineConfig, mergeConfig} from 'vitest/config';
 import baseConfig from './vitest.config.base';
+import {playwright} from '@vitest/browser-playwright';
 
 const isCI = process.env.CI === 'true';
 
@@ -7,8 +8,9 @@ export default mergeConfig(baseConfig, defineConfig({
     test: {
         include: ['test/build/esm.test.ts'],
         browser: {
+            provider: playwright({launchOptions: {channel: isCI ? 'chromium' : 'chrome'}}),
             instances: [
-                {browser: 'chromium', launch: {channel: isCI ? 'chromium' : 'chrome'}},
+                {browser: 'chromium'},
             ],
         },
     }
