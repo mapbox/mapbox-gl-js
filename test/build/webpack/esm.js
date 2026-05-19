1d57634204e72f0308f8ddb9f3187c1549d5f49e
Darren Crossley <darren.crossley@mapbox.com>
2026-05-19 13:37:27 +0200

    more fixes
    GitOrigin-RevId: c3647a578de70a3d35bba02bf1d23bfba8689dd3

--- test/build/webpack/esm.js
+++ test/build/webpack/esm.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
 
-import mapboxgl from 'mapbox-gl/esm';
+import { Map } from 'mapbox-gl/esm';
 
-mapboxgl.accessToken = window.prompt("Enter access token");
-
-const map = new mapboxgl.Map({
+const map = new Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-74.5, 40],
-    zoom: 9
+    zoom: 9,
+    accessToken: window.prompt("Enter access token")
 });
