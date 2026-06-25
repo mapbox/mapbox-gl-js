9b03c984b48199980a527f48072750a810008001
Stepan Kuzmin <533564+stepankuzmin@users.noreply.github.com>
2026-06-25 21:59:10 +0300

    Run typing test in isolation to catch undeclared .d.ts imports
    GitOrigin-RevId: 7c9abd41e8e33073c6a7342036868dd338f52f13

--- test/build/typings/pretest.sh
+++ test/build/typings/pretest.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 set -e
 
-../pack.sh
+DEST="$(cd "$(dirname "$0")" && pwd)"
+TARBALL_DIR="$DEST/.."
+
+"$TARBALL_DIR/pack.sh"
+
+ISO_DIR="$(mktemp -d)"
+trap "rm -rf '$ISO_DIR'" EXIT
+
+cp "$DEST"/package.json \
+   "$DEST"/tsconfig.json \
+   "$DEST"/tsconfig.strict.json \
+   "$DEST"/*.ts \
+   "$ISO_DIR/"
+
+cd "$ISO_DIR"
+npm install --no-save "$TARBALL_DIR/mapbox-gl.tgz"
+npm run tsc:strict
+
+cd "$DEST"
 rm -rf node_modules/mapbox-gl node_modules/@mapbox/mapbox-gl-style-spec
-npm install --no-save ../mapbox-gl.tgz ../mapbox-gl-style-spec.tgz
+npm install --no-save "$TARBALL_DIR/mapbox-gl.tgz" "$TARBALL_DIR/mapbox-gl-style-spec.tgz"
